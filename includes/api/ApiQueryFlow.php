d74e8be6c2c5266ede94c523af31e0830e3c6805
Andrew Garrett <agarrett@wikimedia.org>
2013-08-07 16:58:02 -0700

    Add API modules to Flow.
    For now, just the one Query module, which will bring up the blocks
    that comprise a workflow (given the workflow page name and possibly
    its ID) in a machine readable format. Still definitely needs paging
    and so on.
    
    Will also, in future, implement a proper API interface for non-view
    actions.
    
    Change-Id: I3115567c29af31c6754e297f1f3fc3485edb9deb

---
+++ includes/api/ApiQueryFlow.php
@@ -0,0 +1,87 @@
+<?php
+
+use Flow\WorkflowLoader;
+use Flow\Model\UUID;
+
+class ApiQueryFlow extends ApiQueryBase {
+	protected $loader, $workflow, $definition;
+
+	public function __construct( $query, $moduleName ) {
+		parent::__construct( $query, $moduleName, 'flow' );
+	}
+
+	public function execute() {
+		$this->container = include dirname(__FILE__) . "/../../container.php";
+		// Get the parameters
+		$params = $this->extractRequestParams();
+		$passedParams = json_decode( $params['params'], true );
+
+		$pageTitle = Title::newFromText( $params['page'] );
+		$id = $params['workflow'] ? new UUID( $params['workflow'] ) : null;
+
+		$this->loader = $this->container['factory.loader.workflow']
+			->createWorkflowLoader( $pageTitle, $id );
+
+		$blocks = $this->loader->createBlocks();
+		$blockOutput = array();
+		foreach( $blocks as $block ) {
+			$block->init( $params['action'] );
+
+			$blockParams = array();
+			if ( isset($passedParams[$block->getName()]) ) {
+				$blockParams = $passedParams[$block->getName()];
+			}
+
+			$blockOutput[] = $block->renderAPI( $blockParams ) +
+				array( 'block-name' => $block->getName() );
+		}
+
+		$result = array(
+			'_element' => 'block',
+			'workflow-id' => $this->loader->getWorkflow()->getId()->getHex(),
+		) + $blockOutput;
+
+		$this->getResult()->addValue( 'query', $this->getModuleName(), $result );
+	}
+
+	public function getAllowedParams() {
+		return array(
+			'workflow' => array(
+			),
+			'page' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'action' => array(
+				ApiBase::PARAM_DFLT => 'view',
+			),
+			'params' => array(
+				ApiBase::PARAM_DFLT => '{}',
+			),
+		);
+	}
+
+	public function getParamDescription() {
+		return array(
+			'workflow' => 'Hex-encoded ID of the workflow to query',
+			'page' => 'Title of the page to query'
+		);
+	}
+
+	public function getDescription() {
+		return 'Queries the Flow subsystem for data';
+	}
+
+	public function getExamples() {
+		return array(
+			'api.php?action=query&list=flow&flowpage=Talk:Main_Page',
+		);
+	}
+
+	public function getHelpUrls() {
+		return 'https://www.mediawiki.org/wiki/Flow_Portal';
+	}
+
+	public function getVersion() {
+		return __CLASS__ . '-0.1';
+	}
+}
