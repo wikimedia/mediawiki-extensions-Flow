bdf2f3fd9bd4392fde71fde7172646ae054fd861
Matthias Mullie <git@mullie.eu>
2013-10-22 19:24:12 +0200

    Historical view of a topic
    Specs: https://www.mediawiki.org/wiki/Flow_Portal/Functional_Specifications/History
    
    * Added configurable messages/params per change type
    * Added bundling, messages/params also configurable
    * Group messages per timespan
    * Added some icons: added_*.svg|png & junkdrawer_inverse_*.svg|png
    * Applied to both post & topic
    * Applied design
    
    Big todos according to spec, but as far as I'm concerned not part of this patch:
    
    * Make the topic & post timestamp only link to history if there are >1 history
      things (I did not yet do this because I don't yet want to query for more data)
    * "Actions" per change type (e.g. links "Thanks", "Show changes", ...)
    * I'm pretty sure some history stuff currently shows up wrong (I think,
      currently, hiding a post will also show up in topic history)
    * Board history page
    * We're still missing actions that'll need history, like splitting a topic etc
    * Link to historical revision
    
    Change-Id: I0405498069e6552cada078e079f6ab3bbb920470

--- templates/topic-history.html.php
+++ templates/topic-history.html.php
@@ -1,12 +1,29 @@
 <?php
 
-echo '<h2>' . wfMessage( 'flow-topic-history' )->escaped() . '</h2>';
-echo '<ul class="flow-topic-history">';
-foreach ( $history as $revision ) {
-	echo '<li>'
-		. $revision->getRevisionId()->getHex() . ' : '
-		. $revision->getUserText() . ' : '
-		. wfMessage( $revision->getChangeType() )->parse()
-		. '</li>';
-}
-echo '</ul>';
+$pageTitle = $topic->getArticleTitle();
+$pageLink = $this->urlGenerator->buildUrl( $pageTitle, 'view' );
+$pageTitle = $pageTitle->getText();
+
+$title = $root->getContent( $user, 'wikitext' );
+$title = wfMessage( 'flow-topic-history', $title )->escaped();
+$this->getOutput()->setHtmlTitle( $title );
+$this->getOutput()->setPageTitle( $title );
+
+$timespans = $historyRenderer->getTimespans( $history );
+
+?>
+<div class="flow-history-container">
+	<p class='flow-history-pages'><?php echo wfMessage( 'flow-history-pages-topic', $pageLink, $pageTitle )->parse(); ?></p>
+
+	<div class="flow-history-log">
+		<?php
+			foreach ( $timespans as $text => $timespan ) {
+				$timespan = $history->getTimespan( $timespan['from'], $timespan['to'] );
+				if ( $timespan->numRows() ) {
+					echo "<h2>$text</h2>";
+					echo $historyRenderer->render( $timespan );
+				}
+			}
+		?>
+	</div>
+</div>
