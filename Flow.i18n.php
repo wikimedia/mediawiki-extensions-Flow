c77a212c3eb08cd11daec7acdfc2e5882ce6eb39
Andrew Garrett <agarrett@wikimedia.org>
2013-10-23 13:24:33 +1100

    Post permalinks
    Currently just displays an icon that has a permalink to the appropriate post.
    
    In future we should probably add a popup that gives a copiable URL.
    
    Also, the icon positioning on the RHS is a bit of a mess, we should try to fix that.
    
    Change-Id: Icc7386cffa907aca0c792a36388741dbd8ff8a54

--- Flow.i18n.php
+++ Flow.i18n.php
@@ -142,6 +142,10 @@ $messages['en'] = array(
 	'flow-link-post' => 'post',
 	'flow-link-topic' => 'topic',
 	'flow-link-history' => 'history',
+
+	// Permalink related stuff
+	'flow-topic-permalink-warning' => 'This topic was started on [$2 $1]',
+	'flow-topic-permalink-warning-user-board' => 'This topic was started on [$2 {{GENDER:$1|$1}}\'s board]',
 );
 
 /** Message documentation (Message documentation)
@@ -466,6 +470,13 @@ See also:
 {{Identical|Topic}}',
 	'flow-link-history' => 'Text used when linking to history of a post/topic from recentchanges.
 {{Identical|History}}',
+
+	'flow-topic-permalink-warning' => 'Displayed at the top of a page when a person has clicked on a permanent link to a topic. Parameters:
+* $1: Display text for a link to the board that the topic comes from.
+* $2: URL for a link to the board that the topic comes from.',
+	'flow-topic-permalink-warning-user-board' => 'Displayed at the top of a page when a person has clicked on a permanent link to a topic from a user\'s board. Parameters:
+* $1: The user\'s name. Supports GENDER..
+* $2: URL for a link to the board that the topic comes from.',
 );
 
 /** Asturian (asturianu)
