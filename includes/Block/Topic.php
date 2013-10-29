wikimedia/mediawiki-extensions-Flow

commit 6f99d757143a8014bf197e6c30f392cec945f780

Slightly better abide to MW jshint & coding standards; add docblock
Change-Id: I4df027cf04659a9add142236f35efe9e64a6212e

modules/base/ext.flow.base.js
@@ -1,23 +1,33 @@
-( function($, mw) {
-$( function() {
+( function ( $, mw ) {
+$( function () {
 mw.flow = {
 	'api' : {
-		'executeAction' : function( workflowParam, action, options, render ) {
-			var api = new mw.Api();
-			var deferredObject = $.Deferred();
+		/**
+		 * Execute a Flow action, fires API call.
+		 *
+		 * @param {object} workflowParam API parameters
+		 * @param {string} action Action performed
+		 * @param {object} options Additional API parameters (API key 'params')
+		 * @param {bool} render
+		 * @return {Deferred}
+		 */
+		'executeAction' : function ( workflowParam, action, options, render ) {
+			var api = new mw.Api(),
+				deferredObject = $.Deferred();
 
-			api.post(
-				$.extend(
-					{
-						'action' : 'flow',
-						'flowaction' : action,
-						'gettoken' : 'gettoken'
-					},
-					workflowParam
+			api
+				.post(
+					$.extend(
+						{
+							'action' : 'flow',
+							'flowaction' : action,
+							'gettoken' : 'gettoken'
+						},
+						workflowParam
+					)
 				)
-			)
-				.done( function(data) {
-					request = {
+				.done( function ( data ) {
+					var request = {
 						'action' : 'flow',
 						'flowaction' : action,
 						'params' : $.toJSON( options ),
@@ -31,21 +41,27 @@ mw.flow = {
 					}
 
 					api.post( request )
-						.done( function( ) {
+						.done( function () {
 							deferredObject.resolve.apply( this, arguments );
 						} )
-						.fail( function( ) {
+						.fail( function () {
 							deferredObject.reject.apply( this, arguments );
 						} );
 				} )
-				.fail(function( ) {
-					deferredObject.reject.apply( this, arguments);
+				.fail( function () {
+					deferredObject.reject.apply( this, arguments );
 				} );
 
 			return deferredObject.promise();
 		},
 
-		'read' : function( pageName, workflowId, options ) {
+		/**
+		 * @param {string} pageName
+		 * @param {string} workflowId
+		 * @param {object} options API parameters
+		 * @return {Deferred}
+		 */
+		'read' : function ( pageName, workflowId, options ) {
 			var api = new mw.Api();
 
 			return api.get(
@@ -59,22 +75,29 @@ mw.flow = {
 			);
 		},
 
-		'readBlock' : function( pageName, topicId, blockName, options ) {
+		/**
+		 * @param {string} pageName
+		 * @param {string} topicId
+		 * @param {string} blockName
+		 * @param {object} options API parameters
+		 * @return {Deferred}
+		 */
+		'readBlock' : function ( pageName, topicId, blockName, options ) {
 			var deferredObject = $.Deferred();
 
 			mw.flow.api.read( pageName, topicId, options )
-				.done( function(output) {
+				.done( function ( output ) {
 					// Immediate failure modes
 					if (
-						! output.query ||
-						! output.query.flow ||
+						!output.query ||
+						!output.query.flow ||
 						output.query.flow._element !== 'block'
 					) {
 						deferredObject.fail( 'invalid-result', 'Unable to understand the API result' );
 						return;
 					}
 
-					$.each( output.query.flow, function( index, block ) {
+					$.each( output.query.flow, function ( index, block ) {
 						// Looping through each block
 						if ( block['block-name'] === blockName ) {
 							// Return this block
@@ -85,30 +108,48 @@ mw.flow = {
 					deferredObject.fail( 'invalid-result', 'Unable to find the '+
 						blockName+' block in the API result' );
 				} )
-				.fail( function() {
+				.fail( function () {
 					deferredObject.fail( arguments );
 				} );
 
 			return deferredObject.promise();
 		},
 
-		'readTopicList' : function( pageName, workflowId, options ) {
+		/**
+		 * @param {string} pageName
+		 * @param {string} workflowId
+		 * @param {object} options API parameters
+		 * @return {Deferred}
+		 */
+		'readTopicList' : function ( pageName, workflowId, options ) {
 			return mw.flow.api.readBlock( pageName, workflowId, 'topic_list', options );
 		},
 
-		'readTopic' : function( pageName, topicId, options ) {
+		/**
+		 * @param {string} pageName
+		 * @param {string} topicId
+		 * @param {object} options API parameters
+		 * @return {Deferred}
+		 */
+		'readTopic' : function ( pageName, topicId, options ) {
 			return mw.flow.api.readBlock( pageName, topicId, 'topic', options );
 		},
 
-		'generateTopicAction' : function( actionName, parameterList, promiseFilterCallback ) {
-			return function( workflowId ) {
-				var deferredObject = $.Deferred();
+		/**
+		 * @param {string} actionName
+		 * @param {object} parameterList
+		 * @param {function} promiseFilterCallback
+		 * @return {function}
+		 */
+		'generateTopicAction' : function ( actionName, parameterList, promiseFilterCallback ) {
+			return function ( workflowId ) {
+				var deferredObject = $.Deferred(),
+					requestParams = {},
+					paramIndex = 1,
+					requestArguments = arguments,
+					newDeferredObject;
 
-				var requestParams = {};
-				var paramIndex = 1;
-				var requestArguments = arguments;
-
-				$.each( parameterList, function( key, value ) {
+				$.each( parameterList, function ( key, value ) {
 					requestParams[value] = requestArguments[paramIndex];
 					paramIndex++;
 				} );
@@ -121,31 +162,33 @@ mw.flow = {
 					{ 'topic' :
 						requestParams
 					}, true
-				).done( function(data) {
+				).done( function ( data ) {
+					var output;
+
 					if ( data.flow[actionName].errors ) {
-						deferredObject.reject( 'block-errors', data.flow['reply'].errors );
+						deferredObject.reject( 'block-errors', data.flow.reply.errors );
 						return;
 					}
 
 					if (
-						! data.flow ||
-						! data.flow[actionName] ||
-						! data.flow[actionName].result ||
-						! data.flow[actionName].result['topic']
+						!data.flow ||
+						!data.flow[actionName] ||
+						!data.flow[actionName].result ||
+						!data.flow[actionName].result.topic
 					) {
 						deferredObject.reject( 'invalid-result', 'Unable to find appropriate section in result' );
 						return;
 					}
-					var output = data.flow[actionName].result['topic'];
+					output = data.flow[actionName].result.topic;
 
 					deferredObject.resolve( output, data );
 				} )
-				.fail( function( ) {
+				.fail( function () {
 					deferredObject.reject.apply( this, arguments );
 				} );
 
 				if ( promiseFilterCallback ) {
-					var newDeferredObject = promiseFilterCallback( deferredObject.promise() );
+					newDeferredObject = promiseFilterCallback( deferredObject.promise() );
 					if ( newDeferredObject ) {
 						deferredObject = newDeferredObject;
 					}
@@ -155,7 +198,13 @@ mw.flow = {
 			};
 		},
 
-		'newTopic' : function( workflowParam, title, content ) {
+		/**
+		 * @param {object} workflowParam
+		 * @param {string} title
+		 * @param {string} content
+		 * @return {Deferred}
+		 */
+		'newTopic' : function ( workflowParam, title, content ) {
 			var deferredObject = $.Deferred();
 
 			mw.flow.api.executeAction(
@@ -167,26 +216,28 @@ mw.flow = {
 						'content' : content
 					}
 				}, true
-			).done( function(data) {
+			).done( function ( data ) {
+				var output;
+
 				if ( data.flow['new-topic'].errors ) {
 					deferredObject.reject( 'block-errors', data.flow['new-topic'].errors );
 					return;
 				}
 
 				if (
-					! data.flow ||
-					! data.flow['new-topic'] ||
-					! data.flow['new-topic'].result ||
-					! data.flow['new-topic'].result['topic_list']
+					!data.flow ||
+					!data.flow['new-topic'] ||
+					!data.flow['new-topic'].result ||
+					!data.flow['new-topic'].result.topic_list
 				) {
 					deferredObject.reject( 'invalid-result', 'Unable to find appropriate section in result' );
 					return;
 				}
-				var output = data.flow['new-topic'].result['topic_list'];
+				output = data.flow['new-topic'].result.topic_list;
 
 				deferredObject.resolve( output, data );
 			} )
-			.fail( function( ) {
+			.fail( function () {
 				deferredObject.reject.apply( this, arguments );
 			} );
 
@@ -195,7 +246,10 @@ mw.flow = {
 	}
 };
 
-// Now add all the individual actions
+/**
+ * @param {string} workflowId
+ * @return {Deferred}
+ */
 mw.flow.api.reply = mw.flow.api.generateTopicAction(
 	'reply',
 	[
@@ -204,6 +258,10 @@ mw.flow.api.reply = mw.flow.api.generateTopicAction(
 	]
 );
 
+/**
+ * @param {string} workflowId
+ * @return {Deferred}
+ */
 mw.flow.api.changeTitle = mw.flow.api.generateTopicAction(
 	'edit-title',
 	[
@@ -211,6 +269,10 @@ mw.flow.api.changeTitle = mw.flow.api.generateTopicAction(
 	]
 );
 
+/**
+ * @param {string} workflowId
+ * @return {Deferred}
+ */
 mw.flow.api.editPost = mw.flow.api.generateTopicAction(
 	'edit-post',
 	[
@@ -218,5 +280,5 @@ mw.flow.api.editPost = mw.flow.api.generateTopicAction(
 		'content'
 	]
 );
-});
-})( jQuery, mediaWiki );
+} );
+} )( jQuery, mediaWiki );

modules/discussion/paging.js
@@ -1,5 +1,9 @@
-( function( $, mw ) {
-	var getPagingLink = function( data ) {
+( function ( $, mw ) {
+	/**
+	 * @param {object} data
+	 * @returns {jQuery}
+	 */
+	var getPagingLink = function ( data ) {
 		var direction = data.direction,
 			offset = data.offset,
 			limit = data.limit;
@@ -11,60 +15,60 @@
 			.data( 'offset', offset )
 			.data( 'limit', limit )
 			.append(
-				$('<a/>')
+				$( '<a/>' )
 					.attr( 'href', '#' )
-					.text( mw.msg( 'flow-paging-'+direction ) )
+					.text( mw.msg( 'flow-paging-' + direction ) )
 			);
 	};
 
-	$( document ).on( 'flow_init', function( e ) {
-		$(this).find( '.flow-paging a' ).click( function(e) {
+	$( document ).on( 'flow_init', function () {
+		$( this ).find( '.flow-paging a' ).click( function ( e ) {
 			e.preventDefault();
-			var $pagingLinkDiv = $(this).closest('.flow-paging')
-				.addClass( 'flow-paging-loading' );
+			var $pagingLinkDiv = $( this ).closest( '.flow-paging' )
+					.addClass( 'flow-paging-loading' ),
 
-			var offset = $pagingLinkDiv.data( 'offset' );
-			var direction = $pagingLinkDiv.data( 'direction' );
-			var limit = $pagingLinkDiv.data( 'limit' );
-			var workflowId = $(this).flow( 'getTopicWorkflowId' );
-			var pageName = $(this).closest( '.flow-container' ).data( 'page-title' );
-			var requestLimit;
+				offset = $pagingLinkDiv.data( 'offset' ),
+				direction = $pagingLinkDiv.data( 'direction' ),
+				limit = $pagingLinkDiv.data( 'limit' ),
+				workflowId = $( this ).flow( 'getTopicWorkflowId' ),
+				pageName = $( this ).closest( '.flow-container' ).data( 'page-title' ),
 
-			// One more for paging forward.
-			requestLimit = limit + 1;
+				// One more for paging forward.
+				requestLimit = limit + 1,
 
-			var request = {
-				'topic_list' : {
-					'offset-dir' : direction,
-					'offset-id' : offset,
-					'limit' : requestLimit,
-					'render' : true
-				}
-			};
+				request = {
+					'topic_list' : {
+						'offset-dir' : direction,
+						'offset-id' : offset,
+						'limit' : requestLimit,
+						'render' : true
+					}
+				};
 
 			mw.flow.api.readTopicList( pageName, workflowId, request )
-				.done( function( data ) {
-					var nextPage, prevPage;
-					var topics = [];
-					$.each( data, function( k, v ) {
-						if ( k - 0 == k ) {
+				.done( function ( data ) {
+					var topics = [],
+						$output = $( '<div/>' ),
+						$replaceContent;
+
+					$.each( data, function ( k, v ) {
+						if ( k - 0 === k ) {
 							topics.push( v );
 						}
 					} );
 
-					var $output = $('<div/>');
 
-					if ( direction == 'rev' && data.paging.rev ) {
+					if ( direction === 'rev' && data.paging.rev ) {
 						$output.append(
 							getPagingLink(
 								data.paging.rev
 							)
 						);
 					}
-					$.each( topics, function( k, topic ) {
+					$.each( topics, function ( k, topic ) {
 						$output.append( topic.rendered );
 					} );
-					if ( direction == 'fwd' && data.paging.fwd ) {
+					if ( direction === 'fwd' && data.paging.fwd ) {
 						$output.append(
 							getPagingLink(
 								data.paging.fwd
@@ -72,12 +76,12 @@
 						);
 					}
 
-					var $replaceContent = $output.children();
+					$replaceContent = $output.children();
 					$pagingLinkDiv.next( '.flow-error' ).remove();
 					$pagingLinkDiv.replaceWith( $replaceContent );
 					$replaceContent.trigger( 'flow_init' );
 				} )
-				.fail( function() {
+				.fail( function () {
 					$( '<div/>' )
 						.flow( 'showError', arguments )
 						.insertAfter( $pagingLinkDiv );
@@ -85,23 +89,24 @@
 		} );
 	} );
 
-	$( function() {
-		var $window = $(window);
+	$( function () {
+		var $window = $( window );
 		$window
-			.scroll( function(e) {
-				$( '.flow-paging-fwd' ).each( function() {
-					var $pagingLinkDiv = $( this );
+			.scroll( function () {
+				$( '.flow-paging-fwd' ).each( function () {
+					var $pagingLinkDiv = $( this ),
+
+						// Trigger infinite scroll when the user is half a screenlength
+						// away from the end.
+						windowEnd = $window.scrollTop() + ( 1.5 * $window.height() );
+
 					if ( $pagingLinkDiv.hasClass( 'flow-paging-loading' ) ) {
 						// Already loading
 						return;
 					}
 
-					// Trigger infinite scroll when the user is half a screenlength
-					//  away from the end.
-					var windowEnd = $window.scrollTop() + (1.5 * $window.height() );
-
 					if ( $pagingLinkDiv.position().top < windowEnd ) {
-						$pagingLinkDiv.find('a').click();
+						$pagingLinkDiv.find( 'a' ).click();
 					}
 				} );
 			} );

includes/api/ApiFlow.php
@@ -155,6 +155,7 @@ class ApiFlow extends ApiBase {
 	}
 
 	public function getExamples() {
+		// @todo: fill out an example
 		 return array(
 			 ''
 			 => ''

modules/discussion/forms.js
@@ -1,12 +1,12 @@
-( function( $, mw ) {
-$(document).on( 'flow_init', function( e ) {
-	$container = $( e.target );
-	$container.find('form.flow-reply-form').flow( 'setupEmptyDisabler',
+( function ( $, mw ) {
+$( document ).on( 'flow_init', function ( e ) {
+	var $container = $( e.target );
+	$container.find( 'form.flow-reply-form' ).flow( 'setupEmptyDisabler',
 		['.flow-reply-content'],
 		'.flow-reply-submit'
 	);
 
-	$('form.flow-newtopic-form').flow( 'setupEmptyDisabler',
+	$( 'form.flow-newtopic-form' ).flow( 'setupEmptyDisabler',
 		[
 			'.flow-newtopic-title',
 			'.flow-newtopic-content'
@@ -18,7 +18,7 @@ $(document).on( 'flow_init', function( e ) {
 	$container.flow( 'setupFormHandler',
 		'.flow-newtopic-submit',
 		mw.flow.api.newTopic,
-		function() {
+		function () {
 			$form = $(this).closest( '.flow-newtopic-form' );
 
 			var workflowParam = $container.flow( 'getWorkflowParameters' );
@@ -27,17 +27,16 @@ $(document).on( 'flow_init', function( e ) {
 
 			return [ workflowParam, title, content ];
 		},
-		function( workflowParam, title, content ) {
+		function ( workflowParam, title, content ) {
 			return title && content;
 		},
-		function( promise ) {
+		function ( promise ) {
 			promise
-				.done( function( output ) {
-					$form = $(this).closest( '.flow-newtopic-form' );
-
-					var $newRegion = $( output.rendered )
-						.hide()
-						.insertAfter( $form );
+				.done( function ( output ) {
+					var $form = $( this ).closest( '.flow-newtopic-form' ),
+						$newRegion = $( output.rendered )
+							.hide()
+							.insertAfter( $form );
 
 					$newRegion.trigger( 'flow_init' );
 
@@ -50,7 +49,7 @@ $(document).on( 'flow_init', function( e ) {
 	$container.flow( 'setupFormHandler',
 		'.flow-reply-submit',
 		mw.flow.api.reply,
-		function() {
+		function () {
 			$form = $(this).closest( '.flow-reply-form' );
 
 			var workflowId = $( this ).flow( 'getTopicWorkflowId' );
@@ -63,19 +62,18 @@ $(document).on( 'flow_init', function( e ) {
 
 			return [ workflowId, replyToId, content ];
 		},
-		function( workflowId, replyTo, content ) {
+		function ( workflowId, replyTo, content ) {
 			return content;
 		},
-		function( promise ) {
+		function ( promise ) {
 			promise
-				.done( function( output ) {
-					var $replyContainer = $(this)
-						.closest( '.flow-post-container' )
-						.children( '.flow-post-replies' );
-
-					var $newRegion = $( output.rendered )
-						.hide()
-						.prependTo( $replyContainer );
+				.done( function ( output ) {
+					var $replyContainer = $( this )
+							.closest( '.flow-post-container' )
+							.children( '.flow-post-replies' ),
+						$newRegion = $( output.rendered )
+							.hide()
+							.prependTo( $replyContainer );
 
 					$newRegion.trigger( 'flow_init' );
 
@@ -86,13 +84,13 @@ $(document).on( 'flow_init', function( e ) {
 
 	// Overload 'edit post' link.
 	$container.find( '.flow-action-edit-post a' )
-		.click( function(e) {
+		.click( function ( e ) {
 			e.preventDefault();
-			var $postContainer = $(this).closest( '.flow-post' );
-			var $contentContainer = $postContainer.find( '.flow-post-content' );
-			var workflowId = $( this ).flow( 'getTopicWorkflowId' );
-			var pageName = $(this).closest( '.flow-container' ).data( 'page-title' );
-			var postId = $postContainer.data( 'post-id' );
+			var $postContainer = $( this ).closest( '.flow-post' ),
+				$contentContainer = $postContainer.find( '.flow-post-content' ),
+				workflowId = $( this ).flow( 'getTopicWorkflowId' ),
+				pageName = $( this ).closest( '.flow-container' ).data( 'page-title' ),
+				postId = $postContainer.data( 'post-id' );
 
 			if ( $postContainer.find( '.flow-edit-post-form' ).length ) {
 				return;
@@ -108,12 +106,12 @@ $(document).on( 'flow_init', function( e ) {
 					}
 				}
 			)
-				.done( function( data ) {
-					if ( ! data[0] || data[0]['post-id'] != postId ) {
+				.done( function ( data ) {
+					if ( !data[0] || data[0]['post-id'] !== postId ) {
 						console.dir( data );
-						var $errorDiv = $( '<div/>' )
+						$( '<div/>' )
 							.addClass( 'flow-error' )
-							.text( mw.msg( 'flow-error-other') )
+							.text( mw.msg( 'flow-error-other' ) )
 							.hide()
 							.insertAfter( $contentContainer )
 							.slideDown();
@@ -140,10 +138,10 @@ $(document).on( 'flow_init', function( e ) {
 										.addClass( 'flow-cancel-link' )
 										.addClass( 'mw-ui-destructive' )
 										.attr( 'href', '#' )
-										.click( function(e) {
+										.click( function ( e ) {
 											e.preventDefault();
 											$postForm.slideUp( 'fast',
-												function() {
+												function () {
 													$contentContainer.show();
 													$postForm.remove();
 												}
@@ -166,35 +164,35 @@ $(document).on( 'flow_init', function( e ) {
 					$postContainer.flow( 'setupFormHandler',
 						'.flow-edit-post-submit',
 						mw.flow.api.editPost,
-						function() {
+						function () {
 							var content = $postForm.find( '.flow-edit-post-content' ).val();
 
 							return [ workflowId, postId, content ];
 						},
-						function( workflowId, postId, content ) {
+						function ( workflowId, postId, content ) {
 							return content;
 						},
-						function( promise ) {
-							promise.done( function(output) {
+						function ( promise ) {
+							promise.done( function ( output ) {
 								$contentContainer
 									.empty()
 									.append(
-										$(output.rendered)
-											.find('.flow-post-content')
+										$( output.rendered )
+											.find( '.flow-post-content' )
 											.children()
 									);
 								} );
 						}
 					);
 
-					$('form.flow-edit-post-form').flow( 'setupEmptyDisabler',
+					$( 'form.flow-edit-post-form' ).flow( 'setupEmptyDisabler',
 						[
 							'.flow-edit-post-content'
 						],
 						'.flow-edit-post-submit'
 					);
 				} )
-				.fail( function() {
+				.fail( function () {
 					var $errorDiv = $( '<div/>' )
 						.addClass( 'flow-error' )
 						.hide();
@@ -208,32 +206,32 @@ $(document).on( 'flow_init', function( e ) {
 
 	// Overload 'edit title' link.
 	$container.find( '.flow-action-edit-title a' )
-		.click( function(e) {
+		.click( function ( e ) {
 			e.preventDefault();
 
-			var $topicContainer = $(this).closest( '.flow-topic-container' );
-			var $titleBar = $topicContainer.find( '.flow-topic-title' );
-			var oldTitle = $topicContainer.data( 'title' );
+			var $topicContainer = $( this ).closest( '.flow-topic-container' ),
+				$titleBar = $topicContainer.find( '.flow-topic-title' ),
+				oldTitle = $topicContainer.data( 'title' ),
+				$titleEditForm = $( '<form />' );
 
 			$titleBar.find( '.flow-realtitle' )
 				.hide();
 
-			var $titleEditForm = $( '<form />' );
 			$titleEditForm
 				.addClass( 'flow-edit-title-form' )
 				.append(
-					$('<input />')
+					$( '<input />' )
 						.addClass( 'flow-edit-title-textbox' )
 						.attr( 'type', 'text' )
 						.val( oldTitle )
 				)
 				.append(
-					$('<a/>')
+					$( '<a/>' )
 						.addClass( 'flow-cancel-link' )
 						.addClass( 'mw-ui-destructive' )
 						.attr( 'href', '#' )
 						.text( mw.msg( 'flow-cancel' ) )
-						.click( function(e) {
+						.click( function ( e ) {
 							e.preventDefault();
 							$titleBar.children( 'form' )
 								.remove();
@@ -248,7 +246,7 @@ $(document).on( 'flow_init', function( e ) {
 						.addClass( 'mw-ui-button' )
 						.addClass( 'mw-ui-primary' )
 						.attr( 'type', 'submit' )
-						.val( mw.msg('flow-edit-title-submit' ) )
+						.val( mw.msg( 'flow-edit-title-submit' ) )
 				)
 				.appendTo( $titleBar )
 				.find( '.flow-edit-title-textbox' )
@@ -258,17 +256,17 @@ $(document).on( 'flow_init', function( e ) {
 				$titleEditForm.flow( 'setupFormHandler',
 					'.flow-edit-title-submit',
 					mw.flow.api.changeTitle,
-					function() {
-						workflowId = $topicContainer.data( 'topic-id' );
-						content = $titleEditForm.find( '.flow-edit-title-textbox' ).val();
+					function () {
+						var workflowId = $topicContainer.data( 'topic-id' ),
+							content = $titleEditForm.find( '.flow-edit-title-textbox' ).val();
 
 						return [ workflowId, content ];
 					},
-					function( workflowId, content ) {
+					function ( workflowId, content ) {
 						return content && true;
 					},
-					function( promise ) {
-						promise.done(function( output ) {
+					function ( promise ) {
+						promise.done( function ( output ) {
 							$titleBar.find( '.flow-realtitle' )
 								.empty()
 								.text( output.rendered )

modules/discussion/init.js
@@ -1,5 +1,5 @@
-( function($, mw) {
-	$( function() {
+( function ( $, mw ) {
+	$( function () {
 		$( '.flow-container' ).trigger( 'flow_init' );
-	});
+	} );
 } )( jQuery, mediaWiki );

commit 1ca3af30dcf8325d54f8723ffb74c06106d63322

Fix paging bugs caused by 6f99d757 and another rev I cannot find.
Also fixing some error handling (some things were using .fail() instead of .reject(), other bugs)

Change-Id: Ic653d5c5dc4c6078e94a161f38172e4f08c957f0

includes/Block/Topic.php
@@ -506,7 +506,7 @@ class TopicBlock extends AbstractBlock {
 				'*' => $post->getContent( null, $contentFormat ),
 				'format' => $contentFormat
 			);
-			$output['user'] = $post->getCreatorText();
+			$output['user'] = $post->getCreator()->getName();
 		}
 
 		if ( ! isset( $options['no-children'] ) ) {

modules/base/ext.flow.base.js
@@ -90,7 +90,7 @@ mw.flow = {
 						!output.query.flow ||
 						output.query.flow._element !== 'block'
 					) {
-						deferredObject.fail( 'invalid-result', 'Unable to understand the API result' );
+						deferredObject.reject( 'invalid-result', 'Unable to understand the API result' );
 						return;
 					}
 
@@ -102,11 +102,11 @@ mw.flow = {
 						}
 					} );
 
-					deferredObject.fail( 'invalid-result', 'Unable to find the '+
+					deferredObject.reject( 'invalid-result', 'Unable to find the '+
 						blockName+' block in the API result' );
 				} )
 				.fail( function () {
-					deferredObject.fail( arguments );
+					deferredObject.reject.apply( this, arguments );
 				} );
 
 			return deferredObject.promise();

modules/discussion/paging.js
@@ -24,10 +24,14 @@
 	$( document ).on( 'flow_init', function () {
 		$( this ).find( '.flow-paging a' ).click( function ( e ) {
 			e.preventDefault();
-			var $pagingLinkDiv = $( this ).closest( '.flow-paging' )
-					.addClass( 'flow-paging-loading' ),
+			var $pagingLinkDiv = $( this ).closest( '.flow-paging' );
 
-				offset = $pagingLinkDiv.data( 'offset' ),
+			if( $pagingLinkDiv.hasClass( 'flow-paging-loading' ) ) {
+				return;
+			}
+			$pagingLinkDiv.addClass( 'flow-paging-loading' );
+
+			var offset = $pagingLinkDiv.data( 'offset' ),
 				direction = $pagingLinkDiv.data( 'direction' ),
 				limit = $pagingLinkDiv.data( 'limit' ),
 				workflowId = $( this ).flow( 'getTopicWorkflowId' ),
@@ -52,7 +56,7 @@
 						$replaceContent;
 
 					$.each( data, function ( k, v ) {
-						if ( k - 0 === k ) {
+						if ( parseInt( k, 10 ) == k ) {
 							topics.push( v );
 						}
 					} );