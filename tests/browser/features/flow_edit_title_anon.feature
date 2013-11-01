9467bf57198009b84a81a301dee2bc864b18c78b
cmcmahon <cmcmahon@wikimedia.org>
2013-11-01 09:29:29 -0600

    new test for editing existing title, also alphabetize and cleanup
    Change-Id: I68939276503c33e2354c0f4976d09f7bccf34485

---
+++ tests/browser/features/flow_edit_title_anon.feature
@@ -0,0 +1,11 @@
+@en.wikipedia.beta.wmflabs.org @ee-prototype.wmflabs.org
+
+Feature: Edit title
+
+  Scenario: Click edit title 
+    Given I am on Flow page
+    When I hover over the topic post
+      And I click the pencil icon
+    Then the title text field should be visible
+      And the Change Title button should be enabled
+      And the Cancel button should be visible
--- tests/browser/features/step_definitions/flow_steps.rb
+++ tests/browser/features/step_definitions/flow_steps.rb
@@ -2,43 +2,6 @@ Given(/^I am on Flow page$/) do
   visit FlowPage
 end
 
-When(/^I create a (.+) in Flow new topic$/) do |flow_title|
-  @automated_test_marker = ' browsertest edit'
-  on(FlowPage) do |page|
-    page.new_topic_title_element.when_present.click
-    page.new_topic_title_element.when_present.send_keys(flow_title + @random_string + @automated_test_marker)
-  end
-end
-
-When(/^I create a (.+) into VisualEditor Flow body$/) do |flow_body|
-  on(FlowPage).new_topic_body_ve_element.when_present.send_keys(flow_body + @random_string + @automated_test_marker)
-end
-
-When(/^I create a (.+) into Flow body$/) do |flow_body|
-  on(FlowPage).new_topic_body_element.when_present.send_keys(flow_body + @random_string + @automated_test_marker)
-end
-
-When(/^I click New topic save$/) do
-  on(FlowPage).new_topic_save_element.when_present.click
-end
-
-Then(/^the Flow page should contain (.+)$/) do |flow_topic|
-  on(FlowPage) do |page|
-    page.wait_until(20) do	# 10 seconds wasn't enough on ee-flow...
-      # TODO Also match the regexp '[2-9] seconds ago' in case of delays.
-      # TODO This should look in the particular topic that was added, not
-      # blindly look for text.
-      # It could note the ID of the first div with id 'flow-topic-<UUID>'
-      # before submitting the new topic post,
-      # then afterwards find the flow-topic-<UUID> div *preceding* that and search in there.
-      # Or after submit it could look for the Title of Flow Topic, find the
-      # flow-topic-container containing that and search in there.
-      page.text.include? '1 second ago' or page.text.include? 'just now'
-    end
-    page.flow_body.should match(flow_topic + @random_string + @automated_test_marker)
-  end
-end
-
 Given(/^I have created a Flow topic$/) do
   step 'I am on Flow page'
   step 'I create a Title of Flow Topic in Flow new topic'
@@ -66,6 +29,34 @@ Given(/^the talk and contrib links are not visible$/) do
   end
 end
 
+When(/^I click New topic save$/) do
+  on(FlowPage).new_topic_save_element.when_present.click
+end
+
+When(/^I click the pencil icon$/) do
+  on(FlowPage).pencil_icon_element.when_present.click
+end
+
+When(/^I create a (.+) in Flow new topic$/) do |flow_title|
+  @automated_test_marker = ' browsertest edit'
+  on(FlowPage) do |page|
+    page.new_topic_title_element.when_present.click
+    page.new_topic_title_element.when_present.send_keys(flow_title + @random_string + @automated_test_marker)
+  end
+end
+
+When(/^I create a (.+) into VisualEditor Flow body$/) do |flow_body|
+  on(FlowPage).new_topic_body_ve_element.when_present.send_keys(flow_body + @random_string + @automated_test_marker)
+end
+
+When(/^I create a (.+) into Flow body$/) do |flow_body|
+  on(FlowPage).new_topic_body_element.when_present.send_keys(flow_body + @random_string + @automated_test_marker)
+end
+
+When(/^I hover over the topic post$/) do
+  on(FlowPage).topic_post_element.hover
+end
+
 When(/^I hover over the author link$/) do
   on(FlowPage).author_link_element.fire_event('onmouseover')
 end
@@ -75,4 +66,33 @@ Then(/^links to talk and contrib should be visible$/) do
     page.talk_link_element.should be_visible
     page.contrib_link_element.should be_visible
   end
+end
+
+Then(/^the Cancel button should be visible$/) do
+  on(FlowPage).cancel_button_element.should exist
+end
+
+Then(/^the Change Title button should be enabled$/) do
+  on(FlowPage).change_title_element.should be_visible
+end
+
+Then(/^the Flow page should contain (.+)$/) do |flow_topic|
+  on(FlowPage) do |page|
+    page.wait_until(20) do  # 10 seconds wasn't enough on ee-flow...
+      # TODO Also match the regexp '[2-9] seconds ago' in case of delays.
+      # TODO This should look in the particular topic that was added, not
+      # blindly look for text.
+      # It could note the ID of the first div with id 'flow-topic-<UUID>'
+      # before submitting the new topic post,
+      # then afterwards find the flow-topic-<UUID> div *preceding* that and search in there.
+      # Or after submit it could look for the Title of Flow Topic, find the
+      # flow-topic-container containing that and search in there.
+      page.text.include? '1 second ago' or page.text.include? 'just now'
+    end
+    page.flow_body.should match(flow_topic + @random_string + @automated_test_marker)
+  end
+end
+
+Then(/^the title text field should be visible$/) do
+  on(FlowPage).title_textfield_element.when_present.should be_visible
 end
