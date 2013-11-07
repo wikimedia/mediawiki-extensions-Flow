7a9f509491dc01176369d6cc4a458ac77a4750e3
Andrew Garrett <agarrett@wikimedia.org>
2013-11-07 16:27:26 +1100

    Add access to historical revisions.
    * Includes a refactoring of timestamp display (I could not get to it in
      post.html.php because it is abstracted away)
    * Still needs API module, access to header revisions.
    * Copy is not final.
    
    Change-Id: I4255fb669f4884d984307ec7e8197dd0bb1b7bbe

---
+++ templates/revision-permalink-warning.html.php
@@ -0,0 +1,65 @@
+<?php
+
+$type = $revision->getRevisionType();
+if ( $type == 'post' && $revision->isTopicTitle() ) {
+	$type = 'title';
+}
+
+$timestamp = $revision->getRevisionId()->getTimestampObj();
+$formattedTimestamp = $this->render( 'flow:timestamp.html.php', array(
+	'timestamp' => $timestamp,
+	'tag' => 'span',
+), true );
+
+$urlGenerator = $this->getUrlGenerator();
+
+if ( $revision->getPrevRevisionId() ) {
+	$compareLink = $urlGenerator->generateUrl(
+		$block->getWorkflow(),
+		'compare-revisions',
+		array(
+			$block->getName().'[newRevision]' => $revision->getRevisionId()->getHex(),
+			$block->getName().'[oldRevision]' => $revision->getPrevRevisionId()->getHex()
+		)
+	);
+} else {
+	$compareLink = false;
+}
+
+switch( $revision->getRevisionType() ) {
+	case 'post':
+		$historyLink = $urlGenerator->generateUrl(
+			$block->getWorkflow(),
+			'post-history',
+			array(
+				$block->getName().'[postId]' => $revision->getPostId()->getHex(),
+			)
+		);
+
+		$msgKey = $compareLink ? 'flow-revision-permalink-warning-post' : 'flow-revision-permalink-warning-post-first';
+		$message = wfMessage( $msgKey )
+			->rawParams( $formattedTimestamp )
+			->params(
+				$block->getWorkflow()->getArticleTitle(),
+				$block->getTitleText(),
+				$historyLink
+			);
+
+		if ( $compareLink ) {
+			$message->params( $compareLink );
+		}
+		break;
+	case 'header':
+		// @todo Implement
+		break;
+	default:
+		throw new \MWException( "Unknown revision type: " . $revision->getRevisionType() );
+}
+
+echo Html::rawElement(
+	'div',
+	array(
+		'class' => 'flow-revision-permalink-warning plainlinks',
+	),
+	$message->parse()
+);
