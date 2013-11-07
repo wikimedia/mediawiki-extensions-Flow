42c7ef9a89df0f88e23e1a5c034957099a2b9d2e
Matthias Mullie <git@mullie.eu>
2013-11-08 00:57:16 +0100

    auto-expand text-area as content grows
    Change-Id: Id690ba0462812ed1eaea392ac606cae3f760f626

--- modules/editor/editors/ext.flow.editors.none.js
+++ modules/editor/editors/ext.flow.editors.none.js
@@ -8,6 +8,10 @@
 	mw.flow.editors.none = function ( $node, content ) {
 		this.$node = $node;
 		this.$node.val( content || '' );
+
+		// auto-expansion shouldn't shrink too much; set default height as min
+		this.$node.css( 'min-height', this.$node.height() );
+		this.$node.keyup( this.autoExpand );
 	};
 
 	/**
@@ -18,7 +22,8 @@
 	mw.flow.editors.none.format = 'wikitext';
 
 	mw.flow.editors.none.prototype.destroy = function () {
-
+		// unset min-height that was set for auto-expansion
+		this.$node.css( 'min-height', '' );
 	};
 
 	/**
@@ -26,5 +31,34 @@
 	 */
 	mw.flow.editors.none.prototype.getRawContent = function () {
 		return this.$node.val();
+	};
+
+	/**
+	 * Auto-expand/shrink as content changes.
+	 */
+	mw.flow.editors.none.prototype.autoExpand = function() {
+		var height = $( this ).height(),
+			scrollHeight;
+
+		/*
+		 * Collapse to 0 height to get accurate scrollHeight for the content,
+		 * then restore height.
+		 * Without collapsing, scrollHeight would be the highest of:
+		 * * the content height
+		 * * the height the textarea already has
+		 * Since we're looking to also shrink the textarea when content shrinks,
+		 * we want to ignore that last case (hence the collapsing)
+		 */
+		$( this ).height( 0 );
+		scrollHeight = this.scrollHeight;
+		$( this ).height( height );
+
+		/*
+		 * Only animate height change if there actually is a change; we don't
+		 * want every keystroke firing a 50ms animation.
+		 */
+		if ( scrollHeight != $( this ).height() ) {
+			$( this ).animate( { height: scrollHeight }, 50 );
+		}
 	};
 } ( jQuery, mediaWiki ) );
