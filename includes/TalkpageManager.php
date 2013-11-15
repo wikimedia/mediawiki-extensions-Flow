a50269d0325a2e4afb3ec169e93477b70766c01b
Matthias Mullie <git@mullie.eu>
2013-11-15 16:44:02 +0100

    Insert new revision when Flow is taking over
    Change-Id: I302ef7ed48203176af400bd4e8bcf1f8baa8c486

--- includes/TalkpageManager.php
+++ includes/TalkpageManager.php
@@ -2,6 +2,11 @@
 
 namespace Flow;
 
+use Title;
+use WikiPage;
+use Revision;
+use ContentHandler;
+
 // I got the feeling NinetyNinePercentController was a bit much.
 interface OccupationController {
 	public function isTalkpageOccupied( $title );
@@ -37,8 +42,36 @@ class TalkpageManager implements OccupationController {
 			// wtf?
 			throw new \MWException( 'Non-falsy non-object title passed: ' . var_export( $title, true ) );
 		}
+
 		return in_array( $title->getPrefixedText(), $this->occupiedPages )
 			|| ( in_array( $title->getNamespace(), $this->occupiedNamespaces )
 				&& !$title->isSubpage() );
+	}
+
+	/**
+	 * When a page is taken over by Flow, add a revision.
+	 * First, it provides a clearer history should Flow be disabled again later,
+	 * and a descriptive message when people attempt to use regular API to fetch
+	 * data for this "Page", which will no longer contain any useful content,
+	 * since Flow has taken over.
+	 * Also: Parsoid performs an API call to fetch page information, so we need
+	 * to make sure a page actually exists ;)
+	 *
+	 * @param Article $article
+	 */
+	public function ensureFlowRevision( \Article $article ) {
+		// comment to add to the Revision to indicate Flow taking over
+		$comment = '/* Taken over by Flow */';
+
+		$title = $article->getTitle();
+		$page = $article->getPage();
+		$revision = $page->getRevision();
+
+		// make sure a Flow revision has not yet been inserted
+		if ( $revision === null || $revision->getComment( Revision::RAW ) != $comment ) {
+			$message = wfMessage( 'flow-talk-taken-over' )->text();
+			$content = ContentHandler::makeContent( $message, $title );
+			$page->doEditContent( $content, $comment, EDIT_FORCE_BOT | EDIT_SUPPRESS_RC );
+		}
 	}
 }
