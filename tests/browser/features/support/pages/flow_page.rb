21078abd02e07d39e18b5b8567ae4b8c0d45c6a2
cmcmahon <cmcmahon@wikimedia.org>
2013-11-18 09:43:39 -0700

    new test for editing existing title, also alphabetize and cleanup
    Change-Id: I6e68755b14ec07b0a1b1d36d9ee8e6f88ce4f97f

--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -4,15 +4,19 @@ class FlowPage
   include PageObject
 
   include URL
-  # MEDIAWIKI_URL must have this in $wgFlowOccupyPages array or $wgFlowOccupyNamespaces.
   page_url URL.url('Talk:Flow_QA')
 
   span(:author_link, class: 'flow-creator-simple')
-  a(:talk_link, text: 'Talk')
+  a(:cancel_button, class: 'flow-cancel-link mw-ui-button mw-ui-text')
+  button(:change_title, value: 'Change title')
   a(:contrib_link, text: 'contribs')
-  text_field(:new_topic_title, name: 'topic_list[topic]')
+  div(:flow_body, class: 'flow-container')
   text_area(:new_topic_body, class: 'flow-newtopic-content')
   div(:new_topic_body_ve, class: 've-ce-documentNode ve-ce-branchNode')
   button(:new_topic_save, class: 'flow-newtopic-submit')
-  div(:flow_body, class: 'flow-container')
+  text_field(:new_topic_title, name: 'topic_list[topic]')
+  a(:pencil_icon, text: 'Edit title')
+  a(:talk_link, text: 'Talk')
+  text_field(:title_textfield, class: 'mw-ui-input flow-edit-title-textbox')
+  unordered_list(:topic_post, class: 'flow-topic-posts-meta')
 end
