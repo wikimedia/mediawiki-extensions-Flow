622826e98f969153e72a51047b08f017a5efbadd
cmcmahon <cmcmahon@wikimedia.org>
2013-11-25 15:19:19 -0700

    new test to edit existing title/post
    Change-Id: I14b9d02121d45be266f27420999cfa8167c1a17f

---
+++ tests/browser/features/step_definitions/edit_existing_steps.rb
@@ -0,0 +1,45 @@
+When(/^I click the Edit title pencil icon$/) do
+  on(FlowPage) do |page|
+    page.topic_title_element.hover
+    page.edit_title_icon_element.when_present.click
+  end
+end
+
+When(/^I click the Edit post pencil icon$/) do
+  on(FlowPage) do |page|
+    page.topic_post_element.hover
+    page.edit_post_icon_element.when_present.click
+  end
+end
+
+Then(/^I should be able to edit the title field with (.+)$/) do |edited_title|
+  on(FlowPage).title_edit_element.when_present.send_keys(edited_title + @random_string)
+end
+
+Then(/^I should be able to save the new post body$/) do
+  on(FlowPage).change_post_save_element.when_present.click
+end
+
+Then(/^I should be able to save the new title$/) do
+  on(FlowPage).change_title_save_element.when_present.click
+end
+
+Then(/^the saved topic title should contain (.+)$/) do |edited_title|
+  on(FlowPage) do |page|
+    page.small_spinner_element.when_not_present
+    page.topic_title.should match(edited_title + @random_string)
+  end
+end
+
+Then(/^I should be able to edit the post field with (.+)$/) do |edited_post|
+  on(FlowPage).post_edit_element.when_present.send_keys(edited_post + @random_string)
+end
+
+Then(/^the saved topic body should contain (.+)$/) do |edited_post|
+  on(FlowPage) do |page|
+    page.small_spinner_element.when_not_present
+    page.topic_post.should match(edited_post + @random_string)
+  end
+end
+
+
