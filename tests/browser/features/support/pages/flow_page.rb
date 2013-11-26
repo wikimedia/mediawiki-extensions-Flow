f447681844ad49a5a9f1a994b35fdfcf7f27c49b
mayankmadan <maddiemadan@gmail.com>
2013-11-26 23:45:49 +0530

    Prefer double-quoted strings in Ruby code
    Bug: 57597
    Change-Id: I7c22158ad80e0c99e6cf74a2fb140458a1c3d538

--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -1,17 +1,17 @@
-require 'page-object'
+require "page-object"
 
 class FlowPage
   include PageObject
 
   include URL
   # MEDIAWIKI_URL must have this in $wgFlowOccupyPages array or $wgFlowOccupyNamespaces.
-  page_url URL.url('Talk:Flow_QA')
+  page_url URL.url("Talk:Flow_QA")
 
-  span(:author_link, class: 'flow-creator-simple')
-  a(:talk_link, text: 'Talk')
-  a(:contrib_link, text: 'contribs')
-  text_field(:new_topic_title, name: 'topic_list[topic]')
-  text_area(:new_topic_body, class: 'flow-newtopic-content')
-  button(:new_topic_save, class: 'flow-newtopic-submit')
-  div(:flow_body, class: 'flow-container')
+  span(:author_link, class: "flow-creator-simple")
+  a(:talk_link, text: "Talk")
+  a(:contrib_link, text: "contribs")
+  text_field(:new_topic_title, name: "topic_list[topic]")
+  text_area(:new_topic_body, class: "flow-newtopic-content")
+  button(:new_topic_save, class: "flow-newtopic-submit")
+  div(:flow_body, class: "flow-container")
 end
