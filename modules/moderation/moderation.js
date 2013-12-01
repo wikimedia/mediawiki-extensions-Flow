3da7d1d0f65eca58ef9efa7b6a4d82a8e8db5d29
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2013-12-01 14:45:05 +0200

    Better GENDER support in moderation.js
    * Add full message keys.
    * Add GENDER support for the current user.
    * Pass the post/topic creator's gender as data attributes,
      because the frontend side can't use usernames for gender.
    
    Change-Id: Ic6a86a84396a8f27cf4ae07f4028e3309afce308

--- modules/moderation/moderation.js
+++ modules/moderation/moderation.js
@@ -14,7 +14,7 @@
 			subject = $topicContainer.data( 'title' ),
 			$dialog = $( '<div />' ),
 			$form = $( '<form/>' ),
-			targetType, apiCallback, $resultContainer, user;
+			targetType, apiCallback, $resultContainer, creatorName;
 
 		if ( $postContainer.length === 0 ) {
 			// moderating a topic
@@ -26,37 +26,72 @@
 			$resultContainer = $postContainer;
 			targetType = 'post';
 		}
-		user = $resultContainer.data( 'creator-name' );
+		creatorName = $resultContainer.data( 'creator-name' );
 
 		$dialog
 			.addClass( 'flow-moderation-dialog' )
 			.dialog( {
+				// Messages used here:
+				// flow-moderation-title-suppress-post
+				// flow-moderation-title-delete-post
+				// flow-moderation-title-hide-post
+				// flow-moderation-title-restore-post
+				// flow-moderation-title-suppress-topic
+				// flow-moderation-title-delete-topic
+				// flow-moderation-title-hide-topic
+				// flow-moderation-title-restore-topic
 				'title' : mw.msg( 'flow-moderation-title-' + moderationType + '-' + targetType ),
 				'modal' : true,
 				'buttons' : [
 					{
+						// Messages used here:
+						// flow-moderation-confirm-suppress-post
+						// flow-moderation-confirm-delete-post
+						// flow-moderation-confirm-hide-post
+						// flow-moderation-confirm-restore-post
+						// flow-moderation-confirm-suppress-topic
+						// flow-moderation-confirm-delete-topic
+						// flow-moderation-confirm-hide-topic
+						// flow-moderation-confirm-restore-topic
 						'text' : mw.msg( 'flow-moderation-confirm-' + moderationType + '-' + targetType ),
 						'click' : $(this).flow( 'getFormHandler',
 							apiCallback,
 							function() {
 								var res = [ $topicContainer.data( 'topic-id' ) ];
+
 								if ( $postContainer.length > 0 ) {
 									res.push( $postContainer.data( 'post-id' ) );
 								}
 								res.push( moderationType, $form.find( '#flow-moderation-reason' ).val() );
+
 								return res;
 							},
 							undefined,
 							function( promise ) {
 								promise.done( function( output ) {
-										var confirmationMsg = 'flow-moderation-confirmation-' + moderationType + '-' + targetType,
+										var confirmationMsg,
 											$newContainer = $( output.rendered );
+
+										// Messages used here:
+										// flow-moderation-confirmation-suppress-post
+										// flow-moderation-confirmation-delete-post
+										// flow-moderation-confirmation-hide-post
+										// flow-moderation-confirmation-restore-post
+										// flow-moderation-confirmation-suppress-topic
+										// flow-moderation-confirmation-delete-topic
+										// flow-moderation-confirmation-hide-topic
+										// flow-moderation-confirmation-restore-topic
+										confirmationMsg = 'flow-moderation-confirmation-' +
+											moderationType + '-' + targetType;
 
 										$dialog.empty()
 											.dialog( 'option', 'buttons', null )
 											.append(
-												$( '<p/>' )
-													.text( mw.msg( confirmationMsg, user ) )
+												$( '<p/>' ).text( mw.msg(
+													confirmationMsg,
+													creatorName,
+													mw.user
+												) )
 											);
 
 										$resultContainer.replaceWith( $newContainer );
@@ -65,6 +100,7 @@
 									.fail( function() {
 										var $errorDiv = $( '<div/>' ).flow( 'showError', arguments ),
 											$errors = $form.children( '.flow-error' );
+
 										if ( $errors.length ) {
 											$errors.replaceWith( $errorDiv );
 										} else {
@@ -83,7 +119,23 @@
 					.append(
 						$( 'label' )
 							.attr( 'for', 'flow-moderation-reason' )
-							.text( mw.msg( 'flow-moderation-intro-' + moderationType + '-' + targetType, user, subject ) )
+							.text(
+								mw.message(
+									// Messages used here:
+									// flow-moderation-intro-suppress-topic
+									// flow-moderation-intro-delete-topic
+									// flow-moderation-intro-hide-topic
+									// flow-moderation-intro-restore-topic
+									// flow-moderation-intro-suppress-post
+									// flow-moderation-intro-delete-post
+									// flow-moderation-intro-hide-post
+									// flow-moderation-intro-restore-post
+									'flow-moderation-intro-' + moderationType + '-' + targetType,
+									creatorName, // $1 unused
+									subject, // $2 unused
+									mw.user // $3
+								).text()
+							)
 					)
 			)
 			.append(
