c4d7adca6c2126ef03869f2baf0b97ec2479c837
Erik Bernhardson <ebernhardson@wikimedia.org>
2013-12-03 14:44:10 -0800

    [SCHEMA CHANGE] Shorten uuids to 88 bits.
    Change-Id: I453d9bfdda8b4cd8903939b87bddb4fea339356e

---
+++ db_patches/patch-88bit_uuids.sql
@@ -0,0 +1,29 @@
+
+ALTER TABLE flow_definition
+	CHANGE definition_id definition_id binary(11) not null;
+
+ALTER TABLE flow_workflow
+	CHANGE workflow_id workflow_id binary(11) not null,
+	CHANGE workflow_definition_id workflow_definition_id binary(11) not null;
+
+ALTER TABLE flow_topic_list
+	CHANGE topic_list_id topic_list_id binary(11) not null,
+	CHANGE topic_id topic_id binary(11) default null;
+
+ALTER TABLE flow_tree_revision
+	CHANGE tree_rev_descendant_id tree_rev_descendant_id binary(11) not null,
+	CHANGE tree_rev_id tree_rev_id binary(11) not null,
+	CHANGE tree_parent_id tree_parent_id binary(11) default null;
+
+ALTER TABLE flow_header_revision
+	CHANGE header_workflow_id header_workflow_id binary(11) not null,
+	CHANGE header_rev_id header_rev_id binary(11) not null;
+
+ALTER TABLE flow_revision
+	CHANGE rev_id rev_id binary(11) not null,
+	CHANGE rev_parent_id rev_parent_id binary(11) default null,
+	CHANGE rev_last_edit_id rev_last_edit_id binary(11) default null;
+
+ALTER TABLE flow_tree_node
+	CHANGE tree_ancestor_id tree_ancestor_id binary(11) not null,
+	CHANGE tree_descendant_id tree_descendant_id binary(11) not null;
