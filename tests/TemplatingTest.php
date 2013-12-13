cddefaf61ab1a655cc2e56092a64a3bd8e76bd5d
Erik Bernhardson <ebernhardson@wikimedia.org>
2013-12-13 10:07:33 -0800

    Gracefully handle errors applying redlinks (wmf7)
    An invalid wikilink like [[Foo&bar]], when it gets through to the
    redlink handling, will error out because &bar is not a valid html
    entity.  This first repair sets up more gracefull error handling
    for failures in redlinking(just show the content without redlinks
    applied).  Also partial fix for underlying problem of invalid
    entities.
    
    (Backported to 1.23wmf7)
    
    Bug: 58455
    Change-Id: Iaa36485325668af4bb7b4556e6dedf9c9d3bf346

---
+++ tests/TemplatingTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Flow;
+
+use Title;
+
+class TemplatingTest extends \MediaWikiTestCase {
+
+	// Default values for PostRevision::newFromRow to work
+	static protected $postRow = array(
+		'rev_id' => null,
+		'rev_user_id' => null,
+		'rev_user_text' => null,
+		'rev_parent_id' => null,
+		'rev_change_type' => null,
+		'rev_flags' => null,
+		'rev_content' => null,
+		'rev_mod_state' => null,
+		'rev_mod_user_id' => null,
+		'rev_mod_user_text' => null,
+		'rev_mod_timestamp' => null,
+		'tree_parent_id' => null,
+		'tree_rev_id' => null,
+		'tree_rev_descendant_id' => null,
+		'tree_orig_create_time' => null,
+		'tree_orig_user_id' => null,
+		'tree_orig_user_text' => null,
+	);
+
+	static public function redLinkProvider() {
+		return array(
+			array(
+				'Basic redlink application',
+				// title text from parsoid
+				'Talk:Flow/Bugs',
+				// expect string
+				// @fixme easily breakable, depends on url order
+				htmlentities( 'Talk:Flow/Bugs&action=edit&redlink=1' ),
+			),
+
+			array(
+				'Subpage redlink application',
+				// title text from parsoid
+				'/SubPage',
+				// expect string
+				htmlentities( 'Main_Page/SubPage&action=edit&redlink=1' ),
+			),
+
+			array(
+				'Link containing html entities should be properly handled',
+				// title text from parsoid
+				'Foo&bar',
+				// expect string
+				htmlspecialchars( 'Foo%26bar&action=edit&redlink=1' ),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider redLinkProvider
+	 */
+	public function testRedLinks( $message, $saHref, $expect ) {
+		// needs a page to resolve subpage links against
+		$this->setMwGlobals( 'wgTitle', Title::newMainPage() );
+
+		$parsoid = htmlentities( json_encode( array( 'sa' => array( 'href' => $saHref ) ) ) );
+		$uid = Model\UUID::create( '0509b4bf4b2d616abe79080027a08222' );
+		$rev = Model\PostRevision::fromStorageRow( array(
+			'rev_id' => $uid->getBinary(),
+			'tree_rev_id' => $uid->getBinary(),
+			'rev_content' => '<a rel="mw:WikiLink" data-parsoid="' . $parsoid . '">' . htmlspecialchars( $saHref ) . '</a>',
+			'rev_flags' => 'html'
+		) + self::$postRow );
+
+		$content = $this->mockTemplating()->getContent( $rev, 'html' );
+		$this->assertContains( $expect, $content, $message );
+	}
+
+	protected function mockTemplating() {
+		$urlGenerator = $this->getMockBuilder( 'Flow\UrlGenerator' )
+			->disableOriginalConstructor()
+			->getMock();
+		$output = $this->getMockBuilder( 'OutputPage' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		return new Templating( $urlGenerator, $output );
+	}
+}
