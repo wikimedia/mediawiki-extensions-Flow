41415aff837557b00b6c00eef27e3a371a1488be
Erik Bernhardson <ebernhardson@wikimedia.org>
2013-12-20 15:53:11 -0800

    Simplify edit-title template
    Change-Id: Ia4b8c3f488f6f89d31352daa0385cfb808fdade3

--- templates/edit-title.html.php
+++ templates/edit-title.html.php
@@ -1,45 +1,14 @@
-<?php
-
-echo Html::openElement( 'div', array(
-	'class' => 'flow-topic-container flow-topic-full'
-) );
-echo Html::openElement( 'div', array(
-	'class' => 'flow-edit-title-form flow-element-container'
-) );
-echo Html::openElement( 'form', array(
-	'method' => 'POST',
-	// root post shares its uuid with the workflow
-	'action' => $this->generateUrl( $topicTitle->getPostId(), 'edit-title' ),
-) );
-if ( $block->hasErrors() ) {
-	echo '<ul>';
-	foreach ( $block->getErrors() as $error ) {
-		echo '<li>', $block->getErrorMessage( $error )->escaped() . '</li>';
-	}
-	echo '</ul>';
-}
-
-echo Html::element( 'input', array( 'type' => 'hidden', 'name' => 'wpEditToken', 'value' => $editToken ) ),
-	Html::element(
-		'input',
-		array(
-			'name' => $block->getName() . '[content]',
-			'class' => 'flow-edit-title-textbox mw-ui-input',
-			'value' => $this->getContent( $topicTitle, 'wikitext' ),
-		),
-		$this->getContent( $topicTitle, 'wikitext' )
-	),
-	Html::openElement( 'div', array(
-		'class' => 'flow-edit-title-controls',
-	) ),
-		Html::element( 'input',
-			array(
-				'type' => 'submit',
-				'value' => wfMessage( 'flow-edit-title-submit' )->plain(),
-				'class' => 'mw-ui-button mw-ui-constructive',
-			)
-		),
-	Html::closeElement( 'div' ),
-Html::closeElement( 'form' ),
-Html::closeElement( 'div' ),
-Html::closeElement( 'div' );
+<div class="flow-topic-container flow-topic-full">
+	<div class="flow-edit-title-form flow-element-container">
+		<form method="POST" action="<?php echo $this->editTitleUrl->escaped() ?>">
+			<?php echo $this->errors()->block( $this->block )->escaped() ?>
+			<input type="hidden" name="wpEditToken" value="<?php echo $this->editToken->escaped() ?>">
+			<input name="<?php echo $this->block->getName()->escaped() ?>[content]" value="<?php echo $this->content->escaped() ?>"
+			       class="flow-edit-title-textbox mw-ui-input">
+			<div class="flow-edit-title-controls">
+				<input type="submit" class="mw-ui-button mw-ui-constructive"
+				       value="<?php echo wfMessage( 'flow-edit-title-submit' )->escaped() ?>">
+			</div>
+		</form>
+	</div>
+</div>
