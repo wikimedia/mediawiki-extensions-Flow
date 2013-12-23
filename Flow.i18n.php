eed459f5860131c0a4d3cccf90ce3dd2c7b3f915
Yuki Shira <shirayuking@gmail.com>
2013-12-24 08:41:53 +0900

    i18n: consistency tweaks
    * cannot
    * make lowercase in "flow-topic-comments"
    
    Follow-up To: I36112a23a1c9ea4de53979ff3cd8238952a1b715
    
    Change-Id: If9b63b193390b41045841eaee504e153cf100082

--- Flow.i18n.php
+++ Flow.i18n.php
@@ -107,7 +107,7 @@ $messages['en'] = array(
 	'flow-error-not-allowed' => 'Insufficient permissions to execute this action.',
 	'flow-error-title-too-long' => 'Topic titles are restricted to $1 {{PLURAL:$1|byte|bytes}}.',
 	'flow-error-no-existing-workflow' => 'This workflow does not yet exist.',
-	'flow-error-not-a-post' => 'Topic title can not be saved as a post.',
+	'flow-error-not-a-post' => 'Topic title cannot be saved as a post.',
 	'flow-error-missing-header-content' => 'Header has no content. Content is required to save a header.',
 	'flow-error-missing-prev-revision-identifier' => 'Previous revision identifier is missing.',
 	'flow-error-prev-revision-mismatch' => 'The edit could not be saved: a more recent change has already been submitted.',
@@ -159,7 +159,7 @@ $messages['en'] = array(
 	'flow-history-pages-topic' => 'Appears on [$1 "$2" board]',
 	'flow-history-pages-post' => 'Appears on [$1 $2]',
 	'flow-topic-participants' => '{{PLURAL:$1|$3 started this topic|{{GENDER:$3|$3}}, {{GENDER:$4|$4}}, {{GENDER:$5|$5}} and $2 {{PLURAL:$2|other|others}}|0=No participation yet|2={{GENDER:$3|$3}} and {{GENDER:$4|$4}}|3={{GENDER:$3|$3}}, {{GENDER:$4|$4}} and {{GENDER:$5|$5}}}}',
-	'flow-topic-comments' => '{{PLURAL:$1|$1 Comment|$1 Comments|0={{GENDER:$2|Be the first}} to comment!}}',
+	'flow-topic-comments' => '{{PLURAL:$1|$1 comment|$1 comments|0={{GENDER:$2|Be the first}} to comment!}}',
 	'flow-topic-meta-minimal' => '{{PLURAL:$1|0={{GENDER:$3|Be the first}} to comment!|$1 {{PLURAL:$1|comment|comments}} by $2 {{PLURAL:$2|user|users}}}}',
 
 	'flow-comment-restored' => 'Restored comment',
