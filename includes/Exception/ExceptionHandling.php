2084f8c3ba28be9deeaa4cb0ba8ce2540dba5472
bsitu <bsitu@wikimedia.org>
2013-12-24 17:44:12 -0800

    Reuse code from MWException
    Change-Id: I8b56cbce2c2c41e33f9c7aa571606139829270b0

--- includes/Exception/ExceptionHandling.php
+++ includes/Exception/ExceptionHandling.php
@@ -2,86 +2,55 @@
 
 namespace Flow\Exception;
 
+use MWExceptionHandler;
 use MWException;
-use IcontextSource;
-use Flow\Templating;
-
-/**
- * Handles all kinds of flow exceptions, the purpose is not to show exception trace
- * to end users.  It works by grouping flow exceptions into different categories,
- * then we can add different handling for the different exception types, we also
- * use the error code to present localized error message to users
- */
-class FlowExceptionHandling {
-	/**
-	 * @var Templating
-	 */
-	protected $templating;
-
-	/**
-	 * @var IContextSource
-	 */
-	protected $requestContext;
-
-	/**
-	 * @param Templating
-	 * @param IContextSource
-	 */
-	public function __construct( Templating $templating, IContextSource $requestContext ) {
-		$this->templating = $templating;
-		$this->requestContext = $requestContext;
-	}
-
-	/**
-	 * @param FlowException
-	 */
-	public function handle( FlowException $e ) {
-		// Handle any exception specific error
-		$e->handleError();
-		$this->showError( $e );
-	}
-
-	/**
-	 * @param FlowException
-	 */
-	public function showError( FlowException $e ) {
-		$this->requestContext->getOutput()->addModuleStyles(
-			array( 'mediawiki.ui', 'ext.flow.base' )
-		);
-		$this->requestContext->getOutput()->addHTML(
-			$this->templating->render(
-				'flow:flow-error.html.php',
-				array( 'message' => $e->getErrorMessage() )
-			)
-		);
-	}
-}
+use OutputPage;
 
 /**
  * Flow base exception
  */
 class FlowException extends MWException {
 
+	/**
+	 * Flow exception error code
+	 * @var string
+	 */
 	protected $code;
+
+	/**
+	 * The output object
+	 * @var OutputPage
+	 */
+	protected $output;
 
 	/**
 	 * @param string - The message from exception, used for debugging error
 	 * @param string - The error code used to display error message
 	 */
 	public function __construct( $message, $code = 'default' ) {
+		global $wgOut;
 		parent::__construct( $message );
 		$this->code = $code;
+		// Set output object to the global $wgOut object by default
+		$this->output = $wgOut;
 	}
 
 	/**
-	 * Get the error message to be displayed in user interface
+	 * Set the output object
 	 */
-	public function getErrorMessage() {
+	public function setOutput( OutputPage $output ) {
+		$this->output = $output;
+	}
+
+	/**
+	 * Get the message key for the localized error message
+	 */
+	public function getErrorCode() {
 		$list = $this->getErrorCodeList();
 		if ( !in_array( $this->code, $list ) ) {
 			$this->code = 'default';
 		}
-		return wfMessage( 'flow-error-' . $this->code )->escaped();
+		return 'flow-error-' . $this->code;
 	}
 
 	/**
@@ -91,11 +60,10 @@ class FlowException extends MWException {
 		return array ( 'default' );
 	}
 
-	/**
-	 * Log the exception by defalut, child classes can overwrite this action
-	 */
-	public function handleError() {
-		wfDebugLog( get_class( $this ), $this->getCode() . ': ' . $this->getTraceAsString() );
+	public function report() {
+		MWExceptionHandler::logException( $this );
+		$this->output->showErrorPage( 'errorpagetitle', $this->getErrorCode() );
+		$this->output->output();
 	}
 }
 
@@ -112,6 +80,18 @@ class InvalidInputException extends FlowException {
 			'invalid-workflow'
 		);
 	}
+
+	public function report() {
+		$this->output->setStatusCode( 400 );
+		parent::report();
+	}
+
+	/**
+	 * Do not log exception resulting from input error
+	 */
+	function isLoggable() {
+		return false;
+	}
 }
 
 /**
@@ -120,6 +100,18 @@ class InvalidInputException extends FlowException {
 class InvalidActionException extends FlowException {
 	protected function getErrorCodeList() {
 		return array ( 'invalid-action'	);
+	}
+
+	public function report() {
+		$this->output->setStatusCode( 400 );
+		parent::report();
+	}
+
+	/**
+	 * Do not log exception resulting from input error
+	 */
+	function isLoggable() {
+		return false;
 	}
 }
 
