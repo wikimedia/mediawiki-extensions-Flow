47a02071a73dec312fa868703f341d1133377c74
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-01-03 11:47:50 -0800

    Fix undefined variable in recentchanges formatter
    Change-Id: Ie8f51b61bf6f4e7a6d3e01957090c173028acaec

--- includes/RecentChanges/Formatter.php
+++ includes/RecentChanges/Formatter.php
@@ -84,7 +84,7 @@ class Formatter extends AbstractFormatter {
 			. ' '
 			. $this->changeSeparator()
 			. ' '
-			. $this->getActionDescription( $workflow, $row->blocktype, $revision, $user );
+			. $this->getActionDescription( $workflow, $changeData['block'], $revision, $user );
 	}
 
 	protected function changeSeparator() {
