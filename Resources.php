7dc8411b25ef9c579f67b9129517b09fbbe1d195
Matthias Mullie <git@mullie.eu>
2014-01-06 13:58:01 +0100

    Refactor edit-post JavaScript
    Change-Id: Ice4ef62f762567d234eb92f894ec14b2b85ba357

--- Resources.php
+++ Resources.php
@@ -61,6 +61,7 @@ $wgResourceModules += array(
 			'discussion/ui.js',
 			'discussion/forms.js',
 			'discussion/title.js',
+			'discussion/post.js',
 			'discussion/paging.js',
 		),
 		'dependencies' => array(
