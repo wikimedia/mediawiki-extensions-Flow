041d0d2bacdaa486ab6ee0090e039294f6cf5ec8
Matthias Mullie <git@mullie.eu>
2014-01-07 16:47:39 +0100

    SpamRegex implementation
    Change-Id: I5eca6014056b28b71690c8683ecb442444b86ffa

--- container.php
+++ container.php
@@ -429,8 +429,13 @@ $c['controller.abusefilter'] = $c->share( function( $c ) {
 	return new Flow\SpamFilter\AbuseFilter;
 } );
 
+$c['controller.spamregex'] = $c->share( function( $c ) {
+	return new Flow\SpamFilter\SpamRegex;
+} );
+
 $c['controller.spamfilter'] = $c->share( function( $c ) {
 	return new Flow\SpamFilter\Controller(
+		$c['controller.spamregex'],
 		$c['controller.abusefilter']
 	);
 } );
