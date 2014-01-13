dd11c0129f669cc4719e1250021cc10e128e9b48
Matthias Mullie <git@mullie.eu>
2014-01-13 19:25:04 +0100

    Refactor edit-header JavaScript
    This had recently been refactored, but it's nicer to create a single object for
    it, rather than lots of static functions which constantly need to pass required
    arguments along.
    
    Change-Id: I5c833b3e8b8974065a87ef05029916998c852de4

--- modules/header/forms.js
+++ modules/header/forms.js
@@ -1,82 +1,103 @@
 ( function ( $, mw ) {
 	'use strict';
 
-	mw.flow.header = {
-		/**
-		 * @param {Event} e
-		 */
-		init: function ( e ) {
-			// Overload "edit header" link.
-			$( e.target ).find( '.flow-header-edit-link' ).click( mw.flow.header.edit );
-		},
+	/**
+	 * Initialises header object.
+	 */
+	mw.flow.header = function () {
+		this.$container = $( '#flow-header' );
+		this.workflowId = this.$container.flow( 'getTopicWorkflowId' );
+		this.pageName = this.$container.closest( '.flow-container' ).data( 'page-title' );
 
+		// init edit-header interaction
+		new mw.flow.header.edit( this );
+	};
+
+	/**
+	 * Initialises edit-header interaction object.
+	 *
+	 * @param {object} header
+	 */
+	mw.flow.header.edit = function ( header ) {
+		this.header = header;
+
+		// Overload "edit header" link.
+		this.header.$container.find( '.flow-header-edit-link' ).click( this.edit.bind( this ) );
+	};
+
+	/**
+	 * Fired when edit-header link is clicked.
+	 *
+	 * @param {Event} e
+	 */
+	mw.flow.header.edit.prototype = {
 		/**
-		 * Fired when edit-link is clicked.
+		 * Fired when edit-header is clicked.
 		 *
 		 * @param {Event} e
 		 */
 		edit: function ( e ) {
-			var $flowContainer = $( this ).closest( '.flow-container' ),
-				pageName = $flowContainer.data( 'page-title' ),
-				workflowId = $flowContainer.data( 'workflow-id' ),
-				$headerContainer = $( this ).closest( '#flow-header' );
-
 			// don't follow link that will lead to &action=edit-header
 			e.preventDefault();
+
+			// quit if edit form is already open
+			if ( this.header.$container.find( '.flow-edit-header-form' ).length ) {
+				return;
+			}
 
 			/*
 			 * Fetch current revision data (content, revision id, ...) that
 			 * we'll need to initialise the edit form.
 			 */
-			mw.flow.header.read( pageName, workflowId )
+			this.read()
 				/*
 				 * This is a .then: then callbacks can return a value, which is
 				 * then passed to .done or .fail. This allows us to format the
 				 * returned data to how we want it in .done, or reject (which
 				 * will result in .fail being called) if the data is invalid.
 				 */
-				.then( mw.flow.header.prepareResult.bind( $headerContainer, pageName, workflowId ) )
+				.then( this.prepareResult.bind( this ) )
 				/*
 				 * Once we have successfully fetched & verified the data, the
 				 * edit form can be built.
 				 */
-				.done( mw.flow.header.setupEditForm.bind( $headerContainer ) )
+				.done( this.setupEditForm.bind( this ) )
 				/*
 				 * If anything went wrong (either in original deferred object or
 				 * in the one returned by .then), show an error message.
 				 */
-				.fail( mw.flow.header.showError.bind( $headerContainer ) );
+				.fail( this.showError.bind( this ) );
 		},
 
 		/**
 		 * Fetches header info.
 		 *
 		 * @see includes/Block/Header.php Header::renderAPI
-		 * @param {string} pageName
-		 * @param {string} workflowId
-		 * @returns {jQuery.Deferred}
+		 * @return {jQuery.Deferred}
 		 */
-		read: function ( pageName, workflowId ) {
+		read: function () {
 			return mw.flow.api.readHeader(
-				pageName,
-				workflowId,
-				{ header: { contentFormat: mw.flow.editor.getFormat() } }
+				this.header.pageName,
+				this.header.workflowId,
+				{
+					header: {
+						contentFormat: mw.flow.editor.getFormat()
+					}
+				}
 			);
 		},
 
 		/**
-		 * Processes the result of mw.flow.header.read & the returned deferred
+		 * Processes the result of this.read & the returned deferred
 		 * will be passed to setupEditForm.
 		 *
 		 * if invalid data is encountered, the (new) deferred will be rejected,
 		 * resulting in any fail() bound to be executed.
 		 *
-		 * @param {string} pageName
-		 * @param {string} workflowId
 		 * @param {object} data
 		 * @return {jQuery.Deferred}
 		 */
-		prepareResult: function ( pageName, workflowId, data ) {
+		prepareResult: function ( data ) {
 			var deferred = $.Deferred();
 
 			if ( !data[0] ) {
@@ -84,39 +105,37 @@
 			}
 
 			return deferred.resolve( {
-				page: pageName,
-				workflow: workflowId,
 				content: data[0].missing ? '' : data[0]['*'],
 				format: data[0].missing ? 'wikitext' : data[0].format,
 				revision: data[0]['header-id']
 			} );
 		},
 
+
 		/**
 		 * Builds the edit form.
 		 *
-		 * this = #flow-header
-		 *
-		 * @param {object} data mw.flow.header.prepareResult return value
+		 * @param {object} data this.prepareResult return value
+		 * @param {function} [loadFunction] callback to be executed when form is loaded
 		 */
-		setupEditForm: function ( data ) {
-			var $editLink = $( this ).find( '.flow-header-edit-link' );
+		setupEditForm: function ( data, loadFunction ) {
+			var $editLink = this.header.$container.find( '.flow-header-edit-link' );
 
-			$( this ).find( '#flow-header-content' )
-				.flow(
-					'setupEditForm',
-					'header',
-					{
-						content: data.content,
-						format: data.format
-					},
-					mw.flow.header.submitFunction.bind( this, data )
-				);
+			this.header.$container.find( '#flow-header-content' ).flow(
+				'setupEditForm',
+				'header',
+				{
+					content: data.content,
+					format: data.format
+				},
+				this.submitFunction.bind( this, data ),
+				loadFunction
+			);
 
 			// hide edit link and re-reveal it if the cancel link - which is
 			// added by flow( 'setupEditForm' ) - is clicked.
 			$editLink.hide();
-			$( this ).find( '.flow-cancel-link' ).click( function () {
+			this.header.$container.find( '.flow-cancel-link' ).click( function () {
 				$editLink.show();
 			} );
 		},
@@ -124,31 +143,32 @@
 		/**
 		 * Submit function for flow( 'setupEditForm' ).
 		 *
-		 * this = #flow-header
-		 *
-		 * @param {object} data mw.flow.header.prepareResult return value
+		 * @param {object} data this.prepareResult return value
 		 * @param {string} content
 		 * @return {jQuery.Deferred}
 		 */
 		submitFunction: function ( data, content ) {
-			return mw.flow.api.editHeader( {
-					workflowId: data.workflow,
-					page: data.page
+			var deferred = mw.flow.api.editHeader( {
+					workflowId: this.header.workflowId,
+					page: this.header.pageName
 				},
 				content,
 				data.revision
-			).done( mw.flow.header.render.bind( this ) );
+			);
+
+			deferred.done( this.render.bind( this ) );
+//			deferred.fail( this.conflict.bind( this, deferred ) ); // @todo: not yet implemented
+
+			return deferred;
 		},
 
 		/**
-		 * Called when setupEditForm is resolved.
-		 *
-		 * this = #flow-header
+		 * Called when submitFunction is resolved.
 		 *
 		 * @param {object} output
 		 */
 		render: function ( output ) {
-			$( this )
+			this.header.$container
 				.find( '#flow-header-content' )
 				.empty()
 				.append( $( output.rendered ) );
@@ -157,15 +177,15 @@
 		/**
 		 * Display an error if something when wrong.
 		 *
-		 * this = #flow-header
-		 *
 		 * @param {string} error
-		 * @param {object} data
+		 * @param {object} errorData
 		 */
-		showError: function ( error, data ) {
-			$( this ).flow( 'showError', arguments );
+		showError: function ( error, errorData ) {
+			this.header.$container.flow( 'showError', arguments );
 		}
 	};
 
-	$( document ).flow( 'registerInitFunction', mw.flow.header.init );
+	$( document ).flow( 'registerInitFunction', function () {
+		new mw.flow.header();
+	} );
 } ( jQuery, mediaWiki ) );
