13fc14c5cac00d0002b4617efe2f7b5b26234570
Kunal Mehta <legoktm@gmail.com>
2014-01-14 11:14:38 -0800

    API: Revamp action=flow
    The API is now a modular module, similar to
    action=query. This means that each submodule
    can properly provide help messages and hints in
    the automated API documentation (action=help).
    
    Bug: 57659
    Bug: 55862
    Change-Id: I09f53f8288ad86fee30dc0a4ce4b9af748aea792

---
+++ includes/api/ApiFlowNewTopic.php
@@ -0,0 +1,49 @@
+<?php
+
+class ApiFlowNewTopic extends ApiFlowBase {
+
+	public function __construct( $api, $modName ) {
+		parent::__construct( $api, $modName, 'nt' );
+	}
+
+	/**
+	 * Taken from ext.flow.base.js
+	 * @return string
+	 */
+	protected function getBlockName() {
+		return 'topic_list';
+	}
+
+	protected function getAction() {
+		return 'new-topic';
+	}
+
+	public function getAllowedParams() {
+		return array(
+			'topic' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'content' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+		);
+	}
+
+	public function getParamDescription() {
+		return array(
+			'topic' => 'Text for new topic header',
+			'content' => 'Content for new topic',
+		);
+	}
+
+	public function getDescription() {
+		return 'Creates a new Flow topic on the given workflow';
+	}
+
+	public function getExamples() {
+		return array(
+			'api.php?action=flow&submodule=new-topic&nttopic=Hi&ntcontent=Nice%20to&20meet%20you&workflow=',
+		);
+	}
+
+}
