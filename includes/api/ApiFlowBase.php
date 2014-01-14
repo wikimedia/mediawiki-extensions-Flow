8bdf142dc88259793bc46bcc83824742259792db
Kunal Mehta <legoktm@gmail.com>
2014-01-14 11:14:38 -0800

    API: Revamp action=flow
    The API is now a modular module, similar to
    action=query. This means that each submodule
    can properly provide help messages and hints in
    the automated API documentation (action=help).
    
    Bug: 57659
    Bug: 55862
    Change-Id: I09f53f8288ad86fee30dc0a4ce4b9af748aea792

---
+++ includes/api/ApiFlowBase.php
@@ -0,0 +1,224 @@
+<?php
+
+use Flow\WorkflowLoader;
+use Flow\Model\UUID;
+use Flow\Container;
+use Flow\TalkpageManager;
+use Flow\Block\AbstractBlock;
+use Flow\Model\AbstractRevision;
+
+
+abstract class ApiFlowBase extends ApiBase {
+
+	/** @var WorkflowLoader $loader */
+	protected $loader;
+
+	/** @var Title|bool $page */
+	protected  $page;
+
+	/** @var UUID|null $id */
+	protected $id;
+
+	/** @var bool $render */
+	protected $render;
+
+	/** @var ApiFlow $apiFlow */
+	protected $apiFlow;
+
+	/**
+	 * @param ApiFlow $api
+	 * @param string $modName
+	 * @param string $prefix
+	 */
+	public function __construct( $api, $modName, $prefix = '' ) {
+		$this->apiFlow = $api;
+		parent::__construct( $api->getMain(), $modName, $prefix );
+	}
+
+	/**
+	 * Allows the main ApiFlow instance to set default paramters
+	 * @param Title|bool $page
+	 * @param UUID|null $workflow
+	 */
+	public function setWorkflowParams( $page, $workflow ) {
+		$this->page = $page;
+		$this->id = $workflow;
+	}
+
+	public function doRender( $do = null ) {
+		return wfSetVar( $this->render, $do );
+	}
+
+	/*
+	 * Return the name of the flow action
+	 * @return string
+	 */
+	abstract protected function getAction();
+
+	/**
+	 * @return Container
+	 */
+	protected function getContainer() {
+		return Container::getContainer();
+	}
+
+	/**
+	 * @return WorkflowLoader
+	 */
+	protected function getLoader() {
+		if ( $this->loader === null ) {
+			$container = $this->getContainer();
+			$this->loader = $container['factory.loader.workflow']
+				->createWorkflowLoader( $this->page, $this->id );
+		}
+
+		return $this->loader;
+	}
+
+	/**
+	 * @return TalkpageManager
+	 */
+	protected function getOccupationController() {
+		$container = $this->getContainer();
+		return $container['occupation_controller'];
+	}
+
+	/**
+	 * @return string
+	 */
+	abstract protected function getBlockName();
+
+	/**
+	 * @return DerivativeRequest
+	 */
+	protected function getModifiedRequest() {
+		$extracted = $this->extractRequestParams();
+		// @fixme this is terrible, but we'll fix the interface later
+		$params = array( $this->getBlockName() => $extracted );
+
+		return new DerivativeRequest(
+			$this->getRequest(),
+			$params,
+			$this->getRequest()->wasPosted() // Note that we also enforce this at the API level.
+		);
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getModerationStates() {
+		return array(
+			AbstractRevision::MODERATED_DELETED,
+			AbstractRevision::MODERATED_HIDDEN,
+			AbstractRevision::MODERATED_SUPPRESSED,
+			'restore', // @todo Does this need a constant?
+		);
+	}
+
+	public function execute() {
+		$loader = $this->getLoader();
+		$blocks = $loader->createBlocks();
+		/** @var \Flow\Model\Workflow $workflow */
+		$workflow = $loader->getWorkflow();
+		$action = $this->getAction();
+		$controller = $this->getOccupationController();
+		$user = $this->getUser();
+		$params = $this->extractRequestParams();
+
+		$article = new Article( $workflow->getArticleTitle(), 0 );
+
+		// @fixme: this is a hack; see ParsoidUtils::convert
+		global $wgFlowParsoidTitle;
+		$wgFlowParsoidTitle = $workflow->getArticleTitle();
+
+		$isNew = $workflow->isNew();
+		// Is this making unnecesary db round trips?
+		if ( !$isNew ) {
+			$controller->ensureFlowRevision( $article );
+		}
+
+		/** @var AbstractBlock $block */
+		foreach ( $blocks as $block ) {
+			$block->init( $action, $user );
+		}
+		$result = $this->getResult();
+
+		$blocksToCommit = $loader->handleSubmit( $action, $blocks, $user, $this->getModifiedRequest() );
+		if ( count( $blocksToCommit ) ) {
+			$commitResults = $loader->commit( $workflow, $blocksToCommit );
+			$savedBlocks = array();
+			$result->setIndexedTagName( $savedBlocks, 'block' );
+
+			foreach( $blocksToCommit as $block ) {
+				$savedBlocks[] = $block->getName();
+			}
+
+			$output[$action] = array(
+				'result' => array(),
+			);
+
+			foreach( $commitResults as $key => $value ) {
+				$output[$action]['result'][$key] = $this->processCommitResult( $value, $this->doRender() );
+			}
+			if ( $isNew && !$workflow->isNew() ) {
+				// Workflow was just created, ensure its underlying page is owned by flow
+				$controller->ensureFlowRevision( $article );
+			}
+		} else {
+			$output[$action] = array(
+				'result' => 'error',
+				'errors' => array(),
+			);
+
+			foreach( $blocks as $block ) {
+				if ( $block->hasErrors() ) {
+					$errors = $block->getErrors();
+					$nativeErrors = array();
+
+					foreach( $errors as $key ) {
+						$nativeErrors[$key]['message'] = $block->getErrorMessage( $key )->plain();
+						$nativeErrors[$key]['extra'] = $block->getErrorExtra( $key );
+					}
+
+					$output[$action]['errors'][$block->getName()] = $nativeErrors;
+				}
+			}
+		}
+
+		$this->getResult()->addValue( null, $this->apiFlow->getModuleName(), $output );
+	}
+
+	protected function processCommitResult( $result, $render = true ) {
+		$container = $this->getContainer();
+		$templating = $container['templating'];
+		$output = array();
+		foreach( $result as $key => $value ) {
+			if ( $value instanceof UUID ) {
+				$output[$key] = $value->getAlphadecimal();
+			} elseif ( $key === 'render-function' ) {
+				if ( $render ) {
+					$function = $value;
+					$output['rendered'] = $function( $templating );
+				}
+			} else {
+				$output[$key] = $value;
+			}
+		}
+
+		return $output;
+	}
+
+	public function mustBePosted() {
+		return true;
+	}
+
+	public function isWriteMode() {
+		return true;
+	}
+
+	public function getHelpUrls() {
+		return array(
+			'https://www.mediawiki.org/wiki/Extension:Flow/API#' . $this->getAction(),
+		);
+	}
+}
---
+++ includes/api/ApiFlowModeratePost.php
@@ -0,0 +1,50 @@
+<?php
+
+class ApiFlowModeratePost extends ApiFlowBase {
+
+	public function __construct( $api, $modName ) {
+		parent::__construct( $api, $modName, 'mp' );
+	}
+
+	protected function getBlockName() {
+		return 'topic';
+	}
+
+	protected function getAction() {
+		return 'moderate-post';
+	}
+
+	public function getAllowedParams() {
+		return array(
+			'moderationState' => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_TYPE => $this->getModerationStates(),
+			),
+			'reason' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'postId' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+		);
+	}
+
+	public function getParamDescription() {
+		return array(
+			'moderationState' => 'What level to moderate at',
+			'reason' => 'Reason for moderation',
+			'postId' => 'Id of post to moderate',
+		);
+	}
+
+	public function getDescription() {
+		return 'Moderates a Flow post';
+	}
+
+	public function getExamples() {
+		return array(
+			'api.php?action=flow&submodule=moderate-post&mppostId=050f30e34c87beebcd54080027630f57&mpmoderationState=delete&mpreason=Ahhhh&workflow=',
+		);
+	}
+
+}
