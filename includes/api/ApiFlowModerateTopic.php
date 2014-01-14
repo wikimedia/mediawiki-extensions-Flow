e7d433cea33f7c6b92e25c8bc0e7033f5d9e4c54
Kunal Mehta <legoktm@gmail.com>
2014-01-14 11:14:38 -0800

    API: Revamp action=flow
    The API is now a modular module, similar to
    action=query. This means that each submodule
    can properly provide help messages and hints in
    the automated API documentation (action=help).
    
    Bug: 57659
    Bug: 55862
    Change-Id: I09f53f8288ad86fee30dc0a4ce4b9af748aea792

---
+++ includes/api/ApiFlowModerateTopic.php
@@ -0,0 +1,45 @@
+<?php
+
+class ApiFlowModerateTopic extends ApiFlowBase {
+
+	public function __construct( $api, $modName ) {
+		parent::__construct( $api, $modName, 'mt' );
+	}
+
+	protected function getBlockName() {
+		return 'topic';
+	}
+
+	protected function getAction() {
+		return 'moderate-topic';
+	}
+
+	public function getAllowedParams() {
+		return array(
+			'moderationState' => array(
+				ApiBase::PARAM_REQUIRED => true,
+				ApiBase::PARAM_TYPE => $this->getModerationStates(),
+			),
+			'reason' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+		);
+	}
+
+	public function getParamDescription() {
+		return array(
+			'moderationState' => 'What level to moderate at',
+			'reason' => 'Reason for moderation',
+		);
+	}
+
+	public function getDescription() {
+		return 'Moderates a Flow topic';
+	}
+
+	public function getExamples() {
+		return array(
+			'api.php?action=flow&submodule=moderate-topic&mtmoderationState=delete&mtreason=Ahhhh&workflow=',
+		);
+	}
+}
