8fd0890127a5e53799ddfea2cfcc3b97f4847bb4
Andrew Garrett <agarrett@wikimedia.org>
2014-01-15 11:08:58 -0800

    Add conversion infrastructure with LQT API source.
    Modular design means we can read from the DB, an XML dump, a talk page
    or any other potential source of historical Flow posts if we want to.
    
    Bug: 45088
    Change-Id: If41b45908542bf68521c0b31388ca6d33fd818f0

---
+++ tests/phpunit/Mock/MockImportTopic.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Flow\Tests\Mock;
+
+use Flow\Import\IImportTopic;
+
+class MockImportTopic extends MockImportPost implements IImportTopic {
+	public function __construct( array $attribs = array() ) {
+		parent::__construct( $attribs + array(
+			'summary' => null,
+		) );
+	}
+
+	public function getTopicSummary() {
+		return $this->attribs['summary'];
+	}
+}
