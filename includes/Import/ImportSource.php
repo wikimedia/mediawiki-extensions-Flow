eebd6229017d69287c53e5d2c02c5238876364c8
Andrew Garrett <agarrett@wikimedia.org>
2014-01-15 11:08:58 -0800

    Add conversion infrastructure with LQT API source.
    Modular design means we can read from the DB, an XML dump, a talk page
    or any other potential source of historical Flow posts if we want to.
    
    Bug: 45088
    Change-Id: If41b45908542bf68521c0b31388ca6d33fd818f0

---
+++ includes/Import/ImportSource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Flow\Import;
+
+use Iterator;
+use User;
+
+interface IImportSource {
+	/**
+	 * @return Iterator<IImportTopic>
+	 */
+	function getTopics();
+
+	/**
+	 * @todo We probably need headers too eventually
+	 *
+	 * function getHeader();
+	 */
+}
+
+interface IImportPost {
+	/**
+	 * @return User User to whom this post is attributed
+	 */
+	function getAuthor();
+
+	/**
+	 * @return string Timestamp compatible with wfTimestamp()
+	 */
+	function getCreatedTimestamp();
+
+	/**
+	 * @return string Timestamp compatible with wfTimestamp()
+	 */
+	function getModifiedTimestamp();
+
+	/**
+	 * @return string WikiText
+	 */
+	function getText();
+
+	/**
+	 * @return Iterator<IImportPost>
+	 */
+	function getReplies();
+}
+
+interface IImportTopic extends IImportPost {
+	/**
+	 * @return boolean
+	 */
+	function hasSummary();
+	/**
+	 * Gets the summary, if any, for a topic.
+	 *
+	 * @return IImportSummary
+	 */
+	function getTopicSummary();
+}
+
+interface IImportSummary {
+
+	/**
+	 * @return string WikiText
+	 */
+	function getText();
+
+	/**
+	 * @return User The user who created this summary.
+	 */
+	function getAuthor();
+}
+
+class ImportException extends \MWException {
+}
