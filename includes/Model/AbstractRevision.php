04531178be40ea470e1f0330e5b191a80f8fb356
Andrew Garrett <agarrett@wikimedia.org>
2014-01-15 11:08:58 -0800

    WIP: Add LQT conversion script.
    Currently not quite working correctly, and still needs work
    to add history, but we're getting there!
    
    Converts all LiquidThreads threads in the DB to Flow topics, creating a
    post from each reply and a topic summary from the thread summary. The
    new items have UUIDs corresponding to their original timestamps. Also
    ensures Flow on each article.  Changes fake UUID generation to reduce
    risk of collisions.
    
    Bug: 45088
    
    Change-Id: If41b45908542bf68521c0b31388ca6d33fd818f0

--- includes/Model/AbstractRevision.php
+++ includes/Model/AbstractRevision.php
@@ -344,6 +344,10 @@ abstract class AbstractRevision {
 		return $this->revId;
 	}
 
+	public function setRevisionId( UUID $id ) {
+		$this->revId = $id;
+	}
+
 	/**
 	 * @return boolean
 	 */
--- includes/Model/Workflow.php
+++ includes/Model/Workflow.php
@@ -256,6 +256,18 @@ class Workflow {
 	public function getType() { return $this->type; }
 
 	/**
+	 * Used to change a Workflow's ID before it is saved.
+	 * @param UUID $id The ID to set.
+	 */
+	public function setId( $id ) {
+		if ( ! $this->isNew() ) {
+			throw new Flow\Exception\InvalidActionException( "Cannot change Workflow ID after saving it" );
+		}
+
+		$this->id = $id;
+	}
+
+	/**
 	 * Returns true if the workflow is new as of this request (regardless of
 	 * whether or not is it already saved yet - that's unknown).
 	 *
