9c73a375359a899227c5d81b88b7ef085056f6da
Andrew Garrett <agarrett@wikimedia.org>
2014-01-15 11:08:58 -0800

    Add conversion infrastructure with LQT API source.
    Modular design means we can read from the DB, an XML dump, a talk page
    or any other potential source of historical Flow posts if we want to.
    
    Bug: 45088
    Change-Id: If41b45908542bf68521c0b31388ca6d33fd818f0

---
+++ tests/phpunit/Import/TalkpageImportOperationTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Flow\Tests\Import;
+
+use Flow\Import\PageImportState;
+use Flow\Import\TalkpageImportOperation;
+use Flow\Model\Workflow;
+use Flow\Tests\Mock\MockImportPost;
+use Flow\Tests\Mock\MockImportSource;
+use Flow\Tests\Mock\MockImportSummary;
+use Flow\Tests\Mock\MockImportTopic;
+use Title;
+use User;
+
+class TalkpageImportOperationTest extends \MediaWikiTestCase {
+
+	public function testImportWritesOutNewWorkflows() {
+		$workflow = Workflow::create(
+			'discussion',
+			User::newFromName( '127.0.0.1', false ),
+			Title::newMainPage()
+		);
+		$storage = $this->getMockBuilder( 'Flow\Data\ManagerGroup' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$now = time();
+		$source = new MockImportSource( array(
+			new MockImportTopic( array(
+				'createdTimestamp' => $now - 1000,
+				'summary' => new MockImportSummary(),
+				'replies' => array(
+					new MockImportPost( array(
+						'createdTimestamp' => $now - 1000,
+						'replies' => array(
+							new MockImportPost( array(
+								'createdTimestmap' => $now - 500,
+								'user' => User::newFromName( '10.0.0.2', false ),
+							) ),
+						),
+					) ),
+				),
+			) ),
+		) );
+
+		$op = new TalkpageImportOperation( $source );
+		$op->import( new PageImportState( $workflow, $storage ) );
+	}
+}
