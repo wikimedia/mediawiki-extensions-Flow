64632b34522036720dff4abfb833204b56636516
Kunal Mehta <legoktm@gmail.com>
2014-01-15 09:53:18 -0800

    ApiParsoidUtils: Handle the page not existing nicely
    Change-Id: Ie1e3982ad9280e3afcafa347d88363cd8056f00b

--- includes/api/ApiParsoidUtilsFlow.php
+++ includes/api/ApiParsoidUtilsFlow.php
@@ -10,6 +10,12 @@ class ApiParsoidUtilsFlow extends ApiBase {
 		$params = $this->extractRequestParams();
 		$page = $this->getTitleOrPageId( $params );
 
+		if ( !$page->exists() ) {
+			// ParsoidUtils::convert checks for this, but we can provide
+			// a nicer error here
+			$this->dieUsage( 'Page does not exist', 'invalid-page' );
+		}
+
 		try {
 			$content = ParsoidUtils::convert( $params['from'], $params['to'], $params['content'], $page->getTitle() );
 		} catch ( WikitextException $e ) {
