642326da49ae6b4f1e72a0b6dfad3051e4d3e929
Andrew Garrett <agarrett@wikimedia.org>
2014-01-27 17:10:04 -0800

    Extract wiki and external links, file and template usages from text.
    Store said "References" in a table, and append to MW standard links tables.
    
    Change-Id: If548da37d55f767a16745bc5f72504620883b7d7

--- Hooks.php
+++ Hooks.php
@@ -67,6 +67,7 @@ class FlowHooks {
 		$updater->modifyExtensionField( 'flow_revision', 'rev_change_type', "$dir/db_patches/patch-censor_to_suppress.sql" );
 		$updater->addExtensionField( 'flow_workflow', 'workflow_user_ip', "$dir/db_patches/patch-remove_usernames.sql" );
 		$updater->addExtensionField( 'flow_workflow', 'workflow_user_wiki', "$dir/db_patches/patch-add-wiki.sql" );
+		$updater->addExtensionTable( 'flow_ext_ref', "$dir/db_patches/patch-add-linkstables.sql" );
 
 		require_once __DIR__.'/maintenance/FlowInsertDefaultDefinitions.php';
 		$updater->addPostDatabaseUpdateMaintenance( 'FlowInsertDefaultDefinitions' );
@@ -79,6 +80,9 @@ class FlowHooks {
 
 		require_once __DIR__.'/maintenance/FlowUpdateUserWiki.php';
 		$updater->addPostDatabaseUpdateMaintenance( 'FlowUpdateUserWiki' );
+
+		require_once __DIR__.'/maintenance/FlowPopulateLinksTables.php';
+		$updater->addPostDatabaseUpdateMaintenance( 'FlowPopulateLinksTables' );
 
 		return true;
 	}
@@ -486,6 +490,18 @@ class FlowHooks {
 		}
 
 		$rcRow['cuc_comment'] = $comment;
+
+		return true;
+	}
+
+	public static function onWhatLinksHereProps( $row, $title, $target, &$props ) {
+		Flow\Container::get( 'reference.clarifier' )->onWhatLinksHereProps( $row, $title, $target, $props );
+		return true;
+	}
+
+	public static function onLinksUpdateConstructed( $linksUpdate ) {
+		Flow\Container::get( 'reference.updater.links-tables' )
+			->mutateLinksUpdate( $linksUpdate );
 
 		return true;
 	}
