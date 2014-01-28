604a1bd5d50b497a80b2598c8e8cccbaa727dba3
Andrew Garrett <agarrett@wikimedia.org>
2014-01-27 17:10:04 -0800

    Extract wiki and external links, file and template usages from text.
    Store said "References" in a table, and append to MW standard links tables.
    
    In order to display a link to the post on the Flow board (e.g.
      (file link; _from a post_)"), this needs the core change I679abfbbdbd
    that runs the WhatLinksHereProps hook.
    
    Bug: 57512
    Change-Id: If548da37d55f767a16745bc5f72504620883b7d7

---
+++ tests/ReferenceExtractorTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Flow;
+
+use MediaWikiTestCase;
+use Flow\Container;
+use Title;
+
+/**
+ * @group Database
+ */
+class ReferenceExtractorTestCase extends MediaWikiTestCase {
+	public static function referenceExtractorProvider() {
+		return array(
+			array(
+				'Normal link',
+				'[[My page]]',
+				array(
+					array(
+						'refType' => 'link',
+						'targetType' => 'wiki',
+						'target' => 'My_page',
+					),
+				)
+			),
+			array(
+				'Link with URL encoding issues',
+				'[[User talk:Werdna?]]',
+				array(
+					array(
+						'refType' => 'link',
+						'targetType' => 'wiki',
+						'target' => 'User talk:Werdna?',
+					),
+				),
+			),
+			array(
+				'External link',
+				'[http://www.google.com Google]',
+				array(
+					array(
+						'refType' => 'link',
+						'targetType' => 'url',
+						'target' => 'http://www.google.com',
+					),
+				)
+			),
+			array(
+				'File',
+				'[[File:Image.png]]',
+				array(
+					array(
+						'refType' => 'file',
+						'targetType' => 'wiki',
+						'target' => 'File:Image.png',
+					),
+				)
+			),
+			array(
+				'File with parameters',
+				'[[File:Image.png|25px]]',
+				array(
+					array(
+						'refType' => 'file',
+						'targetType' => 'wiki',
+						'target' => 'File:Image.png',
+					),
+				)
+			),
+			array(
+				'Template',
+				'{{Foo}}',
+				array(
+					array(
+						'refType' => 'template',
+						'targetType' => 'wiki',
+						'target' => 'Template:Foo',
+					),
+				)
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider referenceExtractorProvider
+	 */
+	public function testReferenceExtractor( $description, $wikitext, $expectedOutput ) {
+		$referenceExtractor = Container::get( 'reference.extractor' );
+
+		$html = \Flow\Parsoid\Utils::convert( 'wt', 'html', $wikitext, Title::newFromText( 'UTPage' ) );
+
+		$references = $referenceExtractor->extractReferences( $html );
+
+		$this->assertEquals( $references, $expectedOutput, $html );
+	}
+}
