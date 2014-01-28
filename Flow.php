f8730a434e97fb0f31fb8e7df25222a546244f16
Andrew Garrett <agarrett@wikimedia.org>
2014-01-27 17:10:04 -0800

    Extract wiki and external links, file and template usages from text.
    Store said "References" in a table, and append to MW standard links tables.
    
    In order to display a link to the post on the Flow board (e.g.
      (file link; _from a post_)"), this needs the core change I679abfbbdbd
    that runs the WhatLinksHereProps hook.
    
    Bug: 57512
    Change-Id: If548da37d55f767a16745bc5f72504620883b7d7

--- Flow.php
+++ Flow.php
@@ -82,6 +82,7 @@ $wgAutoloadClasses['Flow\SpamFilter\AbuseFilter'] = $dir . 'includes/SpamFilter/
 $wgAutoloadClasses['Flow\FlowActions'] = $dir . 'includes/FlowActions.php';
 $wgAutoloadClasses['Flow\RevisionActionPermissions'] = $dir . 'includes/RevisionActionPermissions.php';
 $wgAutoloadClasses['Flow\TermsOfUse'] = $dir . 'includes/TermsOfUse.php';
+$wgAutoloadClasses['Flow\ReferenceClarifier'] = $dir . 'includes/ReferenceClarifier.php';
 
 // Classes that model our data
 $wgAutoloadClasses['Flow\Model\Definition'] = $dir . 'includes/Model/Definition.php';
@@ -194,8 +195,20 @@ $wgAutoloadClasses['Flow\Block\TopicListBlock'] = $dir . 'includes/Block/TopicLi
 $wgAutoloadClasses['Flow\Block\TopicBlock'] = $dir . 'includes/Block/Topic.php';
 $wgAutoloadClasses['Flow\Block\TopicSummaryBlock'] = $dir . 'includes/Block/TopicSummary.php';
 
+// Reference extraction and tracking
+$wgAutoloadClasses['Flow\LinksTableUpdater'] = $dir . 'includes/LinksTableUpdater.php';
+$wgAutoloadClasses['Flow\ReferenceExtractor'] = $dir . 'includes/ReferenceExtractor.php';
+$wgAutoloadClasses['Flow\Model\Reference'] = "$dir/includes/Model/Reference.php";
+$wgAutoloadClasses['Flow\Model\WikiReference'] = "$dir/includes/Model/Reference.php";
+$wgAutoloadClasses['Flow\Model\URLReference'] = "$dir/includes/Model/Reference.php";
+$wgAutoloadClasses['Flow\Data\ReferenceRecorder'] = "$dir/includes/Data/ReferenceRecorder.php";
+
 // phpunit helper
+<<<<<<< HEAD
 $wgAutoloadClasses['Flow\Tests\FlowTestCase'] = $dir . 'tests/FlowTestCase.php';
+=======
+$wgAutoloadClasses['Flow\Tests\TestCase'] = $dir . 'tests/TestCase.php';
+>>>>>>> Extract wiki and external links, file and template usages from text.
 $wgAutoloadClasses['Flow\Tests\PostRevisionTestCase'] = $dir . 'tests/PostRevisionTestCase.php';
 
 // API modules
@@ -237,7 +250,12 @@ $wgHooks['InfoAction'][] = 'FlowHooks::onInfoAction';
 $wgHooks['SpecialCheckUserGetLinksFromRow'][] = 'FlowHooks::onSpecialCheckUserGetLinksFromRow';
 $wgHooks['MakeGlobalVariablesScript'][] = 'FlowHooks::onMakeGlobalVariablesScript';
 $wgHooks['CheckUserInsertForRecentChange'][] = 'FlowHooks::onCheckUserInsertForRecentChange';
+<<<<<<< HEAD
 $wgHooks['SkinMinervaDefaultModules'][] = 'FlowHooks::onSkinMinervaDefaultModules';
+=======
+$wgHooks['WhatLinksHereProps'][] = 'FlowHooks::onWhatLinksHereProps';
+$wgHooks['LinksUpdateConstructed'][] = 'FlowHooks::onLinksUpdateConstructed';
+>>>>>>> Extract wiki and external links, file and template usages from text.
 
 // Extension initialization
 $wgExtensionFunctions[] = 'FlowHooks::initFlowExtension';
