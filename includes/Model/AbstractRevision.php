ee04254f3db1149bf1a8d06154ff6cf9e6ab2309
bsitu <bsitu@wikimedia.org>
2014-02-05 10:28:15 -0800

    Add *_user_wiki to all the *_user_id columns
    [Need some more thorough manual testing]
    
    This is to distinguish users and ips from different wikis especially
    for pages like contributions. 
    
    The steps involved:
    
    1. add the columns to production wikis
    2. deploy the code in this patch so the new columns start collecting data
    3. run maintenance script to populate existing records
    4. update cache version key
    5. patch to use the new columns
    
    Change-Id: I439b102e9125c4e6aed988032a21d51b1079f93b

--- includes/Model/AbstractRevision.php
+++ includes/Model/AbstractRevision.php
@@ -53,6 +53,8 @@ abstract class AbstractRevision {
 	 */
 	protected $userIp;
 
+	protected $userWiki;
+
 	/**
 	 * Array of flags strictly related to the content. Flags are reset when
 	 * content changes.
@@ -101,6 +103,8 @@ abstract class AbstractRevision {
 	 */
 	protected $moderatedByUserIp;
 
+	protected $moderatedByUserWiki;
+
 	protected $moderatedReason;
 
 	/**
@@ -119,6 +123,8 @@ abstract class AbstractRevision {
 	 */
 	protected $lastEditUserIp;
 
+	protected $lastEditUserWiki;
+
 	static public function fromStorageRow( array $row, $obj = null ) {
 		if ( $obj === null ) {
 			$obj = new static;
@@ -133,6 +139,7 @@ abstract class AbstractRevision {
 		} elseif ( isset( $row['rev_user_text'] ) && $obj->userId === 0 ) {
 			$obj->userIp = $row['rev_user_text'];
 		}
+		$obj->userWiki = isset( $row['rev_user_wiki'] ) ? $row['rev_user_wiki'] : null;
 		$obj->prevRevision = UUID::create( $row['rev_parent_id'] );
 		$obj->changeType = $row['rev_change_type'];
 	 	$obj->flags = array_filter( explode( ',', $row['rev_flags'] ) );
@@ -149,6 +156,7 @@ abstract class AbstractRevision {
 		} elseif ( isset( $row['rev_mod_user_text'] ) && $obj->moderatedByUserId === 0 ) {
 			$obj->moderatedByUserIp = $row['rev_mod_user_text'];
 		}
+		$obj->moderatedByUserWiki = isset( $row['rev_mod_user_wiki'] ) ? $row['rev_mod_user_wiki'] : null;
 		$obj->moderationTimestamp = $row['rev_mod_timestamp'];
 		$obj->moderatedReason = isset( $row['rev_mod_reason'] ) ? $row['rev_mod_reason'] : null;
 
@@ -167,6 +175,7 @@ abstract class AbstractRevision {
 			$obj->lastEditUserIp = $row['rev_edit_user_text'];
 		}
 		$obj->lastEditUserIp = isset( $row['rev_edit_user_ip'] ) ? $row['rev_edit_user_ip'] : null;
+		$obj->lastEditUserWiki = isset( $row['rev_edit_user_wiki'] ) ? $row['rev_edit_user_wiki'] : null;
 
 		return $obj;
 	}
@@ -176,6 +185,7 @@ abstract class AbstractRevision {
 			'rev_id' => $obj->revId->getBinary(),
 			'rev_user_id' => $obj->userId,
 			'rev_user_ip' => $obj->userIp,
+			'rev_user_wiki' => $obj->userWiki,
 			'rev_parent_id' => $obj->prevRevision ? $obj->prevRevision->getBinary() : null,
 			'rev_change_type' => $obj->changeType,
 			'rev_type' => $obj->getRevisionType(),
@@ -187,12 +197,14 @@ abstract class AbstractRevision {
 			'rev_mod_state' => $obj->moderationState,
 			'rev_mod_user_id' => $obj->moderatedByUserId,
 			'rev_mod_user_ip' => $obj->moderatedByUserIp,
+			'rev_mod_user_wiki' => $obj->moderatedByUserWiki,
 			'rev_mod_timestamp' => $obj->moderationTimestamp,
 			'rev_mod_reason' => $obj->moderatedReason,
 
 			'rev_last_edit_id' => $obj->lastEditId ? $obj->lastEditId->getBinary() : null,
 			'rev_edit_user_id' => $obj->lastEditUserId,
 			'rev_edit_user_ip' => $obj->lastEditUserIp,
+			'rev_edit_user_wiki' => $obj->lastEditUserWiki,
 		);
 	}
 
@@ -207,7 +219,7 @@ abstract class AbstractRevision {
 		}
 		$obj = clone $this;
 		$obj->revId = UUID::create();
-		list( $obj->userId, $obj->userIp ) = self::userFields( $user );
+		list( $obj->userId, $obj->userIp, $obj->userWiki ) = self::userFields( $user );
 		$obj->prevRevision = $this->revId;
 		$obj->changeType = '';
 		return $obj;
@@ -252,14 +264,16 @@ abstract class AbstractRevision {
 				continue;
 			}
 			$rev->moderationState = $state;
-			list( $userId, $userIp ) = self::userFields( $user );
+			list( $userId, $userIp, $userWiki ) = self::userFields( $user );
 			if ( $state === self::MODERATED_NONE ) {
 				$rev->moderatedByUserId = null;
 				$rev->moderatedByUserIp = null;
+				$rev->moderatedByUserWiki = null;
 				$rev->moderationTimestamp = null;
 			} else {
 				$rev->moderatedByUserId = $userId;
 				$rev->moderatedByUserIp = $userIp;
+				$rev->moderatedByUserWiki = $userWiki;
 				$rev->moderationTimestamp = $timestamp;
 			}
 		}
@@ -373,6 +387,10 @@ abstract class AbstractRevision {
 		return $this->userIp;
 	}
 
+	public function getUserWiki() {
+		return $this->userWiki;
+	}
+
 	/**
 	 * Should only be used for setting the initial content.  To set subsequent content
 	 * use self::setNextContent
@@ -423,7 +441,7 @@ abstract class AbstractRevision {
 			$this->content = null;
 			$this->setContent( $content );
 			$this->lastEditId = $this->getRevisionId();
-			list( $this->lastEditUserId, $this->lastEditUserIp ) = self::userFields( $user );
+			list( $this->lastEditUserId, $this->lastEditUserIp, $this->lastEditUserWiki ) = self::userFields( $user );
 		}
 	}
 
@@ -532,12 +550,20 @@ abstract class AbstractRevision {
 		return $this->lastEditUserIp;
 	}
 
+	public function getLastContentEditUserWiki() {
+		return $this->lastEditUserWiki;
+	}
+
 	public function getModeratedByUserId() {
 		return $this->moderatedByUserId;
 	}
 
 	public function getModeratedByUserIp() {
 		return $this->moderatedByUserIp;
+	}
+
+	public function getModeratedByUserWiki() {
+		return $this->moderatedByUserWiki;
 	}
 
 	/**
@@ -548,11 +574,13 @@ abstract class AbstractRevision {
 		if ( $user->isAnon() ) {
 			$userId = 0;
 			$userIp = $user->getName();
+			$userWiki = wfWikiId();
 		} else {
 			$userId = $user->getId();
 			$userIp = null;
+			$userWiki = wfWikiId();
 		}
-		return array( $userId, $userIp );
+		return array( $userId, $userIp, $userWiki );
 	}
 
 	abstract public function getRevisionType();
