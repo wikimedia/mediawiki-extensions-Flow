4aab539192d90a4f5eef801a0d071940e3c325d9
bsitu <bsitu@wikimedia.org>
2014-02-05 10:28:15 -0800

    [SCHEMA CHANGE] Add corresponding *_user_wiki field to all *_user_id fields
    This is to distinguish users and ips from different wikis especially
    for pages like contributions.
    
    The steps involved:
    
    1. deploy https://gerrit.wikimedia.org/r/#/c/112794/
    2. add the columns to production wikis
    3. deploy the code in this patch so the new columns start collecting data
    4. run maintenance script to populate existing records
    5. update cache version key
    6. patch to use the new columns
    
    Change-Id: I439b102e9125c4e6aed988032a21d51b1079f93b

--- container.php
+++ container.php
@@ -173,8 +173,7 @@ $c['storage.workflow'] = $c->share( function( $c ) {
 	$lifecycle = array(
 		new Flow\Data\UserNameListener(
 			$c['repository.username'],
-			array( 'workflow_user_id' ),
-			'workflow_wiki'
+			array( 'workflow_user_id' => 'workflow_user_wiki' )
 		),
 		// $c['storage.user_subs.user_index']
 	);
@@ -253,11 +252,11 @@ $c['storage.header'] = $c->share( function( $c ) {
 		$c['storage.board_history.index'],
 		new Flow\Data\UserNameListener(
 			$c['repository.username'],
-			array( 'rev_user_id', 'rev_mod_user_id', 'rev_edit_user_id' ),
-			null,
-			// @todo composite wiki id + user columns, for now this
-			// works as we only display content from this wiki
-			wfWikiId()
+			array(
+				'rev_user_id' => 'rev_user_wiki',
+				'rev_mod_user_id' => 'rev_mod_user_wiki',
+				'rev_edit_user_id' => 'rev_edit_user_wiki'
+			)
 		),
 	);
 
@@ -340,11 +339,12 @@ $c['storage.post'] = $c->share( function( $c ) {
 		$c['storage.board_history.index'],
 		new Flow\Data\UserNameListener(
 			$c['repository.username'],
-			array( 'rev_user_id', 'rev_mod_user_id', 'rev_edit_user_id', 'tree_orig_user_id' ),
-			null,
-			// @todo composite wiki id + user columns, for now this
-			// works as we only display content from this wiki
-			wfWikiId()
+			array(
+				'rev_user_id' => 'rev_user_wiki',
+				'rev_mod_user_id' => 'rev_mod_user_wiki',
+				'rev_edit_user_id' => 'rev_edit_user_wiki',
+				'tree_orig_user_id' => 'tree_orig_user_wiki'
+			)
 		),
 		$c['collection.cache'],
 	);
