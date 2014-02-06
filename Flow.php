315be9dccfa1d2c0925b6a79087712ee64a06c21
Kunal Mehta <legoktm@gmail.com>
2014-02-05 20:37:33 -0800

    Whitelist action=(un)watch
    Used as a fallback for non-JS users, also unhides
    the 'watch' tab/star
    
    Change-Id: Ie8bbd0a40421d7d279fb896ebaa15a7de8cbd38a

--- Flow.php
+++ Flow.php
@@ -340,4 +340,4 @@ $wgFlowAbuseFilterEmergencyDisableCount = 50;
 $wgFlowAbuseFilterEmergencyDisableAge = 86400; // One day.
 
 // Actions that must pass through to MediaWiki on flow enabled pages
-$wgFlowCoreActionWhitelist = array( 'info', 'protect', 'unprotect' );
+$wgFlowCoreActionWhitelist = array( 'info', 'protect', 'unprotect', 'unwatch', 'watch' );
