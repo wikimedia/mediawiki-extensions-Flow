5089d352d0d07060cc6dad51a87fb941490dc9fe
cmcmahon <cmcmahon@wikimedia.org>
2014-02-07 12:09:04 -0700

    QA Test for Action menu Permalink
    Bug 60659
    
    Change-Id: Iaa7fd8e912d9a084dff7cc5053b8e2f059df5ccb

---
+++ tests/browser/features/action_menu_permalink.feature
@@ -0,0 +1,13 @@
+
+Feature: Actions menu Permalink
+
+Scenario: Actions menu Permalink
+  Given I am on Flow page
+    And I have created a Flow topic
+  When I click Actions menu for the Topic
+    And I click Permalink from the Actions menu
+    And I add 3 comments to the Topic
+    And I click Actions menu for the 3rd comment on the Topic
+    And I click Permalink from the Actions menu
+  Then the highlighted comment should contain the text for the 3rd comment
+
--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -16,16 +16,20 @@ class FlowPage
   end
 
   a(:actions_link, text: "Actions", index: 1)
+  a(:actions_link_permalink_3rd_comment, text: "Actions", index: 4)
   span(:author_link, class: "flow-creator")
   a(:block_user, title: /Special:Block/)
   list_item(:collapsed_view, title: "Collapsed view")
   button(:change_post_save, class: "flow-edit-post-submit")
   button(:change_title_save, class: "flow-edit-title-submit")
+  textarea(:comment_field, name: "topic_topic-reply-content")
+  button(:comment_reply_save, class: "flow-topic-reply-submit")
   a(:contrib_link, text: "contribs")
   a(:edit_post, class: "flow-edit-post-link", index: topic_index)
   a(:edit_title_icon, css: "div.tipsy-inner > div.flow-tipsy-flyout > ul > li.flow-action-edit-title > a.mw-ui-button.flow-edit-topic-link")
   div(:flow_body, class: "flow-container")
   list_item(:full_view, title: "Full view")
+  div(:highlighted_comment, class: "flow-post-highlighted")
 
   # Buttons in a fly-out menu.
   button(:delete_button,         css: "div.tipsy-inner input.flow-delete-post-link")
@@ -38,6 +42,7 @@ class FlowPage
   text_area(:new_topic_body, class: "flow-newtopic-content")
   button(:new_topic_save, class: "flow-newtopic-submit")
   text_field(:new_topic_title, name: "topiclist_topic")
+  a(:permalink, css: "div.tipsy-inner > div.flow-tipsy-flyout > ul > li.flow-action-permalink > a.mw-ui-button.flow-action-permalink-link")
   text_field(:post_edit, class: "flow-edit-post-content flow-disabler")
   div(:small_spinner, class: "mw-spinner mw-spinner-small mw-spinner-inline")
   list_item(:small_view, title: "Small view")
