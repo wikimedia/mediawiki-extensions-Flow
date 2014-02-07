97e65e9ed3d31b93bfe4a28b420d2a3209ad2b25
Matthias Mullie <git@mullie.eu>
2014-02-07 15:38:15 -0800

    Make sure permission checks also check against current revisions
    Change-Id: I4b63b5ab3f428b499dc5318298238b50b17978e8

--- includes/RevisionActionPermissions.php
+++ includes/RevisionActionPermissions.php
@@ -2,6 +2,7 @@
 
 namespace Flow;
 
+use Flow\Exception\InvalidDataException;
 use Flow\Model\AbstractRevision;
 use Closure;
 use User;
@@ -67,11 +68,33 @@ class RevisionActionPermissions {
 			return false;
 		}
 
-		// check if user is allowed to perform action
-		return call_user_func_array(
+		// Check if user is allowed to perform action against this revision
+		$allowed = call_user_func_array(
 			array( $this->user, 'isAllowedAny' ),
 			(array) $permission
 		);
+
+		// if there was no revision object, it's pointless to find last revision
+		if ( $revision === null ) {
+			return $allowed;
+		}
+
+		try {
+			// Also check if the user would be allowed to perform this against
+			// against the most recent revision (unless it's already the most recent
+			// revision) - the last revision is the current state of a revision, so
+			// checking against a revision at one point in time alone isn't enough.
+			$last = $revision->getCollection()->getLastRevision();
+
+			// check if $revision is not already the most recent, to prevent
+			// infinite recursion in this method
+			$isLastRevision = $last->getRevisionId()->getHex() == $revision->getRevisionId()->getHex();
+			return $allowed && ( $isLastRevision || $this->isAllowed( $last, $action ) );
+
+		// If data is not in storage, just return that revision's status
+		} catch ( InvalidDataException $e ) {
+			return $allowed;
+		}
 	}
 
 	/**
@@ -111,7 +134,7 @@ class RevisionActionPermissions {
 	public function getPermission( AbstractRevision $revision = null, $action ) {
 		// $revision may be null if the revision has yet to be created
 		$moderationState = AbstractRevision::MODERATED_NONE;
-		if ( $revision instanceof AbstractRevision ) {
+		if ( $revision !== null ) {
 			$moderationState = $revision->getModerationState();
 		}
 		$permission = $this->actions->getValue( $action, 'permissions', $moderationState );
