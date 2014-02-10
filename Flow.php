1765f3bc625214690940012ccf83077d07c21a86
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-02-10 10:51:53 -0800

    Create maintenance mode
    Prevent all reads/writes from Flow while $wgFlowMaintenanceMode
    is true.  This is necessary for the change to 88bit uuids, no
    reads or writes will succeede while the db is changing from
    128bit to 88bit primary keys.
    
    Change-Id: I23f77255b4c66abc6f39e2048a09aedd3fed547a

--- Flow.php
+++ Flow.php
@@ -339,4 +339,8 @@ $wgFlowAbuseFilterEmergencyDisableCount = 50;
 $wgFlowAbuseFilterEmergencyDisableAge = 86400; // One day.
 
 // Actions that must pass through to MediaWiki on flow enabled pages
-$wgFlowCoreActionWhitelist = array( 'protect', 'unprotect' );
+$wgFlowCoreActionWhitelist = array( 'info', 'protect', 'unprotect', 'unwatch', 'watch' );
+
+// Display a "maintenance" message instead of a flow board on all pages
+$wgFlowMaintenanceMode = false;
+
