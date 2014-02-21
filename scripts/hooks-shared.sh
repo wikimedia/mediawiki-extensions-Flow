5d99fffb96f67dbe5ea080d7c51de9b0f8643715
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-02-21 10:26:52 -0800

    share code between pre-commit and pre-review hooks
    Change-Id: I682be3fc8856fe9e885d9a1f6eeac4ed9a5f93fd

---
+++ scripts/hooks-shared.sh
@@ -0,0 +1,35 @@
+#
+# Shared functionality of the Flow git hooks
+#
+
+realpath() {
+	php -r "echo realpath('$1'), \"\\n\";"
+}
+
+is_vagrant() {
+    DEST='.'
+    while [ "$(realpath $DEST)" != "/" ]; do
+        if [ -f $DEST/Vagrantfile ]; then
+            return 0;
+        fi
+        DEST="$DEST/.."
+    done
+    return 1
+}
+
+make() {
+    if is_vagrant; then
+        vagrant ssh -- cd /vagrant/mediawiki/extensions/Flow '&&' make $* || exit 1
+    else
+        /usr/bin/env make $* || exit 1
+    fi
+}
+
+file_changed_in_commit() {
+	git diff --name-only --cached | grep -P "$1" 2>&1 >/dev/null
+}
+
+file_changed_in_head() {
+	git diff-tree --no-commit-id --name-only -r HEAD | grep -P "$1" 2>&1 >/dev/null
+}
+
