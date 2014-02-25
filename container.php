142e4aca3b1cda47ab4513264ec1d82d7563d215
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-02-25 14:24:16 -0800

    Split reusable parts out of ContributionsQuery
    Change-Id: I8dac314453d1bdffe076007a57cb8b1aa7242431

--- container.php
+++ container.php
@@ -472,8 +472,8 @@ $c['recentchanges.formatter'] = $c->share( function( $c ) {
 $c['contributions.query'] = $c->share( function( $c ) {
 	return new Flow\Formatter\ContributionsQuery(
 		$c['storage'],
-		$c['memcache'],
-		$c['repository.tree']
+		$c['repository.tree'],
+		$c['memcache']
 	);
 } );
 $c['contributions.formatter'] = $c->share( function( $c ) {
