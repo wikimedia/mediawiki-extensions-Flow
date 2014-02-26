5c19fc960b5fbf4b2c928008e37d0641fb257d7a
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-02-25 22:34:08 -0800

    [SCHEMA CHANGE] Drop the definition model
    The definition hangs around but doesn't do anything for us currently. Its
    an idea for the future of user defined workflows, but we dont really know
    how those might work yet.  It requires a network request or two on every
    page and gives us little in reutrn. Based on this lets just remove it for
    now and if needed later can be revived.
    
    TODO: test deployment process, initial plan is:
    * Add unused workflow_type field
    * Deploy patch
    * Run maintenance script (to be written)
    * Drop flow_definition table and workflow_definition_id column
    
    Change-Id: I470e7f1705166420d465df72f907c18e61ee0f89

--- includes/Exception/ExceptionHandling.php
+++ includes/Exception/ExceptionHandling.php
@@ -132,7 +132,6 @@ class InvalidInputException extends FlowException {
 			'invalid-input',
 			'missing-revision',
 			'revision-comparison',
-			'invalid-definition',
 			'invalid-workflow'
 		);
 	}
