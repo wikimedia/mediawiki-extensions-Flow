a2e38bd0a060096f326c5d81bc3d585c932f5436
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-02-25 22:34:08 -0800

    Drop the definition model
    The definition hangs around but doesn't do anything for us currently. Its
    an idea for the future of user defined workflows, but we don't really know
    how those might work yet.  It requires a network request or two on every
    page and gives us little in return. Based on this lets just remove it for
    now and if needed later can be revived.
    
    Deployment process
    * DONE Add unused workflow_type field
    * DONE Deploy patch prior to this one adding code to populate
      workflow_type field
    * DONE Run SQL to set the workflow_type fields
    * Deploy this patch that no longer needs definition table
    * Drop flow_definition table and workflow_definition_id column
    
    Change-Id: I470e7f1705166420d465df72f907c18e61ee0f89

--- db_patches/patch-workflow_lookup_idx.sql
+++ db_patches/patch-workflow_lookup_idx.sql
@@ -1,1 +1,1 @@
-CREATE INDEX /*i*/flow_workflow_lookup ON /*_*/flow_workflow (workflow_wiki, workflow_namespace, workflow_title_text, workflow_definition_id);
+CREATE INDEX /*i*/flow_workflow_lookup ON /*_*/flow_workflow (workflow_wiki, workflow_namespace, workflow_title_text);
