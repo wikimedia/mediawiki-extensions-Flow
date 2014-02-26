3ccd14133ac4a4751aae530d268d0c73f8537351
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-02-25 22:34:08 -0800

    Drop the definition model
    The definition hangs around but doesn't do anything for us currently. Its
    an idea for the future of user defined workflows, but we don't really know
    how those might work yet.  It requires a network request or two on every
    page and gives us little in return. Based on this lets just remove it for
    now and if needed later can be revived.
    
    Deployment process
    * DONE Add unused workflow_type field
    * DONE Deploy patch prior to this one adding code to populate
      workflow_type field
    * DONE Run SQL to set the workflow_type fields
    * TODO Change flow_workflow.workflow_definition_id in production to allow null
    * Deploy this patch that no longer needs definition table
    * Drop flow_definition table and workflow_definition_id column
    
    Change-Id: I470e7f1705166420d465df72f907c18e61ee0f89

--- Hooks.php
+++ Hooks.php
@@ -107,8 +107,8 @@ class FlowHooks {
 			$updater->modifyExtensionField( 'flow_summary_revision', 'summary_workflow_id', "$dir/db_patches/patch-summary2header.sqlite.sql" );
 			$updater->modifyExtensionField( 'flow_revision', 'rev_comment', "$dir/db_patches/patch-rev_change_type.sqlite.sql" );
 			// sqlite ignores field types, this just substr's uuid's to 88 bits
-			$updater->modifyExtensionField( 'flow_definition', 'definition_id', "$dir/db_patches/patch-88bit_uuids.sqlite.sql" );
-			$updater->addExtensionField( 'flow_workflow', 'workflow_type', "$dir/db_patches/patch-add_workflow_type.sqlite.sql" );
+			$updater->modifyExtensionField( 'flow_workflow', 'workflow_id', "$dir/db_patches/patch-88bit_uuids.sqlite.sql" );
+			$updater->addExtensionField( 'flow_workflow', 'workflow_type', "$dir/db_patches/patch-add_workflow_type.sqlite" );
 		} else {
 			// sqlite doesn't support alter table change, it also considers all types the same so
 			// this patch doesn't matter to it.
@@ -118,7 +118,7 @@ class FlowHooks {
 			// rename rev_change_type -> rev_comment, alternate patch is above for sqlite
 			$updater->modifyExtensionField( 'flow_revision', 'rev_comment', "$dir/db_patches/patch-rev_change_type.sql" );
 			// convert 128 bit uuid's into 88bit
-			$updater->modifyExtensionField( 'flow_definition', 'definition_id', "$dir/db_patches/patch-88bit_uuids.sql" );
+			$updater->modifyExtensionField( 'flow_workflow', 'workflow_id', "$dir/db_patches/patch-88bit_uuids.sql" );
 			$updater->addExtensionField( 'flow_workflow', 'workflow_type', "$dir/db_patches/patch-add_workflow_type.sql" );
 		}
 
@@ -135,9 +135,7 @@ class FlowHooks {
 		$updater->modifyExtensionField( 'flow_revision', 'rev_user_ip', "$dir/db_patches/patch-revision_user_ip.sql" );
 		$updater->addExtensionField( 'flow_revision', 'rev_type_id', "$dir/db_patches/patch-rev_type_id.sql" );
 		$updater->addExtensionTable( 'flow_ext_ref', "$dir/db_patches/patch-add-linkstables.sql" );
-
-		require_once __DIR__.'/maintenance/FlowInsertDefaultDefinitions.php';
-		$updater->addPostDatabaseUpdateMaintenance( 'FlowInsertDefaultDefinitions' );
+		$updater->dropExtensionTable( 'flow_definition', "$dir/db_patches/patch-drop_definition.sql" );
 
 		require_once __DIR__.'/maintenance/FlowUpdateRecentChanges.php';
 		$updater->addPostDatabaseUpdateMaintenance( 'FlowUpdateRecentChanges' );
