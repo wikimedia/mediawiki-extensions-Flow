e3d8c5978b3bdb8224162e31de98d80386abce59
Matthias Mullie <git@mullie.eu>
2014-02-28 13:35:24 +0100

    Non-hacky way to feed title to Parsoid
    Gets rid of $wgFlowParsoidTitle for the purpose of calls to Parsoid.
    It still exists for Redlinker - needs to be removed in there some day too.
    
    This introduces some methods in Collection to fetch associated Workflow & Title
    objects. When calling this on multiple collections, these should preferably
    already be batch-loaded in advance (both treeRepo & cache::getMulti will local-
    cache those results efficiently). I'll submit a bug to build some batchloader
    classes - that's too tedious now.
    
    Bug: 58664
    Change-Id: Ifc5c6f3f79890ec59d438b0eec004941c318b214

--- includes/Collection/PostCollection.php
+++ includes/Collection/PostCollection.php
@@ -2,7 +2,14 @@
 
 namespace Flow\Model;
 
+use Flow\Container;
+
 class PostCollection extends LocalCacheAbstractCollection {
+	/**
+	 * @var UUID
+	 */
+	protected $rootId;
+
 	public function getRevisionClass() {
 		return 'Flow\\Model\\PostRevision';
 	}
@@ -11,7 +18,23 @@ class PostCollection extends LocalCacheAbstractCollection {
 		return 'tree_rev_descendant_id';
 	}
 
-	protected static function getIdFromRevision( AbstractRevision $revision ) {
-		return $revision->getPostId();
+	public function getWorkflowId() {
+		// the root post (topic title) has the same id as the workflow
+		if ( !$this->rootId ) {
+			/** @var \Flow\Repository\TreeRepository $treeRepo */
+			$treeRepo = Container::get( 'repository.tree' );
+			$this->rootId = $treeRepo->findRoot( $this->getId() );
+		}
+
+		return $this->rootId;
+	}
+
+	/**
+	 * Returns the topic title collection this post is associated with.
+	 *
+	 * @return PostCollection
+	 */
+	public function getRoot() {
+		return static::newFromId( $this->getWorkflowId() );
 	}
 }
