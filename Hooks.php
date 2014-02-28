9caba33ab5e399649361383b17517693139e7660
Matthias Mullie <git@mullie.eu>
2014-02-28 17:05:55 +0100

    [SCHEMA CHANGE] Add index on revision user data
    It's part of Special:Contributions' WHERE-clause 
    While this is a schema change, it's non-blocking. We can merge
    this whenever, we just have to make sure this is done some day.
    
    Change-Id: Icedaf6b164f920160e6c9b886eb1c32ef0ad802e

--- Hooks.php
+++ Hooks.php
@@ -120,6 +120,7 @@ class FlowHooks {
 		$updater->addExtensionField( 'flow_workflow', 'workflow_user_wiki', "$dir/db_patches/patch-add-wiki.sql" );
 		$updater->addExtensionIndex( 'flow_tree_revision', 'flow_tree_descendant_rev_id', "$dir/db_patches/patch-flow_tree_idx_fix.sql" );
 		$updater->dropExtensionField( 'flow_tree_revision', 'tree_orig_create_time', "$dir/db_patches/patch-tree_orig_create_time.sql" );
+		$updater->addExtensionIndex( 'flow_revision', 'flow_revision_user', "$dir/db_patches/patch-revision_user_idx.sql" );
 
 		require_once __DIR__.'/maintenance/FlowInsertDefaultDefinitions.php';
 		$updater->addPostDatabaseUpdateMaintenance( 'FlowInsertDefaultDefinitions' );
