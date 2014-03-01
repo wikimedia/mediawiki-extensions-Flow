6221f5f64439bbc1924f853cd3f36f913a3301bf
Benjamin Chen <bencmqwiki@gmail.com>
2014-03-01 21:34:06 +0800

    [WIP] Browser test for Thank feature in Flow
    Bug: 61930
    Change-Id: Icd9afc0a12b228bdbb682959e527cdad5d3253b1

---
+++ tests/browser/features/step_definitions/thank_steps.rb
@@ -0,0 +1,26 @@
+Then(/^I do not see a Thank button$/) do
+  on(FlowPage).thank_button_element.should_not exist
+end
+
+Then(/^I do not see a Thanked button$/) do
+  on(FlowPage).thanked_button_element.should_not exist
+end
+
+Then(/^I should see the Thank button be replaced with Thanked button$/) do
+  # TODO: if we decide to use a loading indicator for the thank button
+  # then condition can be changed to loading_indicator.when_not_present
+  @target_container.span_element(class: 'mw-thanks-flow-thanked').when_visible
+end
+
+When(/^I see a Thank button$/) do
+  on(FlowPage).thank_button_element.should be_visible
+  @target_container = on(FlowPage).thank_button_element.parent
+end
+
+When(/^I click on the Thank button$/) do
+  on(FlowPage).thank_button_element.click
+end
+
+Then(/^I should not see the Thank button for that post$/) do
+  on(FlowPage).new_post_element.link_element(class: 'mw-thanks-flow-thank-link').should_not be_visible
+end
