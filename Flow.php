6851a5f739173bfa7d0b6376c6bcd35db285f882
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-03-05 12:12:08 -0800

    Update annotations in RevisionView
    Also introduces a new interface, RevisionCreatorHack.  Basically
    you can't define an 'abstract static function' as it doesn't mean
    anything. To have a statically accessible but un-implemented
    method in an abstract class it must be defined via an interface which
    php will then enforce on child classes.
    
    Change-Id: I149f33cdb7d858d2e562b642f06430551b0526d2

--- Flow.php
+++ Flow.php
@@ -101,6 +101,7 @@ $wgAutoloadClasses['Flow\View\History\HistoryRecord'] = "$dir/includes/View/Hist
 $wgAutoloadClasses['Flow\View\History\HistoryBundle'] = "$dir/includes/View/History/HistoryBundle.php";
 $wgAutoloadClasses['Flow\View\History\HistoryRenderer'] = "$dir/includes/View/History/HistoryRenderer.php";
 $wgAutoloadClasses['Flow\View\Post'] = "$dir/includes/View/Post.php";
+$wgAutoloadClasses['Flow\View\RevisionCreatable'] = "$dir/includes/View/Revision.php";
 $wgAutoloadClasses['Flow\View\RevisionView'] = "$dir/includes/View/Revision.php";
 $wgAutoloadClasses['Flow\View\PostRevisionView'] = "$dir/includes/View/Revision.php";
 $wgAutoloadClasses['Flow\View\HeaderRevisionView'] = "$dir/includes/View/Revision.php";
