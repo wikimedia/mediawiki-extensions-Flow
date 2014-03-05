956e7c6010bfae150f1a820793d0bf62eec4e8c4
bsitu <bsitu@wikimedia.org>
2014-03-05 15:47:39 -0800

    Drop table flow_header_revision
    Remove some update to flow_header_revision as well. since jenkins
    creates the table for unittest from scratch, it would cause error
    when updating a non-existing table
    
    Change-Id: I1af9d1bf5014f84a529de9ad017f14a813b79896

--- maintenance/FlowUpdateRevisionTypeId.php
+++ maintenance/FlowUpdateRevisionTypeId.php
@@ -28,41 +28,48 @@ class FlowUpdateRevisionTypeId extends LoggedUpdateMaintenance {
 		$revId = '';
 		$count = $this->mBatchSize;
 		$dbr = Container::get( 'db.factory' )->getDB( DB_SLAVE );
+
+		// If table flow_header_revision does not exist, that means the wiki
+		// has run the data migration before or the wiki starts from scratch,
+		// there is no point to run the script againt invalid tables
+		if ( $dbr->tableExists( 'flow_header_revision', __METHOD__ ) ) {
+			while ( $count == $this->mBatchSize ) {
+				$count = 0;
+				$res = $dbr->select(
+					array( 'flow_revision', 'flow_tree_revision', 'flow_header_revision' ),
+					array( 'rev_id', 'rev_type', 'tree_rev_descendant_id', 'header_workflow_id' ),
+					array( 'rev_id > ' . $dbr->addQuotes( $revId ) ),
+					__METHOD__,
+					array( 'ORDER BY' => 'rev_id ASC', 'LIMIT' => $this->mBatchSize ),
+					array(
+						'flow_tree_revision' => array( 'LEFT JOIN', 'rev_id=tree_rev_id' ),
+						'flow_header_revision' => array( 'LEFT JOIN', 'rev_id=header_rev_id' )
+					)
+				);
 		
-		while ( $count == $this->mBatchSize ) {
-			$count = 0;
-			$res = $dbr->select(
-				array( 'flow_revision', 'flow_tree_revision', 'flow_header_revision' ),
-				array( 'rev_id', 'rev_type', 'tree_rev_descendant_id', 'header_workflow_id' ),
-				array( 'rev_id > ' . $dbr->addQuotes( $revId ) ),
-				__METHOD__,
-				array( 'ORDER BY' => 'rev_id ASC', 'LIMIT' => $this->mBatchSize ),
-				array(
-					'flow_tree_revision' => array( 'LEFT JOIN', 'rev_id=tree_rev_id' ),
-					'flow_header_revision' => array( 'LEFT JOIN', 'rev_id=header_rev_id' )
-				)
-			);
-	
-			if ( $res ) {
-				foreach ( $res as $row ) {
-					$count++;
-					$revId = $row->rev_id;
-					switch ( $row->rev_type ) {
-						case 'header':
-							$this->output( "processing " . $row->rev_type . ": " . UUID::create( $row->rev_id )->getAlphadecimal() . "\n" );
-							$this->updateRevision( $row->rev_id, $row->header_workflow_id );
-						break;
-						case 'post':
-							$this->output( "processing " . $row->rev_type . ": " . UUID::create( $row->rev_id )->getAlphadecimal() . "\n" );
-							$this->updateRevision( $row->rev_id, $row->tree_rev_descendant_id );
-						break;
+				if ( $res ) {
+					foreach ( $res as $row ) {
+						$count++;
+						$revId = $row->rev_id;
+						switch ( $row->rev_type ) {
+							case 'header':
+								$this->output( "processing " . $row->rev_type . ": " . UUID::create( $row->rev_id )->getAlphadecimal() . "\n" );
+								$this->updateRevision( $row->rev_id, $row->header_workflow_id );
+							break;
+							case 'post':
+								$this->output( "processing " . $row->rev_type . ": " . UUID::create( $row->rev_id )->getAlphadecimal() . "\n" );
+								$this->updateRevision( $row->rev_id, $row->tree_rev_descendant_id );
+							break;
+						}
 					}
+				} else {
+					throw new MWException( 'SQL error in maintenance script ' . __CLASS__ . '::' . __METHOD__ );
 				}
-			} else {
-				throw new MWException( 'SQL error in maintenance script ' . __CLASS__ . '::' . __METHOD__ );
+				wfWaitForSlaves( false, false, $wgFlowCluster );
 			}
-			wfWaitForSlaves( false, false, $wgFlowCluster );
 		}
+
+		return true;
 	}
 
 	private function updateRevision( $revId, $revTypeId ) {
