5c1472c9c76fdd2f408c50e73aa2c9b903a17b35
bsitu <bsitu@wikimedia.org>
2014-03-06 16:36:36 -0800

    Add summary revision type
    This will provide the storage and api access for saving/editing
    a topic summary
    
    Change-Id: Id06006f99c0e7f41f6711ec7ea5a27cbf55fe2c9

---
+++ includes/Collection/SummaryCollection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Flow\Collection;
+
+use Flow\Container;
+use Flow\Model\UUID;
+
+class PostSummaryCollection extends LocalCacheAbstractCollection {
+	/**
+	 * @var UUID
+	 */
+	protected $rootId;
+
+	public function getRevisionClass() {
+		return 'Flow\\Model\\PostSummary';
+	}
+
+	public function getWorkflowId() {
+		// the root post (topic title) has the same id as the workflow
+		if ( !$this->rootId ) {
+			/** @var \Flow\Repository\TreeRepository $treeRepo */
+			$treeRepo = Container::get( 'repository.tree' );
+			$this->rootId = $treeRepo->findRoot( $this->getId() );
+		}
+
+		return $this->rootId;
+	}
+
+}
