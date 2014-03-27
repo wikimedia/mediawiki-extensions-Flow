bcf694f8fdc352276dfa64f924f4cbd88cd68d3d
Matthias Mullie <git@mullie.eu>
2014-03-27 16:09:21 +0100

    No longer need the try-catch fallback; getRootPost will always resolve
    getRootPost used to throw an exception when the root post was not fed to the
    PostRevision object; now it will already look it up using Collection.
    The manual lookup in the catch is now useless.
    
    Change-Id: I3ef5bb164fb9bece0ed2e62454d07d114a670631

--- includes/Formatter/AbstractFormatter.php
+++ includes/Formatter/AbstractFormatter.php
@@ -501,19 +501,7 @@ abstract class AbstractFormatter {
 			return Message::rawParam( htmlspecialchars( $revision->getModeratedReason() ) );
 
 		case 'topic-of-post':
-			try {
-				$content = $this->templating->getContent( $revision->getRootPost(), 'wikitext' );
-			} catch ( DataModelException $e ) {
-				$found = Container::get( 'storage.post' )->find(
-					array( 'tree_rev_descendant_id' => $workflowId ),
-					array( 'sort' => 'rev_id', 'order' => 'DESC', 'limit' => 1 )
-				);
-				if ( !$found ) {
-					wfWarn( __METHOD__ . ': No tree_rev_descendant_id matching ' . $workflowId->getAlphadecimal() );
-					return '';
-				}
-				$content = $this->templating->getContent( reset( $found ), 'wikitext' );
-			}
+			$content = $this->templating->getContent( $revision->getRootPost(), 'wikitext' );
 			return Message::rawParam( htmlspecialchars( $content ) );
 
 		case 'bundle-count':
