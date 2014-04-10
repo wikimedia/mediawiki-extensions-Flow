6445a416d26e9506305ac25a827448cdcc788f70
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-04-10 11:04:09 -0700

    More SafeString for lightncandy
    Change-Id: I4e71da13dec7b4100af569fdd99009136e543287

--- vendor/lightncandy.php
+++ vendor/lightncandy.php
@@ -1397,7 +1397,7 @@ $libstr
         if ($context['flags']['jsobj'] || $context['flags']['jstrue']) {
             return $context['ops']['seperator'] . self::getFuncName($context, $raw ? 'raw' : $context['ops']['enc']) . "($v, \$cx){$context['ops']['seperator']}";
         } else {
-            return $raw ? "{$context['ops']['seperator']}$v{$context['ops']['seperator']}" : "{$context['ops']['seperator']}htmlentities($v, ENT_QUOTES, 'UTF-8'){$context['ops']['seperator']}";
+            return $raw ? "{$context['ops']['seperator']}$v{$context['ops']['seperator']}" : "{$context['ops']['seperator']}LCRun2::enc($v, \$cx){$context['ops']['seperator']}";
         }
     }
 
@@ -1578,7 +1578,9 @@ class LCRun2 {
      * @expect 'a&#039;b' when input 'a\'b', Array()
      */
     public static function enc($var, $cx) {
-        return htmlentities(self::raw($var, $cx), ENT_QUOTES, 'UTF-8');
+        return $var instanceof LCSafeString 
+            ?  $var->string 
+            : htmlentities(self::raw($var, $cx), ENT_QUOTES, 'UTF-8');
     }
 
     /**
@@ -1594,7 +1596,9 @@ class LCRun2 {
      * @expect 'a&#x27;b' when input 'a\'b', Array()
      */
     public static function encq($var, $cx) {
-        return preg_replace('/&#039;/', '&#x27;', htmlentities(self::raw($var, $cx), ENT_QUOTES, 'UTF-8'));
+        return $var instanceof LCSafeString
+            ? $var->string
+            : preg_replace('/&#039;/', '&#x27;', htmlentities(self::raw($var, $cx), ENT_QUOTES, 'UTF-8'));
     }
 
     /**
