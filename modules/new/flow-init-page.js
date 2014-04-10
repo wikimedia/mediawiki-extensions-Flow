43747e90f66e3a8a1fc367ea7116ea3e7007077f
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-04-10 16:07:08 -0700

    Initial import of handlebars templates
    Change-Id: I9043164e3f74404b15e14ea80a5c1fc2c27c7f82

---
+++ modules/new/flow-init-page.js
@@ -0,0 +1,14 @@
+/*!
+ * Runs Flow code, using methods in FlowUI.
+ */
+
+( function ( $ ) {
+	/*
+	 * Now do stuff
+	 * @todo not like this
+	 */
+	$( document ).ready( function () {
+		mw.flow.initComponent( $( '.flow-component' ) );
+	} );
+}( jQuery ) );
+
