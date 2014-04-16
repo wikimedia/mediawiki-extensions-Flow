58583c83aaa8615b363103c1d9df5012c541ae29
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-04-16 14:01:47 -0700

    Slightly faster uuid -> timestamp conversion
    Change-Id: I157da10ff2ea6631de8af2c2de7d4764ba8566d0

--- includes/Model/UUID.php
+++ includes/Model/UUID.php
@@ -199,12 +199,13 @@ class UUID {
 	 */
 	public function getTimestampObj() {
 		if ( $this->timestamp === null ) {
-			// First 6 bytes === 48 bits
-			$hex = $this->getHex();
-			$timePortion = substr( $hex, 0, 12 );
-			$bits_48 = wfBaseConvert( $timePortion, 16, 2, 48 );
-			$bits_46 = substr( $bits_48, 0, 46 );
-			$msTimestamp = wfBaseConvert( $bits_46, 2, 10 );
+			if ( $this->alphadecimalValue ) {
+				$bits = wfBaseConvert( $this->alphadecimalValue, 36, 2, 88 );
+			} else {
+				// First 6 bytes === 48 bits
+				$bits = wfBaseConvert( $this->getHex(), 16, 2, 88 );
+			}
+			$msTimestamp = wfBaseConvert( substr( $bits, 0, 46 ), 2, 10 );
 
 			try {
 				$this->timestamp = new MWTimestamp( intval( $msTimestamp / 1000 ) );
