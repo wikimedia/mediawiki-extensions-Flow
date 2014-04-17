ca43c61acb59d3486d04376cb2d8e0a7bd8fd34c
Matthias Mullie <git@mullie.eu>
2014-04-17 17:03:37 +0200

    [WIP] search
    I'm still very much trying to figure out exactly how Cirrus/Elastica work, and
    how we should implement that in Flow.
    
    I've (kinda) been mimicking Cirrus' setup/classes. There's still *a lot* to be
    implemented, but this is a rough start.
    
    To index all data, run:
        $ php maintenance/FlowForceSearchIndex.php
    To search all indexed data, call the new API: e.g.
        http://mediawiki.dev/api.php?action=flow-search&q=test
    API still needs a lot of work, currently only supports fulltext search.
    The above API call should roughly do this:
        http://localhost:9200/enwiki_flow/topic/_search?pretty=true&q=test
    There'll be more search options down the road. See further down.
    
    Example data mapping:
    
    Topics:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"revisions": [ {
    		"id": "rpvwvywl9po7ih77",
    		"text": "topic title content",
    		"moderation-state": "",
    		"timestamp": "2014-02-25T14:12:40Z"
    	}, {
    		"id": "ropuzninqgyf19ko",
    		"text": "reply content",
    		"moderation-state": "hide",
    		"timestamp": "2014-02-25T14:12:40Z"
    	} ]
    }
    
    Headers:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"text": "header content"
    }
    
    Example queries to ES:
    
    // search for text
    // we only want the topic, don't care about the specific posts
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"term": { "revisions.text": "test" }
    	}
    }'
    
    // find moderated stuff
    // this will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"term": { "revisions.moderation-state": "hide" }
    			}
    		}
    	}
    }'
    
    // find unread posts (will probably just check posts more recent than last visit)
    // will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"range": {
    					"revisions.timestamp": { "gt": "2014-04-01T00:00:00Z" }
    				}
    			}
    		}
    	}
    }'
    
    // search for text, but only in specific page
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"query": {
    				"term": { "revisions.text": "test" }
    			},
    			"filter": {
    				"term": { "pageid": 24 }
    			}
    		}
    	}
    }'
    
    Change-Id: Ib1229acc09b26c65dc9f08c7ea0f19b8191b799b

---
+++ includes/Search/HeaderUpdater.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Flow\Search;
+
+use Flow\Model\Header;
+use ProfileSection;
+
+class HeaderUpdater extends Updater {
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getType() {
+		return Connection::HEADER_TYPE_NAME;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getRevisions( array $conditions = array(), array $options = array() ) {
+		$dbr = $this->dbFactory->getDB( DB_SLAVE );
+
+		// @todo: I need to fetch the latest revisions for every rev_type_id
+
+		$rows = $dbr->select(
+			array( 'flow_revision', 'flow_workflow' ),
+			array( '*' ),
+			$conditions,
+			__METHOD__,
+			array(
+				'ORDER BY' => 'rev_id ASC',
+				'GROUP BY' => 'rev_type_id',
+			) + $options,
+			array(
+				'flow_workflow' => array(
+					'INNER JOIN',
+					array( 'workflow_id = rev_type_id' , 'rev_type' => 'header' )
+				),
+			)
+		);
+
+		return $this->loadRevisionsFromRow( $rows, 'Header' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function buildDocument( /* Header */ $revision ) {
+		/** @var Header $revision */
+
+		$profiler = new ProfileSection( __METHOD__ );
+
+		// get article title associated with this revision
+		$title = $revision->getCollection()->getWorkflow()->getArticleTitle();
+
+		return new \Elastica\Document(
+			$revision->getCollectionId()->getAlphadecimal(),
+			array(
+				'namespace' => $title->getNamespace(),
+				'namespace_text' => $title->getPageLanguage()->getFormattedNsText( $title->getNamespace() ),
+				'pageid' => $title->getArticleID(),
+				'title' => $title->getText(),
+				// headers have no title
+				'timestamp' => wfTimestamp( TS_ISO_8601, $revision->getRevisionId()->getTimestampObj() ),
+				// @todo: I assume we will have to use Templating::getContent() here, to make sure we don't return suppressed content
+				'text' => $revision->getContent( /* @todo: what format? */ ), // @todo: I guess we can/should strip html tags?
+			)
+		);
+	}
+}
