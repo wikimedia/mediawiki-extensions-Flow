bc726bd5a7065da619f326151eba070fca5f2d9b
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-05-01 15:59:25 -0700

    Explicitly check that wiki is specified in TwoStepUsernameQuery
    Change-Id: Icfa0d947429f16f3ac004d41d6c8c94c2eed8795

--- includes/Data/TwoStepUserNameQuery.php
+++ includes/Data/TwoStepUserNameQuery.php
@@ -20,6 +20,10 @@ class TwoStepUserNameQuery implements UserNameQuery {
 	 * @return \ResultWrapper|bool
 	 */
 	public function execute( $wiki, array $userIds ) {
+		if ( !$wiki ) {
+			throw new FlowException( 'No wiki provided with user ids' );
+		}
+
 		$dbr = $this->dbFactory->getWikiDB( DB_SLAVE, array(), $wiki );
 		$res = $dbr->select(
 			'ipblocks',
