96239540a3ba83c2e4c149518bd8318f436220c7
Andrew Garrett <agarrett@wikimedia.org>
2014-05-02 21:55:47 +1000

    Replace occupation w/ ContentHandler integration.
    Also converts all Flow actions into proper first-class MediaWiki
    actions. Currently, these actions are used as a shim to the existing
    Flow calling convention, but in a Future Refactor we can distribute
    the Block-level actions into MediaWiki actions.
    
    This changes the content model of Flow boards, so back up your
    MediaWiki database before trying this patch!
    
    Change-Id: I8363489a11cdb57b8ae105d3d17e9cacc1c48ad3

---
+++ includes/Content/Content.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Flow\Content;
+
+use Article;
+use ContentHandler;
+use Flow\Container;
+use Revision;
+use Title;
+
+abstract class Content {
+	static function onGetDefaultModel( Title $title, &$model ) {
+		$occupationController = Container::get( 'occupation_controller' );
+
+		if ( $occupationController->isTalkpageOccupied( $title ) ) {
+			$model = 'flow-board';
+
+			return false;
+		}
+
+		return true;
+	}
+
+	static function onShowMissingArticle( Article $article ) {
+		if ( $article->getPage()->getContentModel() !== 'flow-board' ) {
+			return true;
+		}
+
+		$emptyContent = ContentHandler::getForModelID( 'flow-board' )->makeEmptyContent();
+
+		$parserOutput = $emptyContent->getParserOutput( $article->getTitle() );
+		$article->getContext()->getOutput()->addParserOutput( $parserOutput );
+
+		return false;
+	}
+
+	static function onFetchContentObject( Article &$article, \Content &$contentObject ) {
+		$occupationController = Container::get( 'occupation_controller' );
+		$title = $article->getTitle();
+
+		if ( $occupationController->isTalkpageOccupied( $title ) ) {
+			$loader = Container::get('factory.loader.workflow')
+				->createWorkflowLoader( $title );
+
+			$newRev = $occupationController->ensureFlowRevision( $article, $loader->getWorkflow() );
+
+			$article->mRevision = $newRev;
+			$article->mContentObject = $newRev->getContent();
+			$contentObject = $newRev->getContent();
+		}
+
+		return true;
+	}
+}
