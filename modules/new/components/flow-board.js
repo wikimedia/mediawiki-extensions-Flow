4de784b5fd657aca1fc56505e9c7107ac1f1c942
S Page <spage@wikimedia.org>
2014-05-01 17:26:32 -0700

    Use Mantle to load front-end handlebars resources
    WIP, depends on gerrit Ia978e6ec9 which adds functionality to Mantle name
    handlebars templates as ResourceLoader templates, and compile them.
    This duplicates similar functionality in flow-handlebars.js, which Flow
    uses for progressive enhancement.
    
    Change-Id: Ifd3a95e0140783ffd680d41f881f8f8a808577da

--- modules/new/components/flow-board.js
+++ modules/new/components/flow-board.js
@@ -47,6 +47,14 @@
 
 		// Restore the last state
 		this.HistoryEngine.restoreLastState();
+
+		// Sample templating using ResourceLoader.
+		mw.mantle.template.get( 'timestamp.html.handlebars' )
+			.render( {
+				time_iso: "1984-04-01 14:36",
+				time_readable: "back in the eighties"
+			} );
+
 	}
 
 	// Register this FlowComponent
