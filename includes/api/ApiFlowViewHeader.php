3a2c18a7bba721e7edf09990ed9e1df97f2a5c48
Matthias Mullie <git@mullie.eu>
2014-05-07 17:06:26 +0200

    Add ApiFlowViewHeader
    Adds a new API module, similar to the existing ones, to request a
    specific header.
    
    Change-Id: I8ba4c0d453d28b324153afb0210d3eed03e1562a

---
+++ includes/api/ApiFlowViewHeader.php
@@ -0,0 +1,46 @@
+<?php
+
+class ApiFlowViewHeader extends ApiFlowBaseGet {
+	public function __construct( $api, $modName ) {
+		parent::__construct( $api, $modName, 'vh' );
+	}
+
+	/**
+	 * Taken from ext.flow.base.js
+	 * @return array
+	 */
+	protected function getBlockNames() {
+		return array( 'header' );
+	}
+
+	protected function getAction() {
+		return 'header-view';
+	}
+
+	public function getAllowedParams() {
+		global $wgFlowContentFormat;
+
+		return array(
+			'contentFormat' => array(
+				ApiBase::PARAM_TYPE => array( 'html', 'wikitext' ),
+				ApiBase::PARAM_DFLT => $wgFlowContentFormat,
+			),
+		);
+	}
+
+	public function getParamDescription() {
+		return array(
+			'contentFormat' => 'Format to return the content in',
+		);
+	}
+
+	public function getDescription() {
+		return 'View a board header';
+	}
+
+	public function getExamples() {
+		return array(
+			'api.php?action=flow&submodule=header-view&vhcontentFormat=wikitext&workflow=',
+		);
+	}
+}
