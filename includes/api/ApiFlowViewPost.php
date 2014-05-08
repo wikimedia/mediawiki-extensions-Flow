c5d450c91cfc105447579aa8910096262e4f5be7
Matthias Mullie <git@mullie.eu>
2014-05-08 09:31:03 +0200

    Add ApiFlowViewPost
    Adds a new API module, similar to the existing ones, to request a
    specific post.
    
    Change-Id: Ia661db49e06e8b5eaa6fb8f43f56b44fc3a2feb0

---
+++ includes/api/ApiFlowViewPost.php
@@ -0,0 +1,54 @@
+<?php
+
+class ApiFlowViewPost extends ApiFlowBaseGet {
+	public function __construct( $api, $modName ) {
+		parent::__construct( $api, $modName, 'vp' );
+	}
+
+	/**
+	 * Taken from ext.flow.base.js
+	 *
+	 * @return array
+	 */
+	protected function getBlockNames() {
+		return array( 'topic' );
+	}
+
+	protected function getAction() {
+		return 'view';
+	}
+
+	public function getAllowedParams() {
+		return array(
+			'postId' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'contentFormat' => array(
+				ApiBase::PARAM_REQUIRED => true,
+			),
+			'no-children' => array(
+				ApiBase::PARAM_REQUIRED => false,
+				ApiBase::PARAM_TYPE => 'boolean',
+				ApiBase::PARAM_DFLT => false,
+			),
+		);
+	}
+
+	public function getParamDescription() {
+		return array(
+			'postId' => 'Id of the post to view',
+			'contentFormat' => 'Format to return the content in (html|wikitext)',
+			'no-children' => 'If set, this won\'t render replies to the requested post',
+		);
+	}
+
+	public function getDescription() {
+		return 'View a post';
+	}
+
+	public function getExamples() {
+		return array(
+			'api.php?action=flow&submodule=view&vppostId=???&vpcontentFormat=wikitext&workflow=',
+		);
+	}
+}
