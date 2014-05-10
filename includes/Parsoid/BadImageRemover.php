fb143b21c6eb8e77384370fd547542b604db5c26
Matthias Mullie <git@mullie.eu>
2014-05-10 14:34:16 +0200

    URLdecode attributes Parsoid feeds us
    Title class tries to build the object based on the DBkey we get from Parsoid's
    data. It looks like not everything in there is urldecoded, which makes
    Title::newFromDBkey fail (return null). We didn't check for the null case, which
    led to a fatal error.
    
    The exact value we were failing on is:
    File:Wolfgang_Amadeus_Mozart_-_Sinfonia_Concertante_für_Violine%2C_Viola_und_Orchester_-_3._Presto.ogg
    
    Bug: 65129
    Change-Id: Icfc989cc773f4dadeb8f24f1b8e2447af8b400d3
    (cherry picked from commit dc19f099162e269cbd78505f0367ac37ae0f75e3)

--- includes/Parsoid/BadImageRemover.php
+++ includes/Parsoid/BadImageRemover.php
@@ -49,8 +49,10 @@ class BadImageRemover implements ContentFixer {
 		$dom = Utils::createDOM( '<?xml encoding="utf-8"?>' . $content );
 		$self = $this;
 		self::forEachImage( $dom, function( DOMNode $linkNode, array $parsoid ) use ( $self, $dom, $title ) {
-			$image = Title::newFromDBkey( $parsoid['sa']['resource'], NS_FILE );
-			if ( wfIsBadImage( $image->getDBkey(), $title ) ) {
+			$image = Title::newFromDBkey( urldecode( $parsoid['sa']['resource'] ), NS_FILE );
+			if ( !$image ) {
+				wfDebugLog( 'Flow', __METHOD__ . ': Could not construct title for node: ' . $dom->saveXML( $linkNode ) );
+			} elseif ( wfIsBadImage( $image->getDBkey(), $title ) ) {
 				$linkNode->parentNode->removeChild( $linkNode );
 			}
 		} );
