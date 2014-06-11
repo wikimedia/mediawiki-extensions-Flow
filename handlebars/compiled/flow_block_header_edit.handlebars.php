48b796cc597153b128842ffde7288ace9b3e075f
Andrew Garrett <agarrett@wikimedia.org>
2014-06-11 18:19:18 +1000

    Implement AJAX preview on frontend-rewrite.
    Change-Id: I5ed43ea3c7a5be653356f818fbee3053fee34f89

--- handlebars/compiled/flow_block_header_edit.handlebars.php
+++ handlebars/compiled/flow_block_header_edit.handlebars.php
@@ -25,17 +25,18 @@
 		' : '').'
 
 		<form method="POST" action="'.htmlentities(((is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['url'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'" flow-api-action="edit-header">
+			<div class="flow-content-preview" style="display: none;"></div>
 			<input type="hidden" name="wpEditToken" value="'.htmlentities(((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['editToken'])) ? $cx['scopes'][0]['editToken'] : null), ENT_QUOTES, 'UTF-8').'" />
 			'.((LCRun3::ifvar($cx, ((is_array($in['revision']) && isset($in['revision']['revisionId'])) ? $in['revision']['revisionId'] : null))) ? '
 				<input type="hidden" name="header_prev_revision" value="'.htmlentities(((is_array($in['revision']) && isset($in['revision']['revisionId'])) ? $in['revision']['revisionId'] : null), ENT_QUOTES, 'UTF-8').'" />
 			' : '').'
-			<textarea name="header_content" class="mw-ui-input" placeholder="'.LCRun3::ch($cx, 'l10n', Array('flow-edit-header-placeholder'), 'encq').'">'.((LCRun3::ifvar($cx, ((is_array($in['submitted']) && isset($in['submitted']['content'])) ? $in['submitted']['content'] : null))) ? ''.htmlentities(((is_array($in['submitted']) && isset($in['submitted']['content'])) ? $in['submitted']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities(((is_array($in['revision']) && isset($in['revision']['content'])) ? $in['revision']['content'] : null), ENT_QUOTES, 'UTF-8').'').'</textarea>
+			<textarea name="header_content" class="mw-ui-input" placeholder="'.LCRun3::ch($cx, 'l10n', Array('flow-edit-header-placeholder'), 'encq').'" data-role="content">'.((LCRun3::ifvar($cx, ((is_array($in['submitted']) && isset($in['submitted']['content'])) ? $in['submitted']['content'] : null))) ? ''.htmlentities(((is_array($in['submitted']) && isset($in['submitted']['content'])) ? $in['submitted']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities(((is_array($in['revision']) && isset($in['revision']['content'])) ? $in['revision']['content'] : null), ENT_QUOTES, 'UTF-8').'').'</textarea>
 			<div class="flow-form-actions flow-form-collapsible">
 				<button data-role="submit"
 					class="flow-ui-button flow-ui-constructive"
 					data-flow-interactive-handler="apiRequest"
 					data-flow-api-handler="submitHeader">'.LCRun3::ch($cx, 'l10n', Array('flow-edit-header-submit'), 'encq').'</button>
-				<button data-role="action" class="flow-ui-button flow-ui-progressive flow-ui-quiet">'.LCRun3::ch($cx, 'l10n', Array('flow-preview'), 'encq').'</button>
+				<button data-flow-api-handler="preview" name="preview" data-role="action" class="flow-ui-button flow-ui-progressive flow-ui-quiet" >'.LCRun3::ch($cx, 'l10n', Array('flow-preview'), 'encq').'</button>
 				<button data-flow-interactive-handler="cancelForm"
 					data-role="cancel"
 					class="flow-ui-button flow-ui-destructive
