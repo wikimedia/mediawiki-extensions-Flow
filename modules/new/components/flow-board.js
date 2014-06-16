80e5903b0ac88b1534be8dc49b4e0418850711e7
Matthias Mullie <git@mullie.eu>
2014-06-16 16:48:48 +0200

    Edit post
    Change-Id: I5dcec17c30f913738c8ae481a11baebd36f44478

--- modules/new/components/flow-board.js
+++ modules/new/components/flow-board.js
@@ -182,6 +182,22 @@
 		 * Adapts the post form for submission to parsoid's API to get a preview, and changes the button to "Edit".
 		 * However, if already previewing, the handles the button's transition from "edit" back to preview.
 		 * @param event
+		 * Before activating post, sends an overrideObject to the API to modify the request params.
+		 * @param {Event} event
+		 * @return {Object}
+		 */
+		FlowBoardComponent.UI.events.apiPreHandlers.activateEditPost = function ( event ) {
+			return {
+				submodule: "post-view",
+				vppostId: $( this ).closest( '.flow-post' ).data( 'flow-id' ),
+				vpcontentFormat: "wikitext"
+			};
+		};
+
+		/**
+		 * Before handling preview, hides the old preview
+		 * and overrides the API request
+		 * @param  {Event} event The event being handled
 		 * @return {Function} Callback to modify the API request
 		 */
 		FlowBoardComponent.UI.events.apiPreHandlers.preview = function ( event ) {
@@ -406,6 +422,42 @@
 				alert( "Error" );
 			}
 		};
+
+		FlowBoardComponent.UI.events.apiHandlers.submitEditPost = function( status, data, jqxhr ) {
+
+			if ( !data || !data.flow || !data.flow['edit-post'] ) {
+				// @todo
+				alert( "Error" );
+				return;
+			}
+
+			var $rendered, html, revision, errors,
+				result = data.flow['edit-post'].result.topic;
+
+			if ( data.flow['edit-post'].status !== 'ok' ) {
+				// rejected by api submodule
+				errors = result;
+			} else if  ( result.errors.length ) {
+				// rejected by topic block
+				errors = result.errors;
+			}
+
+			if ( errors ) {
+				// validation problem
+				html = mw.flow.TemplateEngine.processTemplate( 'flow_errors', { errors: errors } );
+
+				// @todo this probably isn't supposed to be hardcoded
+				$( this ).closest( 'form' ).find( '.flow-errors' ).remove();
+				$( this ).closest( 'form' ).prepend( $( html ) );
+			} else {
+				// success
+				revision = result.revisions[result.posts[result.roots[0]]];
+				html = mw.flow.TemplateEngine.processTemplate( 'flow_post', { revision: revision } );
+
+				$( this ).closest( 'form' ).replaceWith( $( html ).find( '.flow-post-main' ) );
+			}
+		};
+
 		/**
 		 * Triggers a preview of the given content.
 		 * @param {Object} info (status:done|fail, $target: jQuery)
@@ -611,6 +663,51 @@
 			if ( info.status === 'done' ) {
 				// There is no template to render
 				$node.html( data.flow[ 'edit-topic-summary' ].result.topicsummary.revision.content );
+			} else {
+				// @todo fail
+				alert('fail');
+			}
+		};
+
+		/**
+		 * Renders the editable post with the given API response.
+		 * @param {Object} info (status:done|fail, $target: jQuery)
+		 * @param {Object} data
+		 * @param {jqXHR} jqxhr
+		 */
+		FlowBoardComponent.UI.events.apiHandlers.activateEditPost = function ( info, data, jqxhr ) {
+			var flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
+				$post = $( this ).closest( '.flow-post-main' ),
+				$rendered;
+
+			if ( info.status === 'done' ) {
+				// Change "topic" to "topic_edit_post" so that it loads up flow_block_topic_edit_post
+				data.flow['post-view'].result.topic.type = 'topic_edit_post';
+
+				$rendered = $(
+					flowBoard.TemplateEngine.processTemplateGetFragment(
+						'flow_block_loop',
+						{ blocks: data.flow['post-view'].result }
+					)
+				).children();
+
+				// @todo: I'm rendering flow_block_topic_edit_post.handlebars to
+				// also render errors. It also wraps a div.flow-board around
+				// what I want, so I'll discard that parent. This should be
+				// cleaned up some day. Figure this out once we figured out how
+				// we'll handle errors (for one, those rendered errors won't be
+				// removed when the form is destroyed)
+				$rendered = $rendered.children();
+
+				// Set the cancel callback on this form so that it returns to the post
+				flowBoardComponentAddCancelCallback(
+					$rendered.find( 'form' ).addBack( 'form' ),
+					function () {
+						$rendered.replaceWith( $post );
+					}
+				);
+
+				$post.replaceWith( $rendered );
 			} else {
 				// @todo fail
 				alert('fail');
