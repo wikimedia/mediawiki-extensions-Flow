03e9a6f9a2321fae899c5ad720d50269dfc88df5
bsitu <bsitu@wikimedia.org>
2014-06-17 18:21:51 -0700

    WIP: Wire up close & open actions in JavaScript
    * Rename flow_block_topic_close to flow_topic_titlebar_close to be more
    semantically correct.
    * Updated API output to always use topic data for close/open
    is invoking this non-existing function when contentFormat is set)
    
    Change-Id: Iea295e667a3d10cf81b66a3d524f85e1b2db4673

--- handlebars/compiled/flow_block_topic.handlebars.php
+++ handlebars/compiled/flow_block_topic.handlebars.php
@@ -42,9 +42,9 @@
 		').'
 	</div>
 	<div class="flow-topics">
-
+		
 		'.LCRun3::sec($cx, ((is_array($in) && isset($in['roots'])) ? $in['roots'] : null), $in, true, function($cx, $in) {return '
-
+	
 	'.LCRun3::hbch($cx, 'eachPost', Array($cx['scopes'][0],$in), $in, function($cx, $in) {return '
 		<div class="flow-topic flow-load-interactive '.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? 'flow-topic-moderated flow-topic-collapsed-invert' : '').'" id="flow-topic-'.htmlentities(((is_array($in) && isset($in['postId'])) ? $in['postId'] : null), ENT_QUOTES, 'UTF-8').'" data-flow-id="'.htmlentities(((is_array($in) && isset($in['postId'])) ? $in['postId'] : null), ENT_QUOTES, 'UTF-8').'" data-flow-load-handler="topicElement">
 	<div class="flow-topic '.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? 'flow-load-interactive' : '').' '.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? 'flow-topic-moderated flow-topic-collapsed-invert' : '').'" id="flow-topic-'.htmlentities(((is_array($in) && isset($in['postId'])) ? $in['postId'] : null), ENT_QUOTES, 'UTF-8').'" data-flow-id="'.htmlentities(((is_array($in) && isset($in['postId'])) ? $in['postId'] : null), ENT_QUOTES, 'UTF-8').'" data-flow-load-handler="topicElement">
@@ -70,6 +70,59 @@
 		'.LCRun3::ch($cx, 'escapeContent', Array(((is_array($in) && isset($in['summaryFormat'])) ? $in['summaryFormat'] : null),((is_array($in) && isset($in['summary'])) ? $in['summary'] : null)), 'encq').'
 	' : '').'
 </div>
+<div class="flow-topic-edit-summary">
+	'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isTopicSummaryEditable'])) ? $in['isTopicSummaryEditable'] : null))) ? '
+<div class="flow-board-header">
+	'.((LCRun3::ifvar($cx, ((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['errors'])) ? $cx['scopes'][0]['errors'] : null))) ? '
+		<ul>
+			'.LCRun3::sec($cx, ((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['errors'])) ? $cx['scopes'][0]['errors'] : null), $in, true, function($cx, $in) {return '
+				<li>'.htmlentities(((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['message'])) ? $cx['scopes'][0]['message'] : null), ENT_QUOTES, 'UTF-8').'</li>
+			';}).'
+		</ul>
+	' : '').'
+	<form class="flow-edit-form" data-flow-initial-state="collapsed" method="POST"
+	      action="'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? ''.htmlentities(((is_array($in['actions']['reopen']) && isset($in['actions']['reopen']['url'])) ? $in['actions']['reopen']['url'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities(((is_array($in['actions']['close']) && isset($in['actions']['close']['url'])) ? $in['actions']['close']['url'] : null), ENT_QUOTES, 'UTF-8').'').'">
+		<input type="hidden" name="wpEditToken" value="'.htmlentities(((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['editToken'])) ? $cx['scopes'][0]['editToken'] : null), ENT_QUOTES, 'UTF-8').'" />
+		'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['summaryRevId'])) ? $in['summaryRevId'] : null))) ? '
+			<input type="hidden" name="flow_prev_revision" value="'.htmlentities(((is_array($in) && isset($in['summaryRevId'])) ? $in['summaryRevId'] : null), ENT_QUOTES, 'UTF-8').'" />
+		' : '').'
+		<textarea name="flow_summary" data-flow-expandable="true" class="mw-ui-input" type="text">'.((LCRun3::ifvar($cx, ((is_array($cx['scopes'][0]['submitted']) && isset($cx['scopes'][0]['submitted']['content'])) ? $cx['scopes'][0]['submitted']['content'] : null))) ? ''.htmlentities(((is_array($cx['scopes'][0]['submitted']) && isset($cx['scopes'][0]['submitted']['content'])) ? $cx['scopes'][0]['submitted']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['summary'])) ? $in['summary'] : null))) ? ''.htmlentities(((is_array($in) && isset($in['summary'])) ? $in['summary'] : null), ENT_QUOTES, 'UTF-8').'' : '').'').'</textarea>
+		<div class="flow-form-actions flow-form-collapsible">
+			<button
+				data-role="submit"
+				class="flow-ui-button flow-ui-constructive"
+				data-flow-interactive-handler="apiRequest"
+				data-flow-api-target="< .flow-topic-titlebar"
+				data-flow-api-handler="closeOpenTopic">
+					'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? '
+						'.LCRun3::ch($cx, 'l10n', Array('flow-topic-action-reopen-topic'), 'encq').'
+					' : '
+						'.LCRun3::ch($cx, 'l10n', Array('flow-topic-action-close-topic'), 'encq').'
+					').'
+			</button>
+			<button data-role="action" class="flow-ui-button flow-ui-progressive flow-ui-quiet">'.LCRun3::ch($cx, 'l10n', Array('flow-preview'), 'encq').'</button>
+			<a
+				href="'.htmlentities(((is_array($in['links']['topic']) && isset($in['links']['topic']['url'])) ? $in['links']['topic']['url'] : null), ENT_QUOTES, 'UTF-8').'"
+				title="'.htmlentities(((is_array($in['links']['topic']) && isset($in['links']['topic']['title'])) ? $in['links']['topic']['title'] : null), ENT_QUOTES, 'UTF-8').'"
+				data-flow-interactive-handler="cancelForm"
+				data-role="cancel"
+				class="flow-ui-button flow-ui-destructive flow-ui-quiet">
+					'.LCRun3::ch($cx, 'l10n', Array('flow-cancel'), 'encq').'
+			</a>
+			<small class="flow-terms-of-use plainlinks">
+				'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? '
+					'.LCRun3::ch($cx, 'l10n', Array('flow-terms-of-use-reopen-topic'), 'encq').'
+				' : '
+					'.LCRun3::ch($cx, 'l10n', Array('flow-terms-of-use-close-topic'), 'encq').'
+				').'
+			</small>
+		</div>
+	</form>
+</div>
+' : '').'
+
+</div>
+
 
 		'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
 			<div class="flow-menu">
@@ -180,6 +233,9 @@
 					'.((LCRun3::ifvar($cx, ((is_array($in['actions']) && isset($in['actions']['close'])) ? $in['actions']['close'] : null))) ? '
 						<li>
 							<a class="flow-ui-button flow-ui-destructive flow-ui-quiet flow-ui-thin"
+							   data-flow-interactive-handler="apiRequest"
+							   data-flow-api-handler="activateCloseOpenTopic"
+							   data-flow-api-target="< .flow-topic-titlebar"
 							   href="'.htmlentities(((is_array($in['actions']['close']) && isset($in['actions']['close']['url'])) ? $in['actions']['close']['url'] : null), ENT_QUOTES, 'UTF-8').'"
 							   title="'.htmlentities(((is_array($in['actions']['close']) && isset($in['actions']['close']['title'])) ? $in['actions']['close']['title'] : null), ENT_QUOTES, 'UTF-8').'">
 								<span class="wikiglyph wikiglyph-clock"></span>
@@ -190,6 +246,9 @@
 					'.((LCRun3::ifvar($cx, ((is_array($in['actions']) && isset($in['actions']['reopen'])) ? $in['actions']['reopen'] : null))) ? '
 						<li>
 							<a class="flow-ui-button flow-ui-destructive flow-ui-quiet flow-ui-thin"
+							   data-flow-interactive-handler="apiRequest"
+							   data-flow-api-handler="activateCloseOpenTopic"
+							   data-flow-api-target="< .flow-topic-titlebar"
 							   href="'.htmlentities(((is_array($in['actions']['reopen']) && isset($in['actions']['reopen']['url'])) ? $in['actions']['reopen']['url'] : null), ENT_QUOTES, 'UTF-8').'"
 							   title="'.htmlentities(((is_array($in['actions']['reopen']) && isset($in['actions']['reopen']['title'])) ? $in['actions']['reopen']['title'] : null), ENT_QUOTES, 'UTF-8').'">
 								<span class="wikiglyph wikiglyph-clock"></span>
@@ -218,6 +277,7 @@
 		' : '').'
 	</div>
 </div>
+
 
 	'.LCRun3::sec($cx, ((is_array($in) && isset($in['replies'])) ? $in['replies'] : null), $in, true, function($cx, $in) {return '
 		'.LCRun3::hbch($cx, 'eachPost', Array($cx['scopes'][0],$in), $in, function($cx, $in) {return '
