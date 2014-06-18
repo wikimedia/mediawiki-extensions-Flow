c5b0828804305b03d7ab10291a95b34abaf703d8
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-06-18 09:40:12 -0700

    recompile handlebars templates
    Changes all stem from flow_anon_warning.handlebars, looks like
    recompile when changing a <span> to a <div>
    
    Change-Id: Iee54fb56caecaafd16ae94f665b0410b261919ca

--- handlebars/compiled/flow_block_topic_edit_post.handlebars.php
+++ handlebars/compiled/flow_block_topic_edit_post.handlebars.php
@@ -36,10 +36,11 @@
 	
 	<input type="hidden" name="topic_prev_revision" value="'.htmlentities(((is_array($in) && isset($in['revisionId'])) ? $in['revisionId'] : null), ENT_QUOTES, 'UTF-8').'" />
 	'.LCRun3::hbch($cx, 'ifAnonymous', Array(), $in, function($cx, $in) {return '
-			<span class="flow-anon-warning flow-ui-tooltip flow-ui-progressive flow-ui-tooltip-down flow-form-collapsible plainlinks">
-	'.LCRun3::ch($cx, 'l10nParse', Array('flow-anon-warning',LCRun3::ch($cx, 'linkWithReturnTo', Array('Special:Userlogin'), 'encq'),LCRun3::ch($cx, 'linkWithReturnTo', Array('Special:Userlogin/signup'), 'encq')), 'encq').'
+		<div class="flow-anon-warning flow-ui-tooltip flow-ui-progressive flow-ui-tooltip-down flow-form-collapsible plainlinks">
+	'.LCRun3::ch($cx, 'l10nParse', Array('flow-anon-warning',LCRun3::ch($cx, 'linkWithReturnTo', Array('Special:UserLogin'), 'encq'),LCRun3::ch($cx, 'linkWithReturnTo', Array('Special:UserLogin/signup'), 'encq')), 'encq').'
 	<span class="flow-ui-tooltip-triangle"></span>
-</span>
+</div>
+
 	';}).'
 	<textarea name="topic_content" class="mw-ui-input flow-form-collapsible">'.((LCRun3::ifvar($cx, ((is_array($cx['scopes'][0]['submitted']) && isset($cx['scopes'][0]['submitted']['content'])) ? $cx['scopes'][0]['submitted']['content'] : null))) ? ''.htmlentities(((is_array($cx['scopes'][0]['submitted']) && isset($cx['scopes'][0]['submitted']['content'])) ? $cx['scopes'][0]['submitted']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities(((is_array($in) && isset($in['content'])) ? $in['content'] : null), ENT_QUOTES, 'UTF-8').'').'</textarea>
 	<div class="flow-form-actions flow-form-collapsible">
