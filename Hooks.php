38c493ed8e42a2ba35276ec5c63e4c0eebaf819f
Shahyar <shahyar@gmail.com>
2014-06-19 18:32:06 -0400

    Fix preview
    Changes the preview mechanism to render an almost-exact preview of how
     the content will look in its own template, rather than using a generic
     yellow preview box like before (eg. use flow_post to preview a post)
    
    Also adds tooltip helper, flow-ui-tooltip-block class
    Cleans up anon warning to use tooltip helper
    Adds preview warning string
    
    Change-Id: Ibee2698f410db1d4e0113e706ba51376aadbbbc6

--- Hooks.php
+++ Hooks.php
@@ -430,7 +430,7 @@ class FlowHooks {
 	}
 
 	public static function onResourceLoaderGetConfigVars( &$vars ) {
-		global $wgFlowEditorList, $wgFlowDefaultLimit, $wgFlowMaxLimit;
+		global $wgFlowEditorList, $wgFlowDefaultLimit, $wgFlowMaxLimit, $wgUser;
 
 		$vars['wgFlowEditorList'] = $wgFlowEditorList;
 		$vars['wgFlowMaxTopicLength'] = Flow\Model\PostRevision::MAX_TOPIC_LENGTH;
