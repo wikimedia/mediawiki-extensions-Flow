7db3ba0a1b340c21e5f6bccf4cf438cb49f531e4
Shahyar <shahyar@gmail.com>
2014-06-19 18:32:06 -0400

    Fix preview
    Changes the preview mechanism to render an almost-exact preview of how
     the content will look in its own template, rather than using a generic
     yellow preview box like before (eg. use flow_post to preview a post)
    
    Change-Id: Ibee2698f410db1d4e0113e706ba51376aadbbbc6

--- handlebars/compiled/flow_block_header.handlebars.php
+++ handlebars/compiled/flow_block_header.handlebars.php
@@ -23,18 +23,20 @@
 			<p>'.LCRun3::ch($cx, 'l10n', Array('flow-header-empty'), 'encq').'</p>
 		').'
 
-		<div class="flow-board-header-nav">
-			'.((LCRun3::ifvar($cx, ((is_array($in['revision']['actions']) && isset($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit'] : null))) ? '
-				<a href="'.htmlentities(((is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['url'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'"
-					data-flow-api-handler="activateEditHeader"
-					data-flow-api-target="< .flow-board-header"
-					data-flow-interactive-handler="apiRequest"
-					class="flow-ui-button flow-ui-progressive flow-ui-quiet flow-board-header-icon flow-ui-tooltip-target"
-					title="'.htmlentities(((is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['title'])) ? $in['revision']['actions']['edit']['title'] : null), ENT_QUOTES, 'UTF-8').'">
-						<span class="wikiglyph wikiglyph-pencil"></span>
-				</a>
-			' : '').'
-		</div>
+		'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
+			<div class="flow-board-header-nav">
+				'.((LCRun3::ifvar($cx, ((is_array($in['revision']['actions']) && isset($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit'] : null))) ? '
+					<a href="'.htmlentities(((is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['url'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'"
+						data-flow-api-handler="activateEditHeader"
+						data-flow-api-target="< .flow-board-header"
+						data-flow-interactive-handler="apiRequest"
+						class="flow-ui-button flow-ui-progressive flow-ui-quiet flow-board-header-icon flow-ui-tooltip-target"
+						title="'.htmlentities(((is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['title'])) ? $in['revision']['actions']['edit']['title'] : null), ENT_QUOTES, 'UTF-8').'">
+							<span class="wikiglyph wikiglyph-pencil"></span>
+					</a>
+				' : '').'
+			</div>
+		' : '').'
 	</div>
 </div>
 ';
