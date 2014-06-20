e041bfb6961f50732819920e7fbf68b7995fe413
jdlrobson <jdlrobson@gmail.com>
2014-06-20 11:25:12 -0700

    QA: Make flow_anon and flow_logged_in tests pass.
    Change-Id: I77d81ddfe213028a158048c33691aebcd04dbf4b

--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -22,40 +22,66 @@ class FlowPage
   textarea(:edit_header_textbox, css: ".flow-board-header-edit-view textarea")
 
   a(:actions_link_permalink_3rd_comment, text: "Actions", index: 4)
-  span(:author_link, class: "flow-creator")
-  a(:block_user, title: /Special:Block/)
+  a(:author_link, css: ".flow-author a")
   a(:cancel_button, text: "Cancel")
   button(:change_post_save, css: "form.flow-edit-form .flow-edit-submit")
   button(:change_title_save, css: "form.flow-edit-title-form .flow-edit-submit")
   textarea(:comment_field, css: 'form.flow-topic-reply-form > textarea[name="topic_content"]')
   button(:comment_reply_save, css: "form.flow-topic-reply-form .flow-reply-submit")
-  a(:contrib_link, text: "contribs")
   a(:edit_post, class: "flow-edit-post-link", index: topic_index)
   a(:edit_title_icon, css: "div.tipsy-inner > div.flow-tipsy-flyout > ul > li.flow-action-edit-title > a.mw-ui-button.flow-edit-topic-link")
-  div(:flow_body, class: "flow-container")
   div(:flow_topics, class: "flow-topics")
   div(:highlighted_comment, class: "flow-post-highlighted")
+  
+  # Posts
+  ## Top post
+  h2(:flow_first_topic_heading, css: ".flow-topic h2", index: 0)
+  div(:flow_first_topic_body, css: ".flow-post-content", index: 0)
+
+  ### Hover over username behaviour
+  a(:talk_link, css: ".mw-usertoollinks a", index: 0)
+  a(:block_user, css: ".mw-usertoollinks a", index: 1)
 
   # Collapse button
   a(:full_view, href: "#collapser/full")
   a(:small_view, href: "#collapser/compact")
   a(:collapsed_view, href: "#collapser/topics")
 
-  # Buttons in a fly-out menu.
-  button(:delete_button,         css: "div.tipsy-inner input.flow-delete-post-link")
-  button(:hide_button,           css: "div.tipsy-inner input.flow-hide-post-link")
-  button(:suppress_button,       css: "div.tipsy-inner input.flow-suppress-post-link")
-  button(:topic_delete_button,   css: "div.tipsy-inner input.flow-delete-topic-link")
-  button(:topic_hide_button,     css: "div.tipsy-inner input.flow-hide-topic-link")
-  button(:topic_suppress_button, css: "div.tipsy-inner input.flow-suppress-topic-link")
+  # Topic actions menu
+  a(:topic_actions_link, css: ".flow-topic .flow-topic-titlebar .flow-menu-js-drop a", index: 0)
+  ## Menu
+  ul(:topic_actions_menu, css: ".flow-topic .flow-topic-titlebar .flow-menu ul", index: 0)
+  a(:topic_delete_button) do |page|
+    page.topic_actions_menu_element.link_element(title: "Hide topic")
+  end
+  a(:topic_hide_button) do |page|
+    page.topic_actions_menu_element.link_element(title: "Delete topic")
+  end
+  a(:topic_suppress_button) do |page|
+    page.topic_actions_menu_element.link_element(title: "Suppress topic")
+  end
+
+  # Post actions menu
+  a(:post_actions_link, css: ".flow-topic .flow-post .flow-menu-js-drop a", index: 0)
+  ## Menu
+  ul(:post_actions_menu, css: ".flow-topic .flow-post .flow-menu ul", index: 0)
+  a(:hide_button) do |page|
+    page.post_actions_menu_element.link_element(title: "Hide")
+  end
+  a(:delete_button) do |page|
+    page.post_actions_menu_element.link_element(title: "Delete")
+  end
+  a(:suppress_button) do |page|
+    page.post_actions_menu_element.link_element(title: "Suppress")
+  end
 
   # New topic creation
   form(:new_topic_form, css: ".flow-newtopic-form")
-  text_area(:new_topic_body, class: "flow-newtopic-content")
+  text_field(:new_topic_title, name: "topiclist_topic")
+  textarea(:new_topic_body, name: "topiclist_content")
   button(:new_topic_cancel, css: ".flow-newtopic-form .flow-ui-destructive")
   button(:new_topic_preview, css: ".flow-newtopic-form .flow-ui-progressive")
   button(:new_topic_save, css: ".flow-newtopic-form .flow-ui-constructive")
-  text_field(:new_topic_title, name: "topiclist_topic")
   a(:permalink, css: "div.tipsy-inner > div.flow-tipsy-flyout > ul > li.flow-action-permalink > a.mw-ui-button.flow-action-permalink-link")
 
   # Replying
@@ -64,18 +90,10 @@ class FlowPage
   button(:new_reply_preview, css: ".flow-reply-form .flow-ui-progressive")
   button(:new_reply_save, css: ".flow-reply-form .flow-ui-constructive")
 
-  # Find Actions link within a particular class.
-  # With jQuery the selector is simply $( '.flow-post-container
-  # a:contains("Actions")' ) but in page-object, there are no good solutions.
-  # There's https://github.com/cheezy/page-object/wiki/Nested-Elements but it
-  # makes little sense.
-  a(:post_actions_link, xpath: "//div[contains(concat(' ', normalize-space(@class), ' '), ' flow-post-container ')]//a[text()='Actions']", index: 1)
   text_area(:post_edit, css: "form.flow-edit-form .flow-edit-content")
   button(:preview_button, class: "mw-ui-button flow-preview-submit")
   div(:small_spinner, class: "mw-spinner mw-spinner-small mw-spinner-inline")
-  a(:talk_link, text: "Talk")
   text_field(:title_edit, css: "form.flow-edit-title-form .flow-edit-content")
-  a(:topic_actions_link, xpath: "//div[contains(concat(' ', normalize-space(@class), ' '), ' flow-topic-container ')]//a[text()='Actions']", index: actions_index)
   div(:topic_post, class: "flow-post-content", index: topic_index)
   div(:topic_title, class: "flow-topic-title", index: topic_index)
 
