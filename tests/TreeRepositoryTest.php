f027411416c85cb4612f1d88fbb8813411000760
Matthias Mullie <git@mullie.eu>
2014-06-20 21:00:14 +0200

    Hygiene: Get rid of unnecessary fully qualified namespaced classnames
    Change-Id: Ibf16661b1cfff9622158080b91359d88a940b67c

--- tests/TreeRepositoryTest.php
+++ tests/TreeRepositoryTest.php
@@ -3,6 +3,7 @@
 namespace Flow\Tests;
 
 use Flow\Model\UUID;
+use Flow\Repository\TreeRepository;
 use ReflectionClass;
 
 /**
@@ -21,7 +22,7 @@ class TreeRepositoryTest extends FlowTestCase {
 
 	public function testSuccessfulInsert() {
 		$cache = new \HashBagOStuff();
-		$treeRepository = new \Flow\Repository\TreeRepository( $this->mockDbFactory( true ), $cache );
+		$treeRepository = new TreeRepository( $this->mockDbFactory( true ), $cache );
 		$this->assertTrue( $treeRepository->insert( $this->descendant, $this->ancestor ) );
 
 		$reflection = new ReflectionClass( '\Flow\Repository\TreeRepository' );
@@ -41,7 +42,7 @@ class TreeRepositoryTest extends FlowTestCase {
 		// otherwise the exception would skip the cache result test
 		$cache = new \HashBagOStuff();
 		try {
-			$treeRepository = new \Flow\Repository\TreeRepository( $this->mockDbFactory( false ), $cache );
+			$treeRepository = new TreeRepository( $this->mockDbFactory( false ), $cache );
 			$this->assertNull( $treeRepository->insert( $this->descendant, $this->ancestor ) );
 		} catch ( \Exception $e ) {
 			$reflection = new ReflectionClass( '\Flow\Repository\TreeRepository' );
