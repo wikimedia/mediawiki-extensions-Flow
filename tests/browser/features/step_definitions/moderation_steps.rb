7a4c4cca9227d328c8c55dd4837c74905ebac87a
jdlrobson <jdlrobson@gmail.com>
2014-06-24 11:59:37 -0700

    QA: Add tests for suppression
    Change-Id: Ic58bbdb46d0b4b8597b9871965a2d060508f813c

--- tests/browser/features/step_definitions/moderation_steps.rb
+++ tests/browser/features/step_definitions/moderation_steps.rb
@@ -21,3 +21,17 @@ Then(/^the top post should be marked as deleted$/) do
   step 'the page renders in 5 seconds'
   on(FlowPage).flow_first_topic_moderation_msg.should match( 'This topic was deleted' )
 end
+
+When(/^I give reason for suppression as being "(.*?)"$/) do |arg1|
+  on(FlowPage).dialog_input_element.when_present.send_keys( arg1 )
+end
+
+When(/^I click Suppress topic$/) do
+  on(FlowPage).dialog_submit_element.when_present.click
+end
+
+Then(/^the top post should be marked as suppressed$/) do
+  step 'the page renders in 5 seconds'
+  on(FlowPage).flow_first_topic_moderation_msg.should match( 'This topic was suppressed' )
+end
+
