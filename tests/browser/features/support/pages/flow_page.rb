56e7a01a529f68179692dac7439f0a118a40e30a
jdlrobson <jdlrobson@gmail.com>
2014-06-23 18:10:44 -0700

    QA: Fix the action menu permalink tests
    Note the degrading in functionality - reply is broken
    
    Add a new test for permalink for topic.
    Update template so that it removes whitespace (When selecting
    elements in the browser tests ' Permalink ' is different from 'Permalink'
    so the test will fail without this change. May want to revisit other
    template elements in future)
    
    Change-Id: Ic0d972df738c3ca7858a33c136ee776ac8782e64

--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -11,13 +11,11 @@ class FlowPage
   form(:edit_header_form, css: ".flow-board-header-edit-view form")
   textarea(:edit_header_textbox, css: ".flow-board-header-edit-view textarea")
 
-  a(:actions_link_permalink_3rd_comment, text: "Actions", index: 4)
   a(:author_link, css: ".flow-author a")
   a(:cancel_button, text: "Cancel")
   textarea(:comment_field, css: 'form.flow-topic-reply-form > textarea[name="topic_content"]')
   button(:comment_reply_save, css: "form.flow-topic-reply-form .flow-reply-submit")
   div(:flow_topics, class: "flow-topics")
-  div(:highlighted_comment, class: "flow-post-highlighted")
 
   # Collapse button
   a(:full_view, href: "#collapser/full")
@@ -25,6 +23,9 @@ class FlowPage
   a(:collapsed_view, href: "#collapser/topics")
 
   # Posts
+  ## Highlighted post
+  div(:highlighted_post, css: ".flow-post-highlighted")
+
   ## First topic
   h2(:flow_first_topic_heading, css: ".flow-topic h2", index: 0)
   div(:flow_first_topic_body, css: ".flow-post-content", index: 0)
@@ -44,6 +45,9 @@ class FlowPage
   end
   a(:topic_suppress_button) do |page|
     page.topic_actions_menu_element.link_element(text: "Suppress topic")
+  end
+  a(:permalink_button) do |page|
+    page.topic_actions_menu_element.link_element(text: "Permalink")
   end
   a(:edit_title_button) do |page|
     page.topic_actions_menu_element.link_element(text: "Edit title")
@@ -71,6 +75,13 @@ class FlowPage
     page.post_actions_menu_element.link_element(title: "Suppress")
   end
 
+  ### Replies to top post
+  a(:third_post_actions_link, css: ".flow-topic .flow-post .flow-menu-js-drop a", index: 3)
+  ul(:third_post_actions_menu, css: ".flow-topic .flow-post .flow-menu ul", index: 3)
+  a(:actions_link_permalink_3rd_comment) do |page|
+    page.third_post_actions_menu_element.link_element(text: "Permalink")
+  end
+
   # New topic creation
   form(:new_topic_form, css: ".flow-newtopic-form")
   text_field(:new_topic_title, name: "topiclist_topic")
@@ -78,10 +89,11 @@ class FlowPage
   button(:new_topic_cancel, css: ".flow-newtopic-form .flow-ui-destructive")
   button(:new_topic_preview, css: ".flow-newtopic-form .flow-ui-progressive")
   button(:new_topic_save, css: ".flow-newtopic-form .flow-ui-constructive")
-  a(:permalink, css: "div.tipsy-inner > div.flow-tipsy-flyout > ul > li.flow-action-permalink > a.mw-ui-button.flow-action-permalink-link")
 
   # Replying
   form(:new_reply_form, css: ".flow-reply-form")
+  # Is an input when not focused, textarea when focused
+  text_field(:new_reply_input, css: ".flow-reply-form .mw-ui-input")
   button(:new_reply_cancel, css: ".flow-reply-form .flow-ui-destructive")
   button(:new_reply_preview, css: ".flow-reply-form .flow-ui-progressive")
   button(:new_reply_save, css: ".flow-reply-form .flow-ui-constructive")
