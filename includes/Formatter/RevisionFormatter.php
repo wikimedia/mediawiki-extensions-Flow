7cdf385ecdf404cffe59b402ec10e4aabd2f1538
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-06-25 15:50:59 -0700

    Dont even provide action links to blocked users
    Original patch: I6fbc714bc7cb32046cd5f79dd94b26dcf67e23fe
    
    Change-Id: Iaa0d74e9e8626f9309a47f36dadaf61f18358885

--- includes/Formatter/RevisionFormatter.php
+++ includes/Formatter/RevisionFormatter.php
@@ -305,9 +305,23 @@ class RevisionFormatter {
 	 */
 	public function buildActions( FormatterRow $row ) {
 		$section = new \ProfileSection( __METHOD__ );
+		$user = $this->permissions->getUser();
 		$workflow = $row->workflow;
-		$revision = $row->revision;
 		$title = $workflow->getArticleTitle();
+
+		// If a user is blocked from performing actions on this page return
+		// an empty array of actions.
+		//
+		// We only check actual users and not anon's because the anonymous
+		// version can be cached and served to many different ip addresses
+		// which will not all be blocked.
+		if ( !$user->isAnon() &&
+			( $user->isBlockedFrom( $title, true ) || !$title->quickUserCan( 'edit', $user ) )
+		) {
+			return array();
+		}
+
+		$revision = $row->revision;
 		$action = $revision->getChangeType();
 		$workflowId = $workflow->getId();
 		$revId = $revision->getRevisionId();
