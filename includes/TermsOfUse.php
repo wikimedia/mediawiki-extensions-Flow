79d5df1c121089710685abd863cc7b4b5e611f7b
bsitu <bsitu@wikimedia.org>
2014-06-25 16:36:53 -0700

    Re-implement terms-of-use
    Change-Id: I1432d0914d5415465a2c375140428407f1b80405

--- includes/TermsOfUse.php
+++ includes/TermsOfUse.php
@@ -2,124 +2,65 @@
 
 namespace Flow;
 
+use Flow\Exception\InvalidInputException;
+
 /**
  * A helper class that returns the terms of use to be shown when adding new
  * topics and replies and editing existing topics, replies and the board header
  */
 class TermsOfUse {
-	/**
-	 * @var string
-	 */
-	private static $addTopicTerms;
 
 	/**
-	 * @var string
+	 * List of terms of use with corresponding message keys
 	 */
-	private static $replyTerms;
-
-	/**
-	 * @var string
-	 */
-	private static $editTerms;
-
-	/**
-	 * @var string
-	 */
-	private static $summarizeTerms;
-
-	/**
-	 * @var string
-	 */
-	private static $closeTopicTerms;
-
-	/**
-	 * @var string
-	 */
-	private static $reopenTopicTerms;
+	private static $terms = array(
+		'new-topic' => 'flow-terms-of-use-new-topic',
+		'reply' => 'flow-terms-of-use-reply',
+		'edit' => 'flow-terms-of-use-edit',
+		'summarize' => 'flow-terms-of-use-summarize',
+		'close-topic' => 'flow-terms-of-use-close-topic',
+		'reopen-topic' => 'flow-terms-of-use-reopen-topic'
+	);
 
 	/**
 	 * Give a chance for other extensions to specify alternative message strings
 	 * and retrieve and parse the message strings
 	 */
-	private static function setTerms() {
-		$addTopicKey = 'flow-terms-of-use-new-topic';
-		$replyKey = 'flow-terms-of-use-reply';
-		$editKey = 'flow-terms-of-use-edit';
-		$summarizeKey = 'flow-terms-of-use-summarize';
-		$closeTopicKey = 'flow-terms-of-use-close-topic';
-		$reopenTopicKey = 'flow-terms-of-use-reopen-topic';
-
-		// This is getting out of control, maybe we should consider having
-		// one generic message for submitting any user form content, like:
-		// by submitting the content, you agree blah...
-		wfRunHooks( 'FlowTermsOfUse', array( &$addTopicKey, &$replyKey, &$editKey, &$summarizeKey, &$closeTopicKey, &$reopenTopicKey ) );
-
-		self::$addTopicTerms = wfMessage( $addTopicKey )->parse();
-		self::$replyTerms = wfMessage( $replyKey )->parse();
-		self::$editTerms = wfMessage( $editKey )->parse();
-		self::$summarizeTerms = wfMessage( $summarizeKey )->parse();
-		self::$closeTopicTerms = wfMessage( $closeTopicKey )->parse();
-		self::$reopenTopicTerms = wfMessage( $reopenTopicKey )->parse();
+	public static function setTerms() {
+		wfRunHooks(
+			'FlowTermsOfUse',
+			array(
+				&self::$terms['new-topic'],
+				&self::$terms['reply'],
+				&self::$terms['edit'],
+				&self::$terms['summarize'],
+				&self::$terms['close-topic'],
+				&self::$terms['reopen-topic']
+			)
+		);
 	}
 
 	/**
+	 * Get terms of use
+	 * @param string The key for the term of use
 	 * @return string
 	 */
-	public static function getAddTopicTerms() {
-		if ( self::$addTopicTerms === null ) {
-			self::setTerms();
+	public function getTerm( $name = '' ) {
+		if ( $name && !isset( self::$terms[$name] ) ) {
+			throw new InvalidInputException( 'Term ' . $name . ' does not exist', 'invalid-input' );
 		}
-		return self::$addTopicTerms;
-	}
 
-	/**
-	 * @return string
-	 */
-	public static function getReplyTerms() {
-		if ( self::$replyTerms === null ) {
+		static $runHook = false;
+		if ( !$runHook ) {
 			self::setTerms();
+			$runHook = true;
 		}
-		return self::$replyTerms;
-	}
 
-	/**
-	 * @return string
-	 */
-	public static function getEditTerms() {
-		if ( self::$editTerms === null ) {
-			self::setTerms();
+		if ( $name ) {
+			return self::$terms[$name];
+		} else {
+			return self::$terms;
 		}
-		return self::$editTerms;
-	}
-
-	/**
-	 * @return string
-	 */
-	public static function getSummarizeTerms() {
-		if ( self::$summarizeTerms === null ) {
-			self::setTerms();
-		}
-		return self::$summarizeTerms;
-	}
-
-	/**
-	 * @return string
-	 */
-	public static function getCloseTopicTerms() {
-		if ( self::$closeTopicTerms === null ) {
-			self::setTerms();
-		}
-		return self::$closeTopicTerms;
-	}
-
-	/**
-	 * @return string
-	 */
-	public static function getReopenTopicTerms() {
-		if ( self::$reopenTopicTerms === null ) {
-			self::setTerms();
-		}
-		return self::$reopenTopicTerms;
 	}
 
 }
