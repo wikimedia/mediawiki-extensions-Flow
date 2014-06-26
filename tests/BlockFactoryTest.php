33c1f9f711744ed166d817cff74b13e0354f8cd7
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-06-25 21:33:52 -0700

    Split WorkflowLoader into multiple classes
    This patch doesn't add any new code, it just spreads out the code from
    WorkflowLoader into the existing WorkflowLoaderFactory and two new
    classes BlockFactory and SubmissionHandler. The API's for 
    WorkflowLoader and WorkflowLoaderFactory are not changed so no other
    code requires update.
    
    While trying to figure out how to test blocked users going through
    WorkflowLoader I realized the loader does too many disparate things.
    
    This patch splits it into discrete pieces that better match SRP.  In
    addition previously WorkflowLoader was doing a fair bit of work in the
    constructor, going all the way to the database.  Constructors should
    have minimal setup so moved that into WorkflowLoaderFactory
    
    At this point WorkflowLoader is now just a shell, consider further
    refactors.
    
    Note that current intention is to change the forms to use the same
    parameters as API requests expect, and change the backend page rendering
    process such that it makes an internal API call and passes those api results
    to lightncandy.  At that point it may make sense to delete the WorkflowLoader
    and use the individual pieces within FlowApiBase or some such.
    
    This was started against frontend-rewrite branch, but is now against
    the master branch.
    Original gerrit change: I22a36a55e9b4704932e93d2f117a79fa1ea146a6
    
    Change-Id: Id38a93bed37fcf8b91b815121f7c0537f25a525e

---
+++ tests/BlockFactoryTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Flow\Tests;
+
+use Flow\BlockFactory;
+use Flow\Container;
+use ReflectionClass;
+
+/**
+ * @group Flow
+ */
+class BlockFactoryTest extends FlowTestCase {
+
+	public function provideDataCreateBlocks() {
+		return array (
+			array( 'discussion', array( 'Flow\Block\HeaderBlock', 'Flow\Block\TopicListBlock', 'Flow\Block\BoardHistoryBlock' ) ),
+			array( 'topic', array( 'Flow\Block\TopicBlock', 'Flow\Block\TopicSummaryBlock' ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provideDataCreateBlocks
+	 */
+	public function testCreateBlocks( $definitionType, $expectedResults ) {
+		$factory = $this->createBlockFactory();
+		list( $definition, $workflow ) = $this->mockWorkflow( $definitionType );
+
+		$blocks = $factory->createBlocks( $definition, $workflow );
+		$this->assertEquals( count( $blocks ), count( $expectedResults ) );
+
+		$results = array();
+		foreach ( $blocks as $obj ) {
+			$results[] = get_class( $obj );
+		}
+		$this->assertEquals( $results, $expectedResults );
+	}
+
+	/**
+	 * @expectedException \Flow\Exception\InvalidInputException
+	 */
+	public function testCreateBlocksWithInvalidInputException() {
+		$factory = $this->createBlockFactory();
+		list( $definition, $workflow ) = $this->mockWorkflow( 'a-bad-database-flow-definition' );
+		// Trigger InvalidInputException
+		$factory->createBlocks( $definition, $workflow );
+	}
+
+	protected function createBlockFactory() {
+		global $wgLang;
+
+		$storage = $this->getMockBuilder( '\Flow\Data\ManagerGroup' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		// phpunit mocker fails to generate the correct method definition for
+		// NotificationController::getDefaultNotifiedUsers, just use the real one
+		$notificationController = new \Flow\NotificationController( $wgLang );
+
+		$rootPostLoader = $this->getMockBuilder( '\Flow\Data\RootPostLoader' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		return new BlockFactory( $storage, $notificationController, $rootPostLoader );
+	}
+
+	protected function mockWorkflow( $type ) {
+		$definition = $this->getMockBuilder( '\Flow\Model\Definition' )
+			->disableOriginalConstructor()
+			->getMock();
+		$definition->expects( $this->any() )
+			->method( 'getType' )
+			->will( $this->returnValue( $type ) );
+
+		$workflow = $this->getMockBuilder( '\Flow\Model\Workflow' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		return array( $definition, $workflow );
+	}
+}
