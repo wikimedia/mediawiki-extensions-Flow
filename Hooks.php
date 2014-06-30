b130b09219312004d4a6d2dd0b906c4dd6b837b6
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-06-30 15:56:24 -0700

    Enable Topic namespace
    Topics now pretend to be inside the NS_TOPIC namespace.  Because they
    are only pretending there are several special cases to handle either
    by checking if $title->getNamespace() === NS_TOPIC or when
    $workflow->getType() === 'topic'.  This was done rather than moving
    the workflows to a new namespace/titleText combo to maintain the information
    about where a workflow was originaly created, required as part of the
    page breadcrumbs. The alternative method of database changes seemed
    overbearing.
    
    DONE
    * Topic namespace created
    * Minor hack to remove skin links on Topic namespace
    * Removed duplicated workflow parameter from all topic urls
    * introduced Workflow::getOwnerTitle representing page workflow was created at
    
    TODO
    * Remove line underneath empty page title
    
    Change-Id: I98d45f053e0892e2b1f3433ce57f8ed084853103

--- Hooks.php
+++ Hooks.php
@@ -31,8 +31,12 @@ class FlowHooks {
 			global $wgFlowOccupyNamespaces,
 				$wgFlowOccupyPages;
 
+			// NS_TOPIC is always occupied
+			$namespaces = $wgFlowOccupyNamespaces;
+			$namespaces[] = NS_TOPIC;
+
 			self::$occupationController = new TalkpageManager(
-				$wgFlowOccupyNamespaces,
+				array_unique( $namespaces ),
 				$wgFlowOccupyPages
 			);
 		}
@@ -293,6 +297,15 @@ class FlowHooks {
 				if ( !in_array( $action, $wgFlowCoreActionWhitelist ) ) {
 					unset( $links['actions'][$action] );
 				}
+			}
+
+			if ( isset( $links['namespaces']['topic_talk'] ) ) {
+				// hide discussion page in Topic namespace(which is already discussion)
+				unset( $links['namespaces']['topic_talk'] );
+				// hide protection (topic protection is done via moderation)
+				unset( $links['actions']['protect'] );
+				// topic pages are also not movable
+				unset( $links['actions']['move'] );
 			}
 		}
 
