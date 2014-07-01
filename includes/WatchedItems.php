c6426cc23d3fce2c2bd685cc852f6c41be4be2db
bsitu <bsitu@wikimedia.org>
2014-07-01 14:58:16 -0700

    [WIP]Flow topic subscription
    This is in a very draft mode, don't review
    
    watch/unwatch is handled by core watch api. no-js support
    is handled by core action=watch, action=unwatch
    
    Major components missing:
    
    * Tooltip
    
    * Flow topic namespace integration
    
    * Auto subscription
    
    Issue:
    
    * Adding Topic namespace ( eg 520 ) to watchlist table, it automatically
      adds 521 for the same title
    
    * Upon watch/unwatch, need to change the link to action=unwatch/action=watch
    
    Change-Id: Ibf00d44c74a0859053f1aad4f2a962eecc2e85db

---
+++ includes/WatchedItems.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Flow;
+
+use Flow\DbFactory;
+use Flow\Exception\DataModelException;
+use User;
+
+/**
+ * Is there a core object for retriving multiple watchlist items?
+ */
+class WatchedItems {
+
+	protected $user;
+	protected $dbFactory;
+
+	public function __construct( User $user, DbFactory $dbFactory ) {
+		$this->user = $user;
+		$this->dbFactory = $dbFactory;
+	}
+
+	/**
+	 * @param string[] array of UUID string
+	 */
+	public function getWatchStatus( array $titles ) {
+		$result = array_fill_keys( $titles, false );
+
+		if ( !$titles ) {
+			return $result;
+		}
+		if ( !$this->user->getId() ) {
+			return $result;
+		}
+
+		$dbr = $this->dbFactory->getDB( DB_SLAVE );
+		$res = $dbr->select(
+			array( 'watchlist' ),
+			array( 'wl_title' ),
+			array(
+				'wl_user' => $this->user->getId(),
+				'wl_namespace' => NS_TOPIC,
+				'wl_title' => array_map( 'ucfirst', $titles )
+			),
+			__METHOD__
+		);
+		if ( !$res ) {
+			throw new DataModelException( 'query failure', 'process-data' );
+		}
+		foreach ( $res as $row ) {
+			$result[strtolower( $row->wl_title )] = true;
+		}
+		return $result;
+	}
+}
