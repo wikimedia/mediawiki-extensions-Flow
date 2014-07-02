36ba143389b1c25d63a9505899b56538819ca11f
Željko Filipin <zeljko.filipin@gmail.com>
2014-07-02 15:12:46 +0200

    Fixed "no implicit conversion of nil into String (TypeError)"
    This happens if MEDIAWIKI_USER environment variable is not set:
    
    $ bundle exec cucumber
    no implicit conversion of nil into String (TypeError)
    …tests/browser/features/support/pages/user_page.rb:6:in `+'
    …
    
    Change-Id: Id76b4df32f85fa30acacc7641a06eb301f2e9331

--- tests/browser/features/support/pages/user_page.rb
+++ tests/browser/features/support/pages/user_page.rb
@@ -3,7 +3,7 @@ class UserPage
 
   include URL
   # MEDIAWIKI_URL must have this in $wgFlowOccupyPages array or $wgFlowOccupyNamespaces.
-  page_url URL.url("User talk:" + ENV["MEDIAWIKI_USER"])
+  page_url URL.url("User talk:ENV['MEDIAWIKI_USER']")
 
   h1(:first_heading, id: "firstHeading")
 end
