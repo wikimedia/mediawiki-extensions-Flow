8ae7df39785f3689cc3de5b985ad89ea2163359c
Shahyar <shahyar@gmail.com>
2014-07-08 15:13:57 -0400

    Correct mw-ui.enhance test usage for buttons
    Change-Id: I541740b59ed6cc56f3a88a23dc4c8836cbb381a4

--- tests/qunit/new/test_mw-ui.enhance.js
+++ tests/qunit/new/test_mw-ui.enhance.js
@@ -1,31 +1,37 @@
 ( function ( $ ) {
-QUnit.module( 'ext.flow: mediawiki.ui.enhance' );
+	QUnit.module( 'ext.flow: mediawiki.ui.enhance' );
 
-QUnit.test( 'Forms with required fields have certain buttons disabled by default', 6, function( assert ) {
-	var $forms = [
-		$( '<form><input required><button data-role="action" class="flow-ui-button">go</button></form>' ),
-		$( '<form><input required><button data-role="submit" class="flow-ui-button">go</button></form>' ),
-		$( '<form><textarea></textarea><input><button data-role="submit" class="flow-ui-button">go</button></form>' ),
-		$( '<form><textarea required></textarea><button data-role="submit" class="flow-ui-button">go</button></form>' ),
-		$( '<form><textarea required>foo</textarea><button data-role="submit" class="flow-ui-button">go</button></form>' ),
-		$( '<form><textarea required>foo</textarea><input required><button data-role="submit" class="flow-ui-button">go</button></form>' )
-	];
-	$.each( $forms, function() {
-		mw.flow.enhance.enableFormWithRequiredFields( this );
+	QUnit.test( 'Forms with required fields have certain buttons disabled by default', 6, function( assert ) {
+		var $forms = [
+			$( '<form><input class="mw-ui-input" required><button data-role="action" class="flow-ui-button">go</button></form>' ),
+			$( '<form><input class="mw-ui-input" required><button data-role="submit" class="flow-ui-button">go</button></form>' ),
+			$( '<form><textarea class="mw-ui-input"></textarea><input class="mw-ui-input"><button data-role="submit" class="flow-ui-button">go</button></form>' ),
+			$( '<form><textarea class="mw-ui-input" required></textarea><button data-role="submit" class="flow-ui-button">go</button></form>' ),
+			$( '<form><textarea class="mw-ui-input" required>foo</textarea><button data-role="submit" class="flow-ui-button">go</button></form>' ),
+			$( '<form><textarea class="mw-ui-input" required>foo</textarea><input class="mw-ui-input" required><button data-role="submit" class="flow-ui-button">go</button></form>' )
+		];
+
+		$.each( $forms, function() {
+			this.appendTo( 'body' );
+			this.find( '.mw-ui-input' ).trigger( 'keyup' );
+		} );
+
+		assert.strictEqual( $forms[0].find( 'button' ).is( ':disabled' ), true,
+			'Buttons with data-role=action are disabled when required fields are empty.' );
+		assert.strictEqual( $forms[1].find( 'button' ).is( ':disabled' ), true,
+			'Buttons with data-role=action are disabled when required fields are empty.' );
+		assert.strictEqual( $forms[2].find( 'button' ).is( ':disabled' ), false,
+			'Buttons with are enabled when no required fields in form.' );
+		assert.strictEqual( $forms[3].find( 'button' ).is( ':disabled' ), true,
+			'Buttons are disabled when textarea is required but empty.' );
+		assert.strictEqual( $forms[4].find( 'button' ).is( ':disabled' ), false,
+			'Buttons are enabled when required textarea has text.' );
+		assert.strictEqual( $forms[5].find( 'button' ).is( ':disabled' ), true,
+			'Buttons are disabled when required textarea but required input does not.' );
+
+		$.each( $forms, function() {
+			this.remove();
+		} );
 	} );
-
-	assert.strictEqual( $forms[0].find( 'button' ).is( ':disabled' ), true,
-		'Buttons with data-role=action are disabled when required fields are empty.' );
-	assert.strictEqual( $forms[1].find( 'button' ).is( ':disabled' ), true,
-		'Buttons with data-role=action are disabled when required fields are empty.' );
-	assert.strictEqual( $forms[2].find( 'button' ).is( ':disabled' ), false,
-		'Buttons with are enabled when no required fields in form.' );
-	assert.strictEqual( $forms[3].find( 'button' ).is( ':disabled' ), true,
-		'Buttons are disabled when textarea is required but empty.' );
-	assert.strictEqual( $forms[4].find( 'button' ).is( ':disabled' ), false,
-		'Buttons are enabled when required textarea has text.' );
-	assert.strictEqual( $forms[5].find( 'button' ).is( ':disabled' ), true,
-		'Buttons are disabled when required textarea but required input does not.' );
-} );
 
 } ( jQuery ) );
