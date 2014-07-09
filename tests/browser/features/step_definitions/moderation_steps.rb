afd6408ba76a64d8125da2aa50760a6ae71500f9
jdlrobson <jdlrobson@gmail.com>
2014-07-09 16:27:01 -0700

    QA: Add browser test which ensures anonymous users do not see deleted posts
    Change-Id: I6a1aca4073461919df927fcec82ae2f873df621b

--- tests/browser/features/step_definitions/moderation_steps.rb
+++ tests/browser/features/step_definitions/moderation_steps.rb
@@ -1,3 +1,23 @@
+Given(/^I am on a Flow page with a deleted post with heading "(.*?)"$/) do |arg1|
+  step 'I am logged in'
+  step 'I am on Flow page'
+  step 'I create a ' + arg1 + ' in Flow new topic'
+  step 'I create a Body of Flow Topic into Flow body'
+  step 'I click New topic save'
+  step 'the top post should have a heading which contains "' + arg1 + '"'
+  step 'I click the Topic Actions link'
+  step 'I click the Delete topic button'
+  step 'I see a dialog box'
+  step 'I give reason for deletion as being "DeletedPost"'
+  step 'I click Delete topic'
+  step 'the top post should be marked as deleted'
+end
+
+Given(/^I am anonymous$/) do
+  on(FlowPage).logout_element.click
+  visit FlowPage
+end
+
 Given(/^I create a Flow topic with title "(.*?)"$/) do |arg1|
   step 'I create a ' + arg1 + ' in Flow new topic'
   step 'I create a Body of Flow Topic into Flow body'
