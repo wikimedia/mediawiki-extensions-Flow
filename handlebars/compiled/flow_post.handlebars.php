c60398edd7a383663cd7c63431e074869f3546ec
jdlrobson <jdlrobson@gmail.com>
2014-07-09 18:30:14 -0700

    WIP: Make it possible to show toggle visibility of hidden posts
    Change-Id: Ib76489f5d90dede944a47ee90b708f459ed985a6

--- handlebars/compiled/flow_post.handlebars.php
+++ handlebars/compiled/flow_post.handlebars.php
@@ -25,13 +25,18 @@
 			 '.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isMaxThreadingDepth'])) ? $in['isMaxThreadingDepth'] : null))) ? '
 			 data-flow-post-max-depth="1"
 			 ' : '').'>
-	'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? '
-		<div class="flow-post-main flow-moderated-post-content">
-			<span class="wikiglyph wikiglyph-flag"></span>
-			'.LCRun3::ch($cx, 'l10n', Array('post_moderation_state',((is_array($in) && isset($in['moderateState'])) ? $in['moderateState'] : null),((is_array($in) && isset($in['replyToId'])) ? $in['replyToId'] : null),((is_array($in['moderator']) && isset($in['moderator']['name'])) ? $in['moderator']['name'] : null)), 'encq').'
-		</div>' : '<div class="flow-post-main">
-			'.LCRun3::wi($cx, ((is_array($in) && isset($in['creator'])) ? $in['creator'] : null), $in, function($cx, $in) {return '
-				'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
+		<div class="flow-post-main
+		  '.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? 'flow-collapsible-element flow-moderated-post-content flow-component flow-topic-collapsed' : '').'">
+			'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? '
+			<a href="#" class="flow-click-interactive"
+				data-flow-interactive-handler="collapserToggle">
+				<span class="wikiglyph wikiglyph-flag"></span>
+				'.LCRun3::ch($cx, 'l10n', Array('post_moderation_state',((is_array($in) && isset($in['moderateState'])) ? $in['moderateState'] : null),((is_array($in) && isset($in['replyToId'])) ? $in['replyToId'] : null),((is_array($in['moderator']) && isset($in['moderator']['name'])) ? $in['moderator']['name'] : null)), 'encq').'
+			</a>
+			' : '').'
+			<div class="flow-component-hideable">
+				'.LCRun3::wi($cx, ((is_array($in) && isset($in['creator'])) ? $in['creator'] : null), $in, function($cx, $in) {return '
+					'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
 	<span class="flow-author">
 		'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['links'])) ? $in['links'] : null))) ? '
 			<a href="'.htmlentities(((is_array($in['links']['contribs']) && isset($in['links']['contribs']['url'])) ? $in['links']['contribs']['url'] : null), ENT_QUOTES, 'UTF-8').'"
@@ -54,14 +59,13 @@
 	</a></span>
 ').'
 
-			';}).'
-
-			<div class="flow-post-content">
-				'.LCRun3::ch($cx, 'escapeContent', Array(((is_array($in['content']) && isset($in['content']['format'])) ? $in['content']['format'] : null),((is_array($in['content']) && isset($in['content']['content'])) ? $in['content']['content'] : null)), 'encq').'
-			</div>
-			
-			'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
-				<div class="flow-post-meta">
+				';}).'
+				<div class="flow-post-content">
+					'.LCRun3::ch($cx, 'escapeContent', Array(((is_array($in['content']) && isset($in['content']['format'])) ? $in['content']['format'] : null),((is_array($in['content']) && isset($in['content']['content'])) ? $in['content']['content'] : null)), 'encq').'
+				</div>
+				
+				'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
+					<div class="flow-post-meta">
 <span class="flow-post-meta-actions">
 	'.((LCRun3::ifvar($cx, ((is_array($in['actions']) && isset($in['actions']['reply'])) ? $in['actions']['reply'] : null))) ? '
 		<a href="'.htmlentities(((is_array($in['actions']['reply']) && isset($in['actions']['reply']['url'])) ? $in['actions']['reply']['url'] : null), ENT_QUOTES, 'UTF-8').'"
@@ -89,7 +93,7 @@
 '.LCRun3::ch($cx, 'uuidTimestamp', Array(((is_array($in) && isset($in['postId'])) ? $in['postId'] : null),'time_ago'), 'encq').'
 </div>
 
-				<div class="flow-menu">
+					<div class="flow-menu">
 	<div class="flow-menu-js-drop"><a href="javascript:void(0);"><span class="wikiglyph wikiglyph-ellipsis"></span></a></div>
 	<ul class="flow-ui-button-container">
 		'.((LCRun3::ifvar($cx, ((is_array($in['links']) && isset($in['links']['post'])) ? $in['links']['post'] : null))) ? '
@@ -193,12 +197,11 @@
 	</ul>
 </div>
 
-			' : '').'
-		</div>
+				' : '').'
 
-		
-		'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
-			<div class="flow-replies">
+			
+			'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
+				<div class="flow-replies">
 	'.LCRun3::sec($cx, ((is_array($in) && isset($in['replies'])) ? $in['replies'] : null), $in, true, function($cx, $in) {return '
 		'.LCRun3::hbch($cx, 'eachPost', Array(((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['rootBlock'])) ? $cx['scopes'][0]['rootBlock'] : null),$in), $in, function($cx, $in) {return '
 			<!-- eachPost nested replies -->
@@ -207,8 +210,9 @@
 	';}).'
 </div>
 
-		' : '').'
-	').'
+			' : '').'
+			</div>
+		</div>
 	</div>
 ';}).'
 ';
