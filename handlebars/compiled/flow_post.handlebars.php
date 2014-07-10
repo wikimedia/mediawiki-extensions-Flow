a5de94925a97002b9b40ef59271f69c5fd75e68c
jdlrobson <jdlrobson@gmail.com>
2014-07-09 17:07:00 -0700

    Do not show hidden posts by default
    Makes hidden compacts more compact
    
    Change-Id: Idaa06cd231f6c7616f12c4c3cf253057fa214562

--- handlebars/compiled/flow_post.handlebars.php
+++ handlebars/compiled/flow_post.handlebars.php
@@ -28,7 +28,11 @@
 			 '.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isMaxThreadingDepth'])) ? $in['isMaxThreadingDepth'] : null))) ? '
 			 data-flow-post-max-depth="1"
 			 ' : '').'>
-		<div class="flow-post-main">
+	'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? '
+		<div class="flow-post-main flow-moderated-post-content">
+			<span class="wikiglyph wikiglyph-flag"></span>
+			'.LCRun3::ch($cx, 'l10n', Array('post_moderation_state',((is_array($in) && isset($in['moderateState'])) ? $in['moderateState'] : null),((is_array($in) && isset($in['replyToId'])) ? $in['replyToId'] : null),((is_array($in['moderator']) && isset($in['moderator']['name'])) ? $in['moderator']['name'] : null)), 'encq').'
+		</div>' : '<div class="flow-post-main">
 			'.LCRun3::wi($cx, ((is_array($in) && isset($in['creator'])) ? $in['creator'] : null), $in, function($cx, $in) {return '
 				'.((!LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
 	<span class="flow-author">
@@ -55,10 +59,13 @@
 
 			';}).'
 
+<<<<<<< HEAD
 			'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? '
 				<div class="flow-moderated-post-content">'.LCRun3::ch($cx, 'l10n', Array(Array('post_moderation_state',((is_array($in) && isset($in['moderateState'])) ? $in['moderateState'] : null),((is_array($in) && isset($in['replyToId'])) ? $in['replyToId'] : null),((is_array($in['moderator']) && isset($in['moderator']['name'])) ? $in['moderator']['name'] : null)),Array()), 'encq').'</div>
 			' : '').'
 
+=======
+>>>>>>> 9502546... Do not show hidden posts by default
 			<div class="flow-post-content">
 				'.LCRun3::ch($cx, 'escapeContent', Array(Array(((is_array($in['content']) && isset($in['content']['format'])) ? $in['content']['format'] : null),((is_array($in['content']) && isset($in['content']['content'])) ? $in['content']['content'] : null)),Array()), 'encq').'
 			</div>
@@ -211,6 +218,7 @@
 </div>
 
 		' : '').'
+	').'
 	</div>
 ';}).'
 ';
