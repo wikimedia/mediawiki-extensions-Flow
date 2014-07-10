7db9d1a20e929cc4e79f6a2e0ed0cac8f52a9262
jdlrobson <jdlrobson@gmail.com>
2014-07-09 18:30:14 -0700

    WIP: Make it possible to show hidden posts
    Change-Id: Ib76489f5d90dede944a47ee90b708f459ed985a6

--- tests/browser/features/step_definitions/reply_moderation_steps.rb
+++ tests/browser/features/step_definitions/reply_moderation_steps.rb
@@ -9,5 +9,5 @@ end
 Then(/^the 3rd comment should be marked as hidden$/) do
   step 'the page renders in 5 seconds'
   on(FlowPage).third_reply_element.class_name.should match /flow-post-moderated/
-  on(FlowPage).third_reply_moderation_msg.should match( 'This comment was hidden' )
+  on(FlowPage).third_reply_moderation_msg.should match( 'Show comment hidden by' )
 end
