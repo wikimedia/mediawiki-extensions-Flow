2a4a872e76a728859c57bdc473216b3d89ce9af7
jdlrobson <jdlrobson@gmail.com>
2014-07-10 16:38:34 -0700

    Hide load more button with JavaScript when infinite scroll supports
    It shouldn't be visible if infinite scroll is supported.
    Also tweak behaviour of infinite scroll to make it appear more responsive.
    Now, it kicks in when the load more button is a scroll away.
    
    Change-Id: I29e90edd96ee040922f89ce1ec0baf29a02ad0d5

--- modules/new/components/flow-board.js
+++ modules/new/components/flow-board.js
@@ -410,8 +410,8 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.loadMore = function ( info, data, jqxhr ) {
-			var $this = $( this ),
-				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $this ),
+			var $target = $( this ).closest( '.flow-load-more' ),
+				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $target ),
 				$tmp;
 
 			if ( info.status !== 'done' ) {
@@ -420,7 +420,7 @@
 			}
 
 			// Render topiclist template
-			$this.before(
+			$target.before(
 				$tmp = $( flowBoard.TemplateEngine.processTemplateGetFragment(
 					'flow_topiclist_loop',
 					data.flow[ 'view-topiclist' ].result.topiclist
@@ -431,14 +431,14 @@
 
 			// Render load more template
 			if ( data.flow[ 'view-topiclist'].result.topiclist.links.pagination.fwd ) {
-				$this.replaceWith(
+				$target.replaceWith(
 					$tmp = $( flowBoard.TemplateEngine.processTemplateGetFragment(
 						'flow_load_more',
 						data.flow[ 'view-topiclist' ].result.topiclist
 					) ).children()
 				);
 			} else {
-				$this.replaceWith(
+				$target.replaceWith(
 					$tmp = $( flowBoard.TemplateEngine.processTemplateGetFragment(
 						'flow_no_more',
 						{}
@@ -450,7 +450,7 @@
 			FlowBoardComponent.UI.makeContentInteractive( $tmp );
 
 			// Remove the old load button (necessary if the above load_more template returns nothing)
-			$this.remove();
+			$target.remove();
 		};
 
 		/**
@@ -2108,27 +2108,30 @@
 		};
 
 		/**
+		 * Checks whether the user is near the end of the viewport
+		 */
+		function isEndNear( $end ) {
+			var $window = $( window ),
+				threshold = $window.height(),
+				scrollBottom = $window.scrollTop() + $window.height();
+			return scrollBottom + threshold > $end.offset().top;
+		}
+
+		/**
 		 * Called on window.scroll. Checks to see if a FlowBoard needs to have more content loaded.
 		 */
 		FlowBoardComponent.UI.infiniteScrollCheck = function () {
-			var windowHeight = $( window ).height(),
-				scrollPosition = $( window ).scrollTop();
+			$.each( FlowBoardComponent.prototype.getInstances(), function () {
+				var flowBoard = this,
+					$loadMoreNodes = flowBoard.$loadMoreNodes || $();
 
-			if ( scrollPosition > 25 ) {
-				$.each( FlowBoardComponent.prototype.getInstances(), function () {
-					var flowBoard = this,
-						$loadMoreNodes = flowBoard.$loadMoreNodes || $();
-
-					// Check each loadMore button
-					$loadMoreNodes.each( function () {
-						// Only count this button as infinite-scrollable if it appears below the fold
-						// and that we have scrolled at least 50% of the way to it
-						if ( this.offsetTop > windowHeight && scrollPosition / ( this.offsetTop - windowHeight ) > 0.5 ) {
-							$( this ).click();
-						}
-					} );
+				// Check each loadMore button
+				$loadMoreNodes.each( function () {
+					if ( isEndNear( $( this ) ) ) {
+						$( this ).trigger( 'click' );
+					}
 				} );
-			}
+			} );
 		};
 
 		/**
