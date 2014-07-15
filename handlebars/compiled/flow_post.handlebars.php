a61b9dbda8b3e4665289bf2d18e56131f699c0b7
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-07-15 15:07:12 -0700

    {{#if foo.exists}}new {{/if}} should be unless
    Change-Id: Ia8a96385b7795a61326cfc8dfebf6633f243a6e8

--- handlebars/compiled/flow_post.handlebars.php
+++ handlebars/compiled/flow_post.handlebars.php
@@ -35,7 +35,7 @@
 		'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['links'])) ? $in['links'] : null))) ? '
 			<a href="'.htmlentities(((is_array($in['links']['contribs']) && isset($in['links']['contribs']['url'])) ? $in['links']['contribs']['url'] : null), ENT_QUOTES, 'UTF-8').'"
 			   title="'.htmlentities(((is_array($in['links']['contribs']) && isset($in['links']['contribs']['title'])) ? $in['links']['contribs']['title'] : null), ENT_QUOTES, 'UTF-8').'"
-			   class="'.((LCRun3::ifvar($cx, ((is_array($in['links']['contribs']) && isset($in['links']['contribs']['exists'])) ? $in['links']['contribs']['exists'] : null))) ? 'new ' : '').'mw-userlink flow-ui-tooltip-target">
+			   class="'.((!LCRun3::ifvar($cx, ((is_array($in['links']['contribs']) && isset($in['links']['contribs']['exists'])) ? $in['links']['contribs']['exists'] : null))) ? 'new ' : '').'mw-userlink flow-ui-tooltip-target">
 				'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['name'])) ? $in['name'] : null))) ? '
 					'.htmlentities(((is_array($in) && isset($in['name'])) ? $in['name'] : null), ENT_QUOTES, 'UTF-8').'' : '
 					'.LCRun3::ch($cx, 'l10n', Array(Array('flow-anonymous'),Array()), 'encq').'
