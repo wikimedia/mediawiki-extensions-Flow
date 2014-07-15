ccba4e55f9eb15604613516e71b4a3f8e78bdd3c
bsitu <bsitu@wikimedia.org>
2014-07-15 15:09:36 -0700

    "Edited timestamp" should link to diff
    Bug: 67607
    Change-Id: Ia0047f654e9d1d7d819a3c7ffc366c22682aeb68

--- handlebars/compiled/flow_block_topic_moderate_topic.handlebars.php
+++ handlebars/compiled/flow_block_topic_moderate_topic.handlebars.php
@@ -126,14 +126,21 @@
 		'.((LCRun3::ifvar($cx, ((is_array($in['actions']) && isset($in['actions']['thank'])) ? $in['actions']['thank'] : null))) ? '
 			&#8226;
 			'.'
-			<a class="flow-ui-regressive flow-ui-quiet mw-thanks-flow-thank-link"
+			<a class="mw-ui-button mw-ui-quiet mw-thanks-flow-thank-link"
 			   href="'.htmlentities(((is_array($in['actions']['thank']) && isset($in['actions']['thank']['url'])) ? $in['actions']['thank']['url'] : null), ENT_QUOTES, 'UTF-8').'"
 			   title="'.htmlentities(((is_array($in['actions']['thank']) && isset($in['actions']['thank']['title'])) ? $in['actions']['thank']['title'] : null), ENT_QUOTES, 'UTF-8').'">'.htmlentities(((is_array($in['actions']['thank']) && isset($in['actions']['thank']['title'])) ? $in['actions']['thank']['title'] : null), ENT_QUOTES, 'UTF-8').'</a>
 		' : '').'
 	</span>
 	'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['previousRevisionId'])) ? $in['previousRevisionId'] : null))) ? '
-		<!--span class="wikiglyph wikiglyph-clock"></span-->
-		'.LCRun3::ch($cx, 'uuidTimestamp', Array(Array(((is_array($in) && isset($in['revisionId'])) ? $in['revisionId'] : null),'edited_ago'),Array()), 'encq').'
+		'.((LCRun3::ifvar($cx, ((is_array($in['links']) && isset($in['links']['diff-prev'])) ? $in['links']['diff-prev'] : null))) ? '
+			<a href="'.htmlentities(((is_array($in['links']['diff-prev']) && isset($in['links']['diff-prev']['url'])) ? $in['links']['diff-prev']['url'] : null), ENT_QUOTES, 'UTF-8').'"
+			    class="mw-ui-button mw-ui-progressive mw-ui-quiet flow-ui-inline"
+			    title="'.htmlentities(((is_array($in['links']['diff-prev']) && isset($in['links']['diff-prev']['title'])) ? $in['links']['diff-prev']['title'] : null), ENT_QUOTES, 'UTF-8').'">
+		' : '').'
+			'.LCRun3::ch($cx, 'uuidTimestamp', Array(Array(((is_array($in) && isset($in['revisionId'])) ? $in['revisionId'] : null),'edited_ago'),Array()), 'encq').'
+		'.((LCRun3::ifvar($cx, ((is_array($in['links']) && isset($in['links']['diff-prev'])) ? $in['links']['diff-prev'] : null))) ? '
+			</a>
+		' : '').'
 		&#8226;
 	' : '').'
 	'.LCRun3::ch($cx, 'uuidTimestamp', Array(Array(((is_array($in) && isset($in['postId'])) ? $in['postId'] : null),'time_ago'),Array()), 'encq').'
