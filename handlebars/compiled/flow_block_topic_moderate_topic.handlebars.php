6fa4440a3ea15aa66715ca6f5b99066d81e0057d
Matthias Mullie <git@mullie.eu>
2014-07-16 17:09:34 +0200

    Fix error handling
    There's a couple of changes here:
    * added new findUpward method: use with caution as it can potentially seek a lot
      of DOM nodes (but I'm ok with it - it's limited: html will never be nested
      deep enough for it to cause problems)
    * made error handling more generic; no need to pass container - just have error
      container somewhere near ;)
    * a bunch of GET calls had no errors displaying (only removeError was called)
    * switched a couple of flow_errors in templates (mostly to get them to show in
      processing GET stuff)
    
    Bug: 68021
    Change-Id: Id29473cabda78fa9ea5d9ca7d996e9485b0bdbf6

--- handlebars/compiled/flow_block_topic_moderate_topic.handlebars.php
+++ handlebars/compiled/flow_block_topic_moderate_topic.handlebars.php
@@ -59,6 +59,19 @@
 			 data-flow-post-max-depth="1"
 			 ' : '').'>
 		<div class="flow-post-main">
+			<div class="flow-error-container">
+'.((LCRun3::ifvar($cx, ((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['errors'])) ? $cx['scopes'][0]['errors'] : null))) ? '
+	<div class="flow-errors errorbox">
+		<ul>
+			'.LCRun3::sec($cx, ((is_array($cx['scopes'][0]) && isset($cx['scopes'][0]['errors'])) ? $cx['scopes'][0]['errors'] : null), $in, true, function($cx, $in) {return '
+				<li>'.LCRun3::ch($cx, 'html', Array(Array(((is_array($in) && isset($in['message'])) ? $in['message'] : null)),Array()), 'encq').'</li>
+			';}).'
+		</ul>
+	</div>
+' : '').'
+</div>
+
+
 			'.LCRun3::wi($cx, ((is_array($in) && isset($in['creator'])) ? $in['creator'] : null), $in, function($cx, $in) {return '
 				'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isPreview'])) ? $in['isPreview'] : null))) ? '
 	<span class="flow-author"><a href="#" class="mw-userlink">
