6004fece20a6460c6a8a0abd510c678f93926c90
Andrew Garrett <agarrett@wikimedia.org>
2014-07-18 12:02:27 -0700

    Redirect old-style permalinks to new ones.
    In general, if we attempt to load a specific workflow by ID, and
    it comes from the wrong title, then redirect to that title.
    
    Change-Id: I77e6db76e59cd0a194d8a35e11d753ff8238be3f

--- includes/Actions/Action.php
+++ includes/Actions/Action.php
@@ -7,6 +7,7 @@ use Article;
 use ErrorPageError;
 use Flow\Container;
 use Flow\Exception\FlowException;
+use Flow\Exception\InvalidInputException;
 use Flow\Model\UUID;
 use Flow\View;
 use IContextSource;
@@ -84,6 +85,20 @@ class FlowAction extends Action {
 			}
 
 			$view->show( $loader, $action );
+		} catch ( InvalidInputException $e ) {
+			if ( $workflowId ) {
+				// Probably wrong title, redirect to correct one
+				$storage = $container['storage'];
+				$workflow = $storage->get( 'Workflow', $workflowId );
+				$redirTitle = $workflow->getArticleTitle();
+				$query = array( 'workflow' => $workflowId->getAlphadecimal() );
+				$redirUrl = $redirTitle->getLocalUrl( $query );
+
+				$output->redirect( $redirUrl );
+			} else {
+				$e->setOutput( $output );
+				throw $e;
+			}
 		} catch( FlowException $e ) {
 			$e->setOutput( $output );
 			throw $e;
