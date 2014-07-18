d6dea2aef492eea29cddfd3b19fe2507cfb0f7c2
Andrew Garrett <agarrett@wikimedia.org>
2014-07-18 10:09:29 -0700

    New topic notifications for watchers of a board.
    Still to do:
    * Move watch star from content-actions to meet the design at
    <https://trello-attachments.s3.amazonaws.com/52bcc60a6c60b54f360423dc/53a1b06dd9fe0938768ca3ef/1179x771/df52b1c43cfebd6de8b3afe8e989ae77/subscribe_to_board.png>
    
    Change-Id: I4e46a9c003fbdde274b20ac7aef8455eab4a5222

---
+++ tests/NotifiedUsersTest.php
@@ -0,0 +1,87 @@
+<?php
+namespace Flow\Tests;
+
+use Flow\Container;
+use Flow\NotificationController;
+use User;
+use WatchedItem;
+
+class NotifiedUsersTest extends PostRevisionTestCase {
+	public function setUp() {
+		parent::setUp();
+
+		global $wgFlowOccupyPages;
+
+		$wgFlowOccupyPages[] = 'User talk:Flow';
+	}
+	public function testWatchingTopic() {
+		extract( $this->getTestData() );
+
+		$notificationController->subscribeToWorkflow( $user, $topicWorkflow );
+
+		$events = $notificationController->notifyPostChange( 'flow-post-reply',
+			array(
+				'topic-workflow' => $topicWorkflow,
+				'title' => $boardWorkflow->getOwnerTitle(),
+				'user' => $agent,
+				'reply-to' => $topic,
+				'topic-title' => $topic,
+				'revision' => $post,
+			) );
+
+		$this->assertNotifiedUser( $events, $user, $agent );
+	}
+
+	public function testWatchingBoard() {
+		extract( $this->getTestData() );
+
+		$notificationController->subscribeToWorkflow( $user, $boardWorkflow );
+
+		$events = $notificationController->notifyNewTopic( array(
+			'board-workflow' => $boardWorkflow,
+			'topic-workflow' => $topicWorkflow,
+			'title-post' => $topic,
+			'first-post' => $post,
+			'user' => $agent,
+		) );
+
+		$this->assertNotifiedUser( $events, $user, $agent );
+	}
+
+	protected function assertNotifiedUser( array $events, User $notifiedUser, User $notNotifiedUser ) {
+		$users = array();
+		foreach( $events as $event ) {
+			NotificationController::getDefaultNotifiedUsers( $event, $users );
+		}
+
+		$this->assertArrayHasKey( $notifiedUser->getId(), $users );
+		$this->assertArrayNotHasKey( $notNotifiedUser->getId(), $users );
+	}
+
+	protected function getTestData() {
+		$this->generateWorkflowForPost();
+		$topicWorkflow = $this->workflow;
+		$post = $this->generateObject( array(), array(), 1 );
+		$topic = $this->generateObject( array(), array( $post ) );
+		$user = User::newFromName( 'Flow Test User' );
+		$user->addToDatabase();
+		$agent = User::newFromName( 'Flow Test Agent' );
+		$agent->addToDatabase();
+
+		$notificationController = Container::get( 'controller.notification' );
+
+		$boardWorkflow = Container::get( 'factory.loader.workflow' )
+			->createWorkflowLoader( $topicWorkflow->getOwnerTitle() )
+			->getWorkflow();
+
+		return compact( array(
+				'boardWorkflow',
+				'topicWorkflow',
+				'post',
+				'topic',
+				'user',
+				'agent',
+				'notificationController',
+			) );
+	}
+}
