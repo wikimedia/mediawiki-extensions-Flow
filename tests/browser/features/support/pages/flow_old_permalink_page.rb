886de81e5a1b6cd1d47e30a6ed6c600019d7fa28
Andrew Garrett <agarrett@wikimedia.org>
2014-07-18 12:02:27 -0700

    Redirect old-style permalinks to new ones.
    So old permalinks of the form ?title=Talk:<FlowBoard>&workflow=<topicUUID> work.
    In general, if we attempt to load a specific workflow by ID, and
    it comes from the wrong title, then redirect to that title.
    
    Bug: 68224
    Change-Id: I77e6db76e59cd0a194d8a35e11d753ff8238be3f
    (cherry picked from commit ad3ccaf65c9ac40648b08499427112acd6e2d2e7)

---
+++ tests/browser/features/support/pages/flow_old_permalink_page.rb
@@ -0,0 +1,7 @@
+class FlowOldPermalinkPage
+  include PageObject
+  include URL
+
+  @params = {:page => 'Talk:Flow QA', :workflow_id => 'no workflow'}
+  page_url URL.url( params[:page] ) + "?workflow=<%=params[:workflow_id]%>"
+end
