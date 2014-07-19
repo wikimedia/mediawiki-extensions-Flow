2619f97e941f5903111446220c30e8706d4f847f
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2014-07-19 23:17:16 +0300

    Add namespace translations
    Change-Id: Ib8cc18d9e478ab98799a3bc79358387ebc38b338

--- Flow.namespaces.php
+++ Flow.namespaces.php
@@ -12,8 +12,97 @@ if( !defined( 'NS_TOPIC' ) ) {
 	define( 'NS_TOPIC', 2600 );
 }
 
+/** Asturian */
+$namespaceNames['ast'] = array(
+	NS_TOPIC =>  'Asuntu',
+);
+
+/** German */
+$namespaceNames['de'] = array(
+	NS_TOPIC =>  'Thema',
+);
+
 /** English */
 $namespaceNames['en'] = array(
 	NS_TOPIC =>  'Topic',
 );
 
+/** Spanish */
+$namespaceNames['es'] = array(
+	NS_TOPIC =>  'Tema',
+);
+
+/** Persian */
+$namespaceNames['fa'] = array(
+	NS_TOPIC =>  'موضوع',
+);
+
+/** Finnish */
+$namespaceNames['fi'] = array(
+	NS_TOPIC =>  'Aihe',
+);
+
+/** French */
+$namespaceNames['fr'] = array(
+	NS_TOPIC =>  'Sujet',
+);
+
+/** Hebrew */
+$namespaceNames['he'] = array(
+	NS_TOPIC =>  'נושא',
+);
+
+/** Luxembourgish */
+$namespaceNames['lb'] = array(
+	NS_TOPIC =>  'Thema',
+);
+
+/** Latvian */
+$namespaceNames['lv'] = array(
+	NS_TOPIC =>  'Tēma',
+);
+
+/** Macedonian */
+$namespaceNames['mk'] = array(
+	NS_TOPIC =>  'Тема',
+);
+
+/** Dutch */
+$namespaceNames['nl'] = array(
+	NS_TOPIC =>  'Onderwerp',
+);
+
+/** Occitan */
+$namespaceNames['oc'] = array(
+	NS_TOPIC =>  'Subjècte',
+);
+
+/** Polish */
+$namespaceNames['pl'] = array(
+	NS_TOPIC =>  'Temat',
+);
+
+/** Portuguese */
+$namespaceNames['pt'] = array(
+	NS_TOPIC =>  'Tópico',
+);
+
+/** Russian */
+$namespaceNames['ru'] = array(
+	NS_TOPIC =>  'Тема',
+);
+
+/** Slovenian */
+$namespaceNames['sl'] = array(
+	NS_TOPIC =>  'Tema',
+);
+
+/** Ukrainian */
+$namespaceNames['uk'] = array(
+	NS_TOPIC =>  'Тема',
+);
+
+/** Yiddish */
+$namespaceNames['yi'] = array(
+	NS_TOPIC =>  'טעמע',
+);
