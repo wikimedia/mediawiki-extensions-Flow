669427af83bd5bfd3cc2a8be9c278e10529ab606
jdlrobson <jdlrobson@gmail.com>
2014-07-23 11:25:27 -0700

    Watchstar should use mediawiki ui styles not flow ui for buttons
    Separate active button module to make it clearer how and what we can
    upstream.
    
    Bug: 68456
    Change-Id: I186aa67a621f8a8d5cce887b27fbc61acb6a7db0

--- handlebars/compiled/flow_block_topic.handlebars.php
+++ handlebars/compiled/flow_block_topic.handlebars.php
@@ -84,7 +84,7 @@
 		<span class="flow-ui-quiet flow-ui-constructive flow-ui-active">'.htmlentities(((is_array($in) && isset($in['null'])) ? $in['null'] : null), ENT_QUOTES, 'UTF-8').'<span class="wikiglyph wikiglyph-star"></span>'.htmlentities(((is_array($in) && isset($in['null'])) ? $in['null'] : null), ENT_QUOTES, 'UTF-8').'</span>
 	' : '
 		<a href="'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isWatched'])) ? $in['isWatched'] : null))) ? ''.htmlentities(((is_array($in['links']['unwatch-topic']) && isset($in['links']['unwatch-topic']['url'])) ? $in['links']['unwatch-topic']['url'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities(((is_array($in['links']['watch-topic']) && isset($in['links']['watch-topic']['url'])) ? $in['links']['watch-topic']['url'] : null), ENT_QUOTES, 'UTF-8').'').'"
-		   class="flow-ui-quiet flow-ui-constructive '.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isWatched'])) ? $in['isWatched'] : null))) ? 'flow-ui-active' : '').'"
+		   class="mw-ui-quiet mw-ui-constructive mw-ui-button '.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isWatched'])) ? $in['isWatched'] : null))) ? 'flow-ui-active' : '').'"
 		   data-flow-api-handler="watchTopic"
 		   data-flow-api-target="< .flow-topic-watchlist"
 		   data-flow-api-method="POST">'.htmlentities(((is_array($in) && isset($in['null'])) ? $in['null'] : null), ENT_QUOTES, 'UTF-8').'<span class="wikiglyph wikiglyph-star"></span>'.htmlentities(((is_array($in) && isset($in['null'])) ? $in['null'] : null), ENT_QUOTES, 'UTF-8').''.htmlentities(((is_array($in) && isset($in['null'])) ? $in['null'] : null), ENT_QUOTES, 'UTF-8').'<span class="wikiglyph wikiglyph-unstar"></span>'.htmlentities(((is_array($in) && isset($in['null'])) ? $in['null'] : null), ENT_QUOTES, 'UTF-8').'</a>
