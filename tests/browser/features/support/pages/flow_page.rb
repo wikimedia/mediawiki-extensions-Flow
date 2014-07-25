49942361f4aed08b566db20c4f7d0da227da7fbd
Andrew Garrett <agarrett@wikimedia.org>
2014-07-24 18:04:20 -0700

    Move watch star for boards next to the header.
    Currently requires star to be positioned scarily, but it does work,
    and also on Mobile.
    
    Change-Id: Ie5f43cb88c61cf31c5a3eafb026194b88ed94b5e

--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -197,17 +197,31 @@ class FlowPage
   button(:edit_header_save, text: "Save header")
 
   ## Watch and unwatch links
-  div(:first_topic_watchlist_container, class:"flow-topic-watchlist", index:0)
+  div(:first_topic_watchlist_container, css:".flow-topic-watchlist", index:0)
 
   a(:first_topic_watch_link) do |page|
-    page.first_topic_watchlist_container_element.link_element
+    page.first_topic_watchlist_container_element.link_element( css: '.flow-watch-link-watch' )
   end
 
   a(:first_topic_unwatch_link) do |page|
-    page.first_topic_watchlist_container_element.link_element( class: 'flow-ui-active' )
+    page.first_topic_watchlist_container_element.link_element( css: '.flow-watch-link-unwatch' )
   end
 
   a(:first_topic_watchlist_loading_link) do |page|
-    page.first_topic_watchlist_container_element.link_element( class: 'flow-api-inprogress' )
+    page.first_topic_watchlist_container_element.link_element( css: '.flow-api-inprogress' )
+  end
+
+  div(:board_watchlist_container, css:".flow-board-watch-link")
+
+  a(:board_watch_link) do |page|
+    page.board_watchlist_container_element.link_element( css: '.flow-watch-link-watch' )
+  end
+
+  a(:board_unwatch_link) do |page|
+    page.board_watchlist_container_element.link_element( css: '.flow-watch-link-unwatch' )
+  end
+
+  a(:board_watch_loading_link) do |page|
+    page.board_watchlist_container_element.link_element( css: '.flow-api-inprogress' )
   end
 end
