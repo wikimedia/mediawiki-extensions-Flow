87d556d5a77bcea46ca6d911b940e7f5b603cb76
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-07-28 11:29:33 -0700

    Cancelling topic reply preview results in missing form
    The recent re-addition of mw.flow.editor caused an issue with missing
    forms, essentially the preview needs to be undone before the editor
    is destroyed.
    
    Bug: 68591
    Change-Id: I0490e1ac47a6e1e828d6293983015ba4756673ee

--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -186,6 +186,7 @@ class FlowPage
   button(:new_reply_cancel, css: ".flow-reply-form .mw-ui-destructive")
   button(:new_reply_preview, css: ".flow-reply-form .mw-ui-progressive")
   button(:new_reply_save, css: ".flow-reply-form .mw-ui-constructive")
+  div(:new_reply_preview_warning, css: ".flow-reply-form .flow-preview-warning")
 
   # Editing post workflow
   text_area(:post_edit, css: ".flow-edit-post-form textarea")
