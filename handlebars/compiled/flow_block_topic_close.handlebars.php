42475ee20d090c37d772173551695d0f4bb75abc
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-08-25 15:25:12 -0700

    G3: Mobile/nojs edit and reply full topic output
    Change-Id: I958576fdaad17c070542543be46969b77b57d92d

--- handlebars/compiled/flow_block_topic_close.handlebars.php
+++ handlebars/compiled/flow_block_topic_close.handlebars.php
@@ -60,18 +60,21 @@
 							'.LCRun3::ch($cx, 'l10n', Array(Array('flow-topic-action-close-topic'),Array()), 'encq').'
 						').'
 				</button>
-				<button data-flow-api-handler="preview"
-        data-flow-api-target="< form textarea"
-        name="preview"
-        data-role="action"
-        class="mw-ui-button mw-ui-progressive mw-ui-quiet mw-ui-flush-right">'.LCRun3::ch($cx, 'l10n', Array(Array('flow-preview'),Array()), 'encq').'</button>
+				'.LCRun3::hbch($cx, 'progressiveEnhancement', Array(Array(),Array()), $in, function($cx, $in) {return '
+	<button data-flow-api-handler="preview"
+	        data-flow-api-target="< form textarea"
+	        name="preview"
+	        data-role="action"
+	        class="mw-ui-button mw-ui-progressive mw-ui-quiet mw-ui-flush-right"
+	>'.LCRun3::ch($cx, 'l10n', Array(Array('flow-preview'),Array()), 'encq').'</button>
 
-'.LCRun3::hbch($cx, 'progressiveEnhancement', Array(Array(),Array()), $in, function($cx, $in) {return '
 	<button data-flow-interactive-handler="cancelForm"
 	        data-role="cancel"
 	        type="reset"
-	        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right">'.LCRun3::ch($cx, 'l10n', Array(Array('flow-cancel'),Array()), 'encq').'</button>
+	        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right"
+	>'.LCRun3::ch($cx, 'l10n', Array(Array('flow-cancel'),Array()), 'encq').'</button>
 ';}).'
+
 				<small class="flow-terms-of-use plainlinks">
 					'.((LCRun3::ifvar($cx, ((is_array($in) && isset($in['isModerated'])) ? $in['isModerated'] : null))) ? '
 						'.LCRun3::ch($cx, 'l10nParse', Array(Array('flow-terms-of-use-reopen-topic'),Array()), 'encq').'
--- handlebars/compiled/flow_block_topiclist_newtopic.handlebars.php
+++ handlebars/compiled/flow_block_topiclist_newtopic.handlebars.php
@@ -71,18 +71,21 @@
 			<button data-role="submit" data-flow-api-handler="newTopic"
 				data-flow-interactive-handler="apiRequest"
 				class="mw-ui-button mw-ui-constructive mw-ui-flush-right">'.LCRun3::ch($cx, 'l10n', Array(Array('flow-newtopic-save'),Array()), 'encq').'</button>
-			<button data-flow-api-handler="preview"
-        data-flow-api-target="< form textarea"
-        name="preview"
-        data-role="action"
-        class="mw-ui-button mw-ui-progressive mw-ui-quiet mw-ui-flush-right">'.LCRun3::ch($cx, 'l10n', Array(Array('flow-preview'),Array()), 'encq').'</button>
+			'.LCRun3::hbch($cx, 'progressiveEnhancement', Array(Array(),Array()), $in, function($cx, $in) {return '
+	<button data-flow-api-handler="preview"
+	        data-flow-api-target="< form textarea"
+	        name="preview"
+	        data-role="action"
+	        class="mw-ui-button mw-ui-progressive mw-ui-quiet mw-ui-flush-right"
+	>'.LCRun3::ch($cx, 'l10n', Array(Array('flow-preview'),Array()), 'encq').'</button>
 
-'.LCRun3::hbch($cx, 'progressiveEnhancement', Array(Array(),Array()), $in, function($cx, $in) {return '
 	<button data-flow-interactive-handler="cancelForm"
 	        data-role="cancel"
 	        type="reset"
-	        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right">'.LCRun3::ch($cx, 'l10n', Array(Array('flow-cancel'),Array()), 'encq').'</button>
+	        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right"
+	>'.LCRun3::ch($cx, 'l10n', Array(Array('flow-cancel'),Array()), 'encq').'</button>
 ';}).'
+
 			<small class="flow-terms-of-use plainlinks">'.LCRun3::ch($cx, 'l10nParse', Array(Array('flow-terms-of-use-new-topic'),Array()), 'encq').'</small>
 		</div>
 	</form>
