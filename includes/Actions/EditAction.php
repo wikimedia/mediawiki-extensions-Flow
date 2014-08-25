ba1582acbb6577bc336bb64d37ecd82abbfc0696
bsitu <bsitu@wikimedia.org>
2014-08-25 13:50:00 -0700

    Redirect to Flow view action when url action is set to edit
    * Flow doesn't support edit action
    
    * Parameter is lost during the redirect, this only happens
      once for Flow topic without a revision and created before
      topic namespace introduction.
    
    * Removed Flow\Action\Action.exectue(), it doesn't seem to do
      anything, but like werdna to confirm
    
    Bug: 69879
    Change-Id: I3534a42c6fccd081a6b408e9f6d53a931d4ab886

---
+++ includes/Actions/EditAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Flow\Actions;
+
+use IContextSource;
+use Page;
+use Title;
+
+class EditAction extends FlowAction {
+
+	function __construct( Page $page, IContextSource $context ) {
+		parent::__construct( $page, $context, 'edit' );
+	}
+
+	/**
+	 * Flow doesn't support edit action, redirect to the title instead
+	 */
+	public function show() {
+		$title = $this->context->getTitle();
+
+		// There should always be a title since Flow page
+		// is detected by title or namespace, adding this
+		// to prevent some werid cases
+		if ( !$title ) {
+			$title = Title::newMainPage();
+		}
+
+		$this->context->getOutput()->redirect( $title->getFullURL() );
+	}
+
+}
