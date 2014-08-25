1998fdcba6aadf1a1df871ccb965907c81dd57b8
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-08-25 15:25:12 -0700

    WIP: G3: Mobile/nojs edit and reply full topic output
    TODO:
    * Style reply link
    * Preview never worked in no-js
    * Pre-fill 'Someuser:' content? messy with current approach
    * Shahyar is working up a patch that allows the nested
      progressiveEnhancement scripts to work
    
    Change-Id: I958576fdaad17c070542543be46969b77b57d92d

--- includes/TemplateHelper.php
+++ includes/TemplateHelper.php
@@ -175,6 +175,10 @@ class TemplateHelper {
 	static public function processTemplate( $templateName, $args, array $scopes = array() ) {
 		// Undesirable, but lightncandy helpers have to be static methods
 		$template = Container::get( 'lightncandy' )->getTemplate( $templateName );
+		// ugly hack...remove someday
+		if ( !array_key_exists( 'rootBlock', $args ) ) {
+			$args['rootBlock'] = $args;
+		}
 		return call_user_func( $template, $args, $scopes );
 	}
 
@@ -593,15 +597,16 @@ class TemplateHelper {
 		$target = empty( $input['target'] ) ? '' : 'data-target="' . htmlspecialchars( $input['target'] ) . '"';
 		$sectionId = empty( $input['id'] ) ? '' : 'id="' . htmlspecialchars( $input['id'] ) . '"';
 
+
 		return self::html(
 			'<script name="handlebars-template-progressive-enhancement"' .
 				' type="text/x-handlebars-template-progressive-enhancement"' .
 				' data-type="' . $insertionType . '"' .
 				' ' . $target .
 				' ' . $sectionId .
-			'>' .
-				$fn() .
-			'</script>'
+			'>/*<![CDATA[*/' .
+				str_replace( '</script>', '</progressivescript>', $fn() ) .
+			'/*]]>*/</script>'
 		);
 	}
 
