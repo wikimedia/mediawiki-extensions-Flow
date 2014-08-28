60f8243855315572f552ab6b5b15c5253dcbbd82
jdlrobson <jdlrobson@gmail.com>
2014-08-27 17:17:24 -0700

    Update classes of topic during close/reopen actions.
    Add tests.
    Bug: 68328
    
    Change-Id: I15a09c5b057de8e7d24e5c1b953bd36dfe72a40c

--- tests/qunit/new/components/test_flow-board.js
+++ tests/qunit/new/components/test_flow-board.js
@@ -7,10 +7,99 @@ QUnit.test( 'Check Flow is running', 1, function() {
 
 QUnit.module( 'ext.flow: FlowBoardComponent', {
 	setup: function() {
+		var stub;
 		this.$el = $( '<div class="flow-component" data-flow-component="board">' );
 		this.component = mw.flow.initComponent( this.$el );
+		stub = this.sandbox.stub( this.component.API, 'apiCall' );
+		stub.withArgs( {
+			action: 'flow',
+			submodule: 'view-topic',
+			workflow: 's18cjkj1bs3rkt13',
+			page: 'Topic:S18cjkj1bs3rkt13'
+		} ).returns(
+			new $.Deferred().resolve( {
+				flow: {
+					'view-topic': {
+						result: {
+							topic: {
+								roots: [ 's18cjkj1bs3rkt13' ],
+								posts: {
+									s18cjkj1bs3rkt13: '4'
+								},
+								revisions: {
+									'4': {
+										content: {
+											format: 'html',
+											content: 'Hi'
+										},
+										changeType: "close-topic",
+										isModerated: false
+									}
+								}
+							},
+						}
+					}
+				}
+			} )
+		);
+		stub.withArgs( {
+			action: 'flow',
+			submodule: 'view-topic',
+			workflow: 't18cjkj1bs3rkt13',
+			page: 'Topic:T18cjkj1bs3rkt13'
+		} ).returns(
+			new $.Deferred().resolve( {
+				flow: {
+					'view-topic': {
+						result: {
+							topic: {
+								roots: [ 't18cjkj1bs3rkt13' ],
+								posts: {
+									t18cjkj1bs3rkt13: '4'
+								},
+								revisions: {
+									'4': {
+										changeType: "restore-topic",
+										content: {
+											format: 'html',
+											content: 'Hi'
+										},
+										isModerated: true
+									}
+								}
+							},
+						}
+					}
+				}
+			} )
+		);
 		this.UI = this.component.constructor.UI;
 	}
+} );
+
+QUnit.test( 'FlowBoardComponent.UI.events.apiHandlers.preview', 2, function( assert ) {
+	var
+		$topic = $( '<div class="flow-topic" data-flow-id="s18cjkj1bs3rkt13">' ).
+			addClass( 'flow-topic-moderatestate-close flow-topic-moderated' ).
+			appendTo( this.$el ),
+		$titleBar = $( '<div class="flow-topic-titlebar">' ).appendTo( $topic ),
+		info = { status: 'done', $target: $titleBar };
+
+	this.UI.events.apiHandlers.closeOpenTopic.call( $titleBar, info );
+	assert.strictEqual( $topic.hasClass( 'flow-topic-moderated' ), false, 'No longer has the moderated state.' );
+	assert.strictEqual( $topic.hasClass( 'flow-topic-moderatestate-close' ), false, 'No longer has the moderated close state.' );
+} );
+
+QUnit.test( 'FlowBoardComponent.UI.events.apiHandlers.preview', 2, function( assert ) {
+	var
+		$topic = $( '<div class="flow-topic" data-flow-id="t18cjkj1bs3rkt13">' ).
+			appendTo( this.$el ),
+		$titleBar = $( '<div class="flow-topic-titlebar">' ).appendTo( $topic ),
+		info = { status: 'done', $target: $titleBar };
+
+	this.UI.events.apiHandlers.closeOpenTopic.call( $titleBar, info );
+	assert.strictEqual( $topic.hasClass( 'flow-topic-moderated' ), true, 'Has the moderated state.' );
+	assert.strictEqual( $topic.hasClass( 'flow-topic-moderatestate-close' ), true, 'Has the moderated close state.' );
 } );
 
 QUnit.test( 'FlowBoardComponent.UI.events.apiHandlers.preview', 6, function( assert ) {
