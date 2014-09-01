edb76ab1c2d092123614722e6fb5289e5fd11402
jdlrobson <jdlrobson@gmail.com>
2014-09-01 21:54:34 +0100

    Hygiene: Remove uninitialised variables to top
    Change-Id: I85819a71da9038f449cf07c28d8337fb76678ed9

--- modules/new/components/flow-board.js
+++ modules/new/components/flow-board.js
@@ -314,8 +314,8 @@
 		 * @return {Function} Callback to modify the API request
 		 */
 		FlowBoardComponent.UI.events.apiPreHandlers.preview = function ( event ) {
-			var $this = $( this ),
-				callback;
+			var callback,
+				$this = $( this );
 
 			callback = function ( queryMap ) {
 				var content = null;
@@ -404,8 +404,8 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.board = function ( info, data, jqxhr ) {
-			var flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
-				$rendered;
+			var $rendered,
+				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) );
 
 			if ( info.status !== 'done' ) {
 				// Error will be displayed by default, nothing else to wrap up
@@ -430,9 +430,9 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.loadMore = function ( info, data, jqxhr ) {
-			var $target = $( this ).closest( '.flow-load-more' ),
-				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $target ),
-				$tmp;
+			var $tmp,
+				$target = $( this ).closest( '.flow-load-more' ),
+				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $target );
 
 			if ( info.status !== 'done' ) {
 				// Error will be displayed by default, nothing else to wrap up
@@ -495,9 +495,9 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.activateEditHeader = function ( info, data, jqxhr ) {
-			var flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
-				$oldBoardNodes,
-				$rendered;
+			var $rendered,
+				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
+				$oldBoardNodes;
 
 			if ( info.status !== 'done' ) {
 				// Error will be displayed by default & edit conflict handled, nothing else to wrap up
@@ -530,8 +530,8 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.submitHeader = function ( info, data, jqxhr ) {
-			var flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
-				$rendered;
+			var $rendered,
+				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) );
 
 			if ( info.status !== 'done' ) {
 				// Error will be displayed by default & edit conflict handled, nothing else to wrap up
@@ -602,9 +602,8 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.closeOpenTopic = function ( info, data ) {
-			var revision,
-				$target = info.$target, $topicTitleBar,
-				topicId, revisionId,
+			var revision, topicId, revisionId, $topicTitleBar,
+				$target = info.$target,
 				$topic = $target.parents( '.flow-topic' ),
 				self = this,
 				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
@@ -735,12 +734,12 @@
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.preview = function( info, data, jqxhr ) {
 			var revision, creator,
+				$previewContainer,
+				templateParams,
 				$button = $( this ),
 				$form = $button.closest( 'form' ),
 				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $form ),
 				$titleField = $form.find( 'input' ).filter( '[data-role=title]' ),
-				$previewContainer,
-				templateParams,
 				$target = info.$target,
 				username = mw.user.getName(),
 				id = Math.random(),
@@ -912,15 +911,14 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.watchItem = function ( info, data, jqxhr ) {
-			var $target = $( this ),
+			var watchUrl, unwatchUrl,
+				watchType, watchLinkTemplate, $newLink,
+				$target = $( this ),
 				$tooltipTarget = $target.closest( '.flow-watch-link' ),
 				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $tooltipTarget ),
 				isWatched = false,
 				url = $( this ).prop( 'href' ),
-				watchUrl, unwatchUrl,
-				watchType, watchLinkTemplate,
-				links = {},
-				$newLink;
+				links = {};
 
 			if ( info.status !== 'done' ) {
 				// Error will be displayed by default, nothing else to wrap up
@@ -1042,10 +1040,9 @@
 		 * @param {jqXHR} jqxhr
 		 */
 		FlowBoardComponent.UI.events.apiHandlers.activateEditPost = function ( info, data, jqxhr ) {
-			var flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
-				$post = info.$target,
-				$rendered,
-				rootBlock;
+			var $rendered, rootBlock,
+				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
+				$post = info.$target;
 
 			if ( info.status !== 'done' ) {
 				// Error will be displayed by default, nothing else to wrap up
@@ -1319,8 +1316,8 @@
 		 * @param {Event} event
 		 */
 		FlowBoardComponent.UI.events.interactiveHandlers.collapserCollapsibleToggle = function ( event ) {
-			var $target = $( event.target ),
-				topicId, states,
+			var topicId, states,
+				$target = $( event.target ),
 				$component = $( this ).closest( '.flow-component' );
 
 			// Make sure we didn't click on any interactive elements
@@ -1373,11 +1370,10 @@
 		 * @param {Event} event
 		 */
 		FlowBoardComponent.UI.events.interactiveHandlers.editTopicTitle = function( event ) {
-			var $link = $( this ),
+			var $title, flowBoard, $form, cancelCallback, linkParams,
+				$link = $( this ),
 				$topic = $link.closest( '.flow-topic' ),
-				$topicTitleBar = $topic
-					.children( '.flow-topic-titlebar' ),
-				$title, flowBoard, $form, cancelCallback, linkParams;
+				$topicTitleBar = $topic.children( '.flow-topic-titlebar' );
 
 			$form = $topicTitleBar.find( 'form' );
 
@@ -1449,13 +1445,13 @@
 		 */
 		FlowBoardComponent.UI.events.interactiveHandlers.apiRequest = function ( event ) {
 			var $deferred,
+				$target,
+				preHandlerReturn,
 				_this = this,
 				$this = $( this ),
 				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $this ),
 				dataParams = $this.data(),
 				handlerName = dataParams.flowApiHandler,
-				$target,
-				preHandlerReturn,
 				preHandlerReturns = [],
 				info = {
 					$target: null,
@@ -1545,9 +1541,10 @@
 						FlowBoardComponent.UI.events.apiHandlers[ handlerName ].apply( _this, args );
 					} )
 					.fail( function ( code, result ) {
-						var args = Array.prototype.slice.call( arguments, 0 ),
-							$form = $this.closest( 'form' ),
-							errorMsg;
+						var errorMsg,
+							args = Array.prototype.slice.call( arguments, 0 ),
+							$form = $this.closest( 'form' );
+
 						info.status = 'fail';
 						args.unshift( info );
 
@@ -1582,14 +1579,14 @@
 		FlowBoardComponent.UI.events.interactiveHandlers.activateReplyPost = function ( event ) {
 			event.preventDefault();
 
-			var flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
+			var $form,
+				flowBoard = FlowBoardComponent.prototype.getInstanceByElement( $( this ) ),
 				$post = $( this ).closest( '.flow-post' ),
 				$targetPost = $( this ).closest( '.flow-post:not([data-flow-post-max-depth])' ),
 				postId = $targetPost.data( 'flow-id' ),
 				topicTitle = $post.closest( '.flow-topic' ).find( '.flow-topic-title' ).text(),
 				replyToContent = $post.find( '.flow-post-content' ).text() || topicTitle,
-				author = $.trim( $post.find( '.flow-author:first .mw-userlink' ).text() ),
-				$form;
+				author = $.trim( $post.find( '.flow-author:first .mw-userlink' ).text() );
 
 			// Check if reply form has already been opened
 			if ( $post.data( 'flow-replying' ) ) {
@@ -2259,9 +2256,7 @@
 		 * @param  {Element|jQuery} topic The (single) topic element to show
 		 */
 		FlowBoardComponent.UI.expandTopicIfNecessary = function( topic ) {
-			var $component,
-				isFullView,
-				isInverted,
+			var $component, isFullView, isInverted,
 				$topic = $( topic );
 
 			$component = $topic.closest( '.flow-component' );
