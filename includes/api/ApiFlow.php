1c89e824ac9576e5ed542e0a77d1adcbd82dae19
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-09-04 14:59:53 -0700

    Cannot load non-ascii flow pages via api
    This is a temporary fix which will only be deployed to 1.24wmf19
    to allow the new deployment on hewiki and frwiki to test the
    respective pages.
    
    Best guess is the javascript is double encoding the api requests,
    a patch for that will be incoming.
    
    Change-Id: Ib9aa3a6ce9b8900d449a7fc15508a11dfbc67b9a

--- includes/api/ApiFlow.php
+++ includes/api/ApiFlow.php
@@ -106,7 +106,7 @@ class ApiFlow extends ApiBase {
 	 */
 	protected function getPage( $params ) {
 		if ( isset( $params['page'] ) ) {
-			$page = Title::newFromText( $params['page'] );
+			$page = Title::newFromText( urldecode( $params['page'] ) );
 			if ( !$page ) {
 				$this->dieUsage( 'Invalid page provided', 'invalid-page' );
 			}
