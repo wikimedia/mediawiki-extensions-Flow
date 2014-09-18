445e5f253e6e75f494bd68b9a1738fd10b8cb428
Matthias Mullie <git@mullie.eu>
2014-09-18 17:49:50 +0200

    [WIP] search
    includes/api/ApiSearchFlow.php will be the API endpoint.
    includes/Search/* provides the search backend (mimicking Cirrus' classes, but
    adapted to our Flow data.)
    includes/Formatter/SearchQuery is the glue between our API endpoint & the search
    classes - it's similar to our other formatter classes.
    maintenance/FlowForceSearchIndex.php is the script to index all Flow data in ES.
    
    To index all data, run:
        $ php maintenance/FlowForceSearchIndex.php
    To search all indexed data, call the new API: e.g.
        http://mediawiki.dev/api.php?action=flow&submodule=search&qterm=test
    Said API call should translate to this in ES:
        http://localhost:9200/enwiki_flow/topic/_search?pretty=true&q=test
    
    Here's how the data is indexed in ES:
    
    http://localhost:9200/enwiki_flow/topic/:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"revisions": [ {
    		"id": "rpvwvywl9po7ih77",
    		"text": "topic title content",
    		"source_text": "topic title content",
    		"moderation_state": "",
    		"timestamp": "2014-02-25T14:12:40Z",
    		"type": "topic"
    	}, {
    		"id": "ropuzninqgyf19ko",
    		"text": "reply content",
    		"source_text": "reply '''content'''",
    		"moderation_state": "hide",
    		"timestamp": "2014-02-25T14:12:40Z",
    		"type": "post"
    	} ]
    }
    
    http://localhost:9200/enwiki_flow/header/:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"revisions": [ {
                    "id": "s1ijdhjhqeoq2b2r",
                    "text": "header content",
    		"source_text": "header content",
                    "moderation_state": "",
                    "timestamp": "2014-02-07T01:42:57Z",
    		"type": "header"
    	} ]
    }
    
    We can do a full-text search, which can be filtered to only a (couple of)
    page(s) or namespace(s). This should, for example, translate to (in ES):
    
    // search for text, but only in specific page:
    // API: api.php?action=flow&submodule=search&qterm=test&qtitle=Talk:Test
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
            "query": {
                    "filtered": {
                            "query": {
                                    "term": { "revisions.text": "test" }
                            },
                            "filter": {
                                    "term": { "pageid": 24 }
                            }
                    }
            }
    }'
    
    The data stored allows us query for a bit more down the road, e.g.:
    
    Example queries (to ES):
    
    // find moderated stuff
    // this will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"term": { "revisions.moderation_state": "hide" }
    			}
    		}
    	}
    }'
    
    // find unread posts (will probably just check posts more recent than last visit)
    // will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"range": {
    					"revisions.timestamp": { "gt": "2014-04-01T00:00:00Z" }
    				}
    			}
    		}
    	}
    }'
    
    Change-Id: Ib1229acc09b26c65dc9f08c7ea0f19b8191b799b

---
+++ includes/Search/Connection.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Flow\Search;
+
+use Elastica\SearchableInterface;
+
+/**
+ * Provides the connection to the elasticsearch backend.
+ */
+class Connection extends \ElasticaConnection {
+	/**
+	 * Name of the topic type.
+	 *
+	 * @var string
+	 */
+	const TOPIC_TYPE_NAME = 'topic';
+
+	/**
+	 * Name of the header type.
+	 *
+	 * @var string
+	 */
+	const HEADER_TYPE_NAME = 'header';
+
+	/**
+	 * Name of the index that holds Flow data.
+	 *
+	 * @var string
+	 */
+	const FLOW_INDEX_TYPE = 'flow';
+
+	/**
+	 * @return string[]
+	 */
+	public function getServerList() {
+		global $wgFlowSearchServers;
+		return $wgFlowSearchServers;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getMaxConnectionAttempts() {
+		global $wgFlowSearchConnectionAttempts;
+		return $wgFlowSearchConnectionAttempts;
+	}
+
+	/**
+	 * Get all index types we support.
+	 *
+	 * @return string[]
+	 */
+	public static function getAllIndexTypes() {
+		return array( static::TOPIC_TYPE_NAME, static::HEADER_TYPE_NAME );
+	}
+
+	/**
+	 * @param string $name
+	 * @param string|bool $type Type name or false to search entire index
+	 * @return SearchableInterface
+	 */
+	public static function getRevisionType( $name, $type = false ) {
+		$index = static::getIndex( $name, static::FLOW_INDEX_TYPE );
+
+		if ( $type ) {
+			$index = $index->getType( $type );
+		}
+
+		return $index;
+	}
+}
