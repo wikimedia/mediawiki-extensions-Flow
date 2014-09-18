07b8a92fef54d888d475f149d658c4b5f3db6e62
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-09-17 20:16:57 -0700

    Don't treat topic title as wikitext
    Topic titles inserted into l10n via plaintextSnippet were receiving
    template substitution via Message::text().
    
    This patch changes it arround to always output the l10n safe topic-of-post
    property on topic titles so there is always a safe value to stick into
    l10n when dealing with a topic title in a template.
    
    Change-Id: I1ce7a49304abe87ec663c193461def4ee053273d

--- modules/new/components/flow-board.js
+++ modules/new/components/flow-board.js
@@ -1601,9 +1601,8 @@
 						name: author
 					},
 					// text for flow-reply-topic-title-placeholder placeholder
-					content: {
-						content: replyToContent,
-						format: 'plaintext'
+					properties: {
+						'topic-of-post': replyToContent.substr( 0, 200 ).trim()
 					}
 				}
 			) ).children();
