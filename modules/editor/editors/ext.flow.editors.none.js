2b0b0a03b88b368d35b9bbb043bf10c6f7d03511
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-09-23 12:28:34 -0700

    Set a maximum height to auto-expanding text fields
    Limit the height of an auto-expanding text field such that the buttons
    below the textarea do not scroll off-screen.
    
    Bug: 58657
    Change-Id: I3fbed3f7764a8331cdfc313dfdaa781340032ab5

--- modules/editor/editors/ext.flow.editors.none.js
+++ modules/editor/editors/ext.flow.editors.none.js
@@ -6,17 +6,21 @@
 	 * @param {string} [content='']
 	 */
 	mw.flow.editors.none = function ( $node, content ) {
+		var self = this;
+
 		this.$node = $node;
 		this.$node.val( content || '' );
 
-		this.$node.css( 'overflow', 'hidden' );
 		this.$node.css( 'resize', 'none' );
+		this.$node.css( 'overflow-y', 'hidden' );
 
 		// auto-expansion shouldn't shrink too much; set default height as min
 		this.$node.css( 'min-height', this.$node.outerHeight() );
 
 		// initialize at height of existing content & update on every keyup
-		this.$node.keyup( this.autoExpand );
+		this.$node.keyup( function ( event ) {
+			self.autoExpand.call( this );
+		} );
 		this.autoExpand.call( this.$node.get( 0 ), 0 );
 	};
 
@@ -49,9 +53,16 @@
 	 * @param {int} [animationTime] Time in milliseconds to animate to new height.
 	 */
 	mw.flow.editors.none.prototype.autoExpand = function( animationTime ) {
-		var height = $( this ).height(),
-			padding = $( this ).outerHeight() - $( this ).height(),
-			scrollHeight;
+		var scrollHeight, deltaHeight,
+			$this = $( this ),
+			height = $this.height(),
+			padding = $this.outerHeight() - $this.height() + 5,
+			prevScrollHeight = $this.data( 'flow-prev-scroll-height' ),
+			$buttons = $this.closest( 'form' ).find( '.flow-form-actions' ),
+			buttonBottom = $buttons.offset().top + $buttons.height(),
+			windowBottom = $( window ).scrollTop() + $( window ).height(),
+			// additional padding of 20px below button bottom
+			maxHeightIncrease = windowBottom - buttonBottom - 20;
 
 		// if not specified, default animation time = 50
 		if ( typeof animationTime !== 'number' ) {
@@ -67,16 +78,28 @@
 		 * Since we're looking to also shrink the textarea when content shrinks,
 		 * we want to ignore that last case (hence the collapsing)
 		 */
-		$( this ).height( 0 );
+		$this.height( 0 );
 		scrollHeight = this.scrollHeight - padding;
-		$( this ).height( height );
+		$this.height( height );
+		deltaHeight = scrollHeight - height;
 
+		/**
 		/*
 		 * Only animate height change if there actually is a change; we don't
 		 * want every keystroke firing a 50ms animation.
 		 */
-		if ( scrollHeight !== $( this ).height() ) {
-			$( this ).animate( { height: scrollHeight + padding }, animationTime );
+		if ( deltaHeight >= maxHeightIncrease ) {
+			// If we can't expand ensure overflow-y is set to auto
+			$this.css( 'overflow-y', 'auto' );
+		} else if (
+			scrollHeight !== prevScrollHeight &&
+			scrollHeight + 5 !== $this.height()
+		) {
+			$this.css( {
+				height: scrollHeight + padding,
+				'overflow-y': 'hidden'
+			} );
+			$this.data( 'flow-prev-scroll-height', scrollHeight );
 		}
 	};
 
