a9132fa32f7a2012d4faca0db875b53fea35079a
S Page <spage@wikimedia.org>
2014-09-30 12:11:36 -0700

    build: Implement Grunt workflow (via npm test)
    Based on VisualEditor's Gruntfile.
    
    No jsduck (yet) and no build task.
    
    To test, `grunt test`.  This runs csslint, jshint, jscs
    (https://github.com/jscs-dev/node-jscs).
    
    We don't currently comply with Wikimedia coding style, so for now
    most rules have been disabled in jscsrc.
    
    Change-Id: I2400db13194e3ae4b0e7ad1e1f4d241c5d4e6284

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+/*!
+ * Grunt file
+ *
+ * @package Flow
+ */
+
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-csslint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'modules/**/*.js'
+			]
+		},
+		jscs: {
+			src: [ '<%= jshint.all %>', '!modules/new/vendor/**' ]
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			all: 'modules/**/*.css'
+		},
+		banana: {
+			all: 'i18n/'
+		},
+		watch: {
+			files: [
+				'.{csslintrc,jscsrc,jshintignore,jshintrc}',
+				'<%= jshint.all %>',
+				'<%= csslint.all %>'
+			],
+			tasks: 'test'
+		}
+	} );
+
+	grunt.registerTask( 'lint', [ 'jshint', 'jscs', 'csslint', 'banana' ] );
+	grunt.registerTask( 'test', 'lint' );
+	grunt.registerTask( 'default', 'test' );
+};
