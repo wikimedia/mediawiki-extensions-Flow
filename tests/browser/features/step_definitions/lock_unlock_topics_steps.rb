4fe91683355e75b0aca77e8debdf256d72c36c58
Cmcmahon <cmcmahon@wikimedia.org>
2014-10-03 09:31:49 -0700

    QA: update RSpec assertions and alphabetize
    Just bookkeeping, no change to function
    
    Change-Id: Ib4d63c877959339f68c12a0f7220355022fff6c4

--- tests/browser/features/step_definitions/lock_unlock_topics_steps.rb
+++ tests/browser/features/step_definitions/lock_unlock_topics_steps.rb
@@ -1,3 +1,11 @@
+Given(/^I click the Lock topic button$/) do
+  on(FlowPage).topic_lock_button_element.when_present.click
+end
+
+Given(/^I click the Unlock topic button$/) do
+  on(FlowPage).topic_unlock_button_element.when_present.click
+end
+
 Given(/^the top post has been locked$/) do
   step 'I click the Topic Actions link'
   step 'I click the Lock topic button'
@@ -5,12 +13,19 @@ Given(/^the top post has been locked$/) do
   step 'I submit the lock/unlock topic form'
 end
 
-Given(/^I click the Lock topic button$/) do
-  on(FlowPage).topic_lock_button_element.when_present.click
+When(/^I cancel the lock\/unlock topic form$/) do
+  on(FlowPage).topic_lock_form_cancel_button_element.when_present.click
 end
 
-Given(/^I click the Unlock topic button$/) do
-  on(FlowPage).topic_unlock_button_element.when_present.click
+When(/^I expand the top post$/) do
+  on(FlowPage).flow_first_topic_heading_element.when_present.click
+end
+
+When(/^I submit the lock\/unlock topic form$/) do
+  on(FlowPage) do |page|
+    page.topic_lock_form_lock_button_element.when_present.click
+    page.topic_lock_form_lock_button_element.when_not_present
+  end
 end
 
 When(/^I type "(.*?)" as the reason$/) do |reason|
@@ -21,42 +36,34 @@ When(/^I type "(.*?)" as the reason$/) do |reason|
   on(FlowPage).topic_lock_form_reason_element.send_keys(reason)
 end
 
-When(/^I cancel the lock\/unlock topic form$/) do
-  on(FlowPage).topic_lock_form_cancel_button_element.when_present.click
-end
-
-When(/^I submit the lock\/unlock topic form$/) do
+Then(/^I do not see the lock\/unlock form$/) do
   on(FlowPage) do |page|
-    page.topic_lock_form_lock_button_element.when_present.click
-    page.topic_lock_form_lock_button_element.when_not_present
+    page.topic_lock_form_element.when_not_present
+    expect(page.topic_lock_form_element).not_to be_visible
   end
 end
 
-Then(/^the top post is a locked discussion$/) do
-  on(FlowPage).flow_first_topic_moderation_msg_element.when_present.should be_visible
-end
-
-Then(/^the top post is an open discussion$/) do
-  on(FlowPage).flow_first_topic_moderation_msg_element.when_not_present
-end
-
-Then(/^the topic summary of the first topic is "(.*?)"$/) do |text|
-  on(FlowPage).flow_first_topic_summary_element.text.should match text
-end
-
-Then(/^I expand the top post$/) do
-  on(FlowPage).flow_first_topic_heading_element.when_present.click
-end
-
-Then(/^I do not see the lock\/unlock form$/) do
-  on(FlowPage).topic_lock_form_element.when_not_present
+Then(/the original message for the top post has no edit link$/) do
+  expect(on(FlowPage).flow_first_topic_original_post_edit_element).not_to be_visible
 end
 
 Then(/^the original message for the top post has no reply link$/) do
-  on(FlowPage).flow_first_topic_original_post_reply_element.should_not exist
+  expect(on(FlowPage).flow_first_topic_original_post_reply_element).not_to be_visible
 end
 
-Then(/the original message for the top post has no edit link$/) do
-  on(FlowPage).flow_first_topic_original_post_edit_element.should_not exist
+Then(/^the top post is a locked discussion$/) do
+  expect(on(FlowPage).flow_first_topic_moderation_msg_element.when_present).to be_visible
 end
+
+Then(/^the top post is an open discussion$/) do
+  on(FlowPage) do |page|
+    page.flow_first_topic_moderation_msg_element.when_not_present
+    expect(page.flow_first_topic_moderation_msg_element).not_to be_visible
+  end
+end
+
+Then(/^the topic summary of the first topic is "(.*?)"$/) do |text|
+  expect(on(FlowPage).flow_first_topic_summary_element.text).to match text
+end
+
 
