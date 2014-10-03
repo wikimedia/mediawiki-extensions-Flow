8830009014e668374c9ce8a592c4b796e11038ee
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-10-03 13:28:00 -0700

    Hygiene: Push page creation to DeferredUpdates
    In a followup patch to this I add tests to run the flow API
    directly, one issue that was run into is that the doEditPage call
    within the controller starts and ends a transaction, but we are
    currently within the flow transaction and don't want that closed
    yet.  As such push the update into the deferred updates, using
    the queue so it only happens when the commit succedes.
    
    Additionally this call to the occupation controller is responsible
    for 15-20% of the runtime of a new-topic action. This wont likely
    help API consumers, i believe the xhr code is waiting for the request
    to complete, but browsers submitting the form directly should get
    their response on screen faster.
    
    Change-Id: I9bad5d2def0f58bc9cc1751a648ee54bfbcbf4c1

--- includes/Data/Listener/OccupationListener.php
+++ includes/Data/Listener/OccupationListener.php
@@ -7,6 +7,7 @@ use DeferredUpdates;
 use Flow\Data\LifecycleHandler;
 use Flow\Model\Workflow;
 use Flow\OccupationController;
+use SplQueue;
 
 /**
  * Ensures that a given workflow is occupied.  This will be unnecssary
@@ -16,6 +17,9 @@ class OccupationListener implements LifecycleHandler {
 	/** @var OccupationController **/
 	protected $occupationController;
 
+	/** @var SplQueue */
+	protected $deferredQueue;
+
 	/** @var string **/
 	protected $defaultType;
 
@@ -24,10 +28,16 @@ class OccupationListener implements LifecycleHandler {
 
 	/**
 	 * @param OccupationController $occupationController The OccupationController to occupy the page with.
+	 * @param SplQueue             $deferredQueue        Queue of callbacks to run only if commit succedes
 	 * @param string               $defaultType          The workflow type to look for
 	 */
-	public function __construct( OccupationController $occupationController, $defaultType ) {
+	public function __construct(
+		OccupationController $occupationController,
+		SplQueue $deferredQueue,
+		$defaultType
+	) {
 		$this->occupationController = $occupationController;
+		$this->deferredQueue = $deferredQueue;
 		$this->defaultType = $defaultType;
 	}
 
@@ -53,10 +63,13 @@ class OccupationListener implements LifecycleHandler {
 
 	protected function ensureOccupation( Workflow $workflow ) {
 		if ( $this->enabled ) {
-			$this->occupationController->ensureFlowRevision(
-				new Article( $workflow->getArticleTitle() ),
-				$workflow
-			);
+			$controller = $this->occupationController;
+			$this->deferredQueue->push( function() use ( $controller, $workflow ) {
+				$controller->ensureFlowRevision(
+					new Article( $workflow->getArticleTitle() ),
+					$workflow
+				);
+			} );
 		}
 	}
 
