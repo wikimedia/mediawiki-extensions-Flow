832848e6e59c7cb64c385f12f978473e41c6ffdd
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-10-03 11:53:50 -0700

    Refactor out calls to RequestContext::getMain()
    While trying to write unit tests against the API i ran into a problem,
    namely that RequestContext::getMain()->getUser()->getName() always returned
    127.0.0.1 regardless of the user provided to the api in the unit
    test.  This is because the main request context is the wrong context.
    
    Thankfully during the api output refactors we used the context throughout, the
    only changes necessary were to pass the correct context from the Action and API
    wrappers into the block instances, so they could pass it on to the formatters.
    
    This additionally adds a POC test for ApiFlowLockTopic demonstrating that we
    can now pass in a priveleged user and perform priveleged actions.
    
    There is still some mess left behind,  namely the value user value in the
    container which is used in some classes.  I think the best solution here is
    going to be to remove all static calls to Container, and instead create the
    container on entry from API or Action specifically for that context.  One
    container per context, so everything inside points to the right values.  But
    that will require several patches removing calls to Container::get, and probably
    passing containers directly into the block instances.
    
    For now this appears to work, and this isn't actually a problem we've ever seen
    in production, its a theoretical problem that is affecting testability.
    
    Change-Id: Ia22eb12f2bfc5c5e3356a021b6df960fd26290cf

---
+++ tests/phpunit/api/ApiFlowLockTopicTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Flow\Tests\Api;
+
+use Flow\Container;
+use FlowHooks;
+use User;
+
+/**
+ * @group Flow
+ * @group medium
+ */
+class ApiFlowLockTopicTest extends \ApiTestCase {
+	protected function setUp() {
+		$this->setMwGlobals( 'wgFlowOccupyPages', array( 'Talk:Flow QA' ) );
+		parent::setUp();
+	}
+
+	protected function getEditToken() {
+		$tokens = $this->getTokenList( self::$users['sysop'] );
+		return $tokens['edittoken'];
+	}
+
+	protected function doApiRequest(
+		array $params,
+		array $session = null,
+		$appendModule = false,
+		User $user = null
+	) {
+		// reset flow state before each request
+		FlowHooks::resetFlowExtension();
+		Container::reset();
+		$container = Container::getContainer();
+		$container['user'] = $user ?: self::$users['sysop']->user;
+		return parent::doApiRequest( $params, $session, $appendModule, $user );
+	}
+
+	public function testLockTopic() {
+		$workflowId = $this->createTopic();
+		$data = $this->doApiRequest( array(
+			'page' => "Topic:$workflowId",
+			'token' => $this->getEditToken(),
+			'action' => 'flow',
+			'submodule' => 'lock-topic',
+			'cotmoderationState' => 'lock',
+			'cotsummary' => 'fiddle faddle',
+			'cotprev_revision' => null,
+		) );
+
+		$result = $data[0]['flow']['lock-topic']['result']['topic'];
+		$this->assertCount( 0, $result['errors'] );
+		$this->assertEquals( $workflowId, $result['workflowId'] );
+		$this->assertEquals( 'lock-topic', $result['changeType'] );
+		$this->assertTrue( $result['isModerated'] );
+		$this->assertArrayHasKey( 'unlock', $result['actions'] );
+		$this->assertEquals( 'fiddle faddle', $result['moderateReason'] );
+	}
+
+	public function testUnlockTopic() {
+		$workflowId = $this->createTopic();
+		$data = $this->doApiRequest( array(
+			'page' => "Topic:$workflowId",
+			'token' => $this->getEditToken(),
+			'action' => 'flow',
+			'submodule' => 'lock-topic',
+			'cotmoderationState' => 'lock',
+			'cotsummary' => 'fiddle faddle',
+			'cotprev_revision' => null,
+		) );
+		$result = $data[0]['flow']['lock-topic']['result']['topic'];
+		$this->assertCount( 0, $result['errors'] );
+
+		$data = $this->doApiRequest( array(
+			'page' => "Topic:$workflowId",
+			'token' => $this->getEditToken(),
+			'action' => 'flow',
+			'submodule' => 'lock-topic',
+			'cotmoderationState' => 'unlock',
+			'cotsummary' => 'Ether',
+			'cotprev_revision' => $result['summary']['revId'],
+		) );
+
+		$result = $data[0]['flow']['lock-topic']['result']['topic'];
+		$this->assertCount( 0, $result['errors'] );
+		$this->assertEquals( 'restore-topic', $result['changeType'] );
+		$this->assertFalse( $result['isModerated'] );
+		$this->assertArrayHasKey( 'lock', $result['actions'] );
+		$this->assertEquals(
+			'<p data-parsoid=\'{"dsr":[0,5,0,0]}\'>Ether</p>',
+			$result['summary']['content']
+		);
+	}
+
+	protected function createTopic() {
+		$data = $this->doApiRequest( array(
+			'page' => 'Talk:Flow QA',
+			'token' => $this->getEditToken(),
+			'action' => 'flow',
+			'submodule' => 'new-topic',
+			'nttopic' => 'Hi there!',
+			'ntcontent' => '...',
+		) );
+		$this->assertTrue(
+			// @todo we should return the new id much more directly than this
+			isset( $data[0]['flow']['new-topic']['result']['topiclist']['roots'][0] ),
+			'Api response must contain new topic id'
+		);
+
+		return $data[0]['flow']['new-topic']['result']['topiclist']['roots'][0];
+	}
+}
