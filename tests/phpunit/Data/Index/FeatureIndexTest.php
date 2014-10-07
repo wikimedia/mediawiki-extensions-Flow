d5208b488f232b8b0e8c07e230421aa765681440
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-10-07 15:21:47 -0700

    I103: Duplicated topics are returned
    Duplicated topics were being returned when a second query was run but the
    index did not properly slice the results due to looking for the offset-key
    options instead of offset-id.  I don't think it was ever intended for this
    to be two options so all offset-key usages were replaced with offset-id
    and a test was written to verify it slices in the appropriate place.
    
    Change-Id: I2f8d69730b8a88e6270b7408922d4775accf7039

---
+++ tests/phpunit/Data/Index/FeatureIndexTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Flow\Tests\Data\Index;
+
+use Flow\Data\Index\FeatureIndex;
+
+class UniqueFeatureIndexTests extends \MediaWikiTestCase {
+
+	public function testOffsetIdReturnsCorrectPortionOfIndexedValues() {
+		global $wgFlowCacheVersion;
+		$cache = $this->getMockBuilder( 'Flow\Data\BufferedCache' )
+			->disableOriginalConstructor()
+			->getMock();
+		$storage = $this->getMockBuilder( 'Flow\Data\ObjectStorage' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$dbId = FeatureIndex::cachedDbId();
+		$cache->expects( $this->any() )
+			->method( 'getMulti' )
+			->will( $this->returnValue( array(
+				"$dbId:foo:5:$wgFlowCacheVersion" => array(
+					array( 'some_row' => 40 ),
+					array( 'some_row' => 41 ),
+					array( 'some_row' => 42 ),
+					array( 'some_row' => 43 ),
+					array( 'some_row' => 44 ),
+				),
+			) ) );
+		$storage->expects( $this->never() )
+			->method( 'findMulti' );
+
+		$index = new MockFeatureIndex( $cache, $storage, 'foo', array( 'bar' ) );
+
+		$res = $index->find(
+			array( 'bar' => 5 ),
+			array( 'offset-id' => 42 )
+		);
+
+		$this->assertEquals(
+			array(
+				array( 'some_row' => 43, 'bar' => 5 ),
+				array( 'some_row' => 44, 'bar' => 5 ),
+			),
+			array_values( $res ),
+			'Returns items with some_row > provided offset-id of 42'
+		);
+	}
+}
+
+class MockFeatureIndex extends FeatureIndex {
+	public function getLimit() { return 42; }
+	public function queryOptions() { return array(); }
+	public function limitIndexSize( array $values ) { return $values; }
+	public function addToIndex( array $indexed, array $row ) {}
+	public function removeFromIndex( array $indexed, array $row ) {}
+
+	// not abstract, but override for convenience
+	public function getSort() { return array( 'some_row' ); }
+	public function getOrder() { return 'ASC'; }
+}
