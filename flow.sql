f4474d0fb689da2484402ee6e9d5facab71ca6a2
Reedy <reedy@wikimedia.org>
2014-10-08 20:52:07 +0100

    Add missing /*$wgDBTableOptions*/
    Change-Id: Id7fdc99dcef82b487bb5229e8f8edf9399d794b9

--- flow.sql
+++ flow.sql
@@ -145,7 +145,7 @@ CREATE TABLE /*_*/flow_wiki_ref (
 	ref_target_namespace int not null,
 	ref_target_title varbinary(255) not null,
 	ref_type varbinary(16) not null
-);
+) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX /*i*/flow_wiki_ref_pk ON /*_*/flow_wiki_ref
 	(ref_src_namespace, ref_src_title, ref_type, ref_target_namespace, ref_target_title, ref_src_object_type, ref_src_object_id);
@@ -161,7 +161,7 @@ CREATE TABLE /*_*/flow_ext_ref (
 	ref_src_title varbinary(255) not null,
 	ref_target varbinary(255) not null,
 	ref_type varbinary(16) not null
-);
+) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX /*i*/flow_ext_ref_pk ON /*_*/flow_ext_ref
 	(ref_src_namespace, ref_src_title, ref_type, ref_target, ref_src_object_type, ref_src_object_id);
