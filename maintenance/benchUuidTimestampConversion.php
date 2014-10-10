3a0cfa2675e2ed65c74007a3b1a448379248d8c2
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-10-10 13:12:51 -0700

    Perf: Improve speed of conversion from uuid to timestamp
    Measures out as an ~40x increase on my laptop, and this is run at least once
    for every revision we output to get the timestamp it was created at.
    
    Change-Id: Ib933b750845999511bd7eec7c823c2539393d60e

---
+++ maintenance/benchUuidTimestampConversion.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once __DIR__ . '/../../../maintenance/commandLine.inc';
+
+class SlowUuid extends \Flow\Model\UUID {
+	public function getTimestampObj() {
+		if ( $this->timestamp === null ) {
+			if ( $this->alphadecimalValue ) {
+				$bits = wfBaseConvert( $this->alphadecimalValue, 36, 2, 88 );
+			} else {
+				$bits = wfBaseConvert( $this->getHex(), 16, 2, 88 );
+			}
+			$msTimestamp = wfBaseConvert( substr( $bits, 0, 46 ), 2, 10 );
+			$this->timestamp = new MWTimestamp( intval( $msTimestamp / 1000 ) );
+		}
+		return clone $this->timestamp;
+	}
+}
+
+function benchmark( $num, $class ) {
+	$ids = array();
+	for ( ; $num > 0; $num = $num - 10 ) {
+		// ids take awhile to create, so clone a few
+		$ids[] = $new = $class::create();
+		for( $i = 0; $i < 9; ++$i ) {
+			$ids[] = clone $new;
+		}
+	}
+	$start = microtime( true );
+	foreach ( $ids as $id ) {
+		$id->getTimestampObj();
+	}
+
+	return microtime( true ) - $start;
+}
+
+function main() {
+	$time = benchmark( 1000, 'Flow\Model\UUID' );
+	echo "Current implementation took ", $time/1000, "ms per conversion\n";
+
+	$time = benchmark( 1000, 'SlowUUID' );
+	echo "Original implementationTook ", $time/1000, "ms per conversion\n";
+}
+
+for( $i = 0; $i < 10; ++$i ) {
+	main();
+	echo "---\n";
+}
+
