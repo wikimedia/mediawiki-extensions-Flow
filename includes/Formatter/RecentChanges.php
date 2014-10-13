4c5b533c28f860befbc7e568a8026288e2438b27
Matthias Mullie <git@mullie.eu>
2014-10-13 15:08:58 +0200

    Make watchlist topic title link to topic with with unread posts highlighted
    Change-Id: Icbd1f38196f9fbcb72f17cfc28752b3cf5783137

--- includes/Formatter/RecentChanges.php
+++ includes/Formatter/RecentChanges.php
@@ -3,7 +3,9 @@
 namespace Flow\Formatter;
 
 use Flow\Exception\FlowException;
+use Flow\Model\Anchor;
 use Flow\Model\PostRevision;
+use Flow\Model\UUID;
 use Flow\Parsoid\Utils;
 use ChangesList;
 use IContextSource;
@@ -67,5 +69,47 @@ class RecentChanges extends AbstractFormatter {
 			  $ctx
 			) .
 			( Utils::htmlToPlaintext( $description ) ? $separator . $description : '' );
+	}
+
+	/**
+	 * This overrides the default title link to include highlights for the posts
+	 * that have not yet been seen.
+	 *
+	 * @param array $data
+	 * @param FormatterRow $row
+	 * @param IContextSource $ctx
+	 * @return string
+	 */
+	protected function getTitleLink( array $data, FormatterRow $row, IContextSource $ctx ) {
+		if ( !$row instanceof RecentChangesRow ) {
+			// actually, this should be typehint, but can't because this needs
+			// to match the parent's more generic typehint
+			return parent::getTitleLink( $data, $row, $ctx );
+		}
+
+		$watched = $row->recentChange->getAttribute( 'wl_notificationtimestamp' );
+		if ( is_bool( $watched ) ) {
+			// RC & watchlist share most code; the latter is unaware of when
+			// something was watched though, so we'll ignore that here
+			return parent::getTitleLink( $data, $row, $ctx );
+		}
+
+		if ( $watched === null ) {
+			// there is no data for unread posts - they've all been seen
+			return parent::getTitleLink( $data, $row, $ctx );
+		}
+
+		// get comparison UUID corresponding to this last watched timestamp
+		$uuid = UUID::getComparisonUUID( $watched );
+
+		// add highlight details to anchor
+		/** @var Anchor $anchor */
+		$anchor = clone $data['links']['topic'];
+		$anchor->query['fromnotif'] = '1';
+		$anchor->fragment = '#flow-post-' . $uuid->getAlphadecimal();
+		$data['links']['topic'] = $anchor;
+
+		// now pass it on to parent with the new, updated, link ;)
+		return parent::getTitleLink( $data, $row, $ctx );
 	}
 }
