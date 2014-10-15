4dfce4c4c1946de9e89e20960a3cee486ba47f1c
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-10-15 10:00:06 -0700

    J-1: Comment moderation updates
    Details: https://www.mediawiki.org/wiki/Flow/Hide_spec
    
    Change-Id: I176221bb8212f382f9248c4b47247f1ade560c6c

---
+++ modules/contributions/base.js
@@ -0,0 +1,27 @@
+(function( $, mw ) {
+	function clickedFlowLink( event ) {
+		var $container = $( event.delegateTarget ),
+			onComplete = function() {
+				$( event.target ).click();
+			};
+
+		event.preventDefault();
+
+		$container.
+			addClass( 'flow-component' ).
+			data( 'flow-component', 'boardHistory' );
+
+		// if successfull, flow will now handle clicking the target
+		// If that failed still run the onComplete, it will not trigger
+		// our handler and be a normal click this time.
+		mw.loader.using(
+			[ 'ext.flow', 'ext.flow.mediawiki.ui.modal', 'mediawiki.ui.input' ],
+			onComplete,
+			onComplete
+		);
+	}
+
+	$( function() {
+		$( '#bodyContent' ).one( 'click', '.flow-click-interactive', clickedFlowLink );
+	} );
+} )( jQuery, mediaWiki );
