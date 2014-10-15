ef7f051ac17be75eda5db0390d22e4eec4da0862
Cmcmahon <cmcmahon@wikimedia.org>
2014-10-15 10:21:25 -0700

    QA: WIP update RSpec assertions for modern syntax
    Change-Id: I392826b5326105f4042e1fbb893ad6645ee82715

--- tests/browser/features/step_definitions/flow_steps.rb
+++ tests/browser/features/step_definitions/flow_steps.rb
@@ -99,19 +99,15 @@ end
 Then(/^I am on my user page$/) do
   # Get the title of the page without '_' characters
   text = 'User:' + ENV["MEDIAWIKI_USER"].gsub(/_/, ' ')
-  on(UserPage).page_title_element.text.should == text
-end
-
-Then(/^I do not see an actions link$/) do
-  on(FlowPage).actions_link_element.should_not exist
+  expect(on(UserPage).page_title_element.text).to eq(text)
 end
 
 Then(/^I should see a Delete button$/) do
-  on(FlowPage).delete_button_element.should be_visible
+  expect(on(FlowPage).delete_button_element).to be_visible
 end
 
 Then(/^I should see a Delete topic button$/) do
-  on(FlowPage).topic_delete_button_element.when_present.should be_visible
+  expect(on(FlowPage).topic_delete_button_element.when_present).to be_visible
 end
 
 Then(/^I should see a Hide button$/) do
