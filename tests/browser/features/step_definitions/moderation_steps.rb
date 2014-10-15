25f97075279d7dfbd53f283dbdedb612600c9ade
Cmcmahon <cmcmahon@wikimedia.org>
2014-10-15 14:05:15 -0700

    QA: interim step to alphabetize clauses
    Zero changes to function or even to text itself
    
    Change-Id: Icf63ae9b3885730837b864c9358ca74e44b2f0d4

--- tests/browser/features/step_definitions/moderation_steps.rb
+++ tests/browser/features/step_definitions/moderation_steps.rb
@@ -2,17 +2,19 @@
 # click Delete topic') wait for the dialog box to be non-visible
 # afterwards (to confirm API call finished), rather than use
 # timeouts?
-When(/^I see a dialog box$/) do
-  on(FlowPage).dialog_element.when_present.should be_visible
+When(/^I cancel the dialog$/) do
+  on(FlowPage).dialog_cancel_element.when_present.click
 end
 
-When(/^I give reason for hiding as being "(.*?)"$/) do |hide_reason|
-  on(FlowPage) do |page|
-    page.dialog_input_element.when_present.send_keys(hide_reason)
-  end
+When(/^I click Delete topic$/) do
+  on(FlowPage).dialog_submit_element.when_present.click
 end
 
 When(/^I click Hide topic$/) do
+  on(FlowPage).dialog_submit_element.when_present.click
+end
+
+When(/^I click Suppress topic$/) do
   on(FlowPage).dialog_submit_element.when_present.click
 end
 
@@ -23,12 +25,10 @@ When(/^I give reason for deletion as being "(.*?)"$/) do |delete_reason|
   end
 end
 
-When(/^I click Delete topic$/) do
-  on(FlowPage).dialog_submit_element.when_present.click
-end
-
-Then(/^the top post should be marked as deleted$/) do
-  on(FlowPage).flow_first_topic_moderation_msg.when_present.should match( 'This topic was deleted' )
+When(/^I give reason for hiding as being "(.*?)"$/) do |hide_reason|
+  on(FlowPage) do |page|
+    page.dialog_input_element.when_present.send_keys(hide_reason)
+  end
 end
 
 When(/^I give reason for suppression as being "(.*?)"$/) do |suppress_reason|
@@ -38,22 +38,23 @@ When(/^I give reason for suppression as being "(.*?)"$/) do |suppress_reason|
   end
 end
 
-When(/^I click Suppress topic$/) do
-  on(FlowPage).dialog_submit_element.when_present.click
+When(/^I see a dialog box$/) do
+  on(FlowPage).dialog_element.when_present.should be_visible
 end
 
-Then(/^the top post should be marked as suppressed$/) do
-  on(FlowPage).flow_first_topic_moderation_msg.when_present.should match( 'This topic was suppressed' )
-end
-
-When(/^I cancel the dialog$/) do
-  on(FlowPage).dialog_cancel_element.when_present.click
+Then(/^I confirm$/) do
+  on(FlowPage).confirm(true){}
 end
 
 Then(/^I do not see the dialog box$/) do
   on(FlowPage).dialog_element.when_not_present
 end
 
-Then(/^I confirm$/) do
-  on(FlowPage).confirm(true){}
+
+Then(/^the top post should be marked as deleted$/) do
+  on(FlowPage).flow_first_topic_moderation_msg.when_present.should match( 'This topic was deleted' )
+end
+
+Then(/^the top post should be marked as suppressed$/) do
+  on(FlowPage).flow_first_topic_moderation_msg.when_present.should match( 'This topic was suppressed' )
 end
