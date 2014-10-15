2500e12e98da35f3567d135e846561a66fc829b5
Cmcmahon <cmcmahon@wikimedia.org>
2014-10-15 09:55:10 -0700

    QA: interim step, alphabetization only
    Next step is to update the assertions in the Then steps
    
    Change-Id: I13cbaf9359e6b18e6392353a8683ba6c70854976

--- tests/browser/features/step_definitions/flow_steps.rb
+++ tests/browser/features/step_definitions/flow_steps.rb
@@ -1,21 +1,11 @@
+Given(/^I am on a new board$/) do
+  visit NewFlowPage
+end
+
 Given(/^I am on Flow page$/) do
   visit FlowPage
   step "The Flow page is fully loaded"
   step "page has no ResourceLoader errors"
-end
-
-Given(/^The Flow page is fully loaded$/ ) do
-  on(FlowPage).new_topic_body_element.when_not_visible
-end
-
-Then(/^I am on my user page$/) do
-  # Get the title of the page without '_' characters
-  text = 'User:' + ENV["MEDIAWIKI_USER"].gsub(/_/, ' ')
-  on(UserPage).page_title_element.text.should == text
-end
-
-Given(/^I am on a new board$/) do
-  visit NewFlowPage
 end
 
 # @todo: Rewrite to use more generic step below
@@ -31,15 +21,23 @@ Given(/^I have created a Flow topic with title "(.+)"$/) do |title|
 end
 
 Given(/^the author link is visible$/) do
-    on(FlowPage).author_link_element.when_present.should be_visible
+  on(FlowPage).author_link_element.when_present.should be_visible
+end
+
+Given(/^the block author link is not visible$/) do
+  on(FlowPage).usertools_block_user_link_element.should_not be_visible
+end
+
+Given(/^The Flow page is fully loaded$/ ) do
+  on(FlowPage).new_topic_body_element.when_not_visible
+end
+
+Given(/^the talk to author link is not visible$/) do
+  on(FlowPage).usertools_talk_link_element.should_not be_visible
 end
 
 When(/^I am viewing Topic page$/) do
   on(FlowPage).wait_until { @browser.url =~ /Topic/ }
-end
-
-When(/^I click the Post Actions link$/) do
-  on(FlowPage).post_actions_link_element.when_present.click
 end
 
 When(/^I click New topic save$/) do
@@ -51,13 +49,41 @@ When(/^I click New topic save$/) do
   end
 end
 
-Then(/^the Save New Topic button should be disabled$/) do
-  val = on(FlowPage).new_topic_save_element.attribute( "disabled" )
-  expect(val).to eq("true")
+When(/^I click the Delete topic button$/) do
+  on(FlowPage).topic_delete_button_element.when_present.click
+end
+
+When(/^I click the flow creator element$/) do
+  on(FlowPage).author_link_element.click
+end
+
+When(/^I click the Hide topic button$/) do
+  on(FlowPage).topic_hide_button_element.when_present.click
+end
+
+When(/^I click the Post Actions link$/) do
+  on(FlowPage).post_actions_link_element.when_present.click
+end
+
+When(/^I click the Suppress topic button$/) do
+  on(FlowPage).topic_suppress_button_element.when_present.click
 end
 
 When(/^I click the Topic Actions link$/) do
   on(FlowPage).topic_actions_link_element.when_present.click
+end
+
+When(/^I hover over the author link$/) do
+  on(FlowPage).author_link_element.hover
+end
+
+When(/^I see a flow creator element$/) do
+  on(FlowPage).author_link_element.should be_visible
+end
+
+When(/^I type "(.+)" into the new topic content field$/) do |flow_body|
+  body_string = flow_body + @random_string + @automated_test_marker
+  on(FlowPage).new_topic_body_element.when_present.send_keys(body_string)
 end
 
 When(/^I type "(.+)" into the new topic title field$/) do |flow_title|
@@ -70,58 +96,72 @@ When(/^I type "(.+)" into the new topic title field$/) do |flow_title|
   end
 end
 
-When(/^I type "(.+)" into the new topic content field$/) do |flow_body|
-  body_string = flow_body + @random_string + @automated_test_marker
-  on(FlowPage).new_topic_body_element.when_present.send_keys(body_string)
+Then(/^I am on my user page$/) do
+  # Get the title of the page without '_' characters
+  text = 'User:' + ENV["MEDIAWIKI_USER"].gsub(/_/, ' ')
+  on(UserPage).page_title_element.text.should == text
 end
 
-When(/^I hover over the author link$/) do
-  on(FlowPage).author_link_element.hover
+Then(/^I do not see an actions link$/) do
+  on(FlowPage).actions_link_element.should_not exist
 end
 
-When(/^I see a flow creator element$/) do
-  on(FlowPage).author_link_element.should be_visible
+Then(/^I should see a Delete button$/) do
+  on(FlowPage).delete_button_element.should be_visible
 end
 
-When(/^I click the flow creator element$/) do
-  on(FlowPage).author_link_element.click
+Then(/^I should see a Delete topic button$/) do
+  on(FlowPage).topic_delete_button_element.when_present.should be_visible
+end
+
+Then(/^I should see a Hide button$/) do
+  on(FlowPage).hide_button_element.when_present.should be_visible
+end
+
+Then(/^I should see a Hide topic button$/) do
+  on(FlowPage).topic_hide_button_element.when_present.should be_visible
+end
+
+Then(/^I should see a Suppress button$/) do
+  on(FlowPage).suppress_button_element.should be_visible
+end
+
+Then(/^I should see a Suppress topic button$/) do
+  on(FlowPage).topic_suppress_button_element.when_present.should be_visible
 end
 
 Then(/^the block author link does not exist$/) do
    on(FlowPage).usertools_block_user_link_element.should_not exist
 end
 
-Then(/^I do not see an actions link$/) do
-   on(FlowPage).actions_link_element.should_not exist
-end
-
-Given(/^the talk to author link is not visible$/) do
-  on(FlowPage).usertools_talk_link_element.should_not be_visible
-end
-
-Given(/^the block author link is not visible$/) do
-  on(FlowPage).usertools_block_user_link_element.should_not be_visible
-end
-
-Then(/^the talk to author link is visible$/) do
-  on(FlowPage).usertools_talk_link_element.when_present.should be_visible
-end
-
 Then(/^the block author link is visible$/) do
   on(FlowPage).usertools_block_user_link_element.when_present.should be_visible
+end
+
+Then(/^the content of the top post should be visible$/) do
+  on(FlowPage).flow_first_topic_body_element.when_present.should be_visible
+end
+
+Then(/^the content of the top post should not be visible$/) do
+  on(FlowPage).flow_first_topic_body_element.should_not be_visible
 end
 
 Then(/^the preview and cancel buttons have disappeared$/) do
   on(FlowPage) do |page|
     page.wait_until(20) do
       page.preview_button_element.visible? != true &&
-      page.cancel_button_element.visible? != true
+          page.cancel_button_element.visible? != true
     end
   end
 end
 
-Then(/^the top post should not have a heading which contains "(.+)"$/) do |text|
-  on(FlowPage).flow_first_topic_heading.should_not match(text)
+Then(/^the Save New Topic button should be disabled$/) do
+  val = on(FlowPage).new_topic_save_element.attribute( "disabled" )
+  expect(val).to eq("true")
+end
+
+Then(/^the talk to author link is visible$/) do
+  on(FlowPage).usertools_talk_link_element.when_present.should be_visible
 end
 
 Then(/^the top post should have a heading which contains "(.+)"$/) do |text|
@@ -136,46 +176,6 @@ Then(/^the top post should have content which contains "(.+)"$/) do |text|
   on(FlowPage).flow_first_topic_body.should match(text)
 end
 
-Then(/^the content of the top post should be visible$/) do
-  on(FlowPage).flow_first_topic_body_element.when_present.should be_visible
-end
-
-Then(/^the content of the top post should not be visible$/) do
-  on(FlowPage).flow_first_topic_body_element.should_not be_visible
-end
-
-Then(/^I should see a Delete button$/) do
-  on(FlowPage).delete_button_element.should be_visible
-end
-
-Then(/^I should see a Delete topic button$/) do
-  on(FlowPage).topic_delete_button_element.when_present.should be_visible
-end
-
-When(/^I click the Delete topic button$/) do
-  on(FlowPage).topic_delete_button_element.when_present.click
-end
-
-Then(/^I should see a Hide button$/) do
-  on(FlowPage).hide_button_element.when_present.should be_visible
-end
-
-Then(/^I should see a Hide topic button$/) do
-  on(FlowPage).topic_hide_button_element.when_present.should be_visible
-end
-
-When(/^I click the Hide topic button$/) do
-  on(FlowPage).topic_hide_button_element.when_present.click
-end
-
-Then(/^I should see a Suppress button$/) do
-  on(FlowPage).suppress_button_element.should be_visible
-end
-
-Then(/^I should see a Suppress topic button$/) do
-  on(FlowPage).topic_suppress_button_element.when_present.should be_visible
-end
-
-When(/^I click the Suppress topic button$/) do
-  on(FlowPage).topic_suppress_button_element.when_present.click
+Then(/^the top post should not have a heading which contains "(.+)"$/) do |text|
+  on(FlowPage).flow_first_topic_heading.should_not match(text)
 end
