a149022b97e53060d962fc58a198a97444d37c85
Shahyar <shahyar@gmail.com>
2014-10-15 06:46:54 -0400

    H-8. Implement boardHistory component features
    Mostly involves moving functions from board to be globalized
    Only thing implemented on boardHistory is moderation actions, which refresh the whole page
    
    Change-Id: Iff184366aae6017ef43d86e8fd8eb9c5594c5ec3

--- modules/engine/components/board/flow-boardhistory.js
+++ modules/engine/components/board/flow-boardhistory.js
@@ -11,9 +11,44 @@
 	 * @constructor
 	 */
 	function FlowBoardHistoryComponent( $container ) {
-
+		this.bindNodeHandlers( FlowBoardHistoryComponent.UI.events );
 	}
 	OO.initClass( FlowBoardHistoryComponent );
 
-	mw.flow.registerComponent( 'boardHistory', FlowBoardHistoryComponent/*, 'boardAndHistoryBase'*/ );
+	FlowBoardHistoryComponent.UI = {
+		events: {
+			apiHandlers: {}
+		}
+	};
+
+	mw.flow.registerComponent( 'boardHistory', FlowBoardHistoryComponent, 'boardAndHistoryBase' );
+
+	//
+	// API handlers
+	//
+
+	/**
+	 * After submit of a moderation form, process the response.
+	 *
+	 * @param {Object} info (status:done|fail, $target: jQuery)
+	 * @param {Object} data
+	 * @param {jqXHR} jqxhr
+	 */
+	function flowBoardHistoryModerationCallback( info, data, jqxhr ) {
+		if ( info.status !== 'done' ) {
+			// Error will be displayed by default, nothing else to wrap up
+			return;
+		}
+
+		var flowBoardHistory = mw.flow.getPrototypeMethod( 'boardHistory', 'getInstanceByElement' )( $( this ) );
+
+		// Clear the form so we can refresh without the confirmation dialog
+		flowBoardHistory.emitWithReturn( 'cancelForm', $( this ).closest( 'form' ) );
+
+		// @todo implement dynamic updating of the history page instead of this
+		window.location.reload();
+	}
+
+	FlowBoardHistoryComponent.UI.events.apiHandlers.moderateTopic = flowBoardHistoryModerationCallback;
+	FlowBoardHistoryComponent.UI.events.apiHandlers.moderatePost = flowBoardHistoryModerationCallback;
 }( jQuery, mediaWiki ) );
