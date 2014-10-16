653fb5cf76fbf9386f52f2c9c60e5b9ce502d8cd
Cmcmahon <cmcmahon@wikimedia.org>
2014-10-16 14:11:36 -0700

    QA: WIP refactor reply test
    * use when_present properly
    * use proper equality check
    * remove unnecessary single-line page action
    * add assertions to Then steps
    
    Change-Id: If573a972c04f2fcc22935fcf006bd0b7bfca8935

--- tests/browser/features/step_definitions/reply_steps.rb
+++ tests/browser/features/step_definitions/reply_steps.rb
@@ -1,9 +1,8 @@
 Given(/^I am not watching my new Flow topic$/) do
   on(FlowPage) do |page|
-    page.first_topic_unwatch_link_element.should be_visible
-    page.first_topic_unwatch_link_element.click
-    page.wait_until { page.first_topic_unwatch_link_element.visible? === false }
-    page.wait_until {page.first_topic_watchlist_loading_link_element.visible? === false}
+    page.first_topic_unwatch_link_element.when_present.click
+    page.wait_until { page.first_topic_unwatch_link_element.visible? == false }
+    page.wait_until {page.first_topic_watchlist_loading_link_element.visible? == false}
   end
 end
 
@@ -49,20 +48,19 @@ When(/^I start a reply with comment "(.*?)"$/) do |content|
 end
 
 Then(/^I should see an unwatch link on the topic$/) do
-  on(FlowPage) do |page|
-    page.first_topic_unwatch_link_element.should be_visible
-  end
+  expect(on(FlowPage).first_topic_unwatch_link_element).to be_visible
 end
 
 Then(/^the top post's first reply should contain the text "(.+)"$/) do |text|
   on(FlowPage) do |page|
     page.new_reply_save_element.when_not_present
-    page.first_reply_body.should match(text)
+    expect(page.first_reply_body).to match(text)
   end
 end
 
 Then(/^I should see the topic reply form$/) do
   on(FlowPage) do |page|
     page.wait_until { page.new_reply_input_element.visible? }
+    expect(page.new_reply_input_element).to be_visible
   end
 end
