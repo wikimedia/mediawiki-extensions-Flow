5c5c5734f69e80ceff7b3e8987584fb5fc332e5f
Matthias Mullie <git@mullie.eu>
2014-10-20 14:55:21 +0200

    Fix fatal when submitting summary
    Fatal error: Call to undefined method Flow\Model\PostSummary::getRootPost()
    
    Change-Id: I14846dfa4d0c6fcd3708cc296851ae83c7250a78

--- includes/Data/Index/BoardHistoryIndex.php
+++ includes/Data/Index/BoardHistoryIndex.php
@@ -101,7 +101,7 @@ class BoardHistoryIndex extends TopKIndex {
 
 		$found = Container::get( 'storage' )->find(
 			'TopicListEntry',
-			array( 'topic_id' => $object->getRootPost()->getPostId() )
+			array( 'topic_id' => $post->getRootPost()->getPostId() )
 		);
 
 		if ( $found ) {
