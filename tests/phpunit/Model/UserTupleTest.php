819b8aeb09bf0ad00fa9a75db2549798f5464d5e
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-10-22 23:40:03 -0700

    Validate UserTuple constructor input
    Working on the LQT import patch bugs surfaced of bad data getting into
    the database.  This problem has also been seen on ee-flow in spage's
    watchlist. Talked about this with matt the other day, easiest fix is to
    stop the data when it comes into the UserTuple.
    
    This patch adds exceptions to UserTuple::__construct and a test that
    asserts when the exceptions should and should not trigger.
    
    Change-Id: I312b5eee3a6073b469319b6ce8755f8cc3640232

---
+++ tests/phpunit/Model/UserTupleTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Flow\Tests\Model;
+
+use Flow\Model\UserTuple;
+
+/**
+ * @group Flow
+ */
+class UserTupleTest extends \MediaWikiTestCase {
+
+	public function invalidInputProvider() {
+		return array(
+			array( 'foo', 0, ''),
+			array( 'foo', 1234, '127.0.0.1' ),
+			array( '', 0, '127.0.0.1' ),
+			array( 'foo', -25, '' ),
+			array( 'foo', null, '127.0.0.1' ),
+			array( null, 55, '' ),
+		);
+	}
+
+	/**
+	 * @dataProvider invalidInputProvider
+	 * @expectedException Flow\Exception\InvalidDataException
+	 */
+	public function testInvalidInput( $wiki, $id, $ip ) {
+		new UserTuple( $wiki, $id, $ip );
+	}
+
+	public function validInputProvider() {
+		return array(
+			array( 'foo', 42, null ),
+			array( 'foo', 42, '' ),
+			array( 'foo', 0, '127.0.0.1' ),
+			array( 'foo', '0', '10.1.2.3' ),
+			// Required hack for IRCLineFormatter
+			array( 'foo', 0, null ),
+		);
+	}
+
+	/**
+	 * @dataProvider validInputProvider
+	 */
+	public function testValidInput( $wiki, $id, $ip ) {
+		new UserTuple( $wiki, $id, $ip );
+		// no error thrown from constructor
+		$this->assertTrue( true );
+	}
+}
