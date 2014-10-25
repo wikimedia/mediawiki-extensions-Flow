3e8663a4cd24fca7c0276ab9b385195dc8a585a7
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-10-25 02:13:34 -0700

    Test: Add very basic tests for a variety of API methods
    Change-Id: Icc31d684cd4ad5dec0b6c67a69c72b6ddd0ccca7

---
+++ tests/phpunit/api/ApiFlowReplyTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Flow\Tests\Api;
+
+use Flow\Container;
+use FlowHooks;
+use User;
+
+/**
+ * @group Flow
+ * @group medium
+ */
+class ApiFlowReplyTest extends ApiTestCase {
+
+	protected $tablesUsed = array( 'flow_revision', 'flow_tree_revision', 'flow_tree_node' );
+
+	public function testTopLevelReply() {
+		$result = $this->createTopic( 'result' );
+		$workflowId = $result['roots'][0];
+		$topicRevId = $result['posts'][$workflowId][0];
+
+		$data = $this->doApiRequest( array(
+			'page' => "Topic:$workflowId",
+			'token' => $this->getEditToken(),
+			'action' => 'flow',
+			'submodule' => 'reply',
+			'repreplyTo' => $workflowId,
+			'repcontent' => '⎛ ﾟ∩ﾟ⎞⎛ ⍜⌒⍜⎞⎛ ﾟ⌒ﾟ⎞'
+		) );
+
+		$result = $data[0]['flow']['reply']['result']['topic'];
+		$debug = json_encode( $result );
+		$this->assertArrayHasKey( 'errors', $result, $debug );
+		$this->assertCount( 0, $result['errors'], $result );
+
+		$newPostId = end( $result['revisions'][$topicRevId]['replies'] );
+		$newRevisionId = $result['posts'][$newPostId][0];
+		$revision = $result['revisions'][$newRevisionId];
+		$this->assertArrayHasKey( 'changeType', $revision, $debug );
+		$this->assertEquals( 'reply', $revision['changeType'], $debug );
+		$this->assertEquals(
+			'⎛ ﾟ∩ﾟ⎞⎛ ⍜⌒⍜⎞⎛ ﾟ⌒ﾟ⎞',
+			strip_tags( $revision['content']['content'] ),
+			$debug
+		);
+		$this->assertEquals( 'html', $revision['content']['format'], $debug );
+	}
+}
