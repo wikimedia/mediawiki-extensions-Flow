9a4c17f9e75281e9058f6134d085539f6a239b76
Matthew Flaschen <mflaschen@wikimedia.org>
2014-10-27 23:32:13 -0400

    J2f - Add minimal topic list API for TOC
    Support a TOC-only mode for the topic list, and related
    refactoring to tests and topic list code:
    
    * This is implemented by reusing data already calculated by the Pager
      (since that's all that should be needed).
    
      It then formats that in an alternate formatter.  There is now a base
      class, BaseTopicListFormatter, and two subclasses.
    
    * Add tests for ViewTopicList, for the TOC formatter (however, it also
      catches some issues that are common to both TOC-only and regular).
    
    * Fix a bug in TopicListLastUpdatedStorage.
      It was embedding a table name directly into the query.  In that position,
      MediaWiki core didn't know to auto-prefix it.  That meant that if
      wgDBprefix was set (A prefix is used in the tests  ('unittest_'),
      but this is also a user-facing setting), the query would fail.
    
    * Changing to the testing base class to support an intentionally blank
      board, and arbitrary topic titles.
    
    * Add back blank pagination array for empty boards (this time in parent
      class).  I removed this in 2d6d9b4 since it's overwritten for
      non-blank boards, but it is used when buildEmptyResult
      is called directly by TopicList.
    
    * Remove obsolete constructor parameter
    
    * Some small doc changes in TopicListQuery
    
    * Changes max limit to use the wgFlowMaxLimit variable (note, should
      be tested in production).
    
    This will improve performance for the TOC use case.
    
    Change-Id: I3615817edb1500bd63dc4cbd64820fa25c943e21

---
+++ tests/phpunit/api/ApiFlowViewTopicListTest.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace Flow\Tests\Api;
+
+use Title;
+
+/**
+ * @group Flow
+ * @group medium
+ */
+class ApiFlowViewTopicListTest extends ApiTestCase {
+	const TITLE_PREFIX = 'VTL Test ';
+
+	public function testTocOnly() {
+		$topicData = array();
+		for ( $i = 0; $i < 3; $i++ ) {
+			$title = self::TITLE_PREFIX . $i;
+			$topicData[$i]['response'] = $this->createTopic( 'result', $title );
+			$topicData[$i]['id'] = $topicData[$i]['response']['roots'][0];
+			$topicData[$i]['revisionId'] = $topicData[$i]['response']['posts'][$topicData[$i]['id']][0];
+			$topicData[$i]['expectedRevision'] = array(
+				'content' => array(
+					'content' => $title,
+					'format' => 'plaintext'
+				),
+			);
+		}
+
+		$flowQaTitle = Title::newFromText( 'Talk:Flow_QA' );
+
+		$expectedCommonResponse = array(
+			'flow' => array(
+				'view-topiclist' => array(
+					'result' => array(
+						'topiclist' => array(
+							'submitted' => array(
+								'savesortby' => false,
+								'offset-dir' => 'fwd',
+								'offset-id' => null,
+								'offset' => null,
+								'limit' => 2,
+								'render' => false,
+								'toconly' => true
+							),
+							'errors' => array(),
+							'type' => 'topiclist',
+						),
+					),
+					'status' => 'ok',
+				),
+			),
+		);
+
+		$expectedEmptyPageResponse = array_merge_recursive( array(
+			'flow' => array(
+				'view-topiclist' => array(
+					'result' => array(
+						'topiclist' => array(
+							'submitted' => array(
+								'sortby' => 'newest',
+							),
+							'roots' => array(),
+							'posts' => array(),
+							'revisions' => array(),
+							'links' => array(
+								'pagination' => array(),
+							),
+						),
+					),
+				),
+			),
+		), $expectedCommonResponse );
+
+		$actualEmptyPageResponse = $this->doApiRequest(
+			array(
+				'action' => 'flow',
+				'page' => 'Talk:Intentionally blank',
+				'submodule' => 'view-topiclist',
+				'vtllimit' => 2,
+				'vtltoconly' => true,
+			)
+		);
+		$actualEmptyPageResponse = $actualEmptyPageResponse[0];
+
+		$this->assertEquals(
+			$expectedEmptyPageResponse,
+			$actualEmptyPageResponse,
+			'TOC-only output for an empty, but occupied, Flow board'
+		);
+
+		$expectedNewestResponse = array_merge_recursive( array(
+			'flow' => array(
+				'view-topiclist' => array(
+					'result' => array(
+						'topiclist' => array(
+							'submitted' => array(
+								'sortby' => 'newest',
+							),
+							'sortby' => '',
+							'roots' => array(
+								$topicData[2]['id'],
+								$topicData[1]['id'],
+							),
+							'posts' => array(
+								$topicData[2]['id'] => $topicData[2]['response']['posts'][$topicData[2]['id']],
+								$topicData[1]['id'] => $topicData[1]['response']['posts'][$topicData[1]['id']],
+							),
+							'revisions' => array(
+								$topicData[2]['revisionId'] => $topicData[2]['expectedRevision'],
+								$topicData[1]['revisionId'] => $topicData[1]['expectedRevision'],
+							),
+							'links' => array(
+								'pagination' => array(
+									'fwd' => array(
+										'url' => $flowQaTitle->getLinkURL( array(
+											'topiclist_offset-dir' => 'fwd',
+											'topiclist_limit' => '2',
+											'topiclist_offset-id' => $topicData[1]['id'],
+										) ),
+										'title' => 'fwd',
+									),
+								),
+							),
+						),
+					)
+				)
+			)
+		), $expectedCommonResponse );
+
+		$actualNewestResponse = $this->doApiRequest(
+			array(
+				'action' => 'flow',
+				'page' => 'Talk:Flow QA',
+				'submodule' => 'view-topiclist',
+				'vtllimit' => 2,
+				'vtlsortby' => 'newest',
+				'vtltoconly' => true,
+			)
+		);
+		$actualNewestResponse = $actualNewestResponse[0];
+
+		$this->assertEquals(
+			$expectedNewestResponse,
+			$actualNewestResponse,
+			'TOC-only output for "newest" order'
+		);
+
+		// Make it so update order is chronologically (1, 0, 2)
+		// We then expect it to be returned reverse chronologically (2, 0)
+
+		$updateList = array( 1, 0, 2);
+
+		foreach ( $updateList as $updateListInd => $topicDataInd ) {
+			$replyResponse = $this->doApiRequest(
+				array(
+					'action' => 'flow',
+					'page' => Title::makeTitle( NS_TOPIC, $topicData[$topicDataInd]['id'] )->getPrefixedText(),
+					'submodule' => 'reply',
+					'token' => $this->getEditToken(),
+					'repreplyTo' => $topicData[$topicDataInd]['id'],
+					'repcontent' => "Reply to topic $topicDataInd",
+				)
+			);
+
+			// This is because we use timestamps with second granularity.
+			// Without this, the timestamp can be exactly the same
+			// for two topics, which means the ordering is undefined (and thus
+			// untestable).  This was causing failures on Jenkins.
+			//
+			// Possible improvement: Make a simple class for getting the current
+			// time that normally calls wfTimestampNow.  Have an alternative
+			// implementation for tests that can be controlled by an API like
+			// http://sinonjs.org/ (which we use on the client side).
+			// Pimple can be in charge of which is used.
+			if ( $updateListInd !== ( count( $updateList ) - 1 ) ) {
+				sleep( 1 );
+			}
+
+			$replyResponse = $replyResponse[0];
+
+			$responseTopic = $replyResponse['flow']['reply']['result']['topic'];
+			$topicRevisionId = $topicData[$topicDataInd]['revisionId'];
+			$newPostId = end( $responseTopic['revisions'][$topicRevisionId]['replies'] );
+			$topicData[$topicDataInd]['updateTimestamp'] = $responseTopic['revisions'][$newPostId]['timestamp'];
+		}
+
+		$expectedUpdatedResponse = array_merge_recursive( array(
+			'flow' => array(
+				'view-topiclist' => array(
+					'result' => array(
+						'topiclist' => array(
+							'submitted' => array(
+								'sortby' => 'updated',
+							),
+							'sortby' => 'updated',
+							'roots' => array(
+								$topicData[2]['id'],
+								$topicData[0]['id'],
+							),
+							'posts' => array(
+								$topicData[2]['id'] => $topicData[2]['response']['posts'][$topicData[2]['id']],
+								$topicData[0]['id'] => $topicData[0]['response']['posts'][$topicData[0]['id']],
+							),
+							'revisions' => array(
+								$topicData[2]['revisionId'] => $topicData[2]['expectedRevision'],
+								$topicData[0]['revisionId'] => $topicData[0]['expectedRevision'],
+							),
+							'links' => array(
+								'pagination' => array(
+									'fwd' => array(
+										'url' => $flowQaTitle->getLinkURL( array(
+											'topiclist_offset-dir' => 'fwd',
+											'topiclist_limit' => '2',
+											'topiclist_offset' => $topicData[0]['updateTimestamp'],
+											'topiclist_sortby' => 'updated',
+										) ),
+										'title' => 'fwd',
+									),
+								),
+							),
+						),
+					)
+				)
+			)
+		), $expectedCommonResponse );
+
+		$actualUpdatedResponse = $this->doApiRequest(
+			array(
+				'action' => 'flow',
+				'page' => 'Talk:Flow QA',
+				'submodule' => 'view-topiclist',
+				'vtllimit' => 2,
+				'vtlsortby' => 'updated',
+				'vtltoconly' => true,
+			)
+		);
+		$actualUpdatedResponse = $actualUpdatedResponse[0];
+
+		$this->assertEquals(
+			$expectedUpdatedResponse,
+			$actualUpdatedResponse,
+			'TOC-only output for "updated" order'
+		);
+	}
+}
