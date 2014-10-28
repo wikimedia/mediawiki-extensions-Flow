69f537154b1fadade5392d80779dd6c133c84ca0
Shahyar <shahyar@gmail.com>
2014-10-28 10:34:25 -0400

    H-8. Implement boardHistory component features
    Mostly involves moving functions from board to be globalized
    boardHistory only implements moderation actions, which refresh the whole page
    Created a dynamic template for all moderation menu actions
    
    Change-Id: Iff184366aae6017ef43d86e8fd8eb9c5594c5ec3

--- modules/engine/components/board/base/flow-boardandhistory-base.js
+++ modules/engine/components/board/base/flow-boardandhistory-base.js
@@ -10,8 +10,17 @@
 	 * @constructor
 	 */
 	function FlowBoardAndHistoryComponentBase( $container ) {
+		this.bindNodeHandlers( FlowBoardAndHistoryComponentBase.UI.events );
 	}
 	OO.initClass( FlowBoardAndHistoryComponentBase );
+
+	FlowBoardAndHistoryComponentBase.UI = {
+		events: {
+			apiPreHandlers: {},
+			apiHandlers: {},
+			interactiveHandlers: {}
+		}
+	};
 
 	// Register
 	mw.flow.registerComponent( 'boardAndHistoryBase', FlowBoardAndHistoryComponentBase );
@@ -36,9 +45,6 @@
 		// @todo move form stuff
 		this.emitWithReturn( 'makeContentInteractive', this );
 
-		// Bind any necessary event handlers to this board
-		this.bindContainerHandlers( this );
-
 		// Initialize editors, turning them from textareas into editor objects
 		if ( typeof this.editorTimer === 'undefined' ) {
 			/*
@@ -60,13 +66,99 @@
 		return $();
 	};
 
+	//
+	// Interactive handlers
+	//
+
 	/**
-	 * Binds event handlers to individual boards
-	 * @param {FlowBoardAndHistoryComponentBase} flowBoard
+	 *
+	 * @param {Event} event
 	 */
-	FlowBoardAndHistoryComponentBase.prototype.bindContainerHandlers = function ( flowBoard ) {
-		// Load the collapser state from localStorage
-		this.collapserState( flowBoard );
+	FlowBoardAndHistoryComponentBase.UI.events.interactiveHandlers.moderationDialog = function ( event ) {
+		var $form,
+			$this = $( this ),
+			flowComponent = mw.flow.getPrototypeMethod( 'boardAndHistoryBase', 'getInstanceByElement' )( $this ),
+			// hide, delete, suppress
+			// @todo this could just be detected from the url
+			role = $this.data( 'role' ),
+			template = $this.data( 'template' ),
+			params = {
+				editToken: mw.user.tokens.get( 'editToken' ), // might be unnecessary
+				submitted: {
+					moderationState: role
+				},
+				actions: {}
+			},
+			modal;
+
+		event.preventDefault();
+
+		params.actions[role] = { url: $this.attr( 'href' ), title: $this.attr( 'title' ) };
+
+		// Render the modal itself with mw-ui-modal
+		modal = mw.Modal( {
+			open:  $( mw.flow.TemplateEngine.processTemplateGetFragment( template, params ) ).children(),
+			disableCloseOnOutsideClick: true
+		} );
+
+		// @todo remove this data-flow handler forwarder when data-mwui handlers are implemented
+		// Have the events begin bubbling up from $board
+		flowComponent.assignSpawnedNode( modal.getNode(), flowComponent.$board );
+
+		// Run loadHandlers
+		flowComponent.emitWithReturn( 'makeContentInteractive', modal.getContentNode() );
+
+		// Set flowDialogOwner for API callback @todo find a better way of doing this with mw.Modal
+		$form = modal.getContentNode().find( 'form' ).data( 'flow-dialog-owner', $this );
+		// Bind the cancel callback on the form
+		flowComponent.emitWithReturn( 'addFormCancelCallback', $form, function () {
+			mw.Modal.close( this );
+		} );
+
+		modal = null; // avoid permanent reference
+	};
+
+	/**
+	 * Cancels and closes a form. If text has been entered, issues a warning first.
+	 * @param {Event} event
+	 */
+	FlowBoardAndHistoryComponentBase.UI.events.interactiveHandlers.cancelForm = function ( event ) {
+		var target = this,
+			$form = $( this ).closest( 'form' ),
+			flowComponent = mw.flow.getPrototypeMethod( 'boardAndHistoryBase', 'getInstanceByElement' )( $form ),
+			$fields = $form.find( 'textarea, :text' ),
+			changedFieldCount = 0;
+
+		event.preventDefault();
+
+		// Check for non-empty fields of text
+		$fields.each( function () {
+			if ( $( this ).val() !== this.defaultValue ) {
+				changedFieldCount++;
+				return false;
+			}
+		} );
+		// If all the text fields are empty, OR if the user confirms to close this with text already entered, do it.
+		if ( !changedFieldCount || confirm( flowComponent.constructor.static.TemplateEngine.l10n( 'flow-cancel-warning' ) ) ) {
+			// Reset the form content
+			$form[0].reset();
+
+			// Trigger for flow-actions-disabler
+			$form.find( 'textarea, :text' ).trigger( 'keyup' );
+
+			// Hide the form
+			flowComponent.emitWithReturn( 'hideForm', $form );
+
+			// Get rid of existing error messages
+			flowComponent.emitWithReturn( 'removeError', $form );
+
+			// Trigger the cancel callback
+			if ( $form.data( 'flow-cancel-callback' ) ) {
+				$.each( $form.data( 'flow-cancel-callback' ), function ( idx, fn ) {
+					fn.call( target, event );
+				} );
+			}
+		}
 	};
 
 	//
