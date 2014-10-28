ba64fd325a9eec19075aa1659eb6643bff02bd74
Matthias Mullie <git@mullie.eu>
2014-10-28 15:31:56 +0100

    Add automatic edit summaries for new topic & edit post
    Change-Id: Ia41e52f4e25220ab30d0534cb47bd5424493c6af

--- includes/Formatter/RecentChanges.php
+++ includes/Formatter/RecentChanges.php
@@ -4,11 +4,12 @@ namespace Flow\Formatter;
 
 use Flow\Exception\FlowException;
 use Flow\Model\Anchor;
+use ChangesList;
 use Flow\Model\PostRevision;
 use Flow\Model\UUID;
 use Flow\Parsoid\Utils;
-use ChangesList;
 use IContextSource;
+use Linker;
 
 class RecentChanges extends AbstractFormatter {
 	protected function getHistoryType() {
@@ -68,7 +69,50 @@ class RecentChanges extends AbstractFormatter {
 			  $data['size']['new'],
 			  $ctx
 			) .
-			( Utils::htmlToPlaintext( $description ) ? $separator . $description : '' );
+			( Utils::htmlToPlaintext( $description ) ? $separator . $description : '' ) .
+			$this->getEditSummary( $row, $ctx, $data );
+	}
+
+	/**
+	 * @param RecentChangesRow $row
+	 * @param IContextSource $ctx
+	 * @param array $data
+	 * @return string
+	 */
+	public function getEditSummary( RecentChangesRow $row, IContextSource $ctx, array $data ) {
+		// Build description message, piggybacking on history i18n
+		$changeType = $data['changeType'];
+		$actions = $this->permissions->getActions();
+
+		$key = $actions->getValue( $changeType, 'history', 'i18n-message' );
+		// Find specialized message for summary
+		// i18n messages: flow-rev-message-new-post-recentchanges-summary,
+		// flow-rev-message-edit-post-recentchanges-summary
+		$msg = $ctx->msg( $key . '-' . $this->getHistoryType() . '-summary' );
+		if ( !$msg->exists() ) {
+			// No summary for this action
+			return '';
+		}
+
+		$msg = $msg->params( $this->getDescriptionParams( $data, $actions, $changeType ) );
+
+		// Below code is inspired by Linker::formatAutocomments
+		$prefix = $ctx->msg( 'autocomment-prefix' )->inContentLanguage()->escaped();
+		$link = Linker::link(
+			$title = $row->workflow->getOwnerTitle(),
+			$ctx->getLanguage()->getArrow('backwards'),
+			array(),
+			array(),
+			'noclasses'
+		);
+		$summary = '<span class="autocomment">' . $msg->text() . '</span>';
+
+		// '(' + '' + '←' + summary + ')'
+		$text = Linker::commentBlock( $prefix . $link . $summary );
+
+		// Linker::commentBlock escaped everything, but what we built was safe
+		// and should not be escaped so let's go back to decoded entities
+		return htmlspecialchars_decode( $text );
 	}
 
 	/**
