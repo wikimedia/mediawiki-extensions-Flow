0f551f9db29af17e6f6cdb3a13c30ef3cc3a7ae7
Matthias Mullie <git@mullie.eu>
2014-10-29 15:32:31 +0100

    (bug 70148) Fix new topic creation with wikitext storage
    When creating a new topic, ReferenceRecorder will call
    AbstractRevision::getContent (for the initial post),
    which fetches the Workflow object in order to get the
    article title to send to Parsoid.
    However, workflow is only inserted after the post is,
    so the workflow (required to get the associated Title)
    does not yet exist.
    
    Bug: 70148
    Change-Id: Ief9062b8f2bd77a1071c4f839e8b6865c03f102d

--- container.php
+++ container.php
@@ -1153,7 +1153,8 @@ $c['reference.recorder'] = $c->share( function( $c ) {
 	return new Flow\Data\Listener\ReferenceRecorder(
 		$c['reference.extractor'],
 		$c['reference.updater.links-tables'],
-		$c['storage']
+		$c['storage'],
+		$c['deferred_queue']
 	);
 } );
 
