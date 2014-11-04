818f5fae7a1fefc191abd591977047a48bb47bf6
Shahyar <shahyar@gmail.com>
2014-11-03 22:56:38 -0500

    Introduce include-offset option to pager
    Adds the ability to fetch results including the one at offset-id
    
    Change-Id: Ic91f62e7fa06cb9aeab98341326f8a22a69383a1

--- includes/api/ApiFlowViewTopicList.php
+++ includes/api/ApiFlowViewTopicList.php
@@ -42,6 +42,10 @@ class ApiFlowViewTopicList extends ApiFlowBaseGet {
 				ApiBase::PARAM_TYPE => 'string',
 				ApiBase::PARAM_REQUIRED => false,
 			),
+			'include-offset' => array(
+				ApiBase::PARAM_TYPE => 'boolean',
+				ApiBase::PARAM_DFLT => false,
+			),
 			'limit' => array(
 				// @todo once we have a better idea of the performance of this
 				// adjust these to sane defaults
@@ -66,6 +70,7 @@ class ApiFlowViewTopicList extends ApiFlowBaseGet {
 			'savesortby' => 'Save sortby option, if set',
 			'offset-id' => 'Offset value (in UUID format) to start fetching topics at',
 			'offset' => 'Offset value to start fetching topics at',
+			'include-offset' => 'Includes the offset item in the results as well',
 			'limit' => 'Amount of topics to fetch',
 			'render' => 'Renders (in HTML) the topics, if set',
 		);
