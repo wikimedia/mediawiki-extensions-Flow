b05844f7688a3ee2376b0d29f5cda92e326fe74c
EBernhardson <ebernhardson@wikimedia.org>
2014-11-05 01:04:11 +0000

    Revert "Add temporary ext.flow.new module to avoid missing JS"
    Change was temporary, merge after Nov 23 2014
    
    This reverts commit 7c83c10244985e66c4f1a5cb113523cf8f7c48fd.
    
    Change-Id: Ic2e5084eb75cf71b8037fae6b8e2c5412e58b6a5

--- Resources.php
+++ Resources.php
@@ -311,12 +311,6 @@ $wgResourceModules += array(
 			'moment',
 		),
 	) + $mobile,
-	// Work around old Varnish-cached HTML following module rename
-	// Remove on 2014-11-23, > 30 days after
-	// https://gerrit.wikimedia.org/r/#/c/165756/ was merged.
-	'ext.flow.new' => $flowResourceTemplate + array(
-		'dependencies' => 'ext.flow',
-	),
 	'ext.flow' => $flowResourceTemplate + array(
 		'scripts' => array( // Component order is important
 			// MW UI
