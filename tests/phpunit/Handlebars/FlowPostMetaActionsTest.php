17dc903b470bc87d1a3276417e5f8c3685b2cb13
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-11-05 21:13:02 -0800

    Bug: Post edited timestamp special cases
    The timestamp output needs to use the lastEditUser key
    rather than author, because author is the creator of the
    most recent revision which may not be the most recent
    editor.
    
    Adds a couple tests to demonstrate the use case.
    
    Change-Id: Ie4ad4f587bbf138dd5daebafa8bc8458d3a657ca

---
+++ tests/phpunit/Handlebars/FlowPostMetaActionsTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Flow\Tests\Handlebars;
+
+use Flow\Container;
+use Flow\TemplateHelper;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * @group Flow
+ */
+class FlowPostMetaActionsTest extends \MediaWikiTestCase {
+
+	/**
+	 * The specific timestamps used inside are not anything
+	 * in particular, they just match the post and last edit
+	 * uuid's we use in the test.
+	 */
+	public function timestampEditedProvider() {
+		return array(
+			array(
+				'never been edited',
+				// expected
+				'02:52, 1 October 2014',
+				// args
+				array(
+					'isOriginalContent' => true,
+					'author' => 'creator',
+					'creator' => 'creator',
+					'lastEditUser' => null,
+				),
+			),
+
+			array(
+				'last edited by post creator',
+				// expected
+				'Edited 04:21, 9 October 2014',
+				// args
+				array(
+					'isOriginalContent' => false,
+					'author' => 'creator',
+					'creator' => 'creator',
+					'lastEditUser' => 'creator',
+				),
+			),
+
+			array(
+				'last edited by other than post creator',
+				// expected
+				'Edited by author 04:21, 9 October 2014',
+				// args
+				array(
+					'isOriginalContent' => false,
+					'author' => 'author',
+					'creator' => 'creator',
+					'lastEditUser' => 'author',
+				),
+			),
+
+			array(
+				'most recent revision not a content edit',
+				// expected
+				'Edited 04:21, 9 October 2014',
+				// args
+				array(
+					'isOriginalContent' => false,
+					'author' => 'author',
+					'creator' => 'creator',
+					'lastEditUser' => 'creator',
+				),
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider timestampEditedProvider
+	 */
+	public function testTimestampEdited( $message, $expect, $args ) {
+		if ( !class_exists( 'Symfony\Component\DomCrawler\Crawler' ) ) {
+			$this->markTestSkipped( 'DomCrawler component is not available.' );
+			return;
+		}
+
+		$crawler = $this->renderTemplate(
+			'flow_post_meta_actions',
+			array(
+				'actions' => array(),
+				'postId' => 's3chebds95i0atkw',
+				'lastEditId' => 's3ufwcms95i0atkw',
+				'isOriginalContent' => $args['isOriginalContent'],
+				'author' => array(
+					'name' => $args['author'],
+				),
+				'creator' => array(
+					'name' => $args['creator'],
+				),
+				'lastEditUser' => array(
+					'name' => $args['lastEditUser'],
+				),
+			)
+		);
+
+		$text = $crawler->filter( '.flow-post-timestamp' )->text();
+		// normalize whitespace
+		$text = trim( preg_replace( '/\s+/', ' ', $text ) );
+		$this->assertStringStartsWith( $expect, $text, $message );
+	}
+
+	protected function renderTemplate( $templateName, array $args = array() ) {
+		$lc = new TemplateHelper(
+			Container::get( 'lightncandy.template_dir' ),
+			true // force recompile
+		);
+		$renderer = $lc->getTemplate( $templateName );
+
+		return new Crawler( $renderer( $args ) );
+	}
+}
