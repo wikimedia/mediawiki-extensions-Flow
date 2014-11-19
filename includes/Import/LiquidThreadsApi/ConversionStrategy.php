6591e412ca186c5cef94c86f73ecbfa7f6de86ac
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-11-18 18:21:46 -0800

    Implement full wiki lqt conversion
    Ties together the individual page import process into a full-wiki conversion
    process.  This script will move existing lqt pages to an archive page and import
    topics from that archive page back to the original location as flow topics. When an
    LQT page is completely converted the lqt magic word is stripped and the page is
    no longer lqt enabled. If import fails on a particular page the lqt magic word
    remains and a future run of this maint script will try again to import the failed
    topics.
    
    Change-Id: I818bfdeaa12c258529e3831ea61b102e6a847cf4

---
+++ includes/Import/LiquidThreadsApi/ConversionStrategy.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Flow\Import\LiquidThreadsApi;
+
+use DatabaseBase;
+use DateTime;
+use DateTimeZone;
+use Flow\Import\Converter;
+use Flow\Import\IConversionStrategy;
+use Flow\Import\ImportSourceStore;
+use LqtDispatch;
+use Title;
+use WikitextContent;
+
+/**
+ * Converts LiquidThreads pages on a wiki to Flow. This converter is idempotent
+ * when used with an appropriate ImportSourceStore, and may be run many times
+ * without worry for duplicate imports.
+ *
+ * Pages with the LQT magic word will be moved to a subpage of their original location
+ * named 'LQT Archive N' with N increasing starting at 1 looking for the first empty page.
+ * On successful import of an entire page the LQT magic word will be stripped from the
+ * archive version of the page.
+ */
+class ConversionStrategy implements IConversionStrategy {
+	/**
+	 * @var DatabaseBase Slave database for the current wiki
+	 */
+	protected $dbr;
+
+	/**
+	 * @var ImportSourceStore
+	 */
+	protected $sourceStore;
+
+	/**
+	 * @var ApiBackend
+	 */
+	public $api;
+
+	public function __construct(
+		DatabaseBase $dbr,
+		ImportSourceStore $sourceStore,
+		ApiBackend $api
+	) {
+		$this->dbr = $dbr;
+		$this->sourceStore = $sourceStore;
+		$this->api = $api;
+	}
+
+	public function getSourceStore() {
+		return $this->sourceStore;
+	}
+
+	public function getMoveComment( Title $from, Title $to ) {
+		return "Conversion of LQT to Flow from: {$from->getPrefixedText()}";
+	}
+
+	public function getCleanupComment( Title $from, Title $to ) {
+		return "LQT to Flow conversion";
+	}
+
+	public function isConversionFinished( Title $title, Title $movedFrom = null ) {
+		// After successful conversion we strip the LQT magic word
+		if ( LqtDispatch::isLqtPage( $title ) ) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public function createImportSource( Title $title ) {
+		return new ImportSource( $this->api, $title->getPrefixedText() );
+	}
+
+	/**
+	 * Flow does not support viewing the history of the wikitext pages it takes
+	 * over, so those need to be moved out the way. This method decides that
+	 * destination. The archived revisions include the headers displayed with
+	 * lqt and potentially any pre-lqt wikitext talk page content.
+	 *
+	 * @param Title $source
+	 * @return Title
+	 */
+	public function decideArchiveTitle( Title $source ) {
+		return Converter::decideArchiveTitle( $source, array(
+			'%s/LQT Archive %d',
+		) );
+	}
+
+	/**
+	 * @param WikitextContent $content
+	 * @param Title $title
+	 * @return WikitextContent
+	 */
+	public function createArchiveCleanupRevisionContent( WikitextContent $content, Title $title ) {
+		$now = new DateTime( "now", new DateTimeZone( 'GMT' ) );
+		$arguments = implode( '|', array(
+			'from=' . $title->getPrefixedText(),
+			'date=' . $now->format( 'Y-m-d' ),
+		) );
+
+		$newWikitext = preg_replace(
+			'/{{\s*#useliquidthreads:\s*1\s*}}/i',
+			'',
+			$content->getNativeData()
+		);
+		$template = wfMessage( 'flow-importer-lqt-converted-template' )->inContentLanguage()->plain();
+		$newWikitext .= "\n\n{{{$template}|$arguments}}";
+
+		return new WikitextContent( $newWikitext );
+	}
+}
