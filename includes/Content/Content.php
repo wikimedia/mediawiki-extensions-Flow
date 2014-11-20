ba362b33c99441ef8a60d210e5f22ec7e7b1f528
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-11-20 14:33:54 -0800

    Create a CONTENT_MODEL_FLOW_BOARD define
    Change-Id: I422f46652efd7cbc151cd23f6e94c36e0b71a27a

--- includes/Content/Content.php
+++ includes/Content/Content.php
@@ -14,7 +14,7 @@ abstract class Content {
 		$occupationController = \FlowHooks::getOccupationController();
 
 		if ( $occupationController->isTalkpageOccupied( $title ) ) {
-			$model = 'flow-board';
+			$model = CONTENT_MODEL_FLOW_BOARD;
 
 			return false;
 		}
@@ -23,7 +23,7 @@ abstract class Content {
 	}
 
 	static function onShowMissingArticle( Article $article ) {
-		if ( $article->getPage()->getContentModel() !== 'flow-board' ) {
+		if ( $article->getPage()->getContentModel() !== CONTENT_MODEL_FLOW_BOARD ) {
 			return true;
 		}
 
@@ -32,7 +32,7 @@ abstract class Content {
 			throw new FlowException( 'Non-existent topic' );
 		}
 
-		$emptyContent = ContentHandler::getForModelID( 'flow-board' )->makeEmptyContent();
+		$emptyContent = ContentHandler::getForModelID( CONTENT_MODEL_FLOW_BOARD )->makeEmptyContent();
 
 		$parserOutput = $emptyContent->getParserOutput( $article->getTitle() );
 		$article->getContext()->getOutput()->addParserOutput( $parserOutput );
