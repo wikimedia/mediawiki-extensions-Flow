cf2a19e12319a2e7ec19da0a4d25f94239151b67
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-11-20 14:59:29 -0800

    Archive and takeover namespaces with Flow
    Uses the infrastructure we wrote for LQT to move wikitext talk
    pages out of the way and turn them into flow boards.  This does
    not attempt to import individual topics, it does make a stab at
    guessing what the header should be.
    
    Change-Id: Ie785403748cc22bc28ff52a172bfaafad8aaf18b

---
+++ tests/phpunit/Import/Wikitext/ImportSourceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Flow\Tests\Import\Wikitext;
+
+use DateTime;
+use DateTimeZone;
+use Flow\Import\Wikitext\ImportSource;
+use Parser;
+use Title;
+use WikiPage;
+use WikitextContent;
+
+/**
+ * @group Flow
+ * @group Database
+ */
+class ImportSourceTest extends \MediaWikiTestCase {
+
+	protected $tablesUsed = array( 'page', 'revision' );
+
+	public function testGetHeader() {
+		$now = new DateTime( "now", new DateTimeZone( "GMT" ) );
+		$date = $now->format( 'Y-m-d' );
+
+		// create a page with some content
+		$status = WikiPage::factory( Title::newMainPage() )
+			->doEditContent(
+				new WikitextContent( "This is some content\n" ),
+				"and an edit summary"
+			);
+		if ( !$status->isGood() ) {
+			$this->fail( $status->getMessage()->plain() );
+		}
+
+		$source = new ImportSource( Title::newMainPage(), new Parser );
+		$header = $source->getHeader();
+		$this->assertNotNull( $header );
+		$this->assertGreaterThan( 1, strlen( $header->getObjectKey() ) );
+
+		$revisions = iterator_to_array( $header->getRevisions() );
+		$this->assertCount( 1, $revisions );
+
+		$revision = reset( $revisions );
+		$this->assertInstanceOf( 'Flow\Import\IObjectRevision', $revision );
+		$this->assertEquals(
+			"This is some content\n\n{{Wikitext talk page converted to Flow|from=Main Page|date=$date}}",
+			$revision->getText()
+		);
+	}
+}
