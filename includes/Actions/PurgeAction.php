c4b185242cf1f5e13adfc9b62c1ade679fa71ebb
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-11-20 18:31:59 -0800

    Bug: action=purge doesnt purge the topic list sorted by last update
    Change-Id: Ib58850b708204ec63ffce484dbb26ecad6d000c1

--- includes/Actions/PurgeAction.php
+++ includes/Actions/PurgeAction.php
@@ -78,18 +78,36 @@ class PurgeAction extends \PurgeAction {
 	 * @param Workflow $workflow
 	 */
 	protected function fetchDiscussion( Workflow $workflow ) {
+		$results = array();
+		$pagers = array();
 		/** @var ManagerGroup $storage */
 		$storage = Container::get( 'storage' );
-		$pager = new Pager(
+
+		// 'newest' sort order
+		$pagers[] = new Pager(
 			$storage->getStorage( 'TopicListEntry' ),
 			array( 'topic_list_id' => $workflow->getId() ),
 			array( 'pager-limit' => 499 )
 		);
 
-		$results = array();
-		foreach ( $pager->getPage()->getResults() as $entry ) {
-			$results[] = $entry->getId();
+		// 'updated' sort order
+		$pagers[] = new Pager(
+			$storage->getStorage( 'TopicListEntry' ),
+			array( 'topic_list_id' => $workflow->getId() ),
+			array(
+				'pager-limit' => 499,
+				'sort' => 'workflow_last_update_timestamp',
+				'order' => 'desc',
+			)
+		);
+
+		foreach ( $pagers as $pager ) {
+			foreach ( $pager->getPage()->getResults() as $entry ) {
+				// use array key to de-duplicate
+				$results[$entry->getId()->getAlphadecimal()] = $entry->getId();
+			}
 		}
+
 		$this->fetchTopics( $results );
 
 		// purge the board history
