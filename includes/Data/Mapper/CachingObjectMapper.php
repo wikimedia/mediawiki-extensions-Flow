1e10c8c25c58669f6dbbf3fbb7c36df09880e24b
Matthias Mullie <git@mullie.eu>
2014-11-25 14:50:40 +0100

    Make sure there are no datatype mismatches when doing updates
    Change-Id: Ibef4229dc8949d1b7eae0c4f3c906990c0a18663

--- includes/Data/Mapper/CachingObjectMapper.php
+++ includes/Data/Mapper/CachingObjectMapper.php
@@ -120,6 +120,11 @@ class CachingObjectMapper implements ObjectMapper {
 		}
 	}
 
+	public function normalizeRow( array $row ) {
+		$object = call_user_func( $this->fromStorageRow, $row );
+		return call_user_func( $this->toStorageRow, $object );
+	}
+
 	public function clear() {
 		$this->loaded = new MultiDimArray;
 	}
