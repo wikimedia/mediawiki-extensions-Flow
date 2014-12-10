084b43f4f197757228892d6c9b6bad8135e5ce5b
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-12-10 13:30:48 -0800

    Expose the convertToText method via Special:FlowExport
    As we progress towards users being able to enable flow boards
    themselves rather than only when we whitelist them, communities
    need the tools necessary to turn pages they did not want to be
    flow boards back into wikitext.
    
    This patch moves the bulk of the convertToText.php maintenance
    script into a utility class and exposes it from a new special
    page, Special:FlowExport.
    
    Note that this does not perform *any* changes on the wiki itself.  The
    export method simply allows users to get a wikitext representation of
    a flow board.  It will not make any page edits; it'll just print the
    wikitext.
    
    Change-Id: Ib52468323040be273a68a6072dda3811e27982bf

--- autoload.php
+++ autoload.php
@@ -354,6 +354,7 @@ $wgAutoloadClasses += array(
 	'Flow\\Tests\\UrlGeneratorTest' => __DIR__ . '/tests/phpunit/UrlGeneratorTest.php',
 	'Flow\\Tests\\WatchedTopicItemTest' => __DIR__ . '/tests/phpunit/WatchedTopicItemsTest.php',
 	'Flow\\UrlGenerator' => __DIR__ . '/includes/UrlGenerator.php',
+	'Flow\\Utils\\Export' => __DIR__ . '/includes/Utils/Export.php',
 	'Flow\\Utils\\NamespaceIterator' => __DIR__ . '/includes/Utils/NamespaceIterator.php',
 	'Flow\\Utils\\PagesWithPropertyIterator' => __DIR__ . '/includes/Utils/PagesWithPropertyIterator.php',
 	'Flow\\View' => __DIR__ . '/includes/View.php',
