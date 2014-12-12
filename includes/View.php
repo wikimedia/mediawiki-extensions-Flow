6f7bddc9c1d59dda73bd0eab0d73727cd4165d6f
Erik Bernhardson <ebernhardson@wikimedia.org>
2014-12-12 10:50:32 -0800

    Dont output base hrefs
    We switched awhile ago to have redlinker rewrite all urls being output
    as such this base href does nothing but confuse people as to how
    things actually work.
    
    Change-Id: Iebdbeb3efdbea278c26c6cb23cb89fcb6b3905ff

--- includes/View.php
+++ includes/View.php
@@ -69,13 +69,6 @@ class View extends ContextSource {
 		$workflow = $loader->getWorkflow();
 
 		$title = $workflow->getArticleTitle();
-		// Temporary hack to make relative links work when the page is requested as /w/index.php?title=
-		// @todo this wont work when we eventually display posts from multiple source pages,
-		// @todo Patch core to either deprecate /w/index.php?title= and issue redirects, or
-		//   include the <base href="..."> directly from core
-		$out->prependHTML( Html::element( 'base', array(
-			'href' => $title->getLinkURL()
-		) ) );
 
 		$request = $this->getRequest();
 		$user = $this->getUser();
