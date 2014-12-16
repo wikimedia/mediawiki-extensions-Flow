92b72be04e79629dd294da2610083f2115da28af
Dan Duvall <dduvall@wikimedia.org>
2014-12-15 16:41:32 -0800

    (WIP) QA: Simplified using new mw-selenium features
    NO NOT MERGE
    
    Refactored thanks and no-js features to showcase some of the new
    patterns in mw-selenium's experimental branch.
    
    Change-Id: Ib5cdd884eabe95c9c23dcbc32b7968fef8bb60dd

--- tests/browser/features/support/pages/flow_old_permalink_page.rb
+++ tests/browser/features/support/pages/flow_old_permalink_page.rb
@@ -1,7 +1,6 @@
 class FlowOldPermalinkPage
   include PageObject
-  include URL
 
   @params = { page: 'Talk:Flow QA', workflow_id: 'no workflow' }
-  page_url URL.url(params[:page]) + "?workflow=<%=params[:workflow_id]%>"
+  page_url "Talk:Flow QA?workflow=<%=params[:workflow_id]%>"
 end
