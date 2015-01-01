905e3e6cf59bab61889981d35964584dbbffede8
S Page <spage@wikimedia.org>
2014-12-31 17:06:55 -0800

    recent_changes_page is generic, so no Flow prefix
    Change-Id: I2c7b999f48eee78773452293f3745c7e7be57e65

--- tests/browser/features/step_definitions/flow_in_recent_changes_steps.rb
+++ tests/browser/features/step_definitions/flow_in_recent_changes_steps.rb
@@ -3,9 +3,9 @@ When(/^I navigate to the Recent Changes page$/) do
 end
 
 Then(/^the new topic should be in the Recent Changes page$/) do
-  expect(on(RecentChangesPage).flow_recent_changes_element.when_present.text).to match @topic_string
+  expect(on(RecentChangesPage).recent_changes_element.when_present.text).to match @topic_string
 end
 
 Then(/^the new title should be in the Recent Changes page$/) do
-  expect(on(RecentChangesPage).flow_recent_changes_element.when_present.text).to match @edited_topic_string
+  expect(on(RecentChangesPage).recent_changes_element.when_present.text).to match @edited_topic_string
 end
