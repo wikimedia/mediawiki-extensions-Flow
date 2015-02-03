9717921a9f409380ca45c1e5512c5ce968c8d0a1
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-02-02 17:59:06 -0800

    Handle CategoryViewer hooks for prettier output
    Depends on core patch introducing hooks.  Changes the output
    of flow topics within the category pages from:
    
        <a href="...">Topic:S09u3r09ujf</a>
    
    to
    
       <a href="...">Hello can you help me</a> on <a href="...">Talk:Help</a>
    
    Bug: T87793
    Related-core-patch: I182e6e35fcc3a2a298e928e088579bdb22e145ff
    Change-Id: Ia4f2953bcd807ba3990e762a2efcaab428c40147

---
+++ includes/Formatter/CategoryViewerQuery.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Flow\Formatter;
+
+use Flow\Data\ManagerGroup;
+use Flow\Exception\FlowException;
+use Flow\Model\UUID;
+
+/**
+ * This class is necessary so we can inject the name of
+ * a topic title into the category.  Once we have pages
+ * in the topic namespace named after the topic themselves
+ * this can be simplified down to only pre-load the workflow
+ * and not the related posts.
+ */
+class CategoryViewerQuery {
+	/**
+	 * @var PostRevision[]
+	 */
+	protected $posts = array();
+
+	/**
+	 * @var Workflow[]
+	 */
+	protected $workflows = array();
+
+	/**
+	 * @var ManagerGroup
+	 */
+	protected $storage;
+
+	public function __construct( ManagerGroup $storage ) {
+		$this->storage = $storage;
+	}
+
+	/**
+	 * Accepts a result set as sent out to the CategoryViewer::doCategoryQuery
+	 * hook.
+	 *
+	 * @param ResultWrapper|array $rows
+	 */
+	public function loadMetadataBatch( $rows ) {
+		$neededPosts = array();
+		$neededWorkflows = array();
+		foreach ( $rows as $row ) {
+			if ( $row->page_namespace != NS_TOPIC ) {
+				continue;
+			}
+			$uuid = UUID::create( strtolower( $row->page_title ) );
+			if ( $uuid ) {
+				$alpha = $uuid->getAlphadecimal();
+				$neededPosts[$alpha] = array( 'rev_type_id' => $uuid );
+				$neededWorkflows[$alpha] = $uuid;
+			}
+		}
+
+		if ( !$neededPosts ) {
+			return;
+		}
+		$this->posts = $this->storage->findMulti(
+			'PostRevision',
+			$neededPosts,
+			array( 'sort' => 'rev_id', 'order' => 'DESC', 'limit' => 1 )
+		);
+		$workflows = $this->storage->getMulti(
+			'Workflow',
+			$neededWorkflows
+		);
+		// @todo fixme: these should have come back with the apropriate array
+		// key since we passed it in above, but didn't.
+		foreach ( $workflows as $workflow ) {
+			$this->workflows[$workflow->getId()->getAlphadecimal()] = $workflow;
+		}
+	}
+
+	public function getResult( UUID $uuid ) {
+		$alpha = $uuid->getAlphadecimal();
+
+		// Minimal set of data needed for the CategoryViewFormatter
+		$row = new FormatterRow;
+		if ( !isset( $this->posts[$alpha] ) ) {
+			throw new FlowException( '...' );
+		}
+		$row->revision = reset( $this->posts[$alpha] );
+		if ( !isset( $this->workflows[$alpha] ) ) {
+			throw new FlowException( ',,,' );
+		}
+		$row->workflow = $this->workflows[$alpha];
+
+		return $row;
+	}
+}
