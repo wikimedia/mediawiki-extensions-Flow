a3c10c692de719bdeeb9fc44ae5eba02115d7a7e
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-02-02 21:56:55 -0800

    Update references on topic moderation change
    When a topic changes to a moderation state >= hidden it should no longer
    be included in categorization or whatlinkshere tables.  This is a bit of
    an information leak and should be closed.
    
    The patch notices when a topic transitions in visibility and re-runs the
    reference recording code against all the current top revisions within the
    topic.
    
    Bug: T87793
    Change-Id: I30e07f68e578bf9d92d6f443b28bbbfb29462f5c

--- includes/Data/Listener/ReferenceRecorder.php
+++ includes/Data/Listener/ReferenceRecorder.php
@@ -2,16 +2,19 @@
 
 namespace Flow\Data\Listener;
 
+use Flow\Exception\FlowException;
 use Flow\Exception\InvalidDataException;
 use Flow\LinksTableUpdater;
 use Flow\Data\LifecycleHandler;
 use Flow\Data\ManagerGroup;
 use Flow\Model\AbstractRevision;
 use Flow\Model\PostRevision;
+use Flow\Model\PostSummary;
 use Flow\Model\UUID;
 use Flow\Model\Workflow;
 use Flow\Model\Reference;
 use Flow\Parsoid\ReferenceExtractor;
+use Flow\Repository\TreeRepository;
 
 /**
  * Listens for new revisions to be inserted.  Calculates the difference in
@@ -34,10 +37,22 @@ class ReferenceRecorder implements LifecycleHandler {
 	 */
 	protected $linksTableUpdater;
 
-	public function __construct( ReferenceExtractor $referenceExtractor, LinksTableUpdater $linksTableUpdater, ManagerGroup $storage ) {
+	/**
+	 * @var TreeRepository Used to query for the posts within a topic when moderation
+	 *  changes the visibility of a topic.
+	 */
+	protected $treeRepository;
+
+	public function __construct(
+		ReferenceExtractor $referenceExtractor,
+		LinksTableUpdater $linksTableUpdater,
+		ManagerGroup $storage,
+		TreeRepository $treeRepository
+	) {
 		$this->referenceExtractor = $referenceExtractor;
 		$this->linksTableUpdater = $linksTableUpdater;
 		$this->storage = $storage;
+		$this->treeRepository = $treeRepository;
 	}
 
 	public function onAfterLoad( $object, array $old ) {
@@ -53,15 +68,11 @@ class ReferenceRecorder implements LifecycleHandler {
 		}
 		$workflow = $metadata['workflow'];
 
-		// Topic title is plain text, there is no reference to extract
 		if ( $revision instanceof PostRevision && $revision->isTopicTitle() ) {
-			return;
+			list( $added, $removed ) = $this->calculateChangesFromTopic( $workflow, $revision );
+		} else {
+			list( $added, $removed ) = $this->calculateChangesFromExisting( $workflow, $revision );
 		}
-
-		$prevReferences = $this->getExistingReferences( $revision->getRevisionType(), $revision->getCollectionId() );
-		$references = $this->getReferencesFromRevisionContent( $workflow, $revision );
-
-		list( $added, $removed ) = $this->referencesDifference( $prevReferences, $references );
 
 		$this->storage->multiPut( $added );
 		$this->storage->multiRemove( $removed );
@@ -71,20 +82,152 @@ class ReferenceRecorder implements LifecycleHandler {
 	}
 
 	/**
+	 * Compares the references contained within $revision against those stored for
+	 * that revision.  Returns the differences.
+	 *
+	 * @param Workflow $workflow
+	 * @param AbstractRevision $revision
+	 * @param PostRevision|null $root
+	 * @return array Two nested arrays, first the references that were added and
+	 *  second the references that were removed.
+	 */
+	protected function calculateChangesFromExisting(
+		Workflow $workflow,
+		AbstractRevision $revision,
+		PostRevision $root = null
+	) {
+		$prevReferences = $this->getExistingReferences(
+			$revision->getRevisionType(),
+			$revision->getCollectionId()
+		);
+		$references = $this->getReferencesFromRevisionContent( $workflow, $revision, $root );
+
+		return $this->referencesDifference( $prevReferences, $references );
+	}
+
+	/**
+	 * While topic's themselves are plaintext and do not contain any references,
+	 * moderation actions change what references are visible.  When transitioning
+	 * from or to a generically visible state (unmoderated or locked) the entire
+	 * topic + summary needs to be re-evaluated.
+	 *
+	 * @param Workflow $workflow
+	 * @param PostRevision $current Topic revision object that was inserted
+	 * @return array Contains two arrays, first the references to add a second
+	 *  the references to remove
+	 * @throws FlowException
+	 */
+	protected function calculateChangesFromTopic( Workflow $workflow, PostRevision $current ) {
+		if ( $current->isFirstRevision() ) {
+			return array( array(), array() );
+		}
+		$previous = $this->storage->get( 'PostRevision', $current->getPrevRevisionId() );
+		if ( !$previous ) {
+			throw new FlowException( 'Expcted previous revision of ' . $current->getPrevRevisionId()->getAlphadecimal() );
+		}
+
+		$isHidden = self::isHidden( $current );
+		$wasHidden = self::isHidden( $previous );
+
+		if ( $isHidden === $wasHidden ) {
+			return array( array(), array() );
+		}
+
+		// re-run
+		$revisions = $this->collectTopicRevisions( $workflow );
+		$added = array();
+		$removed = array();
+		foreach ( $revisions as $revision ) {
+			list( $add, $remove ) = $this->calculateChangesFromExisting( $workflow, $revision, $current );
+			$added = array_merge( $added, $add );
+			$removed = array_merge( $removed, $remove );
+		};
+
+		return array( $added, $removed );
+	}
+
+	static protected function isHidden( AbstractRevision $revision ) {
+		return $revision->isModerated() && $revision->getModerationState() !== $revision::MODERATED_LOCKED;
+	}
+
+	/**
+	 * Gets all the 'top' revisions within the topic, namely the posts and the
+	 * summary. These are used when a topic changes is visibility via moderation
+	 * to add or remove the relevant references.
+	 *
+	 * @param Workflow $workflow
+	 * @return AbstractRevision[]
+	 */
+	protected function collectTopicRevisions( Workflow $workflow ) {
+		$found = $this->treeRepository->fetchSubtreeNodeList( array( $workflow->getId() ) );
+		$queries = array();
+		foreach ( reset( $found ) as $uuid ) {
+			$queries[] = array( 'rev_type_id' => $uuid );
+		}
+
+		$posts = $this->storage->findMulti(
+			'PostRevision',
+			$queries,
+			array( 'sort' => 'rev_id', 'order' => 'DESC', 'limit' => 1 )
+		);
+
+		// we also need the most recent topic summary if it exists
+		$summaries = $this->storage->find(
+			'PostSummary',
+			array( 'rev_type_id' => $workflow->getId() ),
+			array( 'sort' => 'rev_id', 'order' => 'DESC', 'limit' => 1 )
+		);
+
+		$result = $summaries;
+		// we have to unwrap the posts since we used findMulti, it returns
+		// a separate result set for each query
+		foreach ( $posts as $found ) {
+			$result[] = reset( $found );
+		}
+		return $result;
+	}
+
+	/**
 	 * Pulls references from a revision's content
 	 *
 	 * @param  Workflow $workflow The Workflow that the revision is attached to.
 	 * @param  AbstractRevision $revision The Revision to pull references from.
+	 * @param  PostRevision|null $root
 	 * @return Reference[] Array of References.
 	 */
-	public function getReferencesFromRevisionContent( Workflow $workflow, AbstractRevision $revision ) {
-		$content = $revision->getContent( 'html' );
+	public function getReferencesFromRevisionContent(
+		Workflow $workflow,
+		AbstractRevision $revision,
+		PostRevision $root = null
+	) {
+		// Locked is the only moderated state we still collect references for.
+		if ( self::isHidden( $revision ) ) {
+			return array();
+		}
+
+		// If this is attached to a topic we also need to check its permissions
+		if ( $root === null ) {
+			try {
+				if ( $revision instanceof PostRevision && !$revision->isTopicTitle() ) {
+					$root = $revision->getCollection()->getRoot()->getLastRevision();
+				} elseif ( $revision instanceof PostSummary ) {
+					$root = $revision->getCollection()->getPost()->getRoot()->getLastRevision();
+				}
+			} catch ( FlowException $e ) {
+				// Do nothing - we're likely in a unit test where no root can
+				// be resolved because the revision is created on the fly
+			}
+		}
+
+		if ( $root && ( self::isHidden( $root ) ) ) {
+			return array();
+		}
 
 		return $this->referenceExtractor->getReferences(
 			$workflow,
 			$revision->getRevisionType(),
 			$revision->getCollectionId(),
-			$content
+			$revision->getContent( 'html' )
 		);
 	}
 
