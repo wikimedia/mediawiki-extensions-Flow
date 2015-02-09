432758bf090b5593e1217d57de966768369df3ef
Matthew Flaschen <mflaschen@wikimedia.org>
2015-02-09 18:23:52 -0500

    Disable shared SpamBlacklist during tests.
    This defaults to meta, which we definitely don't want during unit
    tests (and maybe don't want at all).
    
    Bug: T89052
    Change-Id: I0882f1720454f342b456209ad1bc5cd591ba51bf

--- tests/phpunit/FlowTestCase.php
+++ tests/phpunit/FlowTestCase.php
@@ -2,9 +2,27 @@
 
 namespace Flow\Tests;
 
+use Status;
+
+use Flow\Container;
 use Flow\Model\UUID;
 
 class FlowTestCase extends \MediaWikiTestCase {
+	protected function setUp() {
+		parent::setUp();
+
+		// Override dependency injection container to create test environment
+		$container = Container::getContainer();
+
+		// In the test where we actually test Flow\SpamFilter\SpamBlacklist, we
+		// don't use the container to get this.
+		$stub = $this->getMockBuilder('Flow\\SpamFilter\\SpamBlacklist')
+			->disableOriginalConstructor()
+			->getMock();
+
+		$stub->method( 'validate' )->willReturn( Status::newGood() );
+		$container['controller.spamblacklist'] = $stub;
+	}
 
 	/**
 	 * @param mixed $data
