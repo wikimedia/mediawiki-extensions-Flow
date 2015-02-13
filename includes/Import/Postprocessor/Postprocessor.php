a8692d96a55ab40d27f68331ea76bb071dfcaada
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-02-13 01:10:08 -0800

    Refactor import postprocessor interface
    This interface was not very truthful, the commit action was calling a method
    that indicated a full talkpage was imported, but really it was just a piece
    of the page.
    
    This patch removes the talkpage level methods, and move the call site of the
    regular events to be post-commit.  Additionally documented the methods, and
    switched the arguments to take the State objects.  This is a for a followup
    patch which logs about imported topics.
    
    Change-Id: I78fd0fdbd7c2d592e7287fa7f521c9a499385bb9

--- includes/Import/Postprocessor/Postprocessor.php
+++ includes/Import/Postprocessor/Postprocessor.php
@@ -3,12 +3,49 @@
 namespace Flow\Import\Postprocessor;
 
 use Flow\Model\UUID;
+use Flow\Import\IImportHeader;
 use Flow\Import\IImportPost;
 use Flow\Import\IImportTopic;
+use Flow\Import\PageImportState;
+use Flow\Import\TopicImportState;
 
 interface Postprocessor {
-	function afterTopicImported( IImportTopic $topic, UUID $newTopicId );
-	function afterPostImported( IImportPost $post, UUID $topicId, UUID $newPostId );
-	function afterTalkpageImported();
-	function talkpageImportAborted();
+	/**
+	 * Called after the successfull commit of a header. This is
+	 * currently called regardless of if any new content was imported.
+	 *
+	 * @param PageImportState $state
+	 * @param IImportHeader $header
+	 */
+	function afterHeaderImported( PageImportState $state, IImportHeader $header );
+
+	/**
+	 * Called after the import of a single post. This has not yet been
+	 * commited, and serves to inform the postprocessor about topic
+	 * import progress. Only posts that have not been previously
+	 * imported are reported here.
+	 *
+	 * @param TopicImportState $state
+	 * @param IImportPost $post
+	 * @param UUID $newPostId
+	 */
+	function afterPostImported( TopicImportState $state, IImportPost $post, UUID $newPostId );
+
+	/**
+	 * Called after the successful commit of a topic to the database.
+	 * This may or may not have imported any actual posts, it is
+	 * called on all topics run through the process regardless.
+	 *
+	 * @param TopicImportState $state
+	 * @param IImportPost $post
+	 */
+	function afterTopicImported( TopicImportState $state, IImportTopic $topic );
+
+	/**
+	 * Callled when there has been an error in the import process.
+	 * Any information the postprocessor has received since the last
+	 * commit operation should be discarded as it will not be written
+	 * to permenant storage.
+	 */
+	function importAborted();
 }
