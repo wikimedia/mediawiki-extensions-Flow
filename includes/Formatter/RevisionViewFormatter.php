8d429b55b6a196802506e16ce21a09e0160e255d
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-02-24 15:03:43 -0800

    Hygiene: Split RevisionDiffViewFormatter to one class per file
    Change-Id: I4af741169983b46df6f6210d864b63d7cfe9b752

--- includes/Formatter/RevisionViewFormatter.php
+++ includes/Formatter/RevisionViewFormatter.php
@@ -118,32 +118,3 @@ class RevisionViewFormatter {
 	}
 
 }
-
-class RevisionDiffViewFormatter {
-
-	protected $revisionViewFormatter;
-
-	public function __construct( RevisionViewFormatter $revisionViewFormatter ) {
-		$this->revisionViewFormatter = $revisionViewFormatter;
-	}
-
-	/**
-	 * Diff would format against two revisions
-	 */
-	public function formatApi( FormatterRow $newRow, FormatterRow $oldRow, IContextSource $ctx ) {
-		$oldRes = $this->revisionViewFormatter->formatApi( $oldRow, $ctx );
-		$newRes = $this->revisionViewFormatter->formatApi( $newRow, $ctx );
-
-		$oldContent = $oldRow->revision->getContent( 'wikitext' );
-		$newContent = $newRow->revision->getContent( 'wikitext' );
-
-		$differenceEngine = new \DifferenceEngine();
-
-		$differenceEngine->setContent(
-			new \TextContent( $oldContent ),
-			new \TextContent( $newContent )
-		);
-
-		return array( 'new' => $newRes, 'old' => $oldRes, 'diff_content' => $differenceEngine->getDiffBody() );
-	}
-}
