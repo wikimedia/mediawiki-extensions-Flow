f0a649867ecce0ae68a5ae00d715cd0ea5c76bfd
Matthias Mullie <git@mullie.eu>
2015-02-24 16:46:02 +0100

    Batchload logging data
    This is a bit of a hack because logging works differently.
    It's not hook-induced (where we just create the objects ourselves
    and just return some string to the hook caller)
    Instead, core creates these LogFormatter objects per entry.
    
    Notice how I'm also not directly using the loaded metadata (as
    we already do in some other places, iirc). It's batchloaded and
    unused, but because of our buffered cache, whenever it's fetched
    again (via the Collection objects - see parent patch), it'll be
    served from memory instead of doing network request to cache.
    
    Bug: T90066
    Change-Id: Iebeb7dbff5784520240f6b9a756b5b1edf39c7bf

--- autoload.php
+++ autoload.php
@@ -225,6 +225,7 @@ $wgAutoloadClasses += array(
 	'Flow\\Import\\Wikitext\\ImportSource' => __DIR__ . '/includes/Import/Wikitext/ImportSource.php',
 	'Flow\\LinksTableUpdater' => __DIR__ . '/includes/LinksTableUpdater.php',
 	'Flow\\Log\\ActionFormatter' => __DIR__ . '/includes/Log/ActionFormatter.php',
+	'Flow\\Log\\LogQuery' => __DIR__ . '/includes/Log/Query.php',
 	'Flow\\Log\\LqtImportFormatter' => __DIR__ . '/includes/Log/LqtImportFormatter.php',
 	'Flow\\Log\\ModerationLogger' => __DIR__ . '/includes/Log/ModerationLogger.php',
 	'Flow\\Model\\AbstractRevision' => __DIR__ . '/includes/Model/AbstractRevision.php',
