a4d686cb31bc7f1f33840e724562c6e4c5b11830
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-02-24 17:35:57 -0800

    Convert remaining files to one class per file
    Change-Id: I28f3ea86e6a898e1643809ba5258a19c3ea53bb8

--- tests/phpunit/Data/Index/FeatureIndexTest.php
+++ tests/phpunit/Data/Index/FeatureIndexTest.php
@@ -91,14 +91,3 @@ class FeatureIndexTest extends \MediaWikiTestCase {
 	}
 }
 
-class MockFeatureIndex extends FeatureIndex {
-	public function getLimit() { return 42; }
-	public function queryOptions() { return array(); }
-	public function limitIndexSize( array $values ) { return $values; }
-	public function addToIndex( array $indexed, array $row ) {}
-	public function removeFromIndex( array $indexed, array $row ) {}
-
-	// not abstract, but override for convenience
-	public function getSort() { return array( 'some_row' ); }
-	public function getOrder() { return 'ASC'; }
-}
