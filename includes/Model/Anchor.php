d8c4ae670bc68a570f4b1d8a29a46543e934ca4b
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-02-25 16:40:46 -0800

    Pre-support for ApiSerializable interface
    This adds the methods required by the ApiSerializable interface, but does
    not directly implement the class so this can be merged before the dependent
    core patch is merged.  That is required because the core patch is currently
    failing tests due to this.  That patch is I7b37295e88
    
    Bug: T90745
    Change-Id: I2ab697be8e3cab312fe9642de804dc199a8c76f8

--- includes/Model/Anchor.php
+++ includes/Model/Anchor.php
@@ -46,6 +46,13 @@ class Anchor {
 	}
 
 	/**
+	 * @return mixed
+	 */
+	public function serializeForApiResult() {
+		return $this->toArray();
+	}
+
+	/**
 	 * @return string
 	 */
 	public function getLinkURL() {
