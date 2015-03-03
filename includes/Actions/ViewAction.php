ba4b817bcb966e2b02df4c6de389b9b5e309790f
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-03-03 09:56:35 -0800

    Hygiene: Split the giant show function in Flow\\View
    Change-Id: Ie8e59ba2eeaeae328744bf907e4bd57523dbbb86

--- includes/Actions/ViewAction.php
+++ includes/Actions/ViewAction.php
@@ -4,9 +4,45 @@ namespace Flow\Actions;
 
 use IContextSource;
 use Page;
+use Title;
 
 class ViewAction extends FlowAction {
 	function __construct( Page $page, IContextSource $context ) {
 		parent::__construct( $page, $context, 'view' );
+	}
+
+	public function showForAction( $action, OutputPage $output = null ) {
+		parent::showForAction( $action, $output );
+
+		$title = $this->context->getTitle();
+		$this->context->getUser()->clearNotification( $title );
+
+		if ( $output === null ) {
+			$output = $this->context->getOutput();
+		}
+		$output->addCategoryLinks( $this->getCategories( $title ) );
+
+	}
+
+	protected function getCategories( Title $title ) {
+		$id = $title->getArticleId();
+		if ( !$id ) {
+			return array();
+		}
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select(
+			/* from */ 'categorylinks',
+			/* select */ array( 'cl_to', 'cl_sortkey' ),
+			/* conditions */ array( 'cl_from' => $id ),
+			__METHOD__
+		);
+
+		$categories = array();
+		foreach ( $res as $row ) {
+			$categories[$row->cl_to] = $row->cl_sortkey;
+		}
+
+		return $categories;
 	}
 }
