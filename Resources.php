147943ed79fa5edcdc254226f9a75439644fb61f
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-03-04 14:32:22 -0800

    Split preview into feature file
    Change-Id: Iff3b801e71f5b2d48828d4484d0ce5ad1b8ebbfc

--- Resources.php
+++ Resources.php
@@ -318,8 +318,6 @@ $wgResourceModules += array(
 			'engine/misc/flow-api.js',
 			// FlowEventLog
 			'engine/misc/flow-eventlog.js',
-			// wfBaseConvert ported to js
-			'engine/misc/flow-baseconvert.js',
 			// FlowComponent must come before actual components
 			'engine/components/common/flow-component-engines.js',
 			'engine/components/common/flow-component-events.js',
@@ -373,6 +371,7 @@ $wgResourceModules += array(
 		'dependencies' => array(
 			'ext.flow.components',
 			'ext.flow.editor',
+			'ext.flow.preview',
 			'jquery.throttle-debounce',
 			'mediawiki.jqueryMsg',
 			'ext.flow.jquery.conditionalScroll',
@@ -390,6 +389,16 @@ $wgResourceModules += array(
 	'ext.flow.vendor.storer' => $flowResourceTemplate + array(
 		'scripts' => array(
 			'vendor/Storer.js',
+		),
+	) + $mobile,
+	'ext.flow.preview' => $flowResourceTemplate + array(
+		'scripts' => array(
+			'engine/components/board/features/flow-board-preview.js',
+			// wfBaseConvert ported to js
+			'engine/misc/flow-baseconvert.js',
+		),
+		'dependencies' => array(
+			'ext.flow.components'
 		),
 	) + $mobile,
 	'ext.flow.editor' => $flowResourceTemplate + array(
