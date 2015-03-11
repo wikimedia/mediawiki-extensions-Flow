cd0f25e59a3b266a4300c51ef62f86db68ec3a8c
Matthias Mullie <git@mullie.eu>
2015-03-11 13:38:21 +0100

    Index Flow data
    To index all data, run:
        $ php maintenance/FlowForceSearchIndex.php
    
    Here's how the data is indexed in ES:
    
    http://localhost:9200/enwiki_flow/topic/:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"update_timestamp": "2014-02-25T14:12:40Z",
    	"revisions": [ {
    		"id": "rpvwvywl9po7ih77",
    		"text": "topic title content",
    		"source_text": "topic title content",
    		"moderation_state": "",
    		"timestamp": "2014-02-07T01:42:57Z",
    		"update_timestamp": "2014-02-07T01:42:57Z",
    		"type": "topic"
    	}, {
    		"id": "ropuzninqgyf19ko",
    		"text": "reply content",
    		"source_text": "reply '''content'''",
    		"moderation_state": "hide",
    		"timestamp": "2014-02-25T14:12:40Z",
    		"update_timestamp": "2014-02-25T14:12:40Z",
    		"type": "post"
    	} ]
    }
    
    http://localhost:9200/enwiki_flow/header/:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"update_timestamp": "2014-02-07T01:42:57Z",
    	"revisions": [ {
                    "id": "s1ijdhjhqeoq2b2r",
                    "text": "header content",
    		"source_text": "header content",
                    "moderation_state": "",
                    "timestamp": "2014-02-07T01:42:57Z",
    		"update_timestamp": "2014-02-07T01:42:57Z",
    		"type": "header"
    	} ]
    }
    
    We can do a full-text search, which can be filtered to only a (couple of)
    page(s) or namespace(s). This should, for example, translate to (in ES):
    
    // search for text, but only in specific page:
    // API: api.php?action=flow&submodule=search&qterm=test&qtitle=Talk:Test
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
            "query": {
                    "filtered": {
                            "query": {
                                    "term": { "revisions.text": "test" }
                            },
                            "filter": {
                                    "term": { "pageid": 24 }
                            }
                    }
            }
    }'
    
    The data stored allows us query for a bit more down the road, e.g.:
    
    Example queries (to ES):
    
    // find moderated stuff
    // this will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"term": { "revisions.moderation_state": "hide" }
    			}
    		}
    	}
    }'
    
    // find unread posts (will probably just check posts more recent than last visit)
    // will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"range": {
    					"revisions.timestamp": { "gt": "2014-04-01T00:00:00Z" }
    				}
    			}
    		}
    	}
    }'
    
    Fixes T78788
    
    Bug: T78788
    Change-Id: I4c5e868459ba5980b3a13ea402b2f5e2026178b2

---
+++ includes/Search/HeaderUpdater.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Flow\Search;
+
+use Flow\Container;
+use Flow\Data\ManagerGroup;
+use Flow\Model\Header;
+use Flow\Model\UUID;
+use ProfileSection;
+use Sanitizer;
+
+class HeaderUpdater extends Updater {
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getType() {
+		return Connection::HEADER_TYPE_NAME;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getRevisions( array $conditions = array(), array $options = array() ) {
+		$dbr = $this->dbFactory->getDB( DB_SLAVE );
+
+		// get the current (=most recent, =max) revision id for all headers
+		$rows = $dbr->select(
+			array( 'flow_revision', 'flow_workflow' ),
+			array( 'rev_id' => 'MAX(rev_id)' ),
+			$conditions,
+			__METHOD__,
+			array(
+				'ORDER BY' => 'rev_id ASC',
+				'GROUP BY' => 'rev_type_id',
+			) + $options,
+			array(
+				'flow_workflow' => array(
+					'INNER JOIN',
+					array( 'workflow_id = rev_type_id' , 'rev_type' => 'header' )
+				),
+			)
+		);
+
+		$uuids = array();
+		foreach ( $rows as $row ) {
+			$uuids[] = UUID::create( $row->rev_id );
+		}
+
+		/** @var ManagerGroup $storage */
+		$storage = Container::get( 'storage' );
+		return $storage->getStorage( 'Header' )->getMulti( $uuids );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function buildDocument( /* Header */ $revision ) {
+		/** @var Header $revision */
+
+		$profiler = new ProfileSection( __METHOD__ );
+
+		// get article title associated with this revision
+		$title = $revision->getCollection()->getWorkflow()->getArticleTitle();
+
+		// make sure we don't parse text that isn't meant to be parsed (e.g.
+		// topic titles are never meant to be parsed from wikitext to html)
+		$format = $revision->isFormatted() ? 'html' : 'wikitext';
+
+		$creationTimestamp = $revision->getCollectionId()->getTimestampObj();
+		$updateTimestamp = $revision->getRevisionId()->getTimestampObj();
+
+		// for consistency with topics, headers will also get "revisions",
+		// although there's always only 1 revision per document (unlike topics,
+		// which may have multiple sub-posts)
+		return new \Elastica\Document(
+			$revision->getCollectionId()->getAlphadecimal(),
+			array(
+				'namespace' => $title->getNamespace(),
+				'namespace_text' => $title->getPageLanguage()->getFormattedNsText( $title->getNamespace() ),
+				'pageid' => $title->getArticleID(),
+				'title' => $title->getText(),
+				'timestamp' => $creationTimestamp->getTimestamp( TS_ISO_8601 ),
+				'update_timestamp' => $updateTimestamp->getTimestamp( TS_ISO_8601 ),
+				'revisions' => array(
+					'id' => $revision->getCollectionId()->getAlphadecimal(),
+					'text' => trim( Sanitizer::stripAllTags( $revision->getContent( $format ) ) ),
+					'source_text' => $revision->getContent( 'wikitext' ), // for insource: searches
+					'moderation_state' => $revision->getModerationState(), // headers can't (currently) be moderated, so should always be MODERATED_NONE
+					'timestamp' => $creationTimestamp->getTimestamp( TS_ISO_8601 ),
+					'update_timestamp' => $updateTimestamp->getTimestamp( TS_ISO_8601 ),
+					'type' => $revision->getRevisionType(),
+				)
+			)
+		);
+	}
+}
