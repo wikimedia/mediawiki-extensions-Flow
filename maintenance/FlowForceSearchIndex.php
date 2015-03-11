96f1ffedac9741f8f6d2ed833f9315d5d6d31ca4
Matthias Mullie <git@mullie.eu>
2015-03-11 13:38:21 +0100

    Index Flow data
    To index all data, run:
        $ php maintenance/FlowForceSearchIndex.php
    
    Here's how the data is indexed in ES:
    
    http://localhost:9200/enwiki_flow/topic/:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"update_timestamp": "2014-02-25T14:12:40Z",
    	"revisions": [ {
    		"id": "rpvwvywl9po7ih77",
    		"text": "topic title content",
    		"source_text": "topic title content",
    		"moderation_state": "",
    		"timestamp": "2014-02-07T01:42:57Z",
    		"update_timestamp": "2014-02-07T01:42:57Z",
    		"type": "topic"
    	}, {
    		"id": "ropuzninqgyf19ko",
    		"text": "reply content",
    		"source_text": "reply '''content'''",
    		"moderation_state": "hide",
    		"timestamp": "2014-02-25T14:12:40Z",
    		"update_timestamp": "2014-02-25T14:12:40Z",
    		"type": "post"
    	} ]
    }
    
    http://localhost:9200/enwiki_flow/header/:
    {
    	"namespace": 1,
    	"namespace_text": "Talk",
    	"pageid": 2,
    	"title": "Main Page",
    	"timestamp": "2014-02-07T01:42:57Z",
    	"update_timestamp": "2014-02-07T01:42:57Z",
    	"revisions": [ {
                    "id": "s1ijdhjhqeoq2b2r",
                    "text": "header content",
    		"source_text": "header content",
                    "moderation_state": "",
                    "timestamp": "2014-02-07T01:42:57Z",
    		"update_timestamp": "2014-02-07T01:42:57Z",
    		"type": "header"
    	} ]
    }
    
    We can do a full-text search, which can be filtered to only a (couple of)
    page(s) or namespace(s). This should, for example, translate to (in ES):
    
    // search for text, but only in specific page:
    // API: api.php?action=flow&submodule=search&qterm=test&qtitle=Talk:Test
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
            "query": {
                    "filtered": {
                            "query": {
                                    "term": { "revisions.text": "test" }
                            },
                            "filter": {
                                    "term": { "pageid": 24 }
                            }
                    }
            }
    }'
    
    The data stored allows us query for a bit more down the road, e.g.:
    
    Example queries (to ES):
    
    // find moderated stuff
    // this will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"term": { "revisions.moderation_state": "hide" }
    			}
    		}
    	}
    }'
    
    // find unread posts (will probably just check posts more recent than last visit)
    // will return topics, we can then find the specific posts ourselves
    curl -XGET http://localhost:9200/enwiki_flow/topic/_search -d '{
    	"query": {
    		"filtered": {
    			"filter": {
    				"range": {
    					"revisions.timestamp": { "gt": "2014-04-01T00:00:00Z" }
    				}
    			}
    		}
    	}
    }'
    
    Fixes T78788
    
    Bug: T78788
    Change-Id: I4c5e868459ba5980b3a13ea402b2f5e2026178b2

---
+++ maintenance/FlowForceSearchIndex.php
@@ -0,0 +1,124 @@
+<?php
+
+use Flow\Container;
+use Flow\Model\AbstractRevision;
+use Flow\Model\UUID;
+use Flow\Search\Connection;
+use Flow\Search\Updater;
+
+require_once ( getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php'
+	: dirname( __FILE__ ) . '/../../../maintenance/Maintenance.php' );
+
+/**
+ * Similar to CirrusSearch's forceSearchIndex, this will force indexing of Flow
+ * data in ElasticSearch.
+ *
+ * @ingroup Maintenance
+ */
+class FlowForceSearchIndex extends Maintenance {
+	// @todo: do we need to steal more from Cirrus' ForceSearchIndex? What options are important?
+
+	public function __construct() {
+		parent::__construct();
+
+		$this->mDescription = 'Force indexing Flow revisions (headers & topics).';
+
+		$this->setBatchSize( 10 );
+
+		$this->addOption( 'fromId', 'Start indexing at a specific revision id (inclusive).', false, true );
+		$this->addOption( 'toId', 'Stop indexing at a specific revision (inclusive).', false, true );
+		$this->addOption( 'limit', 'Maximum number of revisions to process before exiting the script. Default to unlimited.', false, true );
+		$this->addOption( 'namespace', 'Only index revisions in this given namespace', false, true );
+	}
+
+	public function execute() {
+		global $wgFlowSearchMaintenanceTimeout;
+
+		// Set the timeout for maintenance actions
+		Connection::getSingleton()->setTimeout2( $wgFlowSearchMaintenanceTimeout );
+
+		/** @var Updater[] $updaters */
+		$updaters = Container::get( 'searchindex.updaters' );
+		foreach ( $updaters as $updaterType => $updater ) {
+			$fromId = $this->getOption( 'fromId', null );
+			$fromId = $fromId ? UUID::create( $fromId ) : null;
+			$toId = $this->getOption( 'toId', null );
+			$toId = $toId ? UUID::create( $toId ) : null;
+			$namespace = $this->getOption( 'namespace', null );
+			$numRevisionsToIndex = $this->getOption( 'limit', null );
+			$total = 0;
+
+			while ( true ) {
+				// if a limit was provided, we should make sure to not fetch
+				// more revisions than asked for
+				$options = array( 'LIMIT' => $this->mBatchSize );
+				if ( $numRevisionsToIndex ) {
+					$options['LIMIT'] = min( $numRevisionsToIndex, $this->mBatchSize );
+
+					// since we do this in batches, we'll subtract the size of
+					// each batch until $numRevisionsToIndex is reached
+					$numRevisionsToIndex -= $this->mBatchSize;
+					if ( $options['LIMIT'] <= 0 ) {
+						break;
+					}
+				}
+
+				$conditions = $updater->buildQueryConditions( $fromId, $toId, $namespace );
+				$revisions = $updater->getRevisions( $conditions, $options );
+
+				// stop if we're all out of revisions
+				if ( !$revisions ) {
+					break;
+				}
+
+				$total += $updater->updateRevisions( $revisions, null, null );
+				$this->output( "Indexed $total $updaterType document(s)\n" );
+
+				// prepare for next batch, starting at the next id
+				// prevFromId will default to around unix epoch - there can be
+				// no data before that
+				$prevFromId = $fromId ?: UUID::getComparisonUUID( '1' );
+				$fromId = $this->getNextFromId( $revisions );
+
+				// make sure we don't get stuck in an infinite loop
+				$diff = $prevFromId->getTimestampObj()->diff( $fromId->getTimestampObj() );
+				// invert will be 1 if the diff is a negative time period from
+				// $prevFromId to $fromId, which means that the new $timestamp is
+				// more recent than our current $result
+				if ( $diff->invert ) {
+					$this->error(
+						'Got stuck in an infinite loop.' . "\n" .
+						'workflow_last_update_timestamp is likely incorrect ' .
+						'for some workflows.' . "\n" .
+						'Run maintenance/FlowFixWorkflowLastUpdateTimestamp.php ' .
+						'to automatically fix those.', 1 );
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param AbstractRevision[] $revisions
+	 * @return UUID
+	 */
+	protected function getNextFromId( array $revisions ) {
+		/** @var AbstractRevision $last */
+		$last = end( $revisions );
+
+		if ( $last instanceof \Flow\Model\Header ) {
+			$timestamp = $last->getRevisionId()->getTimestampObj();
+		} else {
+			$timestamp = $last->getCollection()->getWorkflow()->getLastModifiedObj();
+		}
+
+		// $timestamp is the timestamp of the last revision we fetched. fromId
+		// is inclusive, and we don't want to include what we already have here,
+		// so we'll advance 1 more and call that the next fromId
+		$timestamp = (int) $timestamp->getTimestamp( TS_UNIX );
+		return UUID::getComparisonUUID( $timestamp + 1 );
+	}
+}
+
+$maintClass = 'FlowForceSearchIndex';
+require_once RUN_MAINTENANCE_IF_MAIN;
