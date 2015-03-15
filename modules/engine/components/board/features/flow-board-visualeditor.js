ae61ecb3481eb5223bca6fc7b72e31f037f49826
Matthew Flaschen <mflaschen@wikimedia.org>
2015-03-15 01:05:53 -0400

    Refactor and implement mention inspector
    * Refactor to add a target (inheriting from sa); this allows customizing
      the toolbar.  It is now limited to three items, plus the menu.
    
    * Simplify module handling.  The main module is still lazy loaded, and
      that just uses normal dependencies.
    
      Also, dropped handling the experimental flag for now.  Most of the
      experiments would probably not be applicable, and this is still
      experimental itself.
    
    * Implement the initial inspector, with:
      * Toolbar item
      * Auto-complete based on topic authors (case-insensitive, anywhere
        in the string)
      * Full edit and remove support (for remove, both via back-spacing
        and by button)
      * Validation that user exists, with appropriate error messages
        if not.
    
    Depends on:
    * https://gerrit.wikimedia.org/r/#/c/197464/ (mw.util.isIPAddress)
    * https://gerrit.wikimedia.org/r/197475 (suggestions when blank)
    * https://gerrit.wikimedia.org/r/197862 (Jenkins depend on VE)
    * https://gerrit.wikimedia.org/r/#/c/197835/ (Update OOjs to v1.1.6),
      for OO.unique
    
    Bug: T90764
    Bug: T92588
    Change-Id: I0a14f4779c73996d4cb063b27f3330def0baae6d

---
+++ modules/engine/components/board/features/flow-board-visualeditor.js
@@ -0,0 +1,37 @@
+/*!
+ * Expose some functionality on the board object that is needed for VisualEditor.
+ */
+
+( function ( $, mw, OO ) {
+	/**
+	 * FlowBoardComponentVisualEditorFeatureMixin
+	 *
+	 * @this FlowBoardComponent
+	 * @constructor
+	 *
+	 */
+	function FlowBoardComponentVisualEditorFeatureMixin( $container ) {
+	}
+
+	// This is not really VE-specific, but I'm not sure where best to put it.
+	// Also, should we pre-compute this in a loadHandler?
+	/**
+	 * Finds topic authors for the given node
+	 *
+	 * @return Array List of usernames
+	 */
+	function flowVisualEditorGetTopicPosters( $node ) {
+		var $topic = $node.closest( '.flow-topic' ),
+			duplicatedArray;
+
+		// Could use a data attribute to avoid trim.
+		duplicatedArray = $.map( $topic.find( '.flow-author .mw-userlink' ).get(), function ( el ) {
+			return $.trim( $( el ).text() );
+		} );
+		return OO.unique( duplicatedArray );
+	}
+
+	FlowBoardComponentVisualEditorFeatureMixin.prototype.getTopicPosters = flowVisualEditorGetTopicPosters;
+
+	mw.flow.mixinComponent( 'board', FlowBoardComponentVisualEditorFeatureMixin );
+}( jQuery, mediaWiki, OO ) );
