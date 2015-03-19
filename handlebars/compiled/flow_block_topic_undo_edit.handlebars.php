e432e1985746f2ffd816575e451b3831f3cd4bb9
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-03-19 16:05:32 -0700

    Wrap all textarea's for editing in a flow-editor class
    Add a div.flow-editor wrapping all the textareas.  When initializing a specific
    editor add a .flow-editor-<name> class so that css rules can target a specific editor.
    This needs to be a div, rather than on the form, due to some floats that will be used
    with the wikitext (none) editor.
    
    This is in preparation for the editor switching code, spliting
    up the patches to make them easier to review.
    
    Change-Id: Ib28d77cf8d0e375465bc8aab25f5136826b81c0e

--- handlebars/compiled/flow_block_topic_undo_edit.handlebars.php
+++ handlebars/compiled/flow_block_topic_undo_edit.handlebars.php
@@ -58,13 +58,15 @@
 		<input type="hidden" name="topic_prev_revision" value="'.htmlentities((string)((isset($in['current']['revisionId']) && is_array($in['current'])) ? $in['current']['revisionId'] : null), ENT_QUOTES, 'UTF-8').'" />
 		<input type="hidden" name="topic_postId" value="'.htmlentities((string)((isset($in['current']['postId']) && is_array($in['current'])) ? $in['current']['postId'] : null), ENT_QUOTES, 'UTF-8').'" />
 
-		<textarea name="topic_content"
-		          class="mw-ui-input"
-		          data-role="content"
-		          data-flow-preview-template="flow_post"
-		          data-flow-preview-title="'.htmlentities((string)((isset($in['articleTitle']) && is_array($in)) ? $in['articleTitle'] : null), ENT_QUOTES, 'UTF-8').'"
-		          data-flow-username="'.htmlentities((string)((isset($in['current']['creator']['name']) && is_array($in['current']['creator'])) ? $in['current']['creator']['name'] : null), ENT_QUOTES, 'UTF-8').'"
-		>'.((LCRun3::ifvar($cx, ((isset($in['submitted']['content']) && is_array($in['submitted'])) ? $in['submitted']['content'] : null))) ? ''.htmlentities((string)((isset($in['submitted']['content']) && is_array($in['submitted'])) ? $in['submitted']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.((LCRun3::ifvar($cx, ((isset($in['undo']['possible']) && is_array($in['undo'])) ? $in['undo']['possible'] : null))) ? ''.htmlentities((string)((isset($in['undo']['content']) && is_array($in['undo'])) ? $in['undo']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities((string)((isset($in['current']['content']['content']) && is_array($in['current']['content'])) ? $in['current']['content']['content'] : null), ENT_QUOTES, 'UTF-8').'').'').'</textarea>
+		<div class="flow-editor">
+			<textarea name="topic_content"
+			          class="mw-ui-input"
+			          data-role="content"
+			          data-flow-preview-template="flow_post"
+			          data-flow-preview-title="'.htmlentities((string)((isset($in['articleTitle']) && is_array($in)) ? $in['articleTitle'] : null), ENT_QUOTES, 'UTF-8').'"
+			          data-flow-username="'.htmlentities((string)((isset($in['current']['creator']['name']) && is_array($in['current']['creator'])) ? $in['current']['creator']['name'] : null), ENT_QUOTES, 'UTF-8').'"
+			>'.((LCRun3::ifvar($cx, ((isset($in['submitted']['content']) && is_array($in['submitted'])) ? $in['submitted']['content'] : null))) ? ''.htmlentities((string)((isset($in['submitted']['content']) && is_array($in['submitted'])) ? $in['submitted']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.((LCRun3::ifvar($cx, ((isset($in['undo']['possible']) && is_array($in['undo'])) ? $in['undo']['possible'] : null))) ? ''.htmlentities((string)((isset($in['undo']['content']) && is_array($in['undo'])) ? $in['undo']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities((string)((isset($in['current']['content']['content']) && is_array($in['current']['content'])) ? $in['current']['content']['content'] : null), ENT_QUOTES, 'UTF-8').'').'').'</textarea>
+		</div>
 
 		<div class="flow-form-actions flow-form-collapsible">
 			<button class="mw-ui-button mw-ui-constructive">'.LCRun3::ch($cx, 'l10n', array(array('flow-edit-post-submit'),array()), 'encq').'</button>
