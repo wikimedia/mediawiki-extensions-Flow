bfb589d72f2b4a6ede19f3c911272bc8d03495ae
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-03-18 17:31:57 -0700

    Editor switcher
    Create buttons to switch between wikitext and visual editors.  Remembers the
    selected editor and continues to use that on future requests.
    
    To pass the unit tests Ic5b8bc2 in core needs to be merged.
    
    Change-Id: Ibed8693bfd825819aa53fec61385303b29cecfb1

--- Resources.php
+++ Resources.php
@@ -233,6 +233,8 @@ $wgResourceModules += array(
 			'flow-edited-by',
 			// Board header
 			"flow-board-header-browse-topics-link",
+			// editor switching
+			"flow-wikitext-editor-help"
 		),
 	) + $mobile,
 	// @todo: upstream to mediawiki ui
@@ -284,6 +286,7 @@ $wgResourceModules += array(
 			'styles/board/content-preview.less',
 			'styles/board/form-actions.less',
 			'styles/board/terms-of-use.less',
+			'styles/board/editor-switcher.less',
 		),
 	) + $mobile,
 	'ext.flow.board.topic.styles' => $flowResourceTemplate + array(
@@ -357,6 +360,8 @@ $wgResourceModules += array(
 			'engine/components/board/features/flow-board-toc.js',
 			// Feature: VisualEditor
 			'engine/components/board/features/flow-board-visualeditor.js',
+			// Feature: Switch between editors
+			'engine/components/board/features/flow-board-switcheditor.js',
 
 			// Component: FlowBoardHistoryComponent
 			'engine/components/board/flow-boardhistory.js',
@@ -374,6 +379,7 @@ $wgResourceModules += array(
 			'ext.flow.jquery.conditionalScroll',
 			'mediawiki.api',
 			'mediawiki.util',
+			'mediawiki.api.options', // required by switch-editor feature
 		),
 		'messages' => array(
 			'flow-error-external',
@@ -440,6 +446,8 @@ $wgResourceModules += array(
 			// MentionInspectorTool must be after MentionInspector and before MentionContextItem.
 			'editor/editors/visualeditor/ui/contextitem/mw.flow.ve.ui.MentionContextItem.js',
 			'editor/editors/visualeditor/ui/widgets/mw.flow.ve.ui.MentionTargetInputWidget.js',
+			'editor/editors/visualeditor/ui/tools/mw.flow.ve.ui.SwitchEditorTool.js',
+			'editor/editors/visualeditor/ui/actions/mw.flow.ve.ui.SwitchEditorAction.js',
 			'editor/editors/visualeditor/mw.flow.ve.CommandRegistry.js',
 			'editor/editors/visualeditor/mw.flow.ve.TriggerRegistry.js',
 		),
