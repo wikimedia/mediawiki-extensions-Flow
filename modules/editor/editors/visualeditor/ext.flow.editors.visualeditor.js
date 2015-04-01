1ee6d961d7e7cdb6c7b34547681328901a094cca
Matthias Mullie <git@mullie.eu>
2015-04-01 11:32:43 +0200

    Conditionally add switch buttons
    There should be no switch button if the other editor isn't supported.
    
    Since the switch-button under wikitext textarea may not be added, I
    also had to change how that border is added. I've added it on the
    parent div, where textarea & switcher are both children of. With or
    without the switcher, there'll be a complete border.
    
    I'm not too happy with the editors depending on one another (that
    sort of logic should probably be outside of editors) but hey, it works.
    
    VE depends on none through Resources.php. None depends in VE in JS.
    I couldn't add VE as a dependency because VE has no mobile target.
    Since VE won't work on mobile, the resource won't be able to load in
    JS & the switcher won't be attached.
    
    Meanwhile also slightly cleaned up some jsdoc
    
    Bug: T94676
    Change-Id: Ia4a0d4e2b10564afe68a567fd5fa93b20324b7dd
    (cherry picked from commit 3e0cbc544e825a2f7ba3b9952669d4d1ed1a1ac7)

--- modules/editor/editors/visualeditor/ext.flow.editors.visualeditor.js
+++ modules/editor/editors/visualeditor/ext.flow.editors.visualeditor.js
@@ -191,13 +191,16 @@
 			// ES5 support, from es5-skip.js
 			( function () {
 				// This test is based on 'use strict',
-				// which is inherited from the top-leve function.
+				// which is inherited from the top-level function.
 				return !this && !!Function.prototype.bind;
 			}() ) &&
 
 			// Since VE commit e2fab2f1ebf2a28f18b8ead08c478c4fc95cd64e, SVG is required
 			document.createElementNS &&
-			document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect
+			document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect &&
+
+			// ve needs to be turned on as a valid editor
+			mw.config.get( 'wgFlowEditorList' ).indexOf( 'visualeditor' ) !== -1
 		);
 	};
 
