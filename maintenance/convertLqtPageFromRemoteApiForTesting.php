3d7ae66ac46d0413a76639751783b3395e94d324
Matthew Flaschen <mflaschen@wikimedia.org>
2015-04-08 18:46:27 -0700

    WIP: LQT maintenance refactoring
    * Separate convertLqtPage.php into convertLqtPageOnLocalWiki.php
      (testing and production) and
      convertLqtPageFromRemoteApiForTesting.php (testing only).  Although
      there are similarities, they have fundamentally different purposes
      and behavior.  This also allows being strict with required parameters,
      to make them easier to run.
    
    * Fixed a bug with flow-importer-lqt-suppressed-user-template being
      defined wrong (the message is the template name, not the text).
    
    * Use master for both reads and writes
    
    * Warn and skip if the user puts an invalid page title in Converter
      (before it would fail at the move stage, which is unintuitive).
    
    * Fixed a bug with MovedImportPost
    
    * Remove unnecessary options, and just use the right behavior.
    
    * Fix a typo that would break notifications on convertLqt
    
    Bug: T92303
    Bug: T95633
    Bug: T95380
    Change-Id: Idb4c95d189c6de3df3a0c5103a619c7844296b20

---
+++ maintenance/convertLqtPageFromRemoteApiForTesting.php
@@ -0,0 +1,87 @@
+<?php
+
+use Flow\Import\FileImportSourceStore;
+use Flow\Import\LiquidThreadsApi\BaseConversionStrategy as LiquidThreadsApiBaseConversionStrategy;
+use Flow\Import\LiquidThreadsApi\RemoteConversionStrategy as LiquidThreadsApiRemoteConversionStrategy;
+use Flow\Import\LiquidThreadsApi\LocalApiBackend;
+use Flow\Import\LiquidThreadsApi\RemoteApiBackend;
+use Flow\Import\LiquidThreadsApi\ImportSource as LiquidThreadsApiImportSource;
+use Flow\Import\Postprocessor\LqtRedirector;
+use Flow\Import\Postprocessor\LqtNotifications;
+use Psr\Log\LogLevel;
+
+require_once ( getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php'
+	: dirname( __FILE__ ) . '/../../../maintenance/Maintenance.php' );
+
+/**
+ * This is *only* for use in testing, not production.  The primary purpose is to exercise
+ * the API (production also uses the API, but with FauxRequest) and Parsoid.
+ *
+ * This also does not test redirects or notification conversion.
+ */
+class ConvertLqtPageFromRemoteApiForTesting extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Converts LiquidThreads data to Flow data.  Destination page is determined by ConversionStrategy";
+		$this->addOption( 'dstpage', 'Page name of the destination page on the current wiki.  Defaults to same as source', false, true );
+		$this->addOption( 'srcpage', 'Page name of the source page to import from.', true, true );
+		$this->addOption( 'remoteapi', 'Remote API URL to read from', true, true );
+		$this->addOption( 'cacheremoteapidir', 'Cache remote api calls to the specified directory', true, true );
+		$this->addOption( 'logfile', 'File to read and store associations between imported items and their sources', true, true );
+		$this->addOption( 'debug', 'Include debug information to progress report' );
+	}
+
+	public function execute() {
+		$cacheDir = $this->getOption( 'cacheremoteapidir' );
+		if ( !is_dir( $cacheDir ) ) {
+			if ( !mkdir( $cacheDir ) ) {
+				throw new Flow\Exception\FlowException( 'Provided dir for caching remote api calls is not creatable.' );
+			}
+		}
+		if ( !is_writable( $cacheDir ) ) {
+			throw new Flow\Exception\FlowException( 'Provided dir for caching remote api calls is not writable.' );
+		}
+
+		$api = new RemoteApiBackend( $this->getOption( 'remoteapi' ), $cacheDir );
+
+		$importer = Flow\Container::get( 'importer' );
+		$importer->setAllowUnknownUsernames( true );
+
+		$talkPageManagerUser = \FlowHooks::getOccupationController()->getTalkpageManager();
+
+		$srcPageName = $this->getOption( 'srcpage' );
+		if ( $this->hasOption( 'dstpage' ) ) {
+			$dstPageName = $this->getOption( 'dstpage' );
+		} else {
+			$dstPageName = $srcPageName;
+		}
+
+		$dstTitle = Title::newFromText( $dstPageName );
+		$source = new LiquidThreadsApiImportSource(
+			$api,
+			$srcPageName,
+			$talkPageManagerUser
+		);
+
+		$logFilename = $this->getOption( 'logfile' );
+		$sourceStore = new FileImportSourceStore( $logFilename );
+
+		$logger = new MaintenanceDebugLogger( $this );
+		if ( $this->getOption( 'debug' ) ) {
+			$logger->setMaximumLevel( LogLevel::DEBUG );
+		} else {
+			$logger->setMaximumLevel( LogLevel::INFO );
+		}
+
+		$importer->setLogger( $logger );
+		$api->setLogger( $logger );
+
+		$logger->info( "Starting LQT conversion of page $srcPageName" );
+
+		$importer->import( $source, $dstTitle, $sourceStore );
+	}
+}
+
+$maintClass = "ConvertLqtPageFromRemoteApiForTesting";
+require_once ( RUN_MAINTENANCE_IF_MAIN );
