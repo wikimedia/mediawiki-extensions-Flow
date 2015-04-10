f55538d266c08cd86eaeb1e8227ffe12fcee2972
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-04-10 15:31:31 -0700

    Ensure we do not provide null data to insert in ES
    Bug: T95580
    Change-Id: I06fa016b2c01f19788b4c836fe2f4b59f1188a2c

--- includes/Data/Storage/RevisionStorage.php
+++ includes/Data/Storage/RevisionStorage.php
@@ -417,10 +417,14 @@ abstract class RevisionStorage extends DbStorage {
 	}
 
 	protected function insertExternalStore( array $row ) {
+		if ( $row['rev_content'] === null || strlen( $row['rev_content'] ) === 0 ) {
+			throw new DataModelException( "Must have data to write to external storage", 'process-data' );
+		}
 		$url = ExternalStore::insertWithFallback( $this->externalStore, $row['rev_content'] );
 		if ( !$url ) {
 			throw new DataModelException( "Unable to store text to external storage", 'process-data' );
 		}
+
 		$row['rev_content_url'] = $url;
 		if ( $row['rev_flags'] ) {
 			$row['rev_flags'] .= ',external';
