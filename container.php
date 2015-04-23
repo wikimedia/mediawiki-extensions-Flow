f14ffcfdf20cb5118d5e69601c5e9a3c2b9c7b56
Matthew Flaschen <mflaschen@wikimedia.org>
2015-04-23 18:47:11 -0400

    Fix ConvertLqtPageOnLocalWiki for recent core (API) and Flow changes
    Bug: T96325
    Change-Id: I6c9056f03ae8e2770e19e5b2876a0f6c50fd8b81

--- container.php
+++ container.php
@@ -1189,7 +1189,8 @@ $c['importer'] = function( $c ) {
 		$c['factory.loader.workflow'],
 		$c['memcache.buffered'],
 		$c['db.factory'],
-		$c['deferred_queue']
+		$c['deferred_queue'],
+		$c['occupation_controller']
 	);
 
 	$importer->addPostprocessor( new Flow\Import\Postprocessor\SpecialLogTopic(
