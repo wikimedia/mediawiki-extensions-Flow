22651d59e0bf0569948e6e4ebe7c55cd9300f6dd
Moriel Schottlender <moriel@gmail.com>
2015-04-30 18:08:57 -0700

    [wip^2] Transform the new topic button to ooui widgets
    * Don't review me yet, I'm ugly *
    
    Change-Id: I73295e1664e0a226100cd65aa570aeec02e5373c

---
+++ modules/engine/components/common/flow-component-ooui.js
@@ -0,0 +1,141 @@
+/*!
+ * Contains flow-menu functionality.
+ */
+
+( function ( $, mw ) {
+	/**
+	 * Initializes and handles the ooui components
+	 *
+	 * @this FlowComponentOoui
+	 * @constructor
+	 */
+	mw.flow.ooui = function FlowComponentOoui( config ) {
+		config = config || {};
+
+		this.widgets = {};
+		this.title = mw.Title.newFromText( mw.config.get( 'wgPageName' ) );
+
+		// New topic save button
+		this.widgets['flow-newtopic-save'] = OO.ui.infuse( 'flow-newtopic-save' );
+		this.widgets['flow-newtopic-save'].connect( this, { click: this.onNewTopicSaveButtonClick } );
+
+		// New topic cancel
+		this.widgets['flow-newtopic-cancel'] = OO.ui.infuse( 'flow-newtopic-cancel' );
+		this.widgets['flow-newtopic-cancel'].connect( this, { click: this.onCancelNewTopicButtonClick } );
+
+		// Remove the form action
+		this.widgets['flow-newtopic-save'].$element.closest( 'form' ).attr( 'action', '' );
+	};
+	OO.initClass( mw.flow.ooui );
+
+	/**
+	 * Respond to new topic cancel button click and add a topic
+	 */
+	mw.flow.ooui.prototype.onCancelNewTopicButtonClick = function () {
+		var $form = $( '.flow-newtopic-form' ),
+			$button = this.widgets['flow-newtopic-cancel'].$element,
+			$textarea = $form.find( '.flow-editor textarea' ),
+			$subject = $form.find( 'input[name=topiclist_topic]' ),
+			flowBoard = mw.flow.getPrototypeMethod( 'board', 'getInstanceByElement' )( $form );
+
+		if ( $button.length ) {
+			// Clear contents to not trigger the "are you sure you want to
+			// discard your text" warning
+			$subject.val( '' );
+			$textarea.val( $textarea.defaultValue );
+			// $form.find( 'textarea, :text' ).each( function() {
+			// 	$( this ).val( this.defaultValue );
+			// } );
+
+			// remove focus - title input field may still have focus
+			// (submitted via enter key), which it needs to lose:
+			// the form will only re-activate if re-focused
+			document.activeElement.blur();
+
+			// Shrink the form
+			$textarea.addClass( 'flow-input-compressed' );
+			if ( mw.flow.editor && mw.flow.editor.exists( $textarea ) ) {
+				mw.flow.editor.destroy( $textarea );
+			}
+			// Run loadHandlers
+			flowBoard.emitWithReturn( 'makeContentInteractive', $( '.flow-component' ) );//$newtopic );
+
+		}
+	};
+
+	/**
+	 * Respond to topic save button click and add a topic
+	 */
+	mw.flow.ooui.prototype.onNewTopicSaveButtonClick = function () {
+		var api = new mw.Api(),
+			$form = $( '.flow-newtopic-form' ),
+			$boardTopics = $form.closest( 'div.flow-board .flow-topics' ),
+			$textarea = $form.find( '.flow-editor textarea' ),
+			// TODO: Work with actual data model instead of fetching
+			// data from the DOM
+			flowBoard = mw.flow.getPrototypeMethod( 'board', 'getInstanceByElement' )( $form ),
+			topic = $form.find( 'input[name=topiclist_topic]' ).val(),
+			content = mw.flow.editor.getRawContent( $textarea ),
+			format = mw.flow.editor.getFormat( $textarea ) || 'wikitext',
+			buttonData = this.widgets['flow-newtopic-save'].getData();
+
+		// Post new topic
+		// TODO: Use mw.flow.messagePoster here instead
+		return api.post( {
+			action: 'flow',
+			submodule: 'new-topic',
+			page: this.title.getPrefixedDb(),
+			nttopic: topic,
+			ntcontent: content,
+			ntformat: format,
+			token: buttonData.args[0] // editToken
+		} )
+			.then( function ( result ) {
+				return flowBoard.Api.apiCall( {
+					action: 'flow',
+					submodule: 'view-topic',
+					page: result.flow['new-topic'].committed.topiclist['topic-page']
+				} );
+			} )
+			.then(
+				// Success
+				function ( result ) {
+					// TODO: OOUIfy the entire thing!
+					// Update view of the full topic
+					var schemaName = $( this ).data( 'flow-eventlog-schema' ),
+						funnelId = $( this ).data( 'flow-eventlog-funnel-id' ),
+						$stub = $( '<div class="flow-topic"><div></div></div>' ).prependTo( flowBoard.$container.find( '.flow-topics' ) ),
+						$target = $stub.closest( '.flow-topic' ),
+						$newtopic = $( flowBoard.constructor.static.TemplateEngine.processTemplateGetFragment(
+						'flow_topiclist_loop.partial',
+						result.flow['view-topic'].result.topic
+					) ).children();
+
+					flowBoard.logEvent( schemaName, { action: 'save-success', funnelId: funnelId } );
+
+					flowBoard.emitWithReturn( 'cancelForm', $( this ).closest( 'form' ) );
+
+					// remove focus - title input field may still have focus
+					// (submitted via enter key), which it needs to lose:
+					// the form will only re-activate if re-focused
+					document.activeElement.blur();
+					$form.find( 'input[name=topiclist_topic]' ).val( '' );
+
+					$target = $stub.find( 'div' );
+					$target.replaceWith( $newtopic );
+
+					// Run loadHandlers
+					flowBoard.emitWithReturn( 'makeContentInteractive', $( '.flow-component' ) );//$newtopic );
+				},
+				// Failure
+				function ( code, result ) {
+					var errorMsg = flowBoard.constructor.static.getApiErrorMessage( code, result );
+					errorMsg = mw.msg( 'flow-error-fetch-after-open-lock', errorMsg );
+
+					flowBoard.emitWithReturn( 'removeError', $boardTopics );
+					flowBoard.emitWithReturn( 'showError', $boardTopics, errorMsg );
+				}
+			);
+	};
+
+}( jQuery, mediaWiki ) );
