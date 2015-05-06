6466a03f78552f754992bc25e06046d6f337af13
Moriel Schottlender <moriel@gmail.com>
2015-05-05 17:45:37 -0700

    Create a basic data model layer for Flow JS
    Create a dm for Flow; start with a flow Board and Topic models with
    basic fetching from the API.
    
    Bug: T98368
    Change-Id: Iba245037078f548edd2dc10519e8f2c0c650cb00

---
+++ modules/flow/dm/mw.flow.dm.BoardDescription.js
@@ -0,0 +1,22 @@
+/**
+ * Flow board description
+ *
+ * @constructor
+ *
+ * @extends mw.flow.dm.RevisionedContent
+ *
+ * @param {Object} data API data to build topic header with
+ * @param {Object} [config] Configuration options
+ */
+mw.flow.dm.BoardDescription = function mwFlowDmBoardDescription( data, config ) {
+	config = config || {};
+
+	// Parent constructor
+	mw.flow.dm.BoardDescription.super.call( this, config );
+
+	this.populate( data );
+};
+
+/* Initialization */
+
+OO.inheritClass( mw.flow.dm.BoardDescription, mw.flow.dm.RevisionedContent );
