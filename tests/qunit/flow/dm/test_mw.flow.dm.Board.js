e9758cd3cd8af71ce1a6a05b452477e0fd95a71f
Moriel Schottlender <moriel@gmail.com>
2015-05-05 17:45:37 -0700

    [wip] Create a basic data model layer for Flow JS
    Create a dm for Flow; start with a flow Board and Topic models with
    basic fetching from the API.
    
    Bug: T98368
    Change-Id: Iba245037078f548edd2dc10519e8f2c0c650cb00

---
+++ tests/qunit/flow/dm/test_mw.flow.dm.Board.js
@@ -0,0 +1,125 @@
+QUnit.module( 'ext.flow.dm mw.flow.dm.Board' );
+
+/* Tests */
+
+QUnit.test( 'Create board', function ( assert ) {
+	var i, ilen, result, board,
+		executeOperation = function ( obj, operation, params ) {
+			return obj[operation].apply( obj, params );
+		},
+		expectCount = 0,
+		cases = [
+			{
+				method: 'getHash',
+				expected: {
+					id: 'xxx123xxx',
+					isDeleted: false,
+					pagePrefixedDb: 'Special:FlowTestBoardPage',
+					topicCount: 0,
+					description: undefined
+				},
+				msg: 'Get current board initial state'
+			},
+			{
+				method: 'storeComparableHash',
+				ignoreTest: true
+			},
+			{
+				method: 'addItems',
+				ignoreTest: true,
+				params: [
+					[
+						new mw.flow.dm.Topic( 'topic1', {
+							content: {
+								content: 'Topic 1',
+								format: 'plaintext'
+							},
+							lastUpdated: '123123123'
+						} ),
+						new mw.flow.dm.Topic( 'topic2', {
+							content: {
+								content: 'Topic 2',
+								format: 'plaintext'
+							},
+							lastUpdated: '123123321'
+						} ),
+						new mw.flow.dm.Topic( 'topic3', {
+							content: {
+								content: 'Topic 3',
+								format: 'plaintext'
+							},
+							lastUpdated: '123123543'
+						} )
+					]
+				]
+			},
+			{
+				method: 'getItemCount',
+				expected: 3,
+				msg: 'Add topics.'
+			},
+			{
+				method: 'setDescription',
+				ignoreTest: true,
+				params: [
+					new mw.flow.dm.BoardDescription( {
+						content: {
+							content: '<h1>This is test board description.</h1>',
+							format: 'html'
+						}
+					} )
+				]
+			},
+			{
+				method: 'getHash',
+				expected: {
+					id: 'xxx123xxx',
+					isDeleted: false,
+					pagePrefixedDb: 'Special:FlowTestBoardPage',
+					topicCount: 3,
+					description: {
+						articleTitle: undefined,
+						author: undefined,
+						changeType: undefined,
+						content: '<h1>This is test board description.</h1>',
+						contentFormat: 'html',
+						creator: undefined,
+						id: null,
+						lastUpdated: undefined,
+						originalContent: false,
+						previousRevisionId: undefined,
+						revisionId: undefined,
+						timestamp: undefined,
+						watchable: undefined,
+						watched: false,
+						workflowId: undefined
+					}
+				},
+				msg: 'Get current board state with description'
+			},
+			{
+				method: 'hasBeenChanged',
+				expected: true,
+				msg: 'Check if board has changed since breakpoint'
+			}
+		];
+
+	board = new mw.flow.dm.Board( {
+		id: 'xxx123xxx',
+		pageTitle: ( new mw.Title( 'Special:FlowTestBoardPage' ) ),
+		isDeleted: false
+	} );
+
+	expectCount = cases.length;
+	for ( i = 0, ilen = cases.length; i < ilen; i++ ) {
+		result = executeOperation( board, cases[i].method, cases[i].params || [] );
+		if ( !cases[ i ].ignoreTest ) {
+			// Test
+			assert.deepEqual( result, cases[i].expected, cases[i].msg );
+		} else {
+			expectCount--;
+		}
+	}
+
+	QUnit.expect( expectCount );
+} );
