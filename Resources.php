152ff6ee54c2ab5e505fe69afd966625873b3b6c
Moriel Schottlender <moriel@gmail.com>
2015-05-05 17:45:37 -0700

    [wip] Create a basic data model layer for Flow JS
    Create a dm for Flow; start with a flow Board and Topic models with
    basic fetching from the API.
    
    Bug: T98368
    Change-Id: Iba245037078f548edd2dc10519e8f2c0c650cb00

--- Resources.php
+++ Resources.php
@@ -354,6 +354,22 @@ $wgResourceModules += array(
 			'mediawiki.Uri',
 		),
 	) + $mobile,
+	'ext.flow.dm' => $flowResourceTemplate + array(
+		'scripts' => array( // Component order is important
+			'flow/mw.flow.js',
+			'flow/dm/mw.flow.dm.js',
+			'flow/dm/mw.flow.dm.Item.js',
+			'flow/dm/mixins/mw.flow.dm.List.js',
+			'flow/dm/mw.flow.dm.RevisionedContent.js',
+			'flow/dm/mw.flow.dm.BoardDescription.js',
+			'flow/dm/mw.flow.dm.System.js',
+			'flow/dm/mw.flow.dm.Topic.js',
+			'flow/dm/mw.flow.dm.Board.js',
+		),
+		'dependencies' => array(
+			'oojs'
+		)
+	) + $mobile,
 	'ext.flow' => $flowResourceTemplate + array(
 		'scripts' => array( // Component order is important
 			// MW UI
@@ -392,6 +408,7 @@ $wgResourceModules += array(
 			'jquery.throttle-debounce',
 			'mediawiki.jqueryMsg',
 			'ext.flow.jquery.conditionalScroll',
+			'ext.flow.dm',
 			'mediawiki.api',
 			'mediawiki.util',
 			'mediawiki.api.options', // required by switch-editor feature
