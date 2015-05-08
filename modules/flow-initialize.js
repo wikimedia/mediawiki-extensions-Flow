26e9414179a9337c848d2aafe7fb2eb3da176c31
Moriel Schottlender <moriel@gmail.com>
2015-05-07 17:28:22 -0700

    Create an OOUI ToC widget
    Replace the current ToC widget with an OOUI widget that is based
    on the new Flow Data Model.
    
    Bug: T96890
    Bug: T86844
    Change-Id: Ic7162face0265b0e080fce14a5e6b6b10b868de6

--- modules/flow-initialize.js
+++ modules/flow-initialize.js
@@ -9,7 +9,7 @@
 	 * @todo not like this
 	 */
 	$( document ).ready( function () {
-		var flowBoard, dmBoard,
+		var navWidget, flowBoard, dmBoard,
 			$component = $( '.flow-component' );
 
 		// HACK: If there is no component, we are not on a flow
@@ -63,6 +63,57 @@
 		// Get the current default sort
 		flowBoard.topicIdSort = mw.flow.system.getBoard().getSortOrder();
 
+		/* UI Widgets */
+		navWidget = new mw.flow.ui.NavigationWidget( dmBoard, {
+			defaultSort: flowBoard.topicIdSort
+		} );
+		$( '.flow-board-navigation' ).append( navWidget.$element );
+
+		// HACK: These event handlers should be in the prospective widgets
+		// they will move once we have Board UI and Topic UI widgets
+		mw.flow.system.on( 'resetBoardStart', function () {
+			$component.addClass( 'flow-api-inprogress' );
+			// Before we reinitialize the board we have to detach
+			// the navigation widget. This should not be necessary when
+			// the board and topics are OOUI widgets
+			navWidget.$element.detach();
+		} );
+
+		mw.flow.system.on( 'resetBoardEnd', function ( data ) {
+			var $rendered;
+
+			// HACK: Trim the data for only the rendered topics
+			// We can't have the API request use a limit because when we
+			// rebuild the board (when we reorder) we want the full 50
+			// topics for the ToC widget. When the topic- and board-widget
+			// are operational, we should render properly without using this
+			// hack.
+			data.topiclist.roots = data.topiclist.roots.splice( 0, mw.flow.system.getRenderedTopics() );
+
+			$rendered = $(
+				mw.flow.TemplateEngine.processTemplateGetFragment(
+					'flow_block_loop',
+					{ blocks: data }
+				)
+			).children();
+			// Run this on a short timeout so that the other board handler in FlowBoardComponentLoadMoreFeatureMixin can run
+			// TODO: Using a timeout doesn't seem like the right way to do this.
+			setTimeout( function () {
+				// Reinitialize the whole board with these nodes
+				// flowBoard.reinitializeContainer( $rendered );
+				$( '.flow-board' ).empty().append( $rendered[ 1 ] );
+
+				// Since we've replaced the entire board, we need to reinitialize
+				// it. This also takes away the original navWidget, so we need to
+				// make sure it's reinitialized too
+				flowBoard.reinitializeContainer( $rendered );
+				$( '.flow-board-navigation' ).append( navWidget.$element );
+
+				$component.removeClass( 'flow-api-inprogress' );
+			}, 50 );
+
+		} );
+
 		// HACK: On load more, populate the board dm
 		flowBoard.on( 'loadmore', function ( topiclist ) {
 			// Add to the DM board
@@ -84,6 +135,16 @@
 				revision = data.revisions[ revisionId ];
 				topic.populate( revision );
 			}
+		} );
+
+		// Load a topic from the ToC that isn't rendered on
+		// the page yet. This will be gone once board, topic
+		// and post are widgetized.
+		navWidget.on( 'loadTopic', function ( topicId ) {
+			flowBoard.jumpToTopic( topicId );
+		} );
+		navWidget.on( 'reorderTopics', function ( newOrder ) {
+			flowBoard.topicIdSort = newOrder;
 		} );
 
 		if ( mw.flow.data && mw.flow.data.blocks.topiclist && mw.flow.data.blocks.header ) {
