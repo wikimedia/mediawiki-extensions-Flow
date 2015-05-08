5a28945a29e67ba06f837cd5945e0940491844e7
Moriel Schottlender <moriel@gmail.com>
2015-05-07 17:28:22 -0700

    Create an OOUI ToC widget
    Replace the current ToC widget with an OOUI widget that is based
    on the new Flow Data Model.
    
    Bug: T96890
    Bug: T86844
    Change-Id: Ic7162face0265b0e080fce14a5e6b6b10b868de6

--- modules/engine/components/board/base/flow-board-api-events.js
+++ modules/engine/components/board/base/flow-board-api-events.js
@@ -856,6 +856,10 @@
 			$target.replaceWith( $replacement );
 			// Run loadHandlers
 			flowBoard.emitWithReturn( 'makeContentInteractive', $replacement );
+
+			// HACK: Emit an event here so that the flow data model can update
+			// itself based on the API response
+			flowBoard.emit( 'refreshTopic', workflowId, result );
 		} ).fail( function ( code, result ) {
 			var errorMsg = flowBoard.constructor.static.getApiErrorMessage( code, result );
 			errorMsg = mw.msg( 'flow-error-fetch-after-open-lock', errorMsg );
