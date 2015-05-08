eedb5d88e43b4cef417a03a73ba57d3752baa873
Moriel Schottlender <moriel@gmail.com>
2015-05-07 17:28:22 -0700

    [wip] Create an OOUI ToC widget
    Change-Id: Ic7162face0265b0e080fce14a5e6b6b10b868de6

--- modules/flow-initialize.js
+++ modules/flow-initialize.js
@@ -9,6 +9,8 @@
 	 * @todo not like this
 	 */
 	$( document ).ready( function () {
+		var navWidget;
+
 		mw.flow.initComponent( $( '.flow-component' ) );
 
 		// Load data model
@@ -25,6 +27,57 @@
 		mw.flow.system.on( 'populate', function ( topicTitlesById ) {
 			var flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) );
 			flowBoard.topicTitlesById = $.extend( {}, flowBoard.topicTitlesById, topicTitlesById );
+		} );
+
+		mw.flow.system.on( 'resetBoardEnd', function ( data ) {
+			var flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) ),
+				$rendered = $(
+				mw.flow.TemplateEngine.processTemplateGetFragment(
+					'flow_block_loop',
+					{ blocks: data }
+				)
+			).children();
+			// Run this on a short timeout so that the other board handler in FlowBoardComponentLoadMoreFeatureMixin can run
+			// TODO: Using a timeout doesn't seem like the right way to do this.
+			setTimeout( function () {
+				// Reinitialize the whole board with these nodes
+				// flowBoard.reinitializeContainer( $rendered );
+				$( '.flow-board' ).empty().append( $rendered[ 1 ] );
+
+				// Since we've replaced the entire boardd, we need to reinitialize
+				// it. This also takes away the original navWidget, so we need to
+				// make sure it's reinitialized too
+				flowBoard.reinitializeContainer( $rendered );
+				navWidget.$element.appendTo( $( '.flow-board-navigation' ) );
+				navWidget.initialize();
+
+				$( '.flow-component' ).removeClass( 'flow-api-inprogress' );
+			}, 50 );
+
+		} );
+
+		/* UI Widets */
+		navWidget = new mw.flow.ui.NavigationWidget( mw.flow.system );
+
+		$( '.flow-board-navigation' ).append( navWidget.$element );
+		navWidget.initialize();
+
+		// Load a topic from the ToC that isn't rendered on
+		// the page yet. This will be gone once board, topic
+		// and post are widgetized.
+		navWidget.on( 'loadTopic', function ( topicId ) {
+			var flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) );
+			flowBoard.jumpToTopic( topicId );
+		} );
+
+		// HACK: These event handlers should be in the prospective widgets
+		// they will move once we have Board UI and Topic UI widgets
+		mw.flow.system.on( 'resetBoardStart', function () {
+			$( '.flow-component' ).addClass( 'flow-api-inprogress' );
+			// Before we reinitialize the board we have to detach
+			// the navigation widget. This should not be necessary when
+			// the board and topics are OOUI widgets
+			navWidget.$element.detach();
 		} );
 	} );
 }( jQuery ) );
