df1e1f918210a995f7a4234c2599aecb9aa47a52
Moriel Schottlender <moriel@gmail.com>
2015-05-07 17:28:22 -0700

    [WIP] Create an OOUI ToC widget
    Replace the current ToC widget with an OOUI widget that is based
    on the new Flow Data Model.
    
    Bug: T96890
    Change-Id: Ic7162face0265b0e080fce14a5e6b6b10b868de6

--- modules/flow-initialize.js
+++ modules/flow-initialize.js
@@ -9,7 +9,8 @@
 	 * @todo not like this
 	 */
 	$( document ).ready( function () {
-		var $component = $( '.flow-component' );
+		var navWidget, flowBoard, dmBoard,
+			$component = $( '.flow-component' );
 
 		// HACK: If there is no component, we are not on a flow
 		// board at all, and there's no need to load anything.
@@ -21,13 +22,138 @@
 		}
 
 		mw.flow.initComponent( $component );
+		flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) );
 
 		// Load data model
 		mw.flow.system = new mw.flow.dm.System( {
 			pageTitle: mw.Title.newFromText( mw.config.get( 'wgPageName' ) ),
 			tocPostsLimit: 50,
 			renderedTopics: $( '.flow-topic' ).length,
-			boardId: $component.data( 'flow-id' )
+			boardId: $component.data( 'flow-id' ),
+			defaultSort: mw.user.options.get( 'flow-topiclist-sortby' )
+		} );
+
+		dmBoard = mw.flow.system.getBoard();
+		dmBoard.on( 'add', function ( newItems ) {
+			var i, len, item, itemId;
+
+			// Items can be unstubbed anywhere, but they should only be added at the
+			// end (due to TOC infinite scroll or initial population).
+			for ( i = 0, len = newItems.length; i < len; i++ ) {
+				item = newItems[i];
+				itemId = item.getId();
+
+				flowBoard.orderedTopicIds.push( itemId );
+				flowBoard.topicTitlesById[ itemId ] = item.getContent();
+				flowBoard.updateTimestampsByTopicId[ itemId ] = item.getLastUpdate();
+			}
+			flowBoard.sortTopicIds( flowBoard );
+		} );
+
+		// E.g. on topic re-order, before re-population.
+		dmBoard.on( 'clear', function () {
+			flowBoard.orderedTopicIds = [];
+			flowBoard.topicTitlesById = {};
+		} );
+
+		// We shouldn't have to worry about 'remove', since by the time we have filtering,
+		// orderedTopicIds should be gone.
+
+		// HACK: We need to populate the old code when the
+		// new is populated
+		mw.flow.system.on( 'populate', function ( topicInfo ) {
+			$.extend( flowBoard, topicInfo );
+		} );
+
+		// HACK: These event handlers should be in the prospective widgets
+		// they will move once we have Board UI and Topic UI widgets
+		mw.flow.system.on( 'resetBoardStart', function () {
+			$component.addClass( 'flow-api-inprogress' );
+			// Before we reinitialize the board we have to detach
+			// the navigation widget. This should not be necessary when
+			// the board and topics are OOUI widgets
+			navWidget.$element.detach();
+		} );
+
+		mw.flow.system.on( 'resetBoardEnd', function ( data ) {
+			var $rendered;
+
+			// HACK: Trim the data for only the rendered topics
+			// We can't have the API request use a limit because when we
+			// rebuild the board (when we reorder) we want the full 50
+			// topics for the ToC widget. When the topic- and board-widget
+			// are operational, we should render properly without using this
+			// hack.
+			data.topiclist.roots = data.topiclist.roots.splice( 0, mw.flow.system.getRenderedTopics() );
+
+			$rendered = $(
+				mw.flow.TemplateEngine.processTemplateGetFragment(
+					'flow_block_loop',
+					{ blocks: data }
+				)
+			).children();
+			// Run this on a short timeout so that the other board handler in FlowBoardComponentLoadMoreFeatureMixin can run
+			// TODO: Using a timeout doesn't seem like the right way to do this.
+			setTimeout( function () {
+				// Reinitialize the whole board with these nodes
+				// flowBoard.reinitializeContainer( $rendered );
+				$( '.flow-board' ).empty().append( $rendered[ 1 ] );
+
+				// Since we've replaced the entire board, we need to reinitialize
+				// it. This also takes away the original navWidget, so we need to
+				// make sure it's reinitialized too
+				flowBoard.reinitializeContainer( $rendered );
+				$( '.flow-board-navigation' ).append( navWidget.$element );
+				navWidget.initialize();
+
+				$component.removeClass( 'flow-api-inprogress' );
+			}, 50 );
+
+		} );
+
+		// Initialize the old system to accept the default
+		// 'newest' order for the topic order widget
+		// Get the current default sort
+		flowBoard.topicIdSort = mw.flow.system.getBoard().getSortOrder();
+
+		/* UI Widgets */
+		navWidget = new mw.flow.ui.NavigationWidget( dmBoard, {
+			defaultSort: flowBoard.topicIdSort
+		} );
+		$( '.flow-board-navigation' ).append( navWidget.$element );
+		navWidget.initialize();
+
+		// HACK: On load more, populate the board dm
+		flowBoard.on( 'loadmore', function ( topiclist ) {
+			// Add to the DM board
+			mw.flow.system.populateBoardTopicsFromJson( topiclist );
+		} );
+
+		// HACK: Update the DM when topic is refreshed
+		flowBoard.on( 'refreshTopic', function ( workflowId, topicData ) {
+			var revisionId, revision,
+				topic = dmBoard.getItemById( workflowId ),
+				data = topicData.flow['view-topic'].result.topic;
+
+			if ( !topic ) {
+				// New topic
+				mw.flow.system.populateBoardTopicsFromJson( data, 0 );
+			} else {
+				// Topic already exists. Repopulate
+				revisionId = data.posts[ workflowId ];
+				revision = data.revisions[ revisionId ];
+				topic.populate( revision );
+			}
+		} );
+
+		// Load a topic from the ToC that isn't rendered on
+		// the page yet. This will be gone once board, topic
+		// and post are widgetized.
+		navWidget.on( 'loadTopic', function ( topicId ) {
+			flowBoard.jumpToTopic( topicId );
+		} );
+		navWidget.on( 'reorderTopics', function ( newOrder ) {
+			flowBoard.topicIdSort = newOrder;
 		} );
 
 		if ( mw.flow.data && mw.flow.data.blocks.topiclist && mw.flow.data.blocks.header ) {
@@ -41,11 +167,5 @@
 			mw.flow.system.populateBoardFromApi();
 		}
 
-		// HACK: We need to populate the old code when the
-		// new is populated
-		mw.flow.system.on( 'populate', function ( topicTitlesById ) {
-			var flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) );
-			$.extend( flowBoard.topicTitlesById, topicTitlesById );
-		} );
 	} );
 }( jQuery ) );
