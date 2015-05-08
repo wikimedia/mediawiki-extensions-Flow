a31b7a3891e77e8d40c3181d21e3054e514b4f84
Moriel Schottlender <moriel@gmail.com>
2015-05-07 17:28:22 -0700

    [wip] Create an OOUI ToC widget
    Change-Id: Ic7162face0265b0e080fce14a5e6b6b10b868de6

---
+++ modules/flow/dm/api/mw.flow.dm.APIHandler.js
@@ -0,0 +1,66 @@
+/**
+ * Resource Provider object.
+ *
+ * @class
+ * @mixins OO.EventEmitter
+ *
+ * @constructor
+ * @param {Object} [config] Configuration options
+ * @cfg {Object} [apiConstructorParams] Parameters for mw.Api()
+ * @cfg {Object} [requestParams] Parameters for the request
+ */
+mw.flow.dm.APIHandler = function FlowDmAPIHandler( page, config ) {
+	config = config || {};
+
+	// Mixin constructors
+	OO.EventEmitter.call( this );
+
+	this.apiConstructorParams = $.extend( {
+		ajax: {
+			timeout: 5 * 1000, // 5 seconds
+			cache: false
+		}
+	}, config.apiConstructorParams );
+
+	this.page = page;
+
+	this.requestParams = $.extend( {
+		action: 'flow'
+	}, config.requestParams );
+};
+
+/**
+ * General get request
+ * @param {[type]} requestParams [description]
+ * @return {[type]} [description]
+ */
+mw.flow.dm.APIHandler.prototype.get = function ( submodule, requestParams ) {
+	var params = $.extend( this.requestParams, requestParams );
+
+	return ( new mw.Api() ).get( $.extend( { submodule: submodule }, params ) )
+		.then( function ( data ) {
+			return data.flow[ submodule ].result;
+		} );
+};
+
+/**
+ * Send a request to get topic list
+ *
+ * @param {[type]} page [description]
+ * @param {[type]} offsetId [description]
+ * @return {[type]} [description]
+ */
+mw.flow.dm.APIHandler.prototype.getTopicList = function ( offsetId ) {
+	var params = {
+		page: this.page
+	};
+
+	if ( offsetId ) {
+		params.vtloffsetId = offsetId;
+	}
+
+	return this.get(  'view-topiclist', params )
+		.then( function ( data ) {
+			return data.topiclist;
+		} );
+};
