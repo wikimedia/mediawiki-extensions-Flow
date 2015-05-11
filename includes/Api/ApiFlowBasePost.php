e357ed1add7a9c3a9c420fd96b3cc050c739b4a1
Matthias Mullie <git@mullie.eu>
2015-05-11 17:02:41 +0200

    Remove deprecated API params
    'contentFormat' has been replaced by 'format' entirely.
    'metadataonly' has just been removed - we now have read APIs for that data.
    
    Change-Id: Iadc41fa1aa5a0ce23d8316e8430044804927bd31

--- includes/Api/ApiFlowBasePost.php
+++ includes/Api/ApiFlowBasePost.php
@@ -52,27 +52,6 @@ abstract class ApiFlowBasePost extends ApiFlowBase {
 			'committed' => $commitMetadata,
 		) );
 
-		// We used to provide render data along with these POST APIs because we
-		// needed them to render JS. Now we have view-* API's and JS is using
-		// them - we don't need this hack anymore.
-		// We'll let this live on for a little while and warn users who were not
-		// already requesting only metadata that this is soon changing.
-		if ( $this->getParameter( 'metadataonly' ) !== true ) {
-			$output[$action]['result'] = array();
-			foreach( $blocksToCommit as $block ) {
-				// Always return parsed text to client after successful submission?
-				$output[$action]['result'][$block->getName()] = $block->renderApi( $params[$block->getName()] );
-			}
-
-			$this->setWarning(
-				'The API will soon stop providing detailed render data in ' .
-				'flow.[action].result. The metadataonly option will be ' .
-				'removed, and the API will always only provide metadata. ' .
-				'It will behave as it currently does when metadataonly is true. ' .
-				'Start getting render data from view-* API submodules.'
-			);
-		}
-
 		// required until php5.4 which has the JsonSerializable interface
 		array_walk_recursive( $output, function( &$value ) {
 			if ( $value instanceof Anchor ) {
@@ -85,19 +64,6 @@ abstract class ApiFlowBasePost extends ApiFlowBase {
 		} );
 
 		$this->getResult()->addValue( null, $this->apiFlow->getModuleName(), $output );
-	}
-
-	public function getAllowedParams() {
-		return array(
-			'metadataonly' => array(
-				// going to deprecate this (it's becoming default behavior) and
-				// I want to warn people who DON'T set this param, so I don't
-				// want it to default to anything (so I can check for null)
-				ApiBase::PARAM_TYPE => 'boolean',
-				ApiBase::PARAM_DFLT => false,
-				ApiBase::PARAM_REQUIRED => false,
-			),
-		);
 	}
 
 	/**
