39f66f7578599077eeca4e2fb1310d3078be0820
Matthias Mullie <git@mullie.eu>
2015-05-14 15:17:40 +0200

    Don't log exception when missing permissions, just ignore it
    formatApi() fails on a couple of occasions, one of which being when
    a user has insufficient permissions. That's an "acceptable" error:
    we shouldn't log it, just ignore that row.
    Some places already counter this by first checking permissions, then
    passing it off to formatApi() - if that one fails, it's not a
    permission issue & we should log the failure.
    Let's just throw an exception right away if it's a real error case,
    and return false if it's a "this is no error but we can't show you
    the data" case. Makes dealing with this simpler.
    
    One potential regression: this formatApi function is not just called
    from RC, Contribs, ... formatters, but also in other places
    (everywhere...), so adding a new exception in there might break
    those other places.
    The first check was already being logged if it was being hit, and I
    couldn't find it in the logs; so pretty sure we won't see it happen.
    
    Change-Id: Ibc45d1a81e023cd0942374fdc5e4fc6a978f936f

--- includes/Formatter/FeedItemFormatter.php
+++ includes/Formatter/FeedItemFormatter.php
@@ -17,29 +17,13 @@ class FeedItemFormatter extends AbstractFormatter {
 	 * @param FormatterRow $row With properties workflow, revision, previous_revision
 	 * @param IContextSource $ctx
 	 * @return FeedItem|false The requested format, or false on failure
+	 * @throws FlowException
 	 */
 	public function format( FormatterRow $row, IContextSource $ctx ) {
-		try {
-			if ( !$this->permissions->isAllowed( $row->revision, 'history' ) ) {
-				return false;
-			}
-			if ( $row->revision instanceof PostRevision &&
-				!$this->permissions->isAllowed( $row->rootPost, 'history' ) ) {
-				return false;
-			}
-
-			return $this->createFeedItem( $row, $ctx );
-		} catch ( Exception $e ) {
-			\MWExceptionHandler::logException( $e );
-			return false;
-		}
-	}
-
-	protected function createFeedItem( FormatterRow $row, IContextSource $ctx ) {
 		$this->serializer->setIncludeHistoryProperties( true );
-		$data = $this->serializer->formatApi( $row, $ctx );
+		$data = $this->serializer->formatApi( $row, $ctx, 'contributions' );
 		if ( !$data ) {
-			throw new FlowException( 'Could not format data for row ' . $row->revision->getRevisionId()->getAlphadecimal() );
+			return false;
 		}
 
 		$preferredLinks = array(
