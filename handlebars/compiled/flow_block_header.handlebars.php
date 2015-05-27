e36eb31344565c48ac0ab9204bfab68eebfb6b88
Matthias Mullie <git@mullie.eu>
2015-05-27 16:17:26 +0200

    Use default LightnCandy handlebars directive
    We no longer have to specifically list all-but-one handlebars directives,
    the proper fix for that one directive has already landed into the
    LightnCandy version we're using.
    
    Change-Id: I9246d2c49ac27ff673dde26618276f806b421bbf

--- handlebars/compiled/flow_block_header.handlebars.php
+++ handlebars/compiled/flow_block_header.handlebars.php
@@ -17,40 +17,40 @@
 ),
         'blockhelpers' => array(),
         'hbhelpers' => array(),
-        'partials' => array('flow_header_title' => function ($cx, $in) {return '<h2 class="flow-board-header-title">
-	<span class="wikiglyph wikiglyph-speech-bubbles"></span>
-	'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq').'
-</h2>
-';},'flow_errors' => function ($cx, $in) {return '<div class="flow-error-container">
-'.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
-		<ul>
-'.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in) {return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
-';}).'		</ul>
-	</div>
-' : '').'</div>
-';},'flow_header_detail' => function ($cx, $in) {return '<div class="flow-board-header-detail-view">
-	<div class="flow-board-header-nav">
-'.((LCRun3::ifvar($cx, ((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions'])) ? $in['revision']['actions']['edit'] : null))) ? '			<a href="'.htmlentities((string)((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'"
-			   data-flow-api-handler="activateEditHeader"
-			   data-flow-api-target="< .flow-board-header"
-			   data-flow-interactive-handler="apiRequest"
-			   class="mw-ui-button mw-ui-progressive  mw-ui-quiet flow-ui-tooltip-target"
-			   title="'.htmlentities((string)((isset($in['revision']['actions']['edit']['title']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['title'] : null), ENT_QUOTES, 'UTF-8').'">
-					<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-edit flow-board-header-icon"></span>'.LCRun3::ch($cx, 'l10n', array(array('flow-edit-header-link'),array()), 'encq').'
-			</a>
-' : '').'	</div>
-'.((LCRun3::ifvar($cx, ((isset($in['revision']['content']) && is_array($in['revision'])) ? $in['revision']['content'] : null))) ? '		<div class="flow-board-header-content">
-			'.LCRun3::ch($cx, 'escapeContent', array(array(((isset($in['revision']['content']['format']) && is_array($in['revision']['content'])) ? $in['revision']['content']['format'] : null),((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null)),array()), 'encq').'
-		</div>
-' : '').'</div>
-<a href="javascript:void(0);"
-	class="mw-ui-button mw-ui-quiet side-rail-toggle-button"
-	data-flow-interactive-handler="toggleSideRail">
-	<span class="wikiglyph wikiglyph-x pull-right collapse-button"
-		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-collapse-description'),array()), 'encq').'"></span>
-	<span class="wikiglyph wikiglyph-speech-bubbles pull-right expand-button"
-		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-expand-description'),array()), 'encq').'"></span>
-</a>
+        'partials' => array('flow_header_title' => function ($cx, $in, $sp) {return ''.$sp.'<h2 class="flow-board-header-title">
+'.$sp.'	<span class="wikiglyph wikiglyph-speech-bubbles"></span>
+'.$sp.'	'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq').'
+'.$sp.'</h2>
+';},'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
+'.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
+'.$sp.'		<ul>
+'.$sp.''.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in)use($sp){return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
+'.$sp.'';}).'		</ul>
+'.$sp.'	</div>
+'.$sp.'' : '').'</div>
+';},'flow_header_detail' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board-header-detail-view">
+'.$sp.'	<div class="flow-board-header-nav">
+'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions'])) ? $in['revision']['actions']['edit'] : null))) ? '			<a href="'.htmlentities((string)((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'"
+'.$sp.'			   data-flow-api-handler="activateEditHeader"
+'.$sp.'			   data-flow-api-target="< .flow-board-header"
+'.$sp.'			   data-flow-interactive-handler="apiRequest"
+'.$sp.'			   class="mw-ui-button mw-ui-progressive  mw-ui-quiet flow-ui-tooltip-target"
+'.$sp.'			   title="'.htmlentities((string)((isset($in['revision']['actions']['edit']['title']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['title'] : null), ENT_QUOTES, 'UTF-8').'">
+'.$sp.'					<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-edit flow-board-header-icon"></span>'.LCRun3::ch($cx, 'l10n', array(array('flow-edit-header-link'),array()), 'encq').'
+'.$sp.'			</a>
+'.$sp.'' : '').'	</div>
+'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['revision']['content']) && is_array($in['revision'])) ? $in['revision']['content'] : null))) ? '		<div class="flow-board-header-content">
+'.$sp.'			'.LCRun3::ch($cx, 'escapeContent', array(array(((isset($in['revision']['content']['format']) && is_array($in['revision']['content'])) ? $in['revision']['content']['format'] : null),((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null)),array()), 'encq').'
+'.$sp.'		</div>
+'.$sp.'' : '').'</div>
+'.$sp.'<a href="javascript:void(0);"
+'.$sp.'	class="mw-ui-button mw-ui-quiet side-rail-toggle-button"
+'.$sp.'	data-flow-interactive-handler="toggleSideRail">
+'.$sp.'	<span class="wikiglyph wikiglyph-x pull-right collapse-button"
+'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-collapse-description'),array()), 'encq').'"></span>
+'.$sp.'	<span class="wikiglyph wikiglyph-speech-bubbles pull-right expand-button"
+'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-expand-description'),array()), 'encq').'"></span>
+'.$sp.'</a>
 ';},),
         'scopes' => array(),
         'sp_vars' => array('root' => $in),
@@ -59,7 +59,7 @@
     );
     
     return '<div class="flow-board-header">
-'.LCRun3::p($cx, 'flow_header_title', array(array($in),array())).''.LCRun3::p($cx, 'flow_errors', array(array($in),array())).''.LCRun3::p($cx, 'flow_header_detail', array(array($in),array())).'</div>
+'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_detail', array(array($in),array()), '	').'</div>
 ';
 }
 ?>
--- handlebars/compiled/flow_block_header_edit.handlebars.php
+++ handlebars/compiled/flow_block_header_edit.handlebars.php
@@ -17,23 +17,23 @@
 ),
         'blockhelpers' => array(),
         'hbhelpers' => array(),
-        'partials' => array('flow_header_title' => function ($cx, $in) {return '<h2 class="flow-board-header-title">
-	<span class="wikiglyph wikiglyph-speech-bubbles"></span>
-	'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq').'
-</h2>
-';},'flow_errors' => function ($cx, $in) {return '<div class="flow-error-container">
-'.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
-		<ul>
-'.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in) {return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
-';}).'		</ul>
-	</div>
-' : '').'</div>
-';},'flow_form_buttons' => function ($cx, $in) {return '<button data-flow-interactive-handler="cancelForm"
-        data-role="cancel"
-        type="reset"
-        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right flow-js"
-
->'.LCRun3::ch($cx, 'l10n', array(array('flow-cancel'),array()), 'encq').'</button>
+        'partials' => array('flow_header_title' => function ($cx, $in, $sp) {return ''.$sp.'<h2 class="flow-board-header-title">
+'.$sp.'	<span class="wikiglyph wikiglyph-speech-bubbles"></span>
+'.$sp.'	'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq').'
+'.$sp.'</h2>
+';},'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
+'.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
+'.$sp.'		<ul>
+'.$sp.''.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in)use($sp){return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
+'.$sp.'';}).'		</ul>
+'.$sp.'	</div>
+'.$sp.'' : '').'</div>
+';},'flow_form_buttons' => function ($cx, $in, $sp) {return ''.$sp.'<button data-flow-interactive-handler="cancelForm"
+'.$sp.'        data-role="cancel"
+'.$sp.'        type="reset"
+'.$sp.'        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right flow-js"
+'.$sp.'
+'.$sp.'>'.LCRun3::ch($cx, 'l10n', array(array('flow-cancel'),array()), 'encq').'</button>
 ';},),
         'scopes' => array(),
         'sp_vars' => array('root' => $in),
@@ -42,9 +42,9 @@
     );
     
     return '<div class="flow-board-header">
-'.LCRun3::p($cx, 'flow_header_title', array(array($in),array())).'	<div class="flow-board-header-edit-view">
+'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').'	<div class="flow-board-header-edit-view">
 		<form method="POST" action="'.htmlentities((string)((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'" flow-api-action="edit-header">
-'.LCRun3::p($cx, 'flow_errors', array(array($in),array())).'			<input type="hidden" name="wpEditToken" value="'.htmlentities((string)((isset($cx['sp_vars']['root']['editToken']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['editToken'] : null), ENT_QUOTES, 'UTF-8').'" />
+'.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '			').'			<input type="hidden" name="wpEditToken" value="'.htmlentities((string)((isset($cx['sp_vars']['root']['editToken']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['editToken'] : null), ENT_QUOTES, 'UTF-8').'" />
 '.((LCRun3::ifvar($cx, ((isset($in['revision']['revisionId']) && is_array($in['revision'])) ? $in['revision']['revisionId'] : null))) ? '				<input type="hidden" name="header_prev_revision" value="'.htmlentities((string)((isset($in['revision']['revisionId']) && is_array($in['revision'])) ? $in['revision']['revisionId'] : null), ENT_QUOTES, 'UTF-8').'" />
 ' : '').'
 			<div class="flow-editor">
@@ -60,7 +60,7 @@
 					class="mw-ui-button mw-ui-constructive"
 					data-flow-interactive-handler="apiRequest"
 					data-flow-api-handler="submitHeader">'.LCRun3::ch($cx, 'l10n', array(array('flow-edit-header-submit'),array()), 'encq').'</button>
-'.LCRun3::p($cx, 'flow_form_buttons', array(array($in),array())).'				<small class="flow-terms-of-use plainlinks">'.LCRun3::ch($cx, 'l10nParse', array(array('flow-terms-of-use-edit'),array()), 'encq').'</small>
+'.LCRun3::p($cx, 'flow_form_buttons', array(array($in),array()), '				').'				<small class="flow-terms-of-use plainlinks">'.LCRun3::ch($cx, 'l10nParse', array(array('flow-terms-of-use-edit'),array()), 'encq').'</small>
 			</div>
 		</form>
 	</div>
