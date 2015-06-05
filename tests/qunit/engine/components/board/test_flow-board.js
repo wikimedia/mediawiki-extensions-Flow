ae7f9b97e956a005dfc93772be4d8f62a73ea318
Matthew Flaschen <mflaschen@wikimedia.org>
2015-06-04 21:29:53 -0400

    Remove no-op test
    If a test fails to run at all in the forest, is anyone around to
    consider that a failure?
    
    Change-Id: Ifcdc88bab5fd5cd24fe545a0086e198e53b384dc

--- tests/qunit/engine/components/board/test_flow-board.js
+++ tests/qunit/engine/components/board/test_flow-board.js
@@ -1,9 +1,4 @@
 ( function ( $ ) {
-QUnit.module( 'ext.flow: Flow board' );
-
-QUnit.test( 'Check Flow is running', 1, function () {
-	strictEqual( 1, 1, 'Test to see if Flow has a qunit test.' );
-} );
 
 QUnit.module( 'ext.flow: FlowBoardComponent', {
 	setup: function () {
