e121b4b6aeee9671235602ef043e28bf4199c625
Stephane Bisson <sbisson@wikimedia.org>
2015-06-05 11:38:29 -0400

    RC patrolling for Flow
    Patrolling link on
      * board description revision page
      * topic and post revision page
      * board description diff page
      * topic summary diff page
      * topic and post diff page
    
    Uses core's JS and API
    
    Bug: T94452
    Change-Id: I04dcba1248a60f67385f736934207c90048b8ee3

--- handlebars/compiled/flow_revision_diff_header.handlebars.php
+++ handlebars/compiled/flow_revision_diff_header.handlebars.php
@@ -13,10 +13,21 @@
         'constants' => array(),
         'helpers' => array(            'l10n' => 'Flow\TemplateHelper::l10n',
             'l10nParse' => 'Flow\TemplateHelper::l10nParse',
+            'enablePatrollingLink' => 'Flow\TemplateHelper::enablePatrollingLink',
 ),
         'blockhelpers' => array(),
         'hbhelpers' => array(),
-        'partials' => array(),
+        'partials' => array('flow_patrol_diff' => function ($cx, $in, $sp) {return ''.$sp.''.((LCRun3::ifvar($cx, ((isset($in['patrol']) && is_array($in)) ? $in['patrol'] : null))) ? '<div>
+'.$sp.'        <span class="patrollink">
+'.$sp.'            <a class="mw-ui-quiet"
+'.$sp.'               href="'.htmlentities((string)((isset($in['patrol']['url']) && is_array($in['patrol'])) ? $in['patrol']['url'] : null), ENT_QUOTES, 'UTF-8').'"
+'.$sp.'               title="'.LCRun3::ch($cx, 'l10n', array(array('flow-mark-diff-patrolled-link-title'),array()), 'encq').'"
+'.$sp.'               data-role="patrol">
+'.$sp.'                ['.LCRun3::ch($cx, 'l10n', array(array('flow-mark-diff-patrolled-link-text'),array()), 'encq').']
+'.$sp.'            </a>
+'.$sp.'        </span>
+'.$sp.'    </div>
+'.$sp.'    '.LCRun3::ch($cx, 'enablePatrollingLink', array(array(),array()), 'encq').'' : '').'';},),
         'scopes' => array(),
         'sp_vars' => array('root' => $in),
         'lcrun' => 'LCRun3',
@@ -28,6 +39,6 @@
 </a></div>
 '.((LCRun3::ifvar($cx, ((isset($in['previous']) && is_array($in)) ? $in['previous'] : null))) ? '	<div><a href="'.htmlentities((string)((isset($in['previous']) && is_array($in)) ? $in['previous'] : null), ENT_QUOTES, 'UTF-8').'">'.LCRun3::ch($cx, 'l10n', array(array('flow-previous-diff'),array()), 'encq').'</a></div>
 ' : '').''.((LCRun3::ifvar($cx, ((isset($in['next']) && is_array($in)) ? $in['next'] : null))) ? '	<div><a href="'.htmlentities((string)((isset($in['next']) && is_array($in)) ? $in['next'] : null), ENT_QUOTES, 'UTF-8').'">'.LCRun3::ch($cx, 'l10n', array(array('flow-next-diff'),array()), 'encq').'</a></div>
-' : '').'';
+' : '').''.LCRun3::p($cx, 'flow_patrol_diff', array(array($in),array())).'';
 }
 ?>
