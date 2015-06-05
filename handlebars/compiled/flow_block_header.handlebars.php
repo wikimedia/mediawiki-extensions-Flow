a5bb0c010bd467e18c8e70810f0286edc0b4d1cd
Moriel Schottlender <moriel@gmail.com>
2015-06-05 10:55:44 +0300

    [WIP] Output the board description as an ooui widget
    Output the new board description through handlebars as an ooui
    widget using a new template helper 'oouify'
    
    Bug:T101278
    Change-Id: Idedc61a36b59864f0a751c99f3d6101b59b6baba

--- handlebars/compiled/flow_block_header.handlebars.php
+++ handlebars/compiled/flow_block_header.handlebars.php
@@ -14,6 +14,7 @@
         'helpers' => array(            'l10n' => 'Flow\TemplateHelper::l10n',
             'html' => 'Flow\TemplateHelper::htmlHelper',
             'escapeContent' => 'Flow\TemplateHelper::escapeContent',
+            'oouify' => 'Flow\TemplateHelper::oouify',
 ),
         'blockhelpers' => array(),
         'hbhelpers' => array(),
@@ -28,7 +29,7 @@
 '.$sp.'';}).'		</ul>
 '.$sp.'	</div>
 '.$sp.'' : '').'</div>
-';},'flow_header_detail' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board-header-detail-view">
+';},'flow_header_detail_oldsystem' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board-header-detail-view">
 '.$sp.'	<div class="flow-board-header-nav">
 '.$sp.''.((LCRun3::ifvar($cx, ((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions'])) ? $in['revision']['actions']['edit'] : null))) ? '			<a href="'.htmlentities((string)((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'"
 '.$sp.'			   data-flow-api-handler="activateEditHeader"
@@ -42,7 +43,17 @@
 '.$sp.''.((LCRun3::ifvar($cx, ((isset($in['revision']['content']) && is_array($in['revision'])) ? $in['revision']['content'] : null))) ? '		<div class="flow-board-header-content">
 '.$sp.'			'.LCRun3::ch($cx, 'escapeContent', array(array(((isset($in['revision']['content']['format']) && is_array($in['revision']['content'])) ? $in['revision']['content']['format'] : null),((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null)),array()), 'encq').'
 '.$sp.'		</div>
-'.$sp.'' : '').'</div>
+'.$sp.'' : '').'
+'.$sp.'</div>
+'.$sp.'<a href="javascript:void(0);"
+'.$sp.'	class="mw-ui-button mw-ui-quiet side-rail-toggle-button"
+'.$sp.'	data-flow-interactive-handler="toggleSideRail">
+'.$sp.'	<span class="wikiglyph wikiglyph-x pull-right collapse-button"
+'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-collapse-description'),array()), 'encq').'"></span>
+'.$sp.'	<span class="wikiglyph wikiglyph-speech-bubbles pull-right expand-button"
+'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-expand-description'),array()), 'encq').'"></span>
+'.$sp.'</a>
+';},'flow_header_detail' => function ($cx, $in, $sp) {return ''.$sp.''.LCRun3::ch($cx, 'oouify', array(array(((isset($in['revision']['content']['format']) && is_array($in['revision']['content'])) ? $in['revision']['content']['format'] : null),((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null),((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null),((isset($in['revision']['actions']['edit']['title']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['title'] : null)),array('label'=>((isset($in['flow-edit-header-link']) && is_array($in)) ? $in['flow-edit-header-link'] : null),'type'=>'BoardDescriptionWidget','name'=>'flow-board-description')), 'raw').'
 '.$sp.'<a href="javascript:void(0);"
 '.$sp.'	class="mw-ui-button mw-ui-quiet side-rail-toggle-button"
 '.$sp.'	data-flow-interactive-handler="toggleSideRail">
@@ -59,7 +70,7 @@
     );
     
     return '<div class="flow-board-header flow-load-interactive" data-flow-load-handler="loadSideRail">
-'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_detail', array(array($in),array()), '	').'</div>
+'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '	').''.((LCRun3::ifvar($cx, ((isset($in['oldSystem']) && is_array($in)) ? $in['oldSystem'] : null))) ? ''.LCRun3::p($cx, 'flow_header_detail_oldsystem', array(array($in),array()), '		').'' : ''.LCRun3::p($cx, 'flow_header_detail', array(array($in),array()), '		').'').'</div>
 ';
 }
 ?>
