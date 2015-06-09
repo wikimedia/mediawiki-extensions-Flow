9c3c788234dd078323fc696d321b75fac56f5801
Matthew Flaschen <mflaschen@wikimedia.org>
2015-06-09 13:30:16 -0400

    Implement "Mark as resolved" user workflow
    * Resolved topics are collapsible
    * Resolving and reopening a topic initiates the summarize workflow
    * Resolving and reopening a topic on the history page is direct
    * Resolved topics have new style
    * No-JS experience is the same as it was but it does not accept a moderation reason
    * Lock is now called "Mark as resolved" in the UI
    * Unlock is now called "Reopen" in the UI
    
    Bug: T98929
    Change-Id: I823625339531431e0ad6506511a6ac436de6371f

--- FlowActions.php
+++ FlowActions.php
@@ -127,6 +127,7 @@ $wgFlowActions = array(
 		),
 		'root-permissions' => array(
 			PostRevision::MODERATED_NONE => '',
+			PostRevision::MODERATED_LOCKED => '',
 		),
 		'links' => array( 'topic', 'topic-history', 'watch-topic', 'unwatch-topic', 'summary-revision' ),
 		'actions' => array( 'edit-topic-summary', 'lock-topic', 'restore-topic' ),
@@ -155,6 +156,7 @@ $wgFlowActions = array(
 		),
 		'root-permissions' => array(
 			PostRevision::MODERATED_NONE => '',
+			PostRevision::MODERATED_LOCKED => '',
 		),
 		'links' => array( 'topic', 'topic-history', 'diff-post-summary', 'watch-topic', 'unwatch-topic', 'summary-revision' ),
 		'actions' => array( 'edit-topic-summary', 'lock-topic', 'restore-topic', 'undo-edit-topic-summary' ),
@@ -506,7 +508,7 @@ $wgFlowActions = array(
 			// Only non-moderated topic can be locked
 			PostRevision::MODERATED_NONE => array( 'flow-lock', 'flow-delete', 'flow-suppress' ),
 		),
-		'links' => array( 'topic', 'topic-history', 'watch-topic', 'unwatch-topic' ),
+		'links' => array( 'topic', 'topic-history', 'watch-topic', 'unwatch-topic', 'topic-revision' ),
 		'actions' => array( 'edit-topic-summary', 'restore-topic' ),
 		'history' => array(
 			'i18n-message' => 'flow-rev-message-locked-topic',
@@ -787,6 +789,7 @@ $wgFlowActions = array(
 		),
 		'root-permissions' => array(
 			PostRevision::MODERATED_NONE => '',
+			PostRevision::MODERATED_LOCKED => '',
 		),
 		'links' => array(), // @todo
 		'actions' => array(), // view is not a recorded change type, no actions will be requested
--- handlebars/compiled/flow_block_topicsummary_edit.handlebars.php
+++ handlebars/compiled/flow_block_topicsummary_edit.handlebars.php
@@ -16,7 +16,8 @@
             'l10nParse' => 'Flow\TemplateHelper::l10nParse',
 ),
         'blockhelpers' => array(),
-        'hbhelpers' => array(),
+        'hbhelpers' => array(            'ifCond' => 'Flow\TemplateHelper::ifCond',
+),
         'partials' => array('flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
 '.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
 '.$sp.'		<ul>
@@ -24,12 +25,13 @@
 '.$sp.'';}).'		</ul>
 '.$sp.'	</div>
 '.$sp.'' : '').'</div>
-';},'flow_form_buttons' => function ($cx, $in, $sp) {return ''.$sp.'<button data-flow-interactive-handler="cancelForm"
+';},'flow_form_cancel_button' => function ($cx, $in, $sp) {return ''.$sp.'<button data-flow-interactive-handler="cancelForm"
 '.$sp.'        data-role="cancel"
 '.$sp.'        type="reset"
 '.$sp.'        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right flow-js"
 '.$sp.'
-'.$sp.'>'.LCRun3::ch($cx, 'l10n', array(array('flow-cancel'),array()), 'encq').'</button>
+'.$sp.'>
+'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['msg']) && is_array($in)) ? $in['msg'] : null))) ? ''.LCRun3::ch($cx, 'l10n', array(array(((isset($in['msg']) && is_array($in)) ? $in['msg'] : null)),array()), 'encq').'' : ''.LCRun3::ch($cx, 'l10n', array(array('flow-cancel'),array()), 'encq').'').'</button>
 ';},),
         'scopes' => array(),
         'sp_vars' => array('root' => $in),
@@ -46,7 +48,6 @@
 ' : '').'
 			<div class="flow-editor">
 				<textarea class="mw-ui-input"
-				          required
 				          name="'.htmlentities((string)((isset($in['type']) && is_array($in)) ? $in['type'] : null), ENT_QUOTES, 'UTF-8').'_summary"
 				          type="text"
 				          data-role="content"
@@ -60,9 +61,10 @@
 					data-flow-interactive-handler="apiRequest"
 					data-flow-api-handler="summarizeTopic"
 					data-flow-api-target="< .flow-topic-summary-container">
-						'.LCRun3::ch($cx, 'l10n', array(array('flow-topic-action-summarize-topic'),array()), 'encq').'
+						'.LCRun3::ch($cx, 'l10n', array(array('flow-topic-action-update-topic-summary'),array()), 'encq').'
 				</button>
-'.LCRun3::p($cx, 'flow_form_buttons', array(array($in),array()), '				').'				<small class="flow-terms-of-use plainlinks">'.LCRun3::ch($cx, 'l10nParse', array(array('flow-terms-of-use-summarize'),array()), 'encq').'</small>
+
+'.LCRun3::hbch($cx, 'ifCond', array(array(((isset($in['action']) && is_array($in)) ? $in['action'] : null),'===','summarize'),array()), $in, false, function($cx, $in) {return ''.LCRun3::p($cx, 'flow_form_cancel_button', array(array($in),array()), '					').'';}, function($cx, $in) {return ''.((!LCRun3::ifvar($cx, ((isset($in['submitted']['summary']) && is_array($in['submitted'])) ? $in['submitted']['summary'] : null))) ? ''.((!LCRun3::ifvar($cx, ((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null))) ? ''.LCRun3::p($cx, 'flow_form_cancel_button', array(array($in),array('msg'=>'flow-skip-summary')), '							').'' : '').'' : '').'';}).'				<small class="flow-terms-of-use plainlinks">'.LCRun3::ch($cx, 'l10nParse', array(array('flow-terms-of-use-summarize'),array()), 'encq').'</small>
 			</div>
 		</form>
 	</div>
