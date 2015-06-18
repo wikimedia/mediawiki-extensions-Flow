7ec748fba3dc7d60f5874aa5200c825428010bc5
Moriel Schottlender <moriel@gmail.com>
2015-06-18 10:30:00 -0700

    [WIP] BoardDescriptionWidget.js
    Change-Id: I19feefec8a80e127a5bab3011d67892284f07d23

--- Resources.php
+++ Resources.php
@@ -370,6 +370,16 @@ $wgResourceModules += array(
 			'oojs'
 		)
 	) + $mobile,
+	'ext.flow.ui' => $flowResourceTemplate + array(
+		'scripts' => array(
+			'flow/ui/mw.flow.ui.js',
+			'flow/ui/widgets/mw.flow.ui.BoardDescriptionWidget.js',
+		),
+		'dependencies' => array (
+			'oojs-ui',
+			'ext.flow.dm',
+		)
+	) + $mobile,
 	'ext.flow' => $flowResourceTemplate + array(
 		'scripts' => array( // Component order is important
 			// MW UI
@@ -409,6 +419,7 @@ $wgResourceModules += array(
 			'mediawiki.jqueryMsg',
 			'ext.flow.jquery.conditionalScroll',
 			'ext.flow.dm',
+			'ext.flow.ui',
 			'mediawiki.api',
 			'mediawiki.util',
 			'mediawiki.api.options', // required by switch-editor feature
