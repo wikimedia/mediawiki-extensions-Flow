cc20556368e3aff710ca89ff041dd1078116a2c8
Moriel Schottlender <moriel@gmail.com>
2015-06-18 10:30:00 -0700

    Create an OOUI widget for board description
    Bug: T101278
    Bug: T102125
    Change-Id: I19feefec8a80e127a5bab3011d67892284f07d23

--- Resources.php
+++ Resources.php
@@ -387,6 +387,7 @@ $wgResourceModules += array(
 			'flow/ui/widgets/editor/mw.flow.ui.EditorSwitcherWidget.js',
 			'flow/ui/widgets/editor/mw.flow.ui.EditorControlsWidget.js',
 			'flow/ui/widgets/editor/mw.flow.ui.EditorWidget.js',
+			'flow/ui/widgets/mw.flow.ui.BoardDescriptionWidget.js',
 		),
 		'styles' => array(
 			'styles/flow/mw.flow.ui.less',
@@ -409,7 +410,7 @@ $wgResourceModules += array(
 			'oojs-ui',
 			'es5-shim',
 			'ext.flow.dm',
-		),
+		)
 	) + $mobile,
 	'ext.flow' => $flowResourceTemplate + array(
 		'scripts' => array( // Component order is important
@@ -447,7 +448,6 @@ $wgResourceModules += array(
 			'jquery.throttle-debounce',
 			'mediawiki.jqueryMsg',
 			'ext.flow.jquery.conditionalScroll',
-			'ext.flow.dm',
 			'ext.flow.ui',
 			'mediawiki.api',
 			'mediawiki.util',
