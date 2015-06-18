a794d1dbdd9a1582d95e0c866e847f6db611a20e
Moriel Schottlender <moriel@gmail.com>
2015-06-18 10:30:00 -0700

    [WIP] BoardDescriptionWidget.js
    * Not ready for review *
    
    Change-Id: I19feefec8a80e127a5bab3011d67892284f07d23

---
+++ modules/flow/ui/widgets/mw.flow.ui.BoardDescriptionWidget.js
@@ -0,0 +1,104 @@
+( function ( $ ) {
+	/**
+	 * Flow board description widget
+	 *
+	 * @extends OO.ui.Widget
+	 *
+	 * @constructor
+	 * @param {Object} [config]
+	 *
+	 */
+	mw.flow.ui.BoardDescriptionWidget = function mwFlowUiBoardDescriptionWidget( model, config ) {
+		var $content = $();
+
+		this.flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) );
+
+		config = config || {};
+
+		// Parent constructor
+		mw.flow.ui.BoardDescriptionWidget.super.call( this, config );
+
+		this.attachModel( model );
+
+		// Since the content is already displayed, we will "steal" the already created
+		// node to avoid having to render it twice.
+		// Upon creation of this widget, this should be the rendering of the data
+		// that exists in the model. Take care, however, that if this widget is
+		// used elsewhere, the model and rendering must be synchronized.
+		if ( config.$existing ) {
+			$content = config.$existing;
+		}
+
+		this.$content = $( '<div>' )
+			.addClass( 'flow-ui-boardDescriptionWidget-content' )
+			.append( $content );
+
+		this.button = new OO.ui.ButtonWidget( {
+			label: mw.msg( 'flow-edit-header-link' ),
+			framed: false,
+			icon: 'edit',
+			flags: 'progressive',
+			classes: [ 'flow-ui-boardDescriptionWidget-editButton' ]
+		} );
+
+		this.button.connect( this, { click: 'onEditButtonClick' } );
+		// HACK: For the moment, call for the 'old' system's editor by
+		// mimicking the button behavior
+		// this.button.$element
+		// 	.data( 'flow-api-handler', 'activateEditHeader' )
+		// 	.data( 'flow-api-target', '< .flow-board-header' )
+		// 	.data( 'flow-interactive-handler', 'apiRequest' );
+		// this.button.$element.on( 'click', flowBoard.emitWithReturn.bind( flowBoard, 'interactiveHandlerFocus', [ $( '.flow-board-header' ) ] ) );
+
+		// Initialize
+		this.$element
+			.append(
+				this.button.$element,
+				this.$content
+			)
+			.addClass( 'flow-ui-boardDescriptionWidget' );
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.flow.ui.BoardDescriptionWidget, OO.ui.Widget );
+
+	/* Methods */
+
+	mw.flow.ui.BoardDescriptionWidget.prototype.onEditButtonClick = function () {
+console.log( 'edit' );
+		// Disable the button
+		this.button.setDisabled( true );
+
+	};
+
+	/**
+	 * Respond to content change
+	 *
+	 * @param {string} content Description content
+	 * @param {string} contentFormat Description content format
+	 */
+	mw.flow.ui.BoardDescriptionWidget.prototype.onContentChange = function ( content, contentFormat ) {
+		this.$content.empty().append( $.parseHTML( content ) );
+	};
+
+	/**
+	 * Attach a model to the widget
+	 *
+	 * @param {[type]} model [description]
+	 * @return {[type]} [description]
+	 */
+	mw.flow.ui.BoardDescriptionWidget.prototype.attachModel = function ( model ) {
+		if ( this.model ) {
+			this.model.disconnect( this );
+		}
+
+		this.model = model;
+
+		// Events
+		this.model.connect( this, {
+			contentChange: 'onContentChange'
+		} );
+	};
+
+}( jQuery ) );
