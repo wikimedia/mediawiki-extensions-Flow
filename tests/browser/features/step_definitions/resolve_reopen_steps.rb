dfa769aa79b91185da651cbcde47943630a74423
Stephane Bisson <sbisson@wikimedia.org>
2015-06-19 16:35:38 -0400

    Browser tests for "mark as resolved" and "reopen"
    * Also removed tests for lock/unlock
    
    Bug: T102842
    Change-Id: Ibb376845714ea7e19e4b55fc6b45df4d66172dbe

---
+++ tests/browser/features/step_definitions/resolve_reopen_steps.rb
@@ -0,0 +1,91 @@
+
+When(/^I mark the first topic as resolved$/) do
+  on(FlowPage) do |page|
+    page.topic_actions_link_element.when_present.click
+    page.topic_resolve_button_element.when_present.click
+
+    # dismiss the menu
+    page.edit_summary_element.when_present.click
+    page.edit_summary_element.when_present.focus
+  end
+end
+
+When(/^I reopen the first topic$/) do
+  on(FlowPage) do |page|
+    page.topic_actions_link_element.when_present.click
+    page.topic_reopen_button_element.when_present.click
+
+    # dismiss the menu
+    page.edit_summary_element.when_present.click
+    page.edit_summary_element.when_present.focus
+  end
+end
+
+When(/^I skip the summary$/) do
+  on(FlowPage) do |page|
+    page.skip_summary_button_element.when_present.click
+  end
+end
+
+Then(/^the first topic is resolved$/) do
+  on(FlowPage) do |page|
+    expect(page.first_topic_resolved_mark_element).to exist
+  end
+end
+
+Then(/^the first topic is open$/) do
+  on(FlowPage) do |page|
+    expect(page.first_topic_resolved_mark_element).not_to exist
+  end
+end
+
+When(/^I enter "(.*?)" as summary$/) do |summary_text|
+  on(FlowPage) do |page|
+    page.edit_summary_element.when_present.click
+    page.edit_summary_element.when_present.focus
+    page.edit_summary_element.when_present.clear
+    page.edit_summary_element.when_present.send_keys summary_text
+  end
+end
+
+When(/^I click the update summary button$/) do
+  on(FlowPage) do |page|
+    page.update_summary_button_element.when_present.click
+    page.summary_content_element.when_present
+  end
+end
+
+When(/^I click the summarize menu item$/) do
+  on(FlowPage) do |page|
+    page.topic_actions_link_element.when_present.click
+    page.topic_summarize_button_element.when_present.click
+  end
+end
+
+Then(/^the first topic is summarized as "(.*?)"$/) do |summary_text|
+  on(FlowPage) do |page|
+    expect(page.summary_content_element.when_present.text).to eq(summary_text)
+  end
+end
+
+When(/^I keep the summary$/) do
+  on(FlowPage) do |page|
+    page.update_summary_button_element.when_present.click
+  end
+end
+
+Then(/^the first topic is resolved with summary "(.*?)"$/) do |summary_text|
+  step 'the first topic is resolved'
+  step "the first topic is summarized as \"#{summary_text}\""
+end
+
+Given(/^I summarize the first topic as "(.*?)"$/) do |summary_text|
+  step 'I click the summarize menu item'
+  step "I enter \"#{summary_text}\" as summary"
+  step 'I click the update summary button'
+end
+
+When(/^I summarize as "(.*?)"$/) do |summary_text|
+  step "I enter \"#{summary_text}\" as summary"
+  step 'I click the update summary button'
+end
