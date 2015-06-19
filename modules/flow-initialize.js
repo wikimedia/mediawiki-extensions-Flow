37541151172640458e304f8d35d55ec2800f3fa6
Moriel Schottlender <moriel@gmail.com>
2015-06-19 08:48:51 -0700

    Set up events for the new flow ui
    Set up the events and groundwork for the new oouified flow-ui and
    make the data model update itself based on the current flow ui
    system.
    
    Change-Id: I3644fc106bbe6e3974e20a4288acea947f934686

--- modules/flow-initialize.js
+++ modules/flow-initialize.js
@@ -9,7 +9,8 @@
 	 * @todo not like this
 	 */
 	$( document ).ready( function () {
-		var $component = $( '.flow-component' );
+		var flowBoard, dmBoard,
+			$component = $( '.flow-component' );
 
 		// HACK: If there is no component, we are not on a flow
 		// board at all, and there's no need to load anything.
@@ -21,13 +22,68 @@
 		}
 
 		mw.flow.initComponent( $component );
+		flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) );
 
 		// Load data model
 		mw.flow.system = new mw.flow.dm.System( {
 			pageTitle: mw.Title.newFromText( mw.config.get( 'wgPageName' ) ),
 			tocPostsLimit: 50,
 			renderedTopics: $( '.flow-topic' ).length,
-			boardId: $component.data( 'flow-id' )
+			boardId: $component.data( 'flow-id' ),
+			defaultSort: $( '.flow-board' ).data( 'flow-sortby' )
+		} );
+
+		dmBoard = mw.flow.system.getBoard();
+		dmBoard.on( 'add', function ( newItems ) {
+			var i, len, item, itemId;
+
+			// Items can be unstubbed anywhere, but they should only be added at the
+			// end (due to TOC infinite scroll or initial population).
+			for ( i = 0, len = newItems.length; i < len; i++ ) {
+				item = newItems[i];
+				itemId = item.getId();
+
+				flowBoard.orderedTopicIds.push( itemId );
+				flowBoard.topicTitlesById[ itemId ] = item.getContent();
+				flowBoard.updateTimestampsByTopicId[ itemId ] = item.getLastUpdate();
+			}
+			flowBoard.sortTopicIds( flowBoard );
+		} );
+
+		// E.g. on topic re-order, before re-population.
+		dmBoard.on( 'clear', function () {
+			flowBoard.orderedTopicIds = [];
+			flowBoard.topicTitlesById = {};
+		} );
+		// We shouldn't have to worry about 'remove', since by the time we have filtering,
+		// orderedTopicIds should be gone.
+
+		// Initialize the old system to accept the default
+		// 'newest' order for the topic order widget
+		// Get the current default sort
+		flowBoard.topicIdSort = mw.flow.system.getBoard().getSortOrder();
+
+		// HACK: On load more, populate the board dm
+		flowBoard.on( 'loadmore', function ( topiclist ) {
+			// Add to the DM board
+			mw.flow.system.populateBoardTopicsFromJson( topiclist );
+		} );
+
+		// HACK: Update the DM when topic is refreshed
+		flowBoard.on( 'refreshTopic', function ( workflowId, topicData ) {
+			var revisionId, revision,
+				topic = dmBoard.getItemById( workflowId ),
+				data = topicData.flow['view-topic'].result.topic;
+
+			if ( !topic ) {
+				// New topic
+				mw.flow.system.populateBoardTopicsFromJson( data, 0 );
+			} else {
+				// Topic already exists. Repopulate
+				revisionId = data.posts[ workflowId ];
+				revision = data.revisions[ revisionId ];
+				topic.populate( revision );
+			}
 		} );
 
 		if ( mw.flow.data && mw.flow.data.blocks.topiclist && mw.flow.data.blocks.header ) {
@@ -40,12 +96,5 @@
 		} else {
 			mw.flow.system.populateBoardFromApi();
 		}
-
-		// HACK: We need to populate the old code when the
-		// new is populated
-		mw.flow.system.on( 'populate', function ( topicTitlesById ) {
-			var flowBoard = mw.flow.getPrototypeMethod( 'component', 'getInstanceByElement' )( $( '.flow-board' ) );
-			$.extend( flowBoard.topicTitlesById, topicTitlesById );
-		} );
 	} );
 }( jQuery ) );
