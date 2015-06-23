838bdad74bdb471cd484a9e9c3013f0717c909b7
Stephane Bisson <sbisson@wikimedia.org>
2015-06-23 15:37:50 -0400

    Board description browser tests
    Reworked from
      https://gerrit.wikimedia.org/r/#/c/171224
      https://gerrit.wikimedia.org/r/#/c/171225
    
    Change-Id: I13d38cd909a5f2f6fb80571313e21f12f3a8915f

--- tests/browser/features/support/pages/new_flow_page.rb
+++ tests/browser/features/support/pages/new_flow_page.rb
@@ -2,6 +2,6 @@ require "page-object"
 
 class NewFlowPage < FlowPage
   include URL
-  # MEDIAWIKI_URL must have User_talk in $wgFlowOccupyNamespaces.
-  page_url URL.url("User_talk:New page " + Array.new(8) { [*'0'..'9', *'a'..'z', *'A'..'Z'].sample }.join)
+  # MEDIAWIKI_URL must have Flow_test_talk in $wgFlowOccupyNamespaces.
+  page_url URL.url("Flow_test_talk:New page " + Random.srand.to_s)
 end
