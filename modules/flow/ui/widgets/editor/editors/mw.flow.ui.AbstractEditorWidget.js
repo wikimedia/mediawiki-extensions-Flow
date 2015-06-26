723abb0358b6f3470855425b2f528beab1f3bfe5
Moriel Schottlender <moriel@gmail.com>
2015-06-26 16:50:04 -0700

    [WIP] Create an OOUI editor widget
    Refactor the editor system to be an OOUI editor widget.
    
    Bug: T103572
    Change-Id: Ic2353ef6e9155bd921d224d129c7b70cb22ba8a7

---
+++ modules/flow/ui/widgets/editor/editors/mw.flow.ui.AbstractEditorWidget.js
@@ -0,0 +1,141 @@
+//TODO rename me
+( function ( $ ) {
+	/**
+	 * Flow abstract editor widget.
+	 *
+	 * Subclasses must:
+	 * - Set #static-name
+	 * - Implement #getRawContent and #reloadContent
+	 * - Emit #event-change and #event-switch
+	 * - Respect config.switchable
+	 *
+	 * Subclasses probably need to:
+	 * - Set #static-format
+	 * - Implement #static-isSupported
+	 * - Implement #destroy
+	 * - Respect config.placeholder
+	 *
+	 * @extends OO.ui.Widget
+	 * @abstract
+	 *
+	 * @constructor
+	 * @param {Object} [config] Configuration options
+	 * @cfg {boolean} [switchable=false] Whether a tool to switch editors should be displayed
+	 * @cfg {string} [placeholder] Placeholder text for the edit area
+	 */
+	mw.flow.ui.AbstractEditorWidget = function mwFlowUiAbstractEditorWidget( config ) {
+		config = config || {};
+
+		// Parent constructor
+		mw.flow.ui.AbstractEditorWidget.parent.call( this, config );
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.flow.ui.AbstractEditorWidget, OO.ui.Widget );
+
+	/* Events */
+
+	/**
+	 * @event change
+	 * Fired when the contents of the editor change.
+	 */
+
+	/**
+	  * @event switch
+	  * @param {string} [editor] Symbolic name of the specific editor to switch to.
+	  *
+	  * Fired when the user expresses a desire to switch editors. If no specific editor
+	  * name is passed in, or if there is no editor by the given name, EditorSwitcherWidget
+	  * will decide which editor to switch to.
+	  *
+	  * Should not be emitted if config.switchable is false.
+	  */
+
+	/* Static Methods */
+
+	/**
+	 * Type of content to use
+	 *
+	 * @var {string}
+	 */
+	mw.flow.ui.AbstractEditorWidget.static.format = null;
+
+	/**
+	 * Name of this editor
+	 *
+	 * @var string
+	 */
+	mw.flow.ui.AbstractEditorWidget.static.name = null;
+
+	/**
+	 * Check if the editor is supported
+	 *
+	 * @return {boolean} Editor is supported
+	 */
+	mw.flow.ui.AbstractEditorWidget.static.isSupported = function () {
+		return true;
+	};
+
+	/* Methods */
+
+	/**
+	 * Set up the editor.
+	 *
+	 * @method
+	 * @abstract
+	 * @param {string} content Content
+	 * @return {jQuery.Promise} Promise resolved when setup is done
+	 */
+	mw.flow.ui.AbstractEditorWidget.prototype.setup = null;
+
+	/**
+	 * Tear down the editor.
+	 *
+	 * @method
+	 * @abstract
+	 * @return {jQuery.Promise} Promise resolved when teardown is done
+	 */
+	mw.flow.ui.AbstractEditorWidget.prototype.teardown = null;
+
+	/**
+	 * Get the content of the editor
+	 *
+	 * @abstract
+	 * @method
+	 * @return {string} Content of the editor
+	 */
+	mw.flow.ui.AbstractEditorWidget.prototype.getContent = null;
+
+	/**
+	 * Focus on the editor.
+	 *
+	 * @method
+	 * @abstract
+	 */
+	mw.flow.ui.AbstractEditorWidget.prototype.focus = null;
+
+	/**
+	 * Get the format of the content in this editor
+	 *
+	 * @return {string} Content format
+	 */
+	mw.flow.ui.AbstractEditorWidget.prototype.getFormat = function () {
+		return this.constructor.static.format;
+	};
+
+	/**
+	 * Get the name of this editor
+	 *
+	 * @return {string} Editor name
+	 */
+	mw.flow.ui.AbstractEditorWidget.prototype.getName = function () {
+		return this.constructor.static.name;
+	};
+
+	/**
+	 * Destroy the editor widget.
+	 */
+	mw.flow.ui.AbstractEditorWidget.prototype.destroy = function () {
+	};
+}( jQuery ) );
