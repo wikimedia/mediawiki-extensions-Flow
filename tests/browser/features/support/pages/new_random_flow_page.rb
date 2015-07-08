f3613039aedeb83fdcc997937a34c75d760fa937
Moriel Schottlender <moriel@gmail.com>
2015-07-08 15:50:53 -0700

    Create browser tests for Special:EnableFlow
    Bug: T102900
    Change-Id: I6dc46926ae64baff57f3e68b08e9cfe9d7675ca2

---
+++ tests/browser/features/support/pages/new_random_flow_page.rb
@@ -0,0 +1,10 @@
+require "page-object"
+
+class NewRandomFlowPage < WikiPage
+  div(:flow_page, class: 'flow-component board-page')
+  div(:flow_board, class: 'flow-board')
+  # board description
+  a(:edit_description_link, title: "Edit description")
+  div(:description_content, css: ".flow-board-header-content")
+  form(:edit_description_form, css: ".edit-header-form")
+end
