3c18dd824a8445f334a9fdceddbe78e1ead042f2
Moriel Schottlender <moriel@gmail.com>
2015-07-09 15:20:15 -0700

    Upgrade to Selenium 1.4 in browser tests
    Change-Id: I141c431df096d683cbee4d7b329c0c3aeeabfb00

--- tests/browser/features/support/pages/board_history_page.rb
+++ tests/browser/features/support/pages/board_history_page.rb
@@ -1,9 +1,7 @@
 class BoardHistoryPage
   include PageObject
 
-  include URL
-
-  page_url URL.url("Talk:Flow_QA?action=history")
+  page_url "Talk:Flow_QA?action=history"
 
   div(:flow_board_history, class: 'flow-board-history')
 end
