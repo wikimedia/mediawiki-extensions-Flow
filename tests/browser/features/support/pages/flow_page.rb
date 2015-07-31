426c07455c149052deac586d0c4130924681e97f
Matthias Mullie <git@mullie.eu>
2015-07-31 13:37:14 +0200

    Get rid of $wgFlowOccupyPages
    We already didn’t need this anymore: every board is guaranteed to have an
    associated record in `page` already. We retroactively created missing pages
    a while ago with maintenance/FlowUpdateWorkflowPageId.php
    (I67ac1bc3def5cce143b7b08b5d4301fe741df09b). At that same time, we also
    deployed changes to guarantee a new workflow could not be created without
    an associated entry in `page`.
    
    Since page/revision entries exist, we can read the content model from there
    and no longer need $wgFlowOccupyPages.
    
    Theoretically, there could be pages in $wgFlowOccupyPages where a topic was
    never created. After this patch, such empty pages would then no longer be
    recognized as Flow board. I think it's unlikely there are any such pages
    because:
    - they were activated on request, so likely to be used
    - we’ve been activating them using Special:EnableFlow for a while now, which
      creates a `page` entry
    
    Meanwhile also completely removed a bit of code in DeletedContributionsQuery
    hook, where we were trying to short-circuit when viewing a namespace that was
    guaranteed not to have a Flow page, but that code was already not reliable
    since $wgFlowOccupy* aren't the only means of enabling Flow anymore.
    
    Bug: T105574
    Change-Id: I27e8e45951bfe96453c2bd2424f643a343cb7e59

--- tests/browser/features/support/pages/flow_page.rb
+++ tests/browser/features/support/pages/flow_page.rb
@@ -1,4 +1,4 @@
 class FlowPage < AbstractFlowPage
   page_url 'Talk:Flow_QA'
-  # MEDIAWIKI_URL must have this in $wgFlowOccupyPages array or $wgFlowOccupyNamespaces.
+  # MEDIAWIKI_URL must have this in $wgFlowOccupyNamespaces.
 end
