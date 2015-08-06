1ccaa02a7a19b08cda86b091339022e569e2804a
Moriel Schottlender <moriel@gmail.com>
2015-08-05 18:00:24 -0700

    Create a new topic widget
    Bug: T107915
    Change-Id: Ic2e99b916d61538c81ff5f857fa5999aa697a060

--- modules/flow-initialize.js
+++ modules/flow-initialize.js
@@ -9,7 +9,8 @@
 	 * @todo not like this
 	 */
 	$( document ).ready( function () {
-		var dataBlob, navWidget, flowBoard, dmBoard,
+		var dataBlob, navWidget, flowBoard, dmBoard, newTopicWidget,
+			pageTitle = mw.Title.newFromText( mw.config.get( 'wgPageName' ) ),
 			$component = $( '.flow-component' ),
 			$board = $( '.flow-board' ),
 			finishLoading = function () {
@@ -41,7 +42,7 @@
 
 		// Load data model
 		mw.flow.system = new mw.flow.dm.System( {
-			pageTitle: mw.Title.newFromText( mw.config.get( 'wgPageName' ) ),
+			pageTitle: pageTitle,
 			tocPostsLimit: 50,
 			renderedTopics: $( '.flow-topic' ).length,
 			boardId: $component.data( 'flow-id' ),
@@ -162,6 +163,15 @@
 		navWidget.on( 'reorderTopics', function ( newOrder ) {
 			flowBoard.topicIdSort = newOrder;
 		} );
+
+		// New topic form
+		newTopicWidget = new mw.flow.ui.NewTopicWidget( pageTitle.getPrefixedDb() );
+		newTopicWidget.on( 'save', function ( newTopicId ) {
+			// Display the new topic with the old system
+			var $stub = $( '<div class="flow-topic"><div></div></div>' ).prependTo( flowBoard.$container.find( '.flow-topics' ) );
+			return flowBoard.flowBoardComponentRefreshTopic( $stub.find( 'div' ), newTopicId );
+		} );
+		$( 'form.flow-newtopic-form' ).replaceWith( newTopicWidget.$element );
 
 		dataBlob = mw.flow && mw.flow.data;
 		if ( dataBlob && dataBlob.blocks ) {
