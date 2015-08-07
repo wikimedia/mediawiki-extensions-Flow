f30f5ba89738edf90329043d8ed7a94c53a67800
Moriel Schottlender <moriel@gmail.com>
2015-08-06 17:40:55 -0700

    Add categories to the sidebar
    Add categories to the board data model and have a categories widget
    that synchronizes with the model to present the current categories
    in the board.
    
    Bug: T92874
    Bug: T111110
    Change-Id: Ib9b766f9125e1e0c95798db46bf29ca7e0ac0408

---
+++ modules/flow/ui/widgets/mw.flow.ui.CategoryItemWidget.js
@@ -0,0 +1,48 @@
+( function ( $ ) {
+	/**
+	 * Flow board categories widget
+	 *
+	 * @class
+	 * @extends OO.ui.Widget
+	 * @mixin OO.ui.mixin.GroupElement
+	 *
+	 * @constructor
+	 * @param {mw.flow.dm.CategoryItem} categoryModel Category item model
+	 * @param {Object} [config]
+	 * @cfg {boolean} [exists] Category page exists on this wiki
+	 */
+	mw.flow.ui.CategoryItemWidget = function mwFlowUiCategoryItemWidget( categoryModel, config ) {
+		var $link;
+
+		config = config || {};
+
+		// Parent constructor
+		mw.flow.ui.CategoryItemWidget.parent.call( this, config );
+
+		this.model = categoryModel;
+		this.name = this.model.getId();
+		this.title = mw.Title.newFromText( this.name, mw.config.get( 'wgNamespaceIds' ).category );
+		this.exists = this.model.exists();
+
+		$link = $( '<a>' )
+			.attr( 'href', mw.util.getUrl( this.title.getPrefixedDb() ) )
+			.attr( 'title', this.exists ? this.name : mw.msg( 'red-link-title', this.name ) )
+			.text( this.name )
+			.toggleClass( 'new', !this.exists );
+
+		this.$element
+			.addClass( 'flow-ui-categoryItemWidget flow-board-header-category-item' )
+			.append( $link );
+	};
+
+	OO.inheritClass( mw.flow.ui.CategoryItemWidget, OO.ui.Widget );
+
+	/**
+	 * Get the category data
+	 *
+	 * @return {string} Category name
+	 */
+	mw.flow.ui.CategoryItemWidget.prototype.getData = function () {
+		return this.name;
+	};
+}( jQuery ) );
