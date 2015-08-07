781e1896dddf857af217d3c2315d103ad463c787
Moriel Schottlender <moriel@gmail.com>
2015-08-06 17:40:55 -0700

    Add categories to the sidebar
    Add categories to the board data model and have a categories widget
    that synchronizes with the model to present the current categories
    in the board.
    
    Bug: T92874
    Bug: T111110
    Change-Id: Ib9b766f9125e1e0c95798db46bf29ca7e0ac0408

---
+++ modules/flow/dm/mw.flow.dm.Categories.js
@@ -0,0 +1,26 @@
+( function ( $ ) {
+	/**
+	 * Flow Board
+	 *
+	 * @class
+	 * @mixins mw.flow.dm.List
+	 * @mixins OO.EventEmitter
+	 *
+	 * @constructor
+	 * @param {Object} [config] Configuration options
+	 */
+	mw.flow.dm.Categories = function mwFlowDmCategories( config ) {
+		config = config || {};
+
+		// Mixin constructor
+		OO.EventEmitter.call( this, config );
+
+		// Mixin constructor
+		mw.flow.dm.List.call( this, config );
+	};
+
+	/* Initialization */
+
+	OO.mixinClass( mw.flow.dm.Categories, OO.EventEmitter );
+	OO.mixinClass( mw.flow.dm.Categories, mw.flow.dm.List );
+}( jQuery ) );
