18ea183816eb0feefab34595f26d8b6450617d8c
Stephane Bisson <sbisson@wikimedia.org>
2015-08-10 16:44:13 -0400

    Beta feature Flow on user talk page
    When $wgFlowEnableOptInBetaFeature is true,
    a new beta feature allows using to
    enable Flow on their talk page.
    
    There is also a maintenance script to
    auto opt-in users who are already using
    Flow on their user talk page.
    
    Bug: T98270
    Change-Id: Ia9950c4eb1c0e5f912761a65c76c5a2b3b99c8ee

--- defines.php
+++ defines.php
@@ -4,3 +4,4 @@
 define( 'RC_FLOW', 142 ); // Random number chosen.  Can be replaced with rc_source; see bug 72157.
 define( 'NS_TOPIC', 2600 );
 define( 'FLOW_TALK_PAGE_MANAGER_USER', 'Flow talk page manager' );
+define( 'BETA_FEATURE_FLOW_USER_TALK_PAGE', 'beta-feature-flow-user-talk-page' );
--- tests/browser/features/support/pages/wiki_page.rb
+++ tests/browser/features/support/pages/wiki_page.rb
@@ -1,4 +1,9 @@
 class WikiPage
   include PageObject
+
+  page_url "<%=params[:page]%>"
+
   a(:logout, css: "#pt-logout a")
+  div(:content, id: 'mw-content-text')
+  page_section(:flow, FlowComponent, class: 'flow-component')
 end
