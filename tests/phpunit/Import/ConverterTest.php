7bb74ec763b7b6aa983a4e29f9eb8c2be83149b4
Stephane Bisson <sbisson@wikimedia.org>
2015-08-10 16:44:13 -0400

    Beta feature Flow on user talk page
    When $wgFlowEnableOptInBetaFeature is true,
    a new beta feature allows using to
    enable Flow on their talk page.
    
    There is also a maintenance script to
    auto opt-in users who are already using
    Flow ontheir user talk page.
    
    Bug: T98270
    Change-Id: Ia9950c4eb1c0e5f912761a65c76c5a2b3b99c8ee

--- tests/phpunit/Import/ConverterTest.php
+++ tests/phpunit/Import/ConverterTest.php
@@ -8,7 +8,6 @@ use Flow\Import\IConversionStrategy;
 use Flow\Import\Importer;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
-use Title;
 use User;
 
 /**
@@ -20,63 +19,6 @@ class ConverterTest extends \MediaWikiTestCase {
 			'Flow\Import\Converter',
 			$this->createConverter()
 		);
-	}
-
-	public function decideArchiveTitleProvider() {
-		return array(
-			array(
-				'Selects the first pattern if n=1 does exist',
-				// expect
-				'Talk:Flow/Archive 1',
-				// source title
-				Title::newFromText( 'Talk:Flow' ),
-				// formats
-				array( '%s/Archive %d', '%s/Archive%d' ),
-				// existing titles
-				array(),
-			),
-
-			array(
-				'Selects n=2 when n=1 exists',
-				// expect
-				'Talk:Flow/Archive 2',
-				// source title
-				Title::newFromText( 'Talk:Flow' ),
-				// formats
-				array( '%s/Archive %d' ),
-				// existing titles
-				array( 'Talk:Flow/Archive 1' ),
-			),
-
-			array(
-				'Selects the second pattern if n=1 exists',
-				// expect
-				'Talk:Flow/Archive2',
-				// source title
-				Title::newFromText( 'Talk:Flow' ),
-				// formats
-				array( '%s/Archive %d', '%s/Archive%d' ),
-				// existing titles
-				array( 'Talk:Flow/Archive1' ),
-			),
-		);
-	}
-	/**
-	 * @dataProvider decideArchiveTitleProvider
-	 */
-	public function testDecideArchiveTitle( $message, $expect, Title $source, array $formats, array $exists ) {
-		// flip so we can use isset
-		$existsByKey = array_flip( $exists );
-
-		$titleRepo = $this->getMock( 'Flow\Repository\TitleRepository' );
-		$titleRepo->expects( $this->any() )
-			->method( 'exists' )
-			->will( $this->returnCallback( function( Title $title ) use ( $existsByKey ) {
-				return isset( $existsByKey[$title->getPrefixedText()] );
-			} ) );
-
-		$result = Converter::decideArchiveTitle( $source, $formats, $titleRepo );
-		$this->assertEquals( $expect, $result, $message );
 	}
 
 	protected function createConverter(
