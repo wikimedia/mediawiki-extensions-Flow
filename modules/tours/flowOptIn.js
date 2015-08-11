dc722d739bfd6567171b9c671ec931a1846ae5a1
Moriel Schottlender <moriel@gmail.com>
2015-08-11 14:45:43 -0700

    Create a guided tour for first-time users
    Bug: T108266
    Change-Id: I4f9e08d245cd099a52e85ee37597c96c67ebcb1a

---
+++ modules/tours/flowOptIn.js
@@ -0,0 +1,45 @@
+( function ( window, document, $, mw, gt ) {
+	// TODO: Change the link selector when a class is available on it
+	// See phabricator task https://phabricator.wikimedia.org/T105421#1566274
+	var archiveLinkExists = $( '.flow-board-header-content p a' ).length,
+		tour = new gt.TourBuilder( {
+			name: 'flowOptIn'
+		} );
+
+	tour.firstStep( {
+		name: 'newTopic',
+		titlemsg: 'flow-guidedtour-optin-welcome',
+		descriptionmsg: 'flow-guidedtour-optin-welcome-description',
+		attachTo: '.flow-newtopic-form [name="topiclist_topic"]',
+		position: 'bottom',
+		autoFocus: true,
+		closeOnClickOutside: false
+	} )
+	.next( archiveLinkExists ? 'findOldConversations' : 'feedback' );
+
+	if ( archiveLinkExists ) {
+		tour.step( {
+			name: 'findOldConversations',
+			titlemsg: 'flow-guidedtour-optin-find-old-conversations',
+			descriptionmsg: 'flow-guidedtour-optin-find-old-conversations-description',
+			attachTo: '.flow-board-header-content',
+			position: 'bottom',
+			autoFocus: true,
+			closeOnClickOutside: false
+		} )
+		.next( 'feedback' )
+		.back( 'newTopic' );
+	}
+
+	tour.step( {
+		name: 'feedback',
+		titlemsg: 'flow-guidedtour-optin-feedback',
+		descriptionmsg: 'flow-guidedtour-optin-feedback-description',
+		attachTo: '#pt-betafeatures',
+		position: 'bottom',
+		autoFocus: true,
+		closeOnClickOutside: false
+	} )
+	.back( archiveLinkExists ? 'findOldConversations' : 'feedback' );
+
+}( window, document, jQuery, mediaWiki, mediaWiki.guidedTour ) );
