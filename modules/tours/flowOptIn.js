9920d620d403ab563d0fc74cb3019e4a41b6cec3
Moriel Schottlender <moriel@gmail.com>
2015-08-11 14:45:43 -0700

    Create a guided tour for first-time users
    Bug: T108266
    Change-Id: I4f9e08d245cd099a52e85ee37597c96c67ebcb1a

---
+++ modules/tours/flowOptIn.js
@@ -0,0 +1,51 @@
+( function ( window, document, $, mw, gt ) {
+	var archiveLinkExists = $( '.flow-link-to-archive' ).length,
+		tour = new gt.TourBuilder( {
+			name: 'flowOptIn',
+			isSinglePage: true
+		} ),
+		$firstStepDescription = $( '<div>' )
+			.addClass( 'flow-guidedtour-optin-firststep-description' )
+			.append(
+				$( '<div>' )
+					.addClass( 'flow-guidedtour-optin-firststep-description-image' ),
+				mw.msg( 'flow-guidedtour-optin-welcome-description' )
+			);
+
+	tour.firstStep( {
+		name: 'newTopic',
+		titlemsg: 'flow-guidedtour-optin-welcome',
+		description: $firstStepDescription[0].outerHTML,
+		attachTo: '.flow-newtopic-form [name="topiclist_topic"]',
+		position: 'bottom',
+		autoFocus: true,
+		closeOnClickOutside: false
+	} )
+	.next( archiveLinkExists ? 'findOldConversations' : 'feedback' );
+
+	if ( archiveLinkExists ) {
+		tour.step( {
+			name: 'findOldConversations',
+			titlemsg: 'flow-guidedtour-optin-find-old-conversations',
+			descriptionmsg: 'flow-guidedtour-optin-find-old-conversations-description',
+			attachTo: '.flow-ui-boardDescriptionWidget-content',
+			position: 'bottom',
+			autoFocus: true,
+			closeOnClickOutside: false
+		} )
+		.next( 'feedback' )
+		.back( 'newTopic' );
+	}
+
+	tour.step( {
+		name: 'feedback',
+		titlemsg: 'flow-guidedtour-optin-feedback',
+		descriptionmsg: 'flow-guidedtour-optin-feedback-description',
+		attachTo: '#pt-betafeatures',
+		position: 'bottom',
+		autoFocus: true,
+		closeOnClickOutside: false
+	} )
+	.back( archiveLinkExists ? 'findOldConversations' : 'newTopic' );
+
+}( window, document, jQuery, mediaWiki, mediaWiki.guidedTour ) );
