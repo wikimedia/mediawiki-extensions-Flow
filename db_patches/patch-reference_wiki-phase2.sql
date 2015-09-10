bbfb91755cbdc77a554665557d1c2afd4cf4b509
Matthew Flaschen <mflaschen@wikimedia.org>
2015-09-10 14:28:04 -0400

    WIP: use ref_src_workflow_id to find workflow ID directly
    Still need to update the update.php version for non-WMF.
    
    Bug: T111084
    Change-Id: I695d48e56f54303f8694b090569cd2b62322fc3b

--- db_patches/patch-reference_wiki-phase2.sql
+++ db_patches/patch-reference_wiki-phase2.sql
@@ -4,62 +4,29 @@
 -- These updates are idempotent, but must be re-run until 0 rows are
 -- affected for all.
 
--- Populate header and summary wiki references with the appropriate wiki
+-- Populate wiki references with the appropriate wiki
 UPDATE
 	/*_*/flow_wiki_ref, (
-		SELECT ref_src_object_id, workflow_wiki
+		SELECT ref_src_workflow_id, workflow_wiki
 			FROM /*_*/flow_wiki_ref, /*_*/flow_workflow
 		WHERE
-			flow_wiki_ref.ref_src_object_id = flow_workflow.workflow_id AND
-			flow_wiki_ref.ref_src_object_type IN ('header', 'post-summary') AND
+			flow_wiki_ref.ref_src_workflow_id = flow_workflow.workflow_id AND
 			( flow_wiki_ref.ref_src_wiki = '' OR flow_wiki_ref.ref_src_wiki IS NULL )
 		LIMIT 1000
 	) tmp
 	SET flow_wiki_ref.ref_src_wiki = tmp.workflow_wiki
-	WHERE flow_wiki_ref.ref_src_object_id = tmp.ref_src_object_id;
+	WHERE flow_wiki_ref.ref_src_workflow_id = tmp.ref_src_workflow_id;
 
 
--- Populate post wiki references with the appropriate wiki.
-UPDATE
-	/*_*/flow_wiki_ref, (
-		SELECT ref_src_object_id, workflow_wiki
-		FROM /*_*/flow_wiki_ref, /*_*/flow_tree_node, /*_*/flow_workflow
-		WHERE
-			flow_wiki_ref.ref_src_object_id = flow_tree_node.tree_descendant_id AND
-			flow_tree_node.tree_ancestor_id = flow_workflow.workflow_id AND
-			flow_wiki_ref.ref_src_object_type IN ('post') AND
-			( flow_wiki_ref.ref_src_wiki = '' OR flow_wiki_ref.ref_src_wiki IS NULL )
-		LIMIT 1000
-	) tmp
-	SET flow_wiki_ref.ref_src_wiki = tmp.workflow_wiki
-	WHERE flow_wiki_ref.ref_src_object_id = tmp.ref_src_object_id;
-
-
+-- Populate post external references with the appropriate wiki.
 UPDATE
 	/*_*/flow_ext_ref, (
-		SELECT ref_src_object_id, workflow_wiki
+		SELECT ref_src_workflow_id, workflow_wiki
 			FROM /*_*/flow_ext_ref, /*_*/flow_workflow
 		WHERE
-			flow_ext_ref.ref_src_object_id = flow_workflow.workflow_id AND
-			flow_ext_ref.ref_src_object_type IN ('header', 'post-summary') AND
+			flow_ext_ref.ref_src_workflow_id = flow_workflow.workflow_id AND
 			( flow_ext_ref.ref_src_wiki = '' OR flow_ext_ref.ref_src_wiki IS NULL )
 		LIMIT 1000
 	) tmp
 	SET flow_ext_ref.ref_src_wiki = tmp.workflow_wiki
-	WHERE flow_ext_ref.ref_src_object_id = tmp.ref_src_object_id;
-
-
--- Populate post wiki references with the appropriate wiki.
-UPDATE
-	/*_*/flow_ext_ref, (
-		SELECT ref_src_object_id, workflow_wiki
-		FROM /*_*/flow_ext_ref, /*_*/flow_tree_node, /*_*/flow_workflow
-		WHERE
-			flow_ext_ref.ref_src_object_id = flow_tree_node.tree_descendant_id AND
-			flow_tree_node.tree_ancestor_id = flow_workflow.workflow_id AND
-			flow_ext_ref.ref_src_object_type IN ('post') AND
-			( flow_ext_ref.ref_src_wiki = '' OR flow_ext_ref.ref_src_wiki IS NULL )
-		LIMIT 1000
-	) tmp
-	SET flow_ext_ref.ref_src_wiki = tmp.workflow_wiki
-	WHERE flow_ext_ref.ref_src_object_id = tmp.ref_src_object_id;
+	WHERE flow_ext_ref.ref_src_workflow_id = tmp.ref_src_workflow_id;
