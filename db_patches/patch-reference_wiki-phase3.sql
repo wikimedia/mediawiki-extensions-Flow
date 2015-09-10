590e2c3ea229655778085885c272a818a5090616
Matthew Flaschen <mflaschen@wikimedia.org>
2015-09-10 14:28:04 -0400

    Use ref_src_workflow_id to find workflow ID, add index
    Add a temporary index to make lookups by ref_src_workflow_id and
    ref_src_wiki efficient.
    
    The phases have been shifted.
    
    Phase 2 - Just temporary indices
    Phase 3 - Actual backfill
    Phase 4 - Final schema changes
    
    Bug: T111084
    Change-Id: I695d48e56f54303f8694b090569cd2b62322fc3b

--- db_patches/patch-reference_wiki-phase3.sql
+++ db_patches/patch-reference_wiki-phase3.sql
@@ -1,15 +1,32 @@
--- This is the final schema change required for
--- 'Segregate Reference objects by source wiki.'
+-- Phase 2 for adding ref_src_wiki field
+-- Back-fills the field from other data available in the database.
 --
--- After phase 2 is complete (re-running does not affect any more rows),
--- this should be run.
+-- These updates are idempotent, but must be re-run until 0 rows are
+-- affected for all.
 
--- Mark field as not null
-ALTER TABLE /*_*/flow_wiki_ref MODIFY ref_src_wiki varchar(16) binary not null;
-ALTER TABLE /*_*/flow_ext_ref MODIFY ref_src_wiki varchar(16) binary not null;
+-- Populate wiki references with the appropriate wiki
+UPDATE
+	/*_*/flow_wiki_ref, (
+		SELECT ref_src_workflow_id, workflow_wiki
+			FROM /*_*/flow_wiki_ref, /*_*/flow_workflow
+		WHERE
+			flow_wiki_ref.ref_src_workflow_id = flow_workflow.workflow_id AND
+			( flow_wiki_ref.ref_src_wiki = '' OR flow_wiki_ref.ref_src_wiki IS NULL )
+		LIMIT 1000
+	) tmp
+	SET flow_wiki_ref.ref_src_wiki = tmp.workflow_wiki
+	WHERE flow_wiki_ref.ref_src_workflow_id = tmp.ref_src_workflow_id;
 
--- Drop old indexes
-DROP INDEX /*i*/flow_wiki_ref_idx ON /*_*/flow_wiki_ref;
-DROP INDEX /*i*/flow_wiki_ref_revision ON /*_*/flow_wiki_ref;
-DROP INDEX /*i*/flow_ext_ref_idx ON /*_*/flow_ext_ref;
-DROP INDEX /*i*/flow_ext_ref_revision ON /*_*/flow_ext_ref;
+
+-- Populate external references with the appropriate wiki.
+UPDATE
+	/*_*/flow_ext_ref, (
+		SELECT ref_src_workflow_id, workflow_wiki
+			FROM /*_*/flow_ext_ref, /*_*/flow_workflow
+		WHERE
+			flow_ext_ref.ref_src_workflow_id = flow_workflow.workflow_id AND
+			( flow_ext_ref.ref_src_wiki = '' OR flow_ext_ref.ref_src_wiki IS NULL )
+		LIMIT 1000
+	) tmp
+	SET flow_ext_ref.ref_src_wiki = tmp.workflow_wiki
+	WHERE flow_ext_ref.ref_src_workflow_id = tmp.ref_src_workflow_id;
