b9f2c630716bc94894ddf263c7add2fd60b6f964
Matthew Flaschen <mflaschen@wikimedia.org>
2015-09-11 23:36:24 -0400

    WIP: Fix exception on history page
    This was caused by the new normalization code (designed to better
    handle schema changes), along with the fact that BoardHistoryIndex
    could hold different types of revisions.  This meant that after
    e.g. editing a header then making a post, the post would have
    missing fields (the ones a header didn't need) in the history.
    This caused an exception when deserialzing the post.
    
    This patch separates posts and summaries into their own indices.
    Headers just use an existing index that is suitable for this.
    
    While working on this, I also noticed some pagination use cases
    that do not seem to be handled, so this starts working on this too.
    So far the WIP just has the index reject certain queries.
    
    There is a problem with some summary history entries being missing,
    so this is not ready yet.
    
    Bug: T111494
    Bug: T112230
    Change-Id: Ideaba2a1431c4dca10e8706536db30f410930ed0

--- includes/Formatter/BoardHistoryQuery.php
+++ includes/Formatter/BoardHistoryQuery.php
@@ -2,6 +2,7 @@
 
 namespace Flow\Formatter;
 
+use Flow\Data\Storage\RevisionStorage;
 use Flow\Exception\FlowException;
 use Flow\Model\UUID;
 use MWExceptionHandler;
@@ -14,24 +15,30 @@ class BoardHistoryQuery extends AbstractQuery {
 	 * @param string $direction 'rev' or 'fwd'
 	 * @return FormatterRow[]
 	 */
-	public function getResults( UUID $workflowId, $limit = 50, UUID $offset = null, $direction = 'fwd' ) {
-		// Load the history
-		$history = $this->storage->find(
-			'BoardHistoryEntry',
-			array( 'topic_list_id' => $workflowId ),
-			array(
-				'sort' => 'rev_id',
-				'order' => 'DESC',
-				'limit' => $limit,
-				'offset-id' => $offset,
-				'offset-dir' => $direction,
-				'offset-include' => false,
-				'offset-elastic' => false,
-			)
+	public function getResults( UUID $boardWorkflowId, $limit = 50, UUID $offset = null, $direction = 'fwd' ) {
+		$options = array(
+			'sort' => 'rev_id',
+			'order' => 'DESC',
+			'limit' => $limit,
+			'offset-id' => $offset,
+			'offset-dir' => $direction,
+			'offset-include' => false,
+			'offset-elastic' => false,
 		);
 
-		if ( !$history ) {
-			return array();
+		$headerHistory = $this->getHeaderResults( $boardWorkflowId, $options );
+		$topicListHistory = $this->getTopicListResults( $boardWorkflowId, $options );
+		$topicSummaryHistory = $this->getTopicSummaryResults( $boardWorkflowId, $options );
+
+		$history = $headerHistory + $topicListHistory + $topicSummaryHistory;
+
+		// Having merged data from 3 sources, we now have to combine it
+		// (according to the current sort & limit)
+		// We can page through in any order, but a single page is always in descending order (later history first).
+		krsort( $history );
+
+		if ( isset( $options['limit'] ) ) {
+			$history = array_splice( $history, 0, $options['limit'] );
 		}
 
 		// fetch any necessary metadata
@@ -51,5 +58,36 @@ class BoardHistoryQuery extends AbstractQuery {
 		}
 
 		return $results;
+	}
+
+	protected function getHeaderResults( UUID $boardWorkflowId, $options ) {
+		return $this->storage->find(
+			'Header',
+			array(
+				'rev_type_id' => $boardWorkflowId,
+			),
+			$options
+		);
+	}
+
+	protected function getTopicListResults( UUID $boardWorkflowId, $options ) {
+		// Can't use 'PostRevision' because we need to get only the ones with the right board ID.
+		return $this->storage->find(
+			'PostRevisionBoardHistoryEntry',
+			array(
+				'topic_list_id' => $boardWorkflowId,
+			),
+			$options
+		);
+	}
+
+	protected function getTopicSummaryResults( UUID $boardWorkflowId, $options ) {
+		return $this->storage->find(
+			'PostSummaryBoardHistoryEntry',
+			array(
+				'topic_list_id' => $boardWorkflowId,
+			),
+			$options
+		);
 	}
 }
