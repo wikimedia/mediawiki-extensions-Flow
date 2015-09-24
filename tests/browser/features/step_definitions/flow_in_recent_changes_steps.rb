3c70ba8f5be55b0a69ce5b7bb656f993da21e5be
Stephane Bisson <sbisson@wikimedia.org>
2015-09-24 17:25:58 -0400

    Browser tests: Refresh flow page until optin process is finished
    * Also using refresh_until for recent changes tests
    
    Change-Id: Ib379980ae78fe86c120c98c7db2fa380fd37c646

--- tests/browser/features/step_definitions/flow_in_recent_changes_steps.rb
+++ tests/browser/features/step_definitions/flow_in_recent_changes_steps.rb
@@ -3,9 +3,15 @@ When(/^I navigate to the Recent Changes page$/) do
 end
 
 Then(/^the new topic should be in the Recent Changes page$/) do
-  expect(on(RecentChangesPage).recent_changes_element.when_present.text).to match @topic_string
+  on(RecentChangesPage) do |page|
+    page.refresh_until { page.recent_changes.match @topic_string }
+  end
+  # expect(on(RecentChangesPage).recent_changes_element.when_present.text).to match @topic_string
 end
 
 Then(/^the new title should be in the Recent Changes page$/) do
-  expect(on(RecentChangesPage).recent_changes_element.when_present.text).to match @edited_topic_string
+  on(RecentChangesPage) do |page|
+    page.refresh_until { page.recent_changes.match @edited_topic_string }
+  end
+  # expect(on(RecentChangesPage).recent_changes_element.when_present.text).to match @edited_topic_string
 end
