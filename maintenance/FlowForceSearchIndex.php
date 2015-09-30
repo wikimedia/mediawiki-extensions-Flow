f0fa6bfc323535de9580526b62b240dd2b4aed45
Matthias Mullie <git@mullie.eu>
2015-09-30 14:52:17 +0200

    Dump Flow data
    A big chunk of this patch is a refactoring of the search updaters.
    I wanted to reuse the code that already iterators over all header
    & topics, but it was unwieldly. I split that into separate classes
    that are now Iterator objects.
    
    Other changes:
    * Fixed Flow\Search\Connection, which could no longer call
    parent::__construct since that no longer exists
    * Flow\Container couldn’t be autoloaded from maintenance
    script's constructor (autoload not yet initialized, I assume)
    * FlowFixWorkflowLastUpdateTimestamp.php was failing because
    getBinary now returns a UUIDBlob instead of the plain binary string
    
    Bug: T89398
    Change-Id: I52bc7c0ce7813a78f9006ca4b7d931a905726c05

--- maintenance/FlowForceSearchIndex.php
+++ maintenance/FlowForceSearchIndex.php
@@ -1,10 +1,9 @@
 <?php
 
 use Flow\Container;
-use Flow\Model\AbstractRevision;
 use Flow\Model\UUID;
 use Flow\Search\Connection;
-use Flow\Search\Updater;
+use Flow\Search\Updaters\AbstractUpdater;
 
 require_once ( getenv( 'MW_INSTALL_PATH' ) !== false
 	? getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php'
@@ -33,100 +32,44 @@ class FlowForceSearchIndex extends Maintenance {
 
 		$this->addOption( 'fromId', 'Start indexing at a specific revision id (inclusive).', false, true );
 		$this->addOption( 'toId', 'Stop indexing at a specific revision (inclusive).', false, true );
-		$this->addOption( 'limit', 'Maximum number of revisions to process before exiting the script. Default to unlimited.', false, true );
 		$this->addOption( 'namespace', 'Only index revisions in this given namespace', false, true );
-
-		$this->connection = Container::get( 'search.connection' );
 	}
 
 	public function execute() {
 		global $wgFlowSearchMaintenanceTimeout;
 
+		$this->connection = Container::get( 'search.connection' );
+
 		// Set the timeout for maintenance actions
 		$this->connection->setTimeout( $wgFlowSearchMaintenanceTimeout );
 
-		/** @var Updater[] $updaters */
+		/** @var AbstractUpdater[] $updaters */
 		$updaters = Container::get( 'search.index.updaters' );
 		foreach ( $updaters as $updaterType => $updater ) {
 			$fromId = $this->getOption( 'fromId', null );
 			$fromId = $fromId ? UUID::create( $fromId ) : null;
 			$toId = $this->getOption( 'toId', null );
 			$toId = $toId ? UUID::create( $toId ) : null;
+			if ( $toId !== null ) {
+				// AbstractIterator::toId is exclusive, but we want inclusive,
+				// so just feed toId() the next possible UUID (UUID + 1)
+				// We need some base conversion & bcadd because the number may
+				// be too large to be an int.
+				$decimal = wfBaseConvert( $toId->getAlphadecimal(), 36, 10 );
+				$new = bcadd( $decimal, 1, 0 );
+				$hex = wfBaseConvert( $new, 10, 36 );
+				$toId = UUID::create( $hex );
+			}
 			$namespace = $this->getOption( 'namespace', null );
-			$numRevisionsToIndex = $this->getOption( 'limit', null );
 			$total = 0;
 
-			while ( true ) {
-				// if a limit was provided, we should make sure to not fetch
-				// more revisions than asked for
-				$options = array( 'LIMIT' => $this->mBatchSize );
-				if ( $numRevisionsToIndex ) {
-					$options['LIMIT'] = min( $numRevisionsToIndex, $this->mBatchSize );
+			$updater->iterator->setNamespace( $namespace );
+			$updater->iterator->setFrom( $fromId );
+			$updater->iterator->setTo( $toId );
 
-					// since we do this in batches, we'll subtract the size of
-					// each batch until $numRevisionsToIndex is reached
-					$numRevisionsToIndex -= $this->mBatchSize;
-					if ( $options['LIMIT'] <= 0 ) {
-						break;
-					}
-				}
-
-				$conditions = $updater->buildQueryConditions( $fromId, $toId, $namespace );
-				$revisions = $updater->getRevisions( $conditions, $options );
-
-				// stop if we're all out of revisions
-				if ( !$revisions ) {
-					break;
-				}
-
-				$total += $updater->updateRevisions( $revisions, null, null );
-				$this->output( "Indexed $total $updaterType document(s)\n" );
-
-				// prepare for next batch, starting at the next id
-				// prevFromId will default to around unix epoch - there can be
-				// no data before that
-				$prevFromId = $fromId ?: UUID::getComparisonUUID( '1' );
-				$fromId = $this->getNextFromId( $revisions );
-
-				// make sure we don't get stuck in an infinite loop
-				$diff = $prevFromId->getTimestampObj()->diff( $fromId->getTimestampObj() );
-				// invert will be 1 if the diff is a negative time period from
-				// $prevFromId to $fromId, which means that the new $timestamp is
-				// more recent than our current $result
-				if ( $diff->invert ) {
-					$this->error(
-						'Got stuck in an infinite loop.' . "\n" .
-						'workflow_last_update_timestamp is likely incorrect ' .
-						'for some workflows.' . "\n" .
-						'Run maintenance/FlowFixWorkflowLastUpdateTimestamp.php ' .
-						'to automatically fix those.', 1 );
-				}
-
-				// prevent memory from being filled up
-				Container::get( 'storage' )->clear();
-			}
+			$total += $updater->updateRevisions( null, null, $this->mBatchSize );
+			$this->output( "Indexed $total $updaterType document(s)\n" );
 		}
-	}
-
-	/**
-	 * @param AbstractRevision[] $revisions
-	 * @return UUID
-	 */
-	protected function getNextFromId( array $revisions ) {
-		/** @var AbstractRevision $last */
-		$last = end( $revisions );
-
-		if ( $last instanceof \Flow\Model\Header ) {
-			$timestamp = $last->getRevisionId()->getTimestampObj();
-		} else {
-			$timestamp = $last->getCollection()->getWorkflow()->getLastUpdatedObj();
-		}
-
-		// $timestamp is the timestamp of the last revision we fetched. fromId
-		// is inclusive, and we don't want to include what we already have here,
-		// so we'll advance 1 more and call that the next fromId
-		$timestamp = (int) $timestamp->getTimestamp( TS_UNIX );
-		return UUID::getComparisonUUID( $timestamp + 1 );
 	}
 }
 
