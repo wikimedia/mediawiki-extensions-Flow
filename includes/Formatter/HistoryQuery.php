9b68c162837e7c3e3d95cff02d0edbd84d51aa69
Matthew Flaschen <mflaschen@wikimedia.org>
2015-10-12 22:36:29 -0400

    Fix history pagination and give user the number of entries they requested
    * Tune index entries (taking into account by-one overfetching for
      pagination info and the need to exclude certain history entries) to
      make it more likely we can serve entries from the index (even with the
      max UI limit, 500).  Bump index keys for this.
    
    * Keep re-requesting until we get the original number they asked for
      (50, 500) even though we have to exclude some.  Request extra to
      take into account some will be excluded.
    
    * Make offset and 'rev' order work, regardless of limit and regardless
      of whether it fits in the index.
    
      Before, it would only work when it was entirely within the index.
    
      It no longer uses the index when there is an offset, or when reverse
      order is used.  In theory, it could still use it in certain of such
      cases.
    
      But I think it would have to fetch the memcached list in the canAnswer
      phase, and it still wouldn't handle spanning the index.
    
      This requires removing tests of the only-within-the-index
      unctionality (e.g. testOffsetIdReturnsCorrectPortionOfIndexedValues).
    
    * Move includeFromHistory back into *HistoryQuery (using a base class).
      It could be kept in HistoryPager, but it wouldn't be as performant,
      since only the Query's can reasonably know exactly which sub-queries
      could potentially have results excluded.  Doing it in the pager would
      mean requesting 682 of everything, not just the ones vulnerable to
      exclusion.
    
    * Remove offset-elastic option.
    
    Bug: T112230
    Bug: T108291
    Change-Id: Icb18cf15b1810242749a57ca002f4a88449ded25

---
+++ includes/Formatter/HistoryQuery.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Flow\Formatter;
+
+use Flow\FlowActions;
+use Flow\Data\ManagerGroup;
+use Flow\Model\AbstractRevision;
+use Flow\Model\UUID;
+use Flow\Repository\TreeRepository;
+
+abstract class HistoryQuery extends AbstractQuery {
+	// This requests extra to take into account that we will filter some out,
+	// to try to reduce the number of rounds (preferably to 1).
+	// If you raise this, also increase history_index_limit and bump the
+	// key of the indexes using history_index_limit
+	//
+	// This magic number is based on new-post/new-topic being about 26% of post revisions.
+	// (queried from production), since that is the only thing currently excluded.
+	const POST_OVERFETCH_FACTOR = 1.36;
+
+	/**
+	 * @var FlowActions
+	 */
+	protected $actions;
+
+	/**
+	 * @param ManagerGroup $storage
+	 * @param TreeRepository $treeRepo
+	 * @param FlowActions $actions
+	 */
+	public function __construct(
+		ManagerGroup $storage,
+		TreeRepository $treeRepo,
+		FlowActions $actions )
+	{
+		parent::__construct( $storage, $treeRepo );
+		$this->actions = $actions;
+	}
+
+	/**
+	 * @param AbstractRevision $revision
+	 * @return bool
+	 */
+	protected function includeInHistory( AbstractRevision $revision ) {
+		return !$this->actions->getValue( $revision->getChangeType(), 'exclude_from_history' );
+	}
+
+	/**
+	 * Gets query options that are common to all history queries
+	 *
+	 * @param string $direction 'fwd' or 'rev'.  'fwd' means to get items older than
+	 *  the offset.  'rev' means to get items newer.  Either way, an individual page is
+	 *  eventually returned and displayed in descending order.
+	 * @param int $limit Maximum number of items
+	 * @param UUID $offset UUID to use as offset (optional)
+	 */
+	protected function getOptions( $direction, $limit, UUID $offset = null) {
+		return array(
+			'sort' => 'rev_id',
+			'order' => $direction === 'fwd' ? 'DESC' : 'ASC',
+			'limit' => $limit,
+			'offset-id' => $offset,
+			'offset-dir' => $direction,
+			'offset-include' => false,
+		);
+	}
+
+	/**
+	 * Internally re-query as needed to handle items excluded from history
+	 *
+	 * Re-queries until there are no more entries or after filtering, there are the
+	 * desired number of results.
+	 *
+	 * This respects the given order (ASC or DESC), but the reversing for 'rev' is in
+	 * getResults.
+	 *
+	 * @param string $storageClass Storage class ID
+	 * @param array $attributes Query attriutes
+	 * @param array $options Query options, including offset-id and limit
+	 * @param int $overfetchFactor Factor to overfetch by to anticipate excludes
+	 * @return array() Array of history rows
+	 */
+	protected function doInternalQueries( $storageClass, $attributes, $options, $overfetchFactor ) {
+		$result = array();
+
+		$limit = $options['limit'];
+		$internalOffset = $options['offset-id'];
+
+		do {
+			$remainingNeeded = $limit - count( $result );
+
+			// The special cases here are to try reduce dribbling out of final requests (50, 25, 10, 5...).
+			if ( $remainingNeeded < 50 ) {
+				$overfetchFactor = 2;
+			}
+
+			$beforeFilteringCountWanted = max( 10, intval( $overfetchFactor * $remainingNeeded ) );
+
+			// Over-fetch by 1 item so we can figure out when to stop re-querying.
+			$options['limit'] = $beforeFilteringCountWanted + 1;
+
+			$options['offset-id'] = $internalOffset;
+
+			$resultBeforeFiltering = $this->storage->find( $storageClass, $attributes, $options );
+
+			// We over-fetched, now get rid of redundant value for our "real" data
+			$internalOverfetched = null;
+			if ( count( $resultBeforeFiltering ) > $beforeFilteringCountWanted ) {
+				$internalOverfetched = array_pop( $resultBeforeFiltering );
+			}
+
+			$resultAfterFiltering = array_filter( $resultBeforeFiltering, array( $this, 'includeInHistory' ) );
+
+			$internalOffset = end( $resultBeforeFiltering )->getRevisionId();
+			$trimmedResultAfterFiltering = array_slice( $resultAfterFiltering, 0, $remainingNeeded );
+			$result = array_merge( $result, $trimmedResultAfterFiltering );
+		} while( count( $result ) < $limit && $internalOverfetched !== null );
+
+		return $result;
+	}
+
+}
