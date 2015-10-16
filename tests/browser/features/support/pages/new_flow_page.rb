8b47fdd594ddb5d0cf177e1e9c06c187615834a5
Stephane Bisson <sbisson@wikimedia.org>
2015-10-16 13:24:44 -0400

    Browser tests: make NewFlowPage always go to a new page
    Change-Id: If4995f02229b0a2ada012e703c2e799575b543f1

--- tests/browser/features/support/pages/new_flow_page.rb
+++ tests/browser/features/support/pages/new_flow_page.rb
@@ -2,5 +2,5 @@
 class NewFlowPage
   include PageObject
   # MEDIAWIKI_URL must have Flow_test_talk in $wgFlowOccupyNamespaces.
-  page_url './Flow_test_talk:New_page_' + Random.srand.to_s
+  page_url "./Flow_test_talk:New_page_<%= Random.srand.to_s %>"
 end
