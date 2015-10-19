wikimedia/mediawiki-extensions-Flow

commit 6716b382c5e16b88794aaa045d09c0770b9c034a

Move Flow DM population to the bottom
The dm should populate after the navigation widget (and all other
ui widgets) are constructed, so they respond to the 'add' event
on the board.

Bug: T115265
Change-Id: I23252ef30d079ba5361702a34b8d93af882fcde0
(cherry picked from commit 61488455fc42bed3f81812d06a9dcf6eec35b98c)

modules/flow-initialize.js
@@ -99,21 +99,6 @@
 		// We shouldn't have to worry about 'remove', since by the time we have filtering,
 		// orderedTopicIds should be gone.
 
-		// Fall back to mw.flow.data, which was used until September 2015
-		dataBlob = mw.config.get( 'wgFlowData' ) || ( mw.flow && mw.flow.data );
-		if ( dataBlob && dataBlob.blocks ) {
-			// Populate the rendered topics or topic (if we are in a single-topic view)
-			mw.flow.system.populateBoardTopicsFromJson( dataBlob.blocks.topiclist || dataBlob.blocks.topic );
-			// Populate header
-			mw.flow.system.populateBoardDescriptionFromJson( dataBlob.blocks.header || {} );
-			// Populate the ToC topics
-			if ( dataBlob.toc ) {
-				mw.flow.system.populateBoardTopicsFromJson( dataBlob.toc );
-			}
-		} else {
-			mw.flow.system.populateBoardFromApi();
-		}
-
 		// Initialize the old system to accept the default
 		// 'newest' order for the topic order widget
 		// Get the current default sort
@@ -429,6 +414,23 @@
 			// Prevent default
 			return false;
 		} );
+
+		// Fall back to mw.flow.data, which was used until September 2015
+		// NOTICE: This block must be after the initialization of the ui widgets so
+		// they can populate themselves according to the events.
+		dataBlob = mw.config.get( 'wgFlowData' ) || ( mw.flow && mw.flow.data );
+		if ( dataBlob && dataBlob.blocks ) {
+			// Populate the rendered topics or topic (if we are in a single-topic view)
+			mw.flow.system.populateBoardTopicsFromJson( dataBlob.blocks.topiclist || dataBlob.blocks.topic );
+			// Populate header
+			mw.flow.system.populateBoardDescriptionFromJson( dataBlob.blocks.header || {} );
+			// Populate the ToC topics
+			if ( dataBlob.toc ) {
+				mw.flow.system.populateBoardTopicsFromJson( dataBlob.toc );
+			}
+		} else {
+			mw.flow.system.populateBoardFromApi();
+		}
 
 		preloadTopic = OO.getProp( dataBlob, 'blocks', 'topiclist', 'submitted', 'topic' );
 		preloadContent = OO.getProp( dataBlob, 'blocks', 'topiclist', 'submitted', 'content' );

commit e6c4c51227b93857ba056c14b267e594beba23dd

Trivial jscs fix
Previous change 6716b382 introduced a jscs failure (too many blank
lines).  That causes 'npm test' to break subsequent proposed changes.

Remove the duplicate blank line in modules/flow-initialize.js to please
jscs.

Change-Id: I599f554fe2d295124934645d73dea84abb884373

modules/flow-initialize.js
@@ -110,7 +110,6 @@
 		} );
 		$( '.flow-board-navigation' ).append( navWidget.$element );
 
-
 		// HACK: These event handlers should be in the prospective widgets
 		// they will move once we have Board UI and Topic UI widgets
 		mw.flow.system.on( 'resetBoardStart', function () {