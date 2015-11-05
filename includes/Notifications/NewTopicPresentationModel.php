1eebc4e3a7f4c6af3ab198bf55d9dbaad00e5dfe
Stephane Bisson <sbisson@wikimedia.org>
2015-11-04 19:33:32 -0500

    Presentation model for new-topic notification
    Bug: T116845
    Change-Id: I574a3514dc7f911e3e3541cd69d93e41b28dba38

---
+++ includes/Notifications/NewTopicPresentationModel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Flow;
+
+use EchoEventPresentationModel;
+
+class NewTopicPresentationModel extends EchoEventPresentationModel {
+
+	public function canRender() {
+		return (bool)$this->event->getTitle();
+	}
+
+	public function getIconType()
+	{
+		return 'flow-discussion';
+	}
+
+	public function getPrimaryLink()
+	{
+		/** @var UrlGenerator $urlGenerator */
+		$urlGenerator = Container::get( 'url_generator' );
+		$url = $urlGenerator->boardLink( $this->event->getTitle(), 'newest' )->getFullURL();
+		$msg = $this->msg( 'flow-notification-link-text-view-topic' );
+		return array( $url, $msg );
+	}
+
+	public function getHeaderMessage() {
+		$msg = parent::getHeaderMessage();
+		$msg->params( $this->event->getTitle() );
+		$msg->params( $this->event->getExtraParam( 'topic-title' ) );
+		return $msg;
+	}
+
+	public function getBodyMessage() {
+		$msg = $this->msg( 'notification-body-flow-new-topic' );
+		list( $formattedName, $genderName ) = $this->getAgentForOutput();
+		$msg->params( $formattedName, $genderName );
+		$msg->params( $this->event->getTitle() );
+		$msg->params( $this->event->getExtraParam( 'topic-title' ) );
+		return $msg;
+	}
+
+}
