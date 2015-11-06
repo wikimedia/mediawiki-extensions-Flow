916ebaf10aeb299f3cfc22753a6bc27878dd3313
Stephane Bisson <sbisson@wikimedia.org>
2015-11-06 10:23:44 -0500

    Flow notification presentation models
    * new-topic
    * post-edited
    * post-reply
    * topic-renamed (also fixed old-subject)
    * mention
    * flow-enabled-on-talkpage
    
    Bug: T116845
    Depends-On: I74d5ad244dec337a94e59c2118c33e474cd76797
    Change-Id: Ic64688253d0a96d5160bf10c1cfc1269e4df3857

---
+++ includes/Notifications/PostEditedPresentationModel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Flow;
+
+class PostEditedPresentationModel extends FlowPresentationModel {
+
+	public function canRender() {
+		return $this->hasTitle()
+			&& $this->hasValidTopicWorkflowId()
+			&& $this->hasValidPostId();
+	}
+
+	public function getPrimaryLink() {
+		return array(
+			$this->getPostLinkUrl(),
+			$this->msg( 'flow-notification-link-text-view-post' )->text()
+		);
+	}
+
+	public function getHeaderMessage() {
+		if ( $this->isBundled() ) {
+			$msg = $this->getMessageWithAgent( "notification-bundle-header-{$this->type}" );
+			list( $formattedCount, $countForPlural ) = $this->getNotificationCountForOutput( false );
+			$msg->params( $this->event->getExtraParam( 'topic-title' ) );
+			$msg->params( $this->event->getTitle()->getPrefixedText() );
+			$msg->params( $formattedCount );
+			$msg->params( $countForPlural );
+			return $msg;
+		} else {
+			$msg = parent::getHeaderMessage();
+			$msg->params( $this->event->getTitle()->getPrefixedText() );
+			$msg->params( $this->event->getExtraParam( 'topic-title' ) );
+			return $msg;
+		}
+	}
+
+	public function getBodyMessage() {
+		if ( $this->isBundled() ) {
+			return false;
+		} else {
+			$msg = $this->getMessageWithAgent( "notification-body-{$this->type}" );
+			$msg->params( $this->event->getTitle()->getPrefixedText() );
+			$msg->params( $this->event->getExtraParam( 'topic-title' ) );
+			return $msg;
+		}
+	}
+
+}
