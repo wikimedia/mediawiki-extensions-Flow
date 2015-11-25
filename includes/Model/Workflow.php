efad16982031d6cc1012b25b06fd934ea0a10b9a
Matthias Mullie <git@mullie.eu>
2015-11-25 12:21:20 +0100

    Keep known results in memory
    Flow currently executes an enormous amount of (very simple) queries.
    A lot of those come from permission checks (other refactoring are
    necessary at some point, as RevDel is implemented) that are executed
    against the same sets of data more than once.
    
    isAllowed issues queries for:
    * fetching the root post of a revision
    * fetching the workflow/board of a revision
    * fetching the most recent revision of said revision
    We currently need all of these to properly check permissions, but we
    only need to reach out for that data once per revision. Additional
    permission checks for the same object shouldn't have to reach out to
    DB. RevisionFormatter::buildActions loops over a bunch of actions to
    test which can be included in the response. Since we don't keep the
    results around, every revision we need to output fetches these
    things multiple times.
    
    Additionally, the Workflow::isDeleted check (also prompted by
    permissions check) is not cheap: it does a DB lookup every time. We
    need it to do the DB lookup the first time, but we don't need it to
    keep doing that if the result was already known.
    
    This patch dropped the amount of issued queries for a regular board
    views drastically on my machine. By a factor of 3-4.
    
    Bug: T114550
    Change-Id: Ibb20c608d69917f5f991b165f6d847c369cd1a5b

--- includes/Model/Workflow.php
+++ includes/Model/Workflow.php
@@ -73,6 +73,11 @@ class Workflow {
 	protected $ownerTitle;
 
 	/**
+	 * @var bool|null Indicates if associated page_id exists (null if not yet looked up)
+	 */
+	protected $exists;
+
+	/**
 	 * @param array $row
 	 * @param Workflow|null $obj
 	 * @return Workflow
@@ -274,9 +279,13 @@ class Workflow {
 	 * @return bool
 	 */
 	public function isDeleted() {
+		if ( $this->exists === null ) {
+			$this->exists = Title::newFromID( $this->pageId ) !== null;
+		}
+
 		// a board that does not yet exist (because workflow has not yet
 		// been stored) is not deleted, it just doesn't exist yet
-		return !$this->isNew() && Title::newFromID( $this->pageId ) === null;
+		return !$this->isNew() && !$this->exists;
 	}
 
 	/**
