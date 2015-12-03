wikimedia/mediawiki-extensions-Flow

commit 1ac5146c636b88ee1a70115194d55e681ecf4822

Add support for edit summary formatting in topic titles
This adds support for basic links in topic titles using the edit summary
parser.  To do so, it stops using the 'plaintext' format for topic
titles, and adds topic-title-wikitext' and 'topic-title-html'.  There
is also logic to Do What I Mean in limited cases.  For example, if you
request a topic list in 'fixed-html', it will understand that the topic
titles should be 'topic-title-html'.

* getContentInWikitext() and getContentInHtml helpers for when you need
  some type of wikitext/HTML and don't care whether it's the topic
  title subset or the full version.

* Rename Flow\Parsoid\Utils since it handles more than Parsoid (even
  before it did in some cases).

* Rename PostRevision::create to PostRevision::createTopicPost to make
  clear it's specifically for the root post of a topic.

* Show text version of the HTML (i.e. the displayed text of the links,
  no brackets) in some cases per spec.

* Simplify description PHP widget by removing content format.

* Explicitly do not track references in topic titles by short-circuiting.
Bug: T59153
Change-Id: I8d6bd8090de242c2b1761003cce04d590d7f9ae6

includes/Conversion/Utils.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Flow\Parsoid;
+namespace Flow\Conversion;
 
 use DOMDocument;
 use DOMNode;
@@ -11,6 +11,7 @@ use Flow\Exception\InvalidDataException;
 use Flow\Exception\NoParserException;
 use Flow\Exception\WikitextException;
 use Language;
+use Linker;
 use MultiHttpClient;
 use OutputPage;
 use RequestContext;
@@ -20,10 +21,12 @@ use VirtualRESTServiceClient;
 
 abstract class Utils {
 	/**
-	 * Convert from/to wikitext/html.
+	 * Convert from/to wikitext <=> html or topic-title-wikitext => topic-title-html.
+	 * Only these pairs are supported.  html => wikitext requires Parsoid, and
+	 * topic-title-html => topic-title-wikitext is not supported.
 	 *
-	 * @param string $from Format of content to convert: html|wikitext
-	 * @param string $to Format to convert to: html|wikitext
+	 * @param string $from Format of content to convert: html|wikitext|topic-title-wikitext
+	 * @param string $to Format to convert to: html|wikitext|topic-title-html
 	 * @param string $content
 	 * @param Title $title
 	 * @return string
@@ -35,10 +38,22 @@ abstract class Utils {
 			return $content;
 		}
 
-		if ( self::isParsoidConfigured() ) {
-			return self::parsoid( $from, $to, $content, $title );
+		if ( $from === 'wt' ){
+			$from = 'wikitext';
+		}
+
+		if ( $from === 'wikitext' || $from === 'html' ) {
+			if ( $to === 'wikitext' || $to === 'html' ) {
+				if ( self::isParsoidConfigured() ) {
+					return self::parsoid( $from, $to, $content, $title );
+				} else {
+					return self::parser( $from, $to, $content, $title );
+				}
+			} else {
+				throw new WikitextException( "Conversion from '$from' to '$to' was requested, but this is not supported." );
+			}
 		} else {
-			return self::parser( $from, $to, $content, $title );
+			return self::commentParser( $from, $to, $content, $title );
 		}
 	}
 
@@ -81,11 +96,7 @@ abstract class Utils {
 	protected static function parsoid( $from, $to, $content, Title $title ) {
 		$serviceClient = self::getServiceClient();
 
-		if ( $from == 'html' ) {
-			$from = 'html';
-		} elseif ( in_array( $from, array( 'wt', 'wikitext' ) ) ) {
-			$from = 'wikitext';
-		} else {
+		if ( $from !== 'html' && $from !== 'wikitext' ) {
 			throw new WikitextException( 'Unknown source format: ' . $from, 'process-wikitext' );
 		}
 
@@ -127,6 +138,22 @@ abstract class Utils {
 	}
 
 	/**
+	 * Convert from/to topic-title-wikitext/topic-title-html using Linker::formatLinksInComment
+	 *
+	 * @param string $from Format of content to convert: topic-title-wikitext
+	 * @param string $to Format of content to convert to: topic-title-html
+	 * @param string $content Content to convert, in topic-title-wikitext format.
+	 * @return string $content in HTML
+	 */
+	protected static function commentParser( $from, $to, $content ) {
+		if ( $from !== 'topic-title-wikitext' || $to !== 'topic-title-html' ) {
+			throw new WikitextException( "Conversion from '$from' to '$to' was requested, but this is not supported." );
+		}
+
+		return Linker::formatLinksInComment( $content );
+	}
+
+	/**
 	 * Convert from/to wikitext/html using Parser.
 	 *
 	 * This only supports wikitext to HTML.
@@ -139,7 +166,7 @@ abstract class Utils {
 	 * @throws WikitextException When the conversion is unsupported
 	 */
 	protected static function parser( $from, $to, $content, Title $title ) {
-		if ( $from !== 'wikitext' && $to !== 'html' ) {
+		if ( $from !== 'wikitext' || $to !== 'html' ) {
 			throw new WikitextException( "Conversion from '$from' to '$to' was requested, but core's Parser only supports 'wikitext' to 'html' conversion", 'process-wikitext' );
 		}
 

commit d7a4c4f17bc937c27ec980738cc9173233c7a545

[SECURITY] Escape HTML characters in topic titles
Linker::formatComment() does this too. Presumably that function
wasn't used directly because formatAutoComments() is undesired,
but we should still call escapeHtmlAllowEntities().

This fixes in XSS vulnerability in topic titles, which was
a regression from 1ac5146c63.

Bug: T120921
Change-Id: I3e0e6f21cf97851fcf929fc40cd036c297d03f59

includes/Conversion/Utils.php
@@ -15,6 +15,7 @@ use Linker;
 use MultiHttpClient;
 use OutputPage;
 use RequestContext;
+use Sanitizer;
 use Title;
 use User;
 use VirtualRESTServiceClient;
@@ -150,7 +151,7 @@ abstract class Utils {
 			throw new WikitextException( "Conversion from '$from' to '$to' was requested, but this is not supported." );
 		}
 
-		return Linker::formatLinksInComment( $content );
+		return Linker::formatLinksInComment( Sanitizer::escapeHtmlAllowEntities( $content ) );
 	}
 
 	/**