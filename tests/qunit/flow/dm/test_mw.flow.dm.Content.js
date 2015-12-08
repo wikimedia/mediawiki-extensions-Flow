b9696f5438e9028e7396034f5cbc435e25f38957
Stephane Bisson <sbisson@wikimedia.org>
2015-12-08 10:13:26 -0500

    Show topic title in plaintext in ToC
    Make topic title available in plaintext so it
    can be shown in ToC without any links
    or markup.
    
    Introduced mw.flow.dm.Content.js to handle
    multiple representations of content.
    
    Bug: T119537
    Change-Id: Ib8c7d9428aba3d45d27efb4bf6a8497f2ce42e82

---
+++ tests/qunit/flow/dm/test_mw.flow.dm.Content.js
@@ -0,0 +1,30 @@
+QUnit.module( 'ext.flow.dm mw.flow.dm.Content' );
+
+/* Tests */
+
+QUnit.test( 'Stores different content representations (formats)', function ( assert ) {
+	var contentFromServer = {
+		content: 'content in default format (wikitext, for instance)',
+		format: 'wikitext',
+		html: 'content in html format',
+		plaintext: 'content in plaintext format'
+	};
+
+	var content = new mw.flow.dm.Content( contentFromServer );
+
+	assert.equal( content.get( 'html' ), 'content in html format' );
+	assert.equal( content.get( 'wikitext' ), 'content in default format (wikitext, for instance)' );
+	assert.equal( content.get(), 'content in default format (wikitext, for instance)' );
+	assert.equal( content.get( 'unknown format' ), null );
+
+	QUnit.expect( 4 );
+} );
+
+QUnit.test( 'Behaves when empty', function ( assert ) {
+	var content = new mw.flow.dm.Content();
+
+	assert.equal( content.get(), null );
+	assert.equal( content.get( 'whatever format' ), null );
+
+	QUnit.expect( 2 );
+} );
