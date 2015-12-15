a39c9a84143e2973836fd29adbc14e458e0933de
Matthias Mullie <git@mullie.eu>
2015-12-15 13:22:55 +0100

    Make patch-reference_wiki*.sql work with prefixed tables
    Bug: T121486
    Change-Id: I88a73e2a77b6a3feece45222ef85de045938126c

--- db_patches/patch-reference_wiki.sql
+++ db_patches/patch-reference_wiki.sql
@@ -5,14 +5,14 @@
 ALTER TABLE /*_*/flow_wiki_ref ADD COLUMN ref_src_wiki varchar(16) binary not null;
 
 -- Drop indexes for adjustment
-DROP INDEX /*i*/flow_wiki_ref_idx ON /*_*/flow_wiki_ref;
-DROP INDEX /*i*/flow_wiki_ref_revision ON /*_*/flow_wiki_ref;
+DROP INDEX IF EXISTS /*i*/flow_wiki_ref_idx ON /*_*/flow_wiki_ref;
+DROP INDEX IF EXISTS /*i*/flow_wiki_ref_revision ON /*_*/flow_wiki_ref;
 
 -- Populate wiki references with the appropriate wiki
 UPDATE /*_*/flow_wiki_ref, /*_*/flow_workflow
-	SET flow_wiki_ref.ref_src_wiki = flow_workflow.workflow_wiki
+	SET ref_src_wiki = workflow_wiki
 	WHERE
-		flow_wiki_ref.ref_src_workflow_id = flow_workflow.workflow_id;
+		ref_src_workflow_id = workflow_id;
 
 -- Recreate new indexes
 CREATE INDEX /*i*/flow_wiki_ref_idx_v2 ON /*_*/flow_wiki_ref
@@ -25,14 +25,14 @@ CREATE INDEX /*i*/flow_wiki_ref_revision_v2 ON /*_*/flow_wiki_ref
 ALTER TABLE /*_*/flow_ext_ref ADD COLUMN ref_src_wiki varchar(16) binary not null;
 
 -- Drop indexes
-DROP INDEX /*i*/flow_ext_ref_idx ON /*_*/flow_ext_ref;
-DROP INDEX /*i*/flow_ext_ref_revision ON /*_*/flow_ext_ref;
+DROP INDEX IF EXISTS /*i*/flow_ext_ref_idx ON /*_*/flow_ext_ref;
+DROP INDEX IF EXISTS /*i*/flow_ext_ref_revision ON /*_*/flow_ext_ref;
 
 -- Populate external references with the appropriate wiki
 UPDATE /*_*/flow_ext_ref, /*_*/flow_workflow
-	SET flow_ext_ref.ref_src_wiki = flow_workflow.workflow_wiki
+	SET ref_src_wiki = workflow_wiki
 	WHERE
-		flow_ext_ref.ref_src_workflow_id = flow_workflow.workflow_id;
+		ref_src_workflow_id = workflow_id;
 
 -- Recreate new indexes
 CREATE INDEX /*i*/flow_ext_ref_idx_v2 ON /*_*/flow_ext_ref
