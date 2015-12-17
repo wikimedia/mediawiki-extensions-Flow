326c044233e43fb882f248f5ea4346f5cffcf675
Stephane Bisson <sbisson@wikimedia.org>
2015-12-17 13:39:36 -0500

    [WIP] Various fixes to edit-title
    * Introduced TopicTitleWidget for editing
      - standardize cancel button
      - add anon warning
      - update browser tests
    
    * Fixed no-js editing
    
    Bug: T100851
    Bug: T118427
    Bug: T104252
    Change-Id: I24eafe0a2dbdad7a133d81c089deae0d0feb8589

---
+++ modules/flow/ui/widgets/mw.flow.ui.TopicTitleWidget.js
@@ -0,0 +1,126 @@
+( function ( $ ) {
+	/**
+	 * Topic title widget
+	 *
+	 * @class
+	 * @extends OO.ui.Widget
+	 *
+	 * @constructor
+	 * @param {string} topicId
+	 * @param {Object} [config]
+	 */
+	mw.flow.ui.TopicTitleWidget = function mwFlowUiTopicTitleWidget( topicId, config ) {
+		mw.flow.ui.TopicTitleWidget.parent.call( this, config );
+
+		this.topicId = topicId;
+		this.api = new mw.flow.dm.APIHandler(
+			'Topic:' + topicId
+		);
+
+		this.anonWarning = new mw.flow.ui.AnonWarningWidget();
+		this.anonWarning.toggle( true );
+
+		this.error = new OO.ui.LabelWidget( {
+			classes: [ 'flow-ui-topicTitleWidget-error flow-errors errorbox' ]
+		} );
+		this.error.toggle( false );
+
+		this.captcha = new mw.flow.dm.Captcha();
+		this.captchaWidget = new mw.flow.ui.CaptchaWidget( this.captcha );
+
+		this.input = new OO.ui.TextInputWidget( {
+			classes: [ 'flow-ui-topicTitleWidget-titleInput' ],
+			autofocus: true
+		} );
+
+		this.termsLabel = new OO.ui.LabelWidget( {
+			classes: [ 'flow-ui-topicTitleWidget-termsLabel' ],
+			label: $( $.parseHTML( mw.message( 'flow-terms-of-use-edit' ).parse() ) )
+		} );
+
+		this.controls = $( '<div>' ).addClass( 'flow-ui-topicTitleWidget-controls' );
+		this.buttons = $( '<div>' ).addClass( 'flow-ui-topicTitleWidget-buttons' );
+		this.saveButton = new OO.ui.ButtonWidget( {
+			flags: [ 'primary', 'constructive' ],
+			label: mw.msg( 'flow-edit-title-submit' ),
+			classes: [ 'flow-ui-topicTitleWidget-saveButton' ]
+		} );
+
+		this.cancelButton = new OO.ui.ButtonWidget( {
+			flags: 'destructive',
+			framed: false,
+			label: mw.msg( 'flow-cancel' ),
+			classes: [ 'flow-ui-topicTitleWidget-cancelButton' ]
+		} );
+		this.buttons.append(
+			this.cancelButton.$element,
+			this.saveButton.$element
+		);
+		this.controls.append(
+			this.termsLabel.$element,
+			this.buttons,
+			$( '<div>' ).css( 'clear', 'both' )
+		);
+
+		// Events
+		this.saveButton.connect( this, { click: [ 'onSaveButtonClick' ] } );
+		this.cancelButton.connect( this, { click: [ 'emit', 'cancel' ] } );
+
+		this.$element
+			.addClass( 'flow-ui-topicTitleWidget' )
+			.append(
+				this.anonWarning.$element,
+				this.error.$element,
+				this.captchaWidget.$element,
+				this.input.$element,
+				this.controls
+			);
+
+		this.input.pushPending();
+		var widget = this;
+		this.api.getPost( topicId, topicId, 'wikitext' ).then(
+			function ( topic ) {
+				var content = OO.getProp( topic, 'content', 'content' ),
+					currentRevisionId = topic.revisionId;
+
+				widget.api.setCurrentRevision( currentRevisionId );
+				widget.input.setValue( content );
+			},
+			function ( error ) {
+				widget.error.setLabel( mw.msg( 'flow-error-external', error ) );
+				widget.error.toggle( true );
+			}
+		).always(
+			function () {
+				widget.input.popPending();
+			}
+		);
+
+	};
+
+	/* Initialization */
+
+	OO.inheritClass( mw.flow.ui.TopicTitleWidget, OO.ui.Widget );
+
+	/* Methods */
+
+	mw.flow.ui.TopicTitleWidget.prototype.onSaveButtonClick = function () {
+		var content = this.input.getValue(),
+			captcha = this.captchaWidget.getResponse(),
+			widget = this;
+		this.api.saveTopicTitle( this.topicId, content, captcha ).then(
+			function ( workflowId ) {
+				widget.emit( 'saveContent', workflowId );
+			},
+			function ( errorCode, errorObj ) {
+				widget.captcha.update( errorCode, errorObj );
+				if ( !widget.captcha.isRequired() ) {
+					widget.error.setLabel( errorObj.error && errorObj.error.info || errorObj.exception );
+					widget.error.toggle( true );
+				}
+			}
+		);
+	};
+
+
+}( jQuery ) );
