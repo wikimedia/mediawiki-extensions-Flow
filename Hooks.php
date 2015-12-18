f96a5fe7e5759988190b71566c040bb0efca7880
Catrope <roan.kattouw@gmail.com>
2015-12-18 00:50:21 +0000

    Revert "Fix a database connection that was not cluster-aware in Nuke hook"
    In order to revert 0c70f5291a82838ae4b0f53148a046fe884d955e
    
    This reverts commit b85fcf1081d222d1e9df0edf4a539c6b4ed4cc03.
    
    Change-Id: I6c55aff4a714d5904263c302d917cdcb8120808e

--- Hooks.php
+++ Hooks.php
@@ -1856,8 +1856,7 @@ class FlowHooks {
 		$newLimit = $limit - count( $pages );
 		if ( $newLimit > 0 ) {
 
-			$dbFactory = Container::get( 'db.factory' );
-			$dbr = $dbFactory->getDB( DB_SLAVE );
+			$dbr = wfGetDB( DB_SLAVE );
 
 			$userSelect = array( 'username' => "IFNULL(tree_orig_user_ip, user_name)" );
 			$userWhere = array();
