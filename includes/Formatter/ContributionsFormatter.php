f60f8c2ca8292641588c7991789c2bbd45dc2b44
IoannisKydonis <gianiskido@gmail.com>
2016-01-09 23:52:35 +0200

    Don't hardcode English parentheses in one message
    Bug: T115175
    Change-Id: Ib6e675705ac6c8236461812ff967620444ca05ca

--- includes/Formatter/ContributionsFormatter.php
+++ includes/Formatter/ContributionsFormatter.php
@@ -89,8 +89,7 @@ class ContributionsFormatter extends AbstractFormatter {
 
 		/** @var Anchor $anchor */
 		$anchor = $data['actions'][$key];
-
-		return ' (' . Html::rawElement(
+		$message = ' ' . wfMessage( 'parentheses' )->rawParams( Html::rawElement(
 			'a',
 			array(
 				'href' => $anchor->getFullURL(),
@@ -100,6 +99,8 @@ class ContributionsFormatter extends AbstractFormatter {
 				'class' => 'flow-history-moderation-action flow-click-interactive',
 			),
 			$ctx->msg( $msg )->escaped()
-		) . ')';
+		) )->escaped() . ' ';
+		
+		return $message;
 	}
 }
