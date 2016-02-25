8dee5df1efba82768e918652a41e1ae8a82d3f69
Matthias Mullie <git@mullie.eu>
2016-02-25 15:24:25 +0100

    Add notifications for summary changes
    Bug: T125119
    Change-Id: I627bd79abeb453491039292cf57d6b51451c659f

---
+++ includes/Notifications/SummaryEditedPresentationModel.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Flow;
+
+use Title;
+
+class SummaryEditedPresentationModel extends FlowPresentationModel {
+	public function getIconType() {
+		return 'flow-topic-renamed';
+	}
+
+	public function canRender() {
+		return $this->hasTitle()
+			&& $this->hasValidTopicWorkflowId()
+			&& $this->event->getExtraParam( 'revision-id' ) !== null;
+	}
+
+	public function getPrimaryLink() {
+		return $this->getViewTopicLink();
+	}
+
+	public function getSecondaryLinks() {
+		if ( $this->isBundled() ) {
+			return array( $this->getBoardLink() );
+		} else {
+			$links = array( $this->getAgentLink() );
+			if ( $this->isUserTalkPage() && $this->event->getExtraParam( 'prev-revision-id' ) !== null ) {
+				$links[] = $this->getDiffLink();
+			} else {
+				$links[] = $this->getBoardLink();
+			}
+			return $links;
+		}
+	}
+
+	protected function getHeaderMessageKey() {
+		if ( $this->isBundled() ) {
+			$key = "notification-bundle-header-{$this->type}";
+		} elseif ( $this->event->getExtraParam( 'prev-revision-id' ) === null ) {
+			$key = parent::getHeaderMessageKey() . '-first';
+		} else {
+			$key = parent::getHeaderMessageKey();
+		}
+
+		if ( $this->isUserTalkPage() ) {
+			$key .= '-user-talk';
+		}
+
+		return $key;
+	}
+
+	public function getHeaderMessage() {
+		$msg = $this->msg( $this->getHeaderMessageKey() );
+		$msg->plaintextParams( $this->getTopicTitle() );
+		$msg->params( $this->getViewingUserForGender() );
+		return $msg;
+	}
+
+	public function getBodyMessage() {
+		$key = "notification-body-{$this->type}";
+		if ( $this->isUserTalkPage() ) {
+			$key .= '-user-talk';
+		}
+
+		return $this->msg( $key )->params( $this->getContentSnippet() );
+	}
+
+	protected function getDiffLink() {
+		/** @var UrlGenerator $urlGenerator */
+		$urlGenerator = Container::get( 'url_generator' );
+		$anchor = $urlGenerator->diffSummaryLink(
+			Title::newFromText( $this->event->getExtraParam( 'topic-workflow' )->getAlphadecimal(), NS_TOPIC ),
+			$this->event->getExtraParam( 'topic-workflow' ),
+			$this->event->getExtraParam( 'revision-id' )
+		);
+
+		return array(
+			'url' => $anchor->getFullURL(),
+			'label' => $this->msg( 'notification-link-text-view-changes' )->params( $this->getViewingUserForGender() )->text(),
+			'description' => '',
+			'icon' => 'changes',
+			'prioritized' => true,
+		);
+	}
+}
