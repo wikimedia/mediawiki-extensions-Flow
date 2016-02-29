wikimedia/mediawiki-extensions-Flow

commit d10e4402e05a48ae012479a70536302773a0e5f0

Better error handling and hook use for moves
Stop using removed MoveAbort hooks.  Update how we do move checking
to new hooks.

Involves refactoring allowCreation to separate out different
forms of move errors (technical issues or user permissions).

Bug: T127693
Bug: T126389
Change-Id: I9727e19a761038e3eef40df9ae1d70d39bb82bea

Hooks.php
@@ -1248,54 +1248,60 @@ class FlowHooks {
 		return true;
 	}
 
+	/**
+	 * Checks whether this is a valid move technically.  MovePageIsValidMove should not
+	 * be affected by the specific user, or user permissions.
+	 *
+	 * Those are handled in onMovePageCheckPermissions, called later.
+	 *
+	 * @param Title $oldTitle Old title
+	 * @param Title $newTitle New title
+	 * @param Status $status Status to update with any technical issues
+	 *
+	 * @return true to continue, false to abort the hook
+	 */
 	public static function onMovePageIsValidMove( Title $oldTitle, Title $newTitle, Status $status ) {
-		global $wgUser;
-
-		// We only care about moving flow boards
+		// We only care about moving Flow boards, and *not* moving Flow topics
+		// (but both are CONTENT_MODEL_FLOW_BOARD)
 		if ( $oldTitle->getContentModel() !== CONTENT_MODEL_FLOW_BOARD ) {
 			return true;
 		}
 
-		// pages within the Topic namespace are not movable
+		// Pages within the Topic namespace are not movable
+		// This is also checked by NamespaceIsMovable.
 		if ( $oldTitle->getNamespace() === NS_TOPIC ) {
 			$status->fatal( 'flow-error-move-topic' );
 			return false;
 		}
 
-		// valid if the destination is already a valid flow location (=default flow-board model)
-		if ( ContentHandler::getDefaultModelFor( $newTitle ) === CONTENT_MODEL_FLOW_BOARD ) {
-			return true;
-		}
-
-		// valid if the user has permissions to create a new board wherever
 		$occupationController = self::getOccupationController();
-		$creationStatus = $occupationController->allowCreation( $newTitle, $wgUser );
-		if ( !$creationStatus->isGood() ) {
-			$status->fatal( 'flow-error-move-no-create-permissions' );
-			return false;
-		}
+		$flowStatus = $occupationController->checkIfCreationTechnicallyAllowed( $newTitle, /*mustNotExist*/ true );
+		$status->merge( $flowStatus );
 
 		return true;
 	}
 
 	/**
-	 * Moves of Flow topic pages (NS_TOPIC) are never permitted, and board moves
-	 * are not always permitted.
+	 * Checks whether user has permission to move the board.
 	 *
-	 * @param Title $oldTitle
-	 * @param Title $newTitle
-	 * @param User $user
-	 * @param string|null $error Null coming in; assign (textual) error message when failing
-	 * @param string $reason
-	 * @return bool
+	 * Technical restrictions are handled in onMovePageIsValidMove, called earlier.
+	 *
+	 * @param Title $oldTitle Old title
+	 * @param Title $newTitle New title
+	 * @param User $user User doing the move
+	 * @param string $reason Reason for the move
+	 * @param Status $status Status updated with any permissions issue
+	 *
+	 * @return true to continue, false to abort the hook
 	 */
-	public static function onAbortMove( $oldTitle, $newTitle, $user, &$error, $reason ) {
-		$status = new Status();
-		self::onMovePageIsValidMove( $oldTitle, $newTitle, $status );
-		if ( !$status->isOK() ) {
-			$error = $status->getHTML();
-			return false;
-		}
+	public static function onMovePageCheckPermissions( Title $oldTitle, Title $newTitle, User $user, $reason, Status $status ) {
+		$occupationController = self::getOccupationController();
+
+		$permissionStatus = $occupationController->checkIfUserHasPermission(
+			$newTitle,
+			$user
+		);
+		$status->merge( $permissionStatus );
 
 		return true;
 	}

Flow.php
@@ -139,8 +139,8 @@ $wgHooks['MessageCache::get'][] = 'FlowHooks::onMessageCacheGet';
 $wgHooks['WatchArticle'][] = 'FlowHooks::onWatchArticle';
 $wgHooks['UnwatchArticle'][] = 'FlowHooks::onWatchArticle';
 $wgHooks['CanonicalNamespaces'][] = 'FlowHooks::onCanonicalNamespaces';
+$wgHooks['MovePageCheckPermissions'][] = 'FlowHooks::onMovePageCheckPermissions';
 $wgHooks['MovePageIsValidMove'][] = 'FlowHooks::onMovePageIsValidMove';
-$wgHooks['AbortMove'][] = 'FlowHooks::onAbortMove';
 $wgHooks['TitleMoveStarting'][] = 'FlowHooks::onTitleMoveStarting';
 $wgHooks['TitleMoveCompleting'][] = 'FlowHooks::onTitleMoveCompleting';
 $wgHooks['TitleSquidURLs'][] = 'FlowHooks::onTitleSquidURLs';

container.php
@@ -1298,7 +1298,8 @@ $c['board_mover'] = function( $c ) {
 		$c['db.factory'],
 		$c['memcache.local_buffered'],
 		$c['storage'],
-		$c['occupation_controller']->getTalkpageManager()
+		$c['occupation_controller']->getTalkpageManager(),
+		$c['occupation_controller']
 	);
 };
 

includes/Api/ApiFlow.php
@@ -123,9 +123,9 @@ class ApiFlow extends ApiBase {
 			// Just check for permissions, nothing else to do. The Flow board
 			// will be put in place right before the rest of the data is stored
 			// (in SubmissionHandler::commit), after everything's been validated.
-			$status = $controller->allowCreation( $page, $this->getUser() );
+			$status = $controller->checkedAllowCreation( $page, $this->getUser() );
 			if ( !$status->isGood() ) {
-				$this->dieUsage( "Page provided does not have Flow enabled and allowCreation failed with: " . $status->getMessage()->parse(), 'invalid-page' );
+				$this->dieUsage( "Page provided does not have Flow enabled and checkedAllowCreation failed with: " . $status->getMessage()->parse(), 'invalid-page' );
 			}
 		}
 

includes/BoardMover.php
@@ -31,15 +31,21 @@ class BoardMover {
 	protected $nullEditUser;
 
 	/**
+	 * @var TalkpageManager
+	 */
+	protected $talkpageManager;
+
+	/**
 	 * @var DatabaseBase|null
 	 */
 	protected $dbw;
 
-	public function __construct( DbFactory $dbFactory, BufferedCache $cache, ManagerGroup $storage, User $nullEditUser ) {
+	public function __construct( DbFactory $dbFactory, BufferedCache $cache, ManagerGroup $storage, User $nullEditUser, TalkpageManager $talkpageManager ) {
 		$this->dbFactory = $dbFactory;
 		$this->cache = $cache;
 		$this->storage = $storage;
 		$this->nullEditUser = $nullEditUser;
+		$this->talkpageManager = $talkpageManager;
 	}
 
 	/**
@@ -58,6 +64,11 @@ class BoardMover {
 
 		// All reads must go through master to help ensure consistency
 		$this->dbFactory->forceMaster();
+
+		// This is only safe because we have called
+		// checkIfCreationTechnicallyAllowed and (usually) checkIfUserHasPermission.
+		// See FlowHooks and TalkpageManager.
+		$this->talkpageManager->forceAllowCreation( $newPage );
 
 		// Open a transaction, this will be closed from self::commit.
 		$this->dbw = $this->dbFactory->getDB( DB_MASTER );

includes/Dump/Importer.php
@@ -94,8 +94,15 @@ class Importer {
 		// create page if it does not yet exist
 		/** @var OccupationController $occupationController */
 		$occupationController = Container::get( 'occupation_controller' );
-		$occupationController->allowCreation( $title, $occupationController->getTalkpageManager() );
-		$occupationController->ensureFlowRevision( new \Article( $title ), $this->boardWorkflow );
+		$creationStatus = $occupationController->checkedAllowCreation( $title, $occupationController->getTalkpageManager() );
+		if ( !$creationStatus->isOK() ) {
+			throw new MWException( $creationStatus->getWikiText() );
+		}
+
+		$ensureStatus = $occupationController->ensureFlowRevision( new \Article( $title ), $this->boardWorkflow );
+		if ( !$ensureStatus->isOK() ) {
+			throw new MWException( $ensureStatus->getWikiText() );
+		}
 
 		$this->put( $this->boardWorkflow, array() );
 	}

includes/Import/Importer.php
@@ -543,13 +543,13 @@ class TalkpageImportOperation {
 		$state->logger->debug( 'Workflow isNew: ' . var_export( $isNew, true ) );
 		if ( $isNew ) {
 			// Explicitly allow creation of board
-			$allowCreationStatus = $this->occupationController->allowCreation(
+			$creationStatus = $this->occupationController->checkedAllowCreation(
 				$destinationTitle,
 				$this->occupationController->getTalkpageManager(),
 				/* $mustNotExist = */ true
 			);
-			if ( !$allowCreationStatus->isGood() ) {
-				throw new ImportException( "allowCreation failed to allow the import destination, with the following error:\n" . $allowCreationStatus->getWikiText() );
+			if ( !$creationStatus->isGood() ) {
+				throw new ImportException( "checkedAllowCreation failed to allow the import destination, with the following error:\n" . $creationStatus->getWikiText() );
 			}
 
 			// Makes sure the page exists and a Flow-specific revision has been inserted

includes/Import/OptInController.php
@@ -258,8 +258,8 @@ class OptInController {
 		$loaderFactory = Container::get( 'factory.loader.workflow' );
 		$page = $title->getPrefixedText();
 
-		$allowCreationStatus = $this->occupationController->allowCreation( $title, $this->user, false );
-		if ( !$allowCreationStatus->isGood() ) {
+		$creationStatus = $this->occupationController->checkedAllowCreation( $title, $this->user, false );
+		if ( !$creationStatus->isGood() ) {
 			$this->fatal( 'flow-special-enableflow-board-creation-not-allowed', $page );
 		}
 

commit bb918f5b4c426730d4111e64b2148d2eba101c9d

Fix regression requiring flow-create-board for non-Flow moves
Caused by d10e4402e05a48ae012479a70536302773a0e5f0

Change-Id: I3e41190f50086b6368e91f940acbe0d3fd1a5dd2

Hooks.php
@@ -1295,6 +1295,11 @@ class FlowHooks {
 	 * @return true to continue, false to abort the hook
 	 */
 	public static function onMovePageCheckPermissions( Title $oldTitle, Title $newTitle, User $user, $reason, Status $status ) {
+		// Only affect moves if the source has Flow content model
+		if ( $oldTitle->getContentModel() !== CONTENT_MODEL_FLOW_BOARD ) {
+			return true;
+		}
+
 		$occupationController = self::getOccupationController();
 
 		$permissionStatus = $occupationController->checkIfUserHasPermission(