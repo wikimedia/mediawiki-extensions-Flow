f968cb2336549db3bcdb68d2ee2f3027be555321
Stephane Bisson <sbisson@wikimedia.org>
2016-03-04 16:02:21 -0500

    [WIP] [POC] Moderate notifications
    Uses Echo moderation capabilities
    from Iacd098573efd92bb1e3fcd7da4cd40cea9522f15
    
    Bug: T93673
    Depends-On: Iacd098573efd92bb1e3fcd7da4cd40cea9522f15
    Change-Id: If76d60b35746dafb6aa4a31e64bc515ae35d7a0a

--- includes/Block/Topic.php
+++ includes/Block/Topic.php
@@ -22,6 +22,8 @@ use Flow\Model\UUID;
 use Flow\Model\Workflow;
 use Flow\Repository\RootPostLoader;
 use Message;
+use MWEchoNotifPage;
+use Title;
 
 class TopicBlock extends AbstractBlock {
 
@@ -450,6 +452,22 @@ class TopicBlock extends AbstractBlock {
 				$this->storage->put( $this->newRevision, $metadata );
 				$this->workflow->updateLastUpdated( $this->newRevision->getRevisionId() );
 				$this->storage->put( $this->workflow, $metadata );
+
+				$moderate = $this->newRevision->isModerated();
+				if ( $this->action === 'moderate-topic' ) {
+					$title = Title::newFromText( 'Topic:' . $this->newRevision->getRootPost()->getPostId()->getAlphadecimal() );
+					\DeferredUpdates::addCallableUpdate( function () use ( $title, $moderate ) {
+						$notifPage = MWEchoNotifPage::newFromTitle( $title );
+						if ( $moderate ) {
+							$notifPage->moderate();
+						} else {
+							$notifPage->unmoderate();
+						}
+					} );
+				} elseif ( $this->action === 'moderate-post' ) {
+					// todo: how to handle 'moderate-post'?
+
+				}
 			}
 
 			$newRevision = $this->newRevision;
