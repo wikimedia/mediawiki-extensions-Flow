4bca334afb8ea3ae5bc62859006bf4c737fe179a
Matthew Flaschen <mflaschen@wikimedia.org>
2016-03-24 21:49:51 -0400

    Disable submit buttons if we know the user can't edit
    For performance reasons, we can not compute this with 100% accuracy
    at page load (we could maybe do it when they open the editor, but this
    doesn't).
    
    Also show "Not editable" in description when we know it's not editable
    (quickUserCan or user is blocked).
    
    However, there is a core variable (wgIsProbablyEditable) that tells you
    when they definitely can *not* edit.  However, We keep using the
    similar 'editable' status for BoardDescriptionWidget since it already
    uses a model.
    
    This uses that information to disable save buttons and show the user an
    indication of why.  Different messages are shown depending on whether
    the user is logged in and the type of protection or edit restriction.
    
    Properly handle board description protection when header does not exist.
    
    Centralize most permission checking in the Workflow object.  Make sure
    we always check the correct title and check for create permissions if
    and only if they're creating.
    
    Remove some redundant permissions checks elsewhere.
    
    I kept the new topic and reply text boxes that show by default, per the
    mockup, but removed the reply links, in order to avoid hacking up the
    backend actions (we need the replyto id to render the link, but that is
    only returned when reply is considered a valid action)
    
    Known issue - The buttons still show for 'user blocked', because in
    that case the page is "probably editable" and the new topic and reply
    widget are not wired up to their models.  The error message on-save
    still displays correct.
    
    For no-JS, all the links are now gone (and it says "Not editable")
    except new topic.  That blocks it on submit.
    
    Remove some dead code in related areas.
    
    Bug: T108762
    Bug: T127774
    Change-Id: I7507a67e9ca9dfc778f795c96604951e85f9b3f6

--- handlebars/compiled/flow_block_header.handlebars.php
+++ handlebars/compiled/flow_block_header.handlebars.php
@@ -13,7 +13,6 @@
         'constants' => array(),
         'helpers' => array(            'l10n' => 'Flow\TemplateHelper::l10n',
             'html' => 'Flow\TemplateHelper::htmlHelper',
-            'escapeContent' => 'Flow\TemplateHelper::escapeContent',
             'oouify' => 'Flow\TemplateHelper::oouify',
 ),
         'blockhelpers' => array(),
@@ -22,38 +21,19 @@
 '.$sp.'	<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-speechBubbles"></span>
 '.$sp.'	'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq').'
 '.$sp.'</h2>
-';},'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
+';},'flow_header_edit_restrictions' => function ($cx, $in, $sp) {return ''.$sp.''.((!LCRun3::ifvar($cx, ((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions'])) ? $in['revision']['actions']['edit'] : null))) ? '	<p class="flow-board-description-restricted">
+'.$sp.'		'.LCRun3::ch($cx, 'oouify', array(array('lock'),array('type'=>'IconWidget','classes'=>'flow-board-description-restricted-icon')), 'raw').'
+'.$sp.'
+'.$sp.'		<span class="flow-board-description-restricted-label">'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header-can-not-edit'),array()), 'encq').'</span>
+'.$sp.'        </p>
+'.$sp.'' : '').'';},'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
 '.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
 '.$sp.'		<ul>
 '.$sp.''.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in)use($sp){return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
 '.$sp.'';}).'		</ul>
 '.$sp.'	</div>
 '.$sp.'' : '').'</div>
-';},'flow_header_detail_oldsystem' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board-header-detail-view">
-'.$sp.'	<div class="flow-board-header-nav">
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions'])) ? $in['revision']['actions']['edit'] : null))) ? '			<a href="'.htmlentities((string)((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'"
-'.$sp.'			   data-flow-api-handler="activateEditHeader"
-'.$sp.'			   data-flow-api-target="< .flow-board-header"
-'.$sp.'			   data-flow-interactive-handler="apiRequest"
-'.$sp.'			   class="mw-ui-button mw-ui-progressive  mw-ui-quiet flow-ui-tooltip-target"
-'.$sp.'			   title="'.htmlentities((string)((isset($in['revision']['actions']['edit']['title']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['title'] : null), ENT_QUOTES, 'UTF-8').'">
-'.$sp.'					<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-edit flow-board-header-icon"></span>'.LCRun3::ch($cx, 'l10n', array(array('flow-edit-header-link'),array()), 'encq').'
-'.$sp.'			</a>
-'.$sp.'' : '').'	</div>
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['revision']['content']) && is_array($in['revision'])) ? $in['revision']['content'] : null))) ? '		<div class="flow-board-header-content">
-'.$sp.'			'.LCRun3::ch($cx, 'escapeContent', array(array(((isset($in['revision']['content']['format']) && is_array($in['revision']['content'])) ? $in['revision']['content']['format'] : null),((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null)),array()), 'encq').'
-'.$sp.'		</div>
-'.$sp.'' : '').'
-'.$sp.'</div>
-'.$sp.'<a href="javascript:void(0);"
-'.$sp.'	class="mw-ui-button mw-ui-quiet side-rail-toggle-button"
-'.$sp.'	data-flow-interactive-handler="toggleSideRail">
-'.$sp.'	<span class="wikiglyph wikiglyph-x pull-right collapse-button"
-'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-collapse-description'),array()), 'encq').'"></span>
-'.$sp.'	<span class="wikiglyph wikiglyph-speech-bubbles pull-right expand-button"
-'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-expand-description'),array()), 'encq').'"></span>
-'.$sp.'</a>
-';},'flow_header_detail' => function ($cx, $in, $sp) {return ''.$sp.''.LCRun3::ch($cx, 'oouify', array(array(((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null),((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null),((isset($in['revision']['actions']['edit']['title']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['title'] : null)),array('label'=>((isset($in['flow-edit-header-link']) && is_array($in)) ? $in['flow-edit-header-link'] : null),'type'=>'BoardDescriptionWidget','name'=>'flow-board-description')), 'raw').'
+';},'flow_header_detail' => function ($cx, $in, $sp) {return ''.$sp.''.LCRun3::ch($cx, 'oouify', array(array(((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null),((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null),((isset($in['revision']['actions']['edit']['title']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['title'] : null)),array('type'=>'BoardDescriptionWidget','name'=>'flow-board-description')), 'raw').'
 '.$sp.'<a href="javascript:void(0);"
 '.$sp.'	class="mw-ui-button mw-ui-quiet side-rail-toggle-button"
 '.$sp.'	data-flow-interactive-handler="toggleSideRail">
@@ -80,7 +60,7 @@
     );
     
     return '<div class="flow-board-header flow-load-interactive" data-flow-load-handler="loadSideRail">
-'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '	').''.((LCRun3::ifvar($cx, ((isset($in['oldSystem']) && is_array($in)) ? $in['oldSystem'] : null))) ? ''.LCRun3::p($cx, 'flow_header_detail_oldsystem', array(array($in),array()), '		').'' : ''.LCRun3::p($cx, 'flow_header_detail', array(array($in),array()), '		').'').''.LCRun3::p($cx, 'flow_header_categories', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_footer', array(array($in),array()), '	').'</div>
+'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_edit_restrictions', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_detail', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_categories', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_footer', array(array($in),array()), '	').'</div>
 ';
 }
 ?>
