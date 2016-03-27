e534ae3e35ced8c6ac5fb7958d9f06650ed30733
Paladox <thomasmulhall410@yahoo.com>
2016-03-27 16:40:23 +0100

    Fix flow being tested with generic tests
    This moves hook BeforeCreateEchoEvent into initFlowExtension function.
    
    I'm not sure why it failed generic tests and passed non generic tests. But
    doing it this way fix it.
    
    Bug: T128627
    Change-Id: I75942a3b65475cb05c4215cb02dc5de65708be52

--- Flow.php
+++ Flow.php
@@ -124,8 +124,6 @@ $wgHooks['DeletedContributionsLineEnding'][] = 'FlowHooks::onDeletedContribution
 $wgHooks['ApiFeedContributions::feedItem'][] = 'FlowHooks::onContributionsFeedItem';
 $wgHooks['AbuseFilter-computeVariable'][] = 'FlowHooks::onAbuseFilterComputeVariable';
 $wgHooks['AbortEmailNotification'][] = 'FlowHooks::onAbortEmailNotification';
-$wgHooks['EchoAbortEmailNotification'][] = 'FlowHooks::onEchoAbortEmailNotification';
-$wgHooks['BeforeEchoEventInsert'][] = 'FlowHooks::onBeforeEchoEventInsert';
 $wgHooks['ArticleEditUpdateNewTalk'][] = 'FlowHooks::onArticleEditUpdateNewTalk';
 $wgHooks['InfoAction'][] = 'FlowHooks::onInfoAction';
 $wgHooks['SpecialCheckUserGetLinksFromRow'][] = 'FlowHooks::onSpecialCheckUserGetLinksFromRow';
@@ -165,7 +163,6 @@ $wgHooks['MergeAccountFromTo'][] = 'FlowHooks::onMergeAccountFromTo';
 $wgHooks['LiquidThreadsIsLqtPage'][] = 'FlowHooks::onIsLiquidThreadsPage';
 
 // Echo integration
-$wgHooks['BeforeCreateEchoEvent'][] = 'Flow\NotificationController::onBeforeCreateEchoEvent';
 $wgHooks['EchoGetBundleRules'][] = 'Flow\NotificationController::onEchoGetBundleRules';
 
 // Beta feature Flow on user talk page
