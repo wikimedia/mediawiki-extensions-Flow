74941a1a6fe07e297f8a3b7c5b5ef558ebf32859
Paladox <thomasmulhall410@yahoo.com>
2016-06-27 11:35:10 +0100

    Update extension to be compatible with zordius/lightncandy 0.90
    Also update handlebars to 4.0.5 since lightncandy 0.90 is based on that
    version.
    
    Also since 0.90 drops the support for it to automatically add <?php and ?>
    to the generated files we need to manually support this otherwise we have
    to manually do it our self's.
    
    Change-Id: Ic214841fd06bc218678811e73c49251fc9338061

--- handlebars/compiled/flow_block_topic_lock.handlebars.php
+++ handlebars/compiled/flow_block_topic_lock.handlebars.php
@@ -1,4 +1,45 @@
-<?php return function ($in, $debugopt = 1) {
+<?php use \LightnCandy\SafeString as SafeString;use \LightnCandy\Runtime as LR;return function ($in, $options = null) {
+    $helpers = array();
+    $partials = array('flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
+'.$sp.''.((LR::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), false)) ? '	<div class="flow-errors errorbox">
+'.$sp.'		<ul>
+'.$sp.''.LR::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), null, $in, true, function($cx, $in)use($sp){return '				<li>'.LR::encq($cx, ((isset($in['html']) && is_array($in)) ? $in['html'] : null)).'</li>
+'.$sp.'';}).'		</ul>
+'.$sp.'	</div>
+'.$sp.'' : '').'</div>
+';},
+'flow_form_cancel_button' => function ($cx, $in, $sp) {return ''.$sp.'<button data-flow-interactive-handler="cancelForm"
+'.$sp.'        data-role="cancel"
+'.$sp.'        type="reset"
+'.$sp.'        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right flow-js"
+'.$sp.'
+'.$sp.'>
+'.$sp.''.((LR::ifvar($cx, ((isset($in['msg']) && is_array($in)) ? $in['msg'] : null), false)) ? ''.LR::encq($cx, ((isset($in['l10n']) && is_array($in)) ? $in['l10n'] : null)).'' : ''.LR::encq($cx, ((isset($in['l10n']) && is_array($in)) ? $in['l10n'] : null)).'').'</button>
+';},
+'flow_topic_titlebar_lock' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board">
+'.$sp.'	<div class="flow-topic-summary-container">
+'.$sp.'		<div class="flow-topic-summary">
+'.$sp.'			<form class="flow-edit-form" data-flow-initial-state="expanded" method="POST"
+'.$sp.'				  action="'.((LR::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null), false)) ? ''.LR::encq($cx, ((isset($in['actions']['unlock']['url']) && is_array($in['actions']['unlock'])) ? $in['actions']['unlock']['url'] : null)).'' : ''.LR::encq($cx, ((isset($in['actions']['lock']['url']) && is_array($in['actions']['lock'])) ? $in['actions']['lock']['url'] : null)).'').'">
+'.$sp.''.LR::p($cx, 'flow_errors', array(array($in),array()), '				').'				<input type="hidden" name="wpEditToken" value="'.LR::encq($cx, ((isset($cx['sp_vars']['root']['editToken']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['editToken'] : null)).'" />
+'.$sp.'				<input type="hidden" name="flow_reason" value="'.((LR::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null), false)) ? ''.LR::encq($cx, ((isset($in['l10n']) && is_array($in)) ? $in['l10n'] : null)).'' : ''.LR::encq($cx, ((isset($in['l10n']) && is_array($in)) ? $in['l10n'] : null)).'').'" />
+'.$sp.'				<div class="flow-form-actions flow-form-collapsible">
+'.$sp.'					<button data-role="submit"
+'.$sp.'					        class="mw-ui-button mw-ui-constructive"
+'.$sp.'					>
+'.$sp.''.((LR::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null), false)) ? '							'.LR::encq($cx, ((isset($in['l10n']) && is_array($in)) ? $in['l10n'] : null)).'
+'.$sp.'' : '							'.LR::encq($cx, ((isset($in['l10n']) && is_array($in)) ? $in['l10n'] : null)).'
+'.$sp.'').'					</button>
+'.$sp.''.LR::p($cx, 'flow_form_cancel_button', array(array($in),array()), '					').'					<small class="flow-terms-of-use plainlinks">
+'.$sp.''.((LR::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null), false)) ? '							'.LR::encq($cx, ((isset($in['l10nParse']) && is_array($in)) ? $in['l10nParse'] : null)).'
+'.$sp.'' : '							'.LR::encq($cx, ((isset($in['l10nParse']) && is_array($in)) ? $in['l10nParse'] : null)).'
+'.$sp.'').'					</small>
+'.$sp.'				</div>
+'.$sp.'			</form>
+'.$sp.'		</div>
+'.$sp.'	</div>
+'.$sp.'</div>
+';});
     $cx = array(
         'flags' => array(
             'jstrue' => false,
@@ -6,62 +47,24 @@
             'spvar' => true,
             'prop' => false,
             'method' => false,
+            'lambda' => false,
             'mustlok' => false,
+            'mustlam' => false,
             'echo' => false,
-            'debug' => $debugopt,
+            'partnc' => false,
+            'knohlp' => false,
+            'debug' => isset($options['debug']) ? $options['debug'] : 1,
         ),
         'constants' => array(),
-        'helpers' => array(            'l10n' => 'Flow\TemplateHelper::l10n',
-            'html' => 'Flow\TemplateHelper::htmlHelper',
-            'l10nParse' => 'Flow\TemplateHelper::l10nParse',
-),
-        'blockhelpers' => array(),
-        'hbhelpers' => array(),
-        'partials' => array('flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
-'.$sp.'		<ul>
-'.$sp.''.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in)use($sp){return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
-'.$sp.'';}).'		</ul>
-'.$sp.'	</div>
-'.$sp.'' : '').'</div>
-';},'flow_form_cancel_button' => function ($cx, $in, $sp) {return ''.$sp.'<button data-flow-interactive-handler="cancelForm"
-'.$sp.'        data-role="cancel"
-'.$sp.'        type="reset"
-'.$sp.'        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right flow-js"
-'.$sp.'
-'.$sp.'>
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['msg']) && is_array($in)) ? $in['msg'] : null))) ? ''.LCRun3::ch($cx, 'l10n', array(array(((isset($in['msg']) && is_array($in)) ? $in['msg'] : null)),array()), 'encq').'' : ''.LCRun3::ch($cx, 'l10n', array(array('flow-cancel'),array()), 'encq').'').'</button>
-';},'flow_topic_titlebar_lock' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board">
-'.$sp.'	<div class="flow-topic-summary-container">
-'.$sp.'		<div class="flow-topic-summary">
-'.$sp.'			<form class="flow-edit-form" data-flow-initial-state="expanded" method="POST"
-'.$sp.'				  action="'.((LCRun3::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null))) ? ''.htmlentities((string)((isset($in['actions']['unlock']['url']) && is_array($in['actions']['unlock'])) ? $in['actions']['unlock']['url'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities((string)((isset($in['actions']['lock']['url']) && is_array($in['actions']['lock'])) ? $in['actions']['lock']['url'] : null), ENT_QUOTES, 'UTF-8').'').'">
-'.$sp.''.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '				').'				<input type="hidden" name="wpEditToken" value="'.htmlentities((string)((isset($cx['sp_vars']['root']['editToken']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['editToken'] : null), ENT_QUOTES, 'UTF-8').'" />
-'.$sp.'				<input type="hidden" name="flow_reason" value="'.((LCRun3::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null))) ? ''.LCRun3::ch($cx, 'l10n', array(array('flow-rev-message-restore-topic-reason'),array()), 'encq').'' : ''.LCRun3::ch($cx, 'l10n', array(array('flow-rev-message-lock-topic-reason'),array()), 'encq').'').'" />
-'.$sp.'				<div class="flow-form-actions flow-form-collapsible">
-'.$sp.'					<button data-role="submit"
-'.$sp.'					        class="mw-ui-button mw-ui-constructive"
-'.$sp.'					>
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null))) ? '							'.LCRun3::ch($cx, 'l10n', array(array('flow-topic-action-unlock-topic'),array()), 'encq').'
-'.$sp.'' : '							'.LCRun3::ch($cx, 'l10n', array(array('flow-topic-action-lock-topic'),array()), 'encq').'
-'.$sp.'').'					</button>
-'.$sp.''.LCRun3::p($cx, 'flow_form_cancel_button', array(array($in),array()), '					').'					<small class="flow-terms-of-use plainlinks">
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['isLocked']) && is_array($in)) ? $in['isLocked'] : null))) ? '							'.LCRun3::ch($cx, 'l10nParse', array(array('flow-terms-of-use-unlock-topic'),array()), 'encq').'
-'.$sp.'' : '							'.LCRun3::ch($cx, 'l10nParse', array(array('flow-terms-of-use-lock-topic'),array()), 'encq').'
-'.$sp.'').'					</small>
-'.$sp.'				</div>
-'.$sp.'			</form>
-'.$sp.'		</div>
-'.$sp.'	</div>
-'.$sp.'</div>
-';},),
+        'helpers' => isset($options['helpers']) ? array_merge($helpers, $options['helpers']) : $helpers,
+        'partials' => isset($options['partials']) ? array_merge($partials, $options['partials']) : $partials,
         'scopes' => array(),
-        'sp_vars' => array('root' => $in),
-        'lcrun' => 'LCRun3',
-
+        'sp_vars' => isset($options['data']) ? array_merge(array('root' => $in), $options['data']) : array('root' => $in),
+        'blparam' => array(),
+        'runtime' => '\LightnCandy\Runtime',
     );
     
-    return ''.LCRun3::p($cx, 'flow_topic_titlebar_lock', array(array($in),array())).'
+    return ''.LR::p($cx, 'flow_topic_titlebar_lock', array(array($in),array())).'
 ';
-}
+};
 ?>
