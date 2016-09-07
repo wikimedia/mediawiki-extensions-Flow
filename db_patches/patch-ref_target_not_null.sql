2fb479ca3a6f484f99abaa38812d63a38b91c1d1
Mark A. Hershberger <mah@everybody.org>
2016-09-06 20:18:11 -0400

    Drop and re-create the index
    MySQL will give an error if you just change to a blob
        Error: 1170 BLOB/TEXT column 'ref_target' used in key specification without a key length
    
    Bug: T110446
    Change-Id: Icfc080768005a314970498e94861a1ab9c068549
    (cherry picked from commit 5bf7057a1a01967aa05f6296cf850587ffedb419)

--- db_patches/patch-ref_target_not_null.sql
+++ db_patches/patch-ref_target_not_null.sql
@@ -1,1 +1,4 @@
+ALTER TABLE /*_*/flow_ext_ref DROP KEY flow_ext_ref_idx_v2;
 ALTER TABLE /*_*/flow_ext_ref CHANGE ref_target ref_target BLOB NOT NULL;
+CREATE INDEX /*i*/flow_ext_ref_idx_v2 ON /*_*/flow_ext_ref
+	(ref_src_wiki, ref_src_namespace, ref_src_title, ref_type, ref_target(255), ref_src_object_type, ref_src_object_id);
