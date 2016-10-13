3747a24cc81df1a2e8e3c0c50a781bd6d3dc97d1
Kavitha Muthu <kmuthu@wikimedia.org>
2016-10-13 12:23:54 -0700

    Hide orange alert when the user is viewing the topic page related to their user talk page
    Bug: T137954
    Change-Id: Ie742b2d0ecfa8dfc48e820923516f2ca4f90ffa5
    Depends-Id: I0b49c3a5eb0a027f8839ab86e77c527357907dd0

--- Flow.php
+++ Flow.php
@@ -126,6 +126,7 @@ $wgHooks['DeletedContributionsLineEnding'][] = 'FlowHooks::onDeletedContribution
 $wgHooks['ApiFeedContributions::feedItem'][] = 'FlowHooks::onContributionsFeedItem';
 $wgHooks['AbuseFilter-computeVariable'][] = 'FlowHooks::onAbuseFilterComputeVariable';
 $wgHooks['AbortEmailNotification'][] = 'FlowHooks::onAbortEmailNotification';
+$wgHooks['EchoAbortOrangeAlert'][] = 'FlowHooks::onEchoAbortOrangeAlert';
 $wgHooks['EchoAbortEmailNotification'][] = 'FlowHooks::onEchoAbortEmailNotification';
 $wgHooks['BeforeEchoEventInsert'][] = 'FlowHooks::onBeforeEchoEventInsert';
 $wgHooks['ArticleEditUpdateNewTalk'][] = 'FlowHooks::onArticleEditUpdateNewTalk';
