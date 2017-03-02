7d3160345906d512ae8965fc90f107e04baaa9b8
Matthew Flaschen <mflaschen@wikimedia.org>
2017-03-02 17:18:31 -0500

    Fix Flow autoload data and gen-autoload.php
    Convert gen-autoload.php to standard maintenance script so it
    loads autoloader (was getting P5012, not autoloaded).
    
    Then fix the autoload data itself (regression from
    9d087957aebaa28900033211936497df29f9e99a that removed
    MaintenanceDebugLogger)
    
    Change-Id: Ibbc95c2bdd0e7012cf05a6c9196869aed1e99989

--- scripts/gen-autoload.php
+++ scripts/gen-autoload.php
@@ -1,27 +1,40 @@
 <?php
 
-require_once __DIR__ . '/../../../includes/utils/AutoloadGenerator.php';
+require_once ( getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' ) . '/maintenance/Maintenance.php'
+	: dirname( __FILE__ ) . '/../../../maintenance/Maintenance.php' );
 
-function main() {
-	$base = dirname( __DIR__ );
-	$generator = new AutoloadGenerator( $base );
-	foreach ( array( 'includes', 'tests/phpunit', 'vendor' ) as $dir ) {
-		$generator->readDir( $base . '/' . $dir );
+/**
+ * Generates Flow autoload info
+ */
+
+class GenerateFlowAutoload extends Maintenance {
+	public function __construct() {
+		$this->mDescription = 'Generates Flow autoload data';
 	}
-	foreach ( glob( $base . '/*.php' ) as $file ) {
-		$generator->readFile( $file );
+
+	public function execute() {
+		$base = dirname( __DIR__ );
+		$generator = new AutoloadGenerator( $base );
+		foreach ( array( 'includes', 'tests/phpunit', 'vendor' ) as $dir ) {
+			$generator->readDir( $base . '/' . $dir );
+		}
+		foreach ( glob( $base . '/*.php' ) as $file ) {
+			$generator->readFile( $file );
+		}
+		// read entire maint dir, move helper to includes? to core?
+		$generator->readFile( $base . '/maintenance/MaintenanceDebugLogger.php' );
+
+		$target = $generator->getTargetFileInfo();
+
+		file_put_contents(
+			$target['filename'],
+			$generator->getAutoload( basename( __DIR__ ) . '/' . basename( __FILE__ ) )
+		);
+
+		echo "Done.\n\n";
 	}
-	// read entire maint dir, move helper to includes? to core?
-	$generator->readFile( $base . '/maintenance/MaintenanceDebugLogger.php' );
-
-	$target = $generator->getTargetFileInfo();
-
-	file_put_contents(
-		$target['filename'],
-		$generator->getAutoload( basename( __DIR__ ) . '/' . basename( __FILE__ ) )
-	);
-
-	echo "Done.\n\n";
 }
 
-main();
+$maintClass = "GenerateFlowAutoload";
+require_once ( RUN_MAINTENANCE_IF_MAIN );
