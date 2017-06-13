b4e168db2af62b8375aca7625d09512abdba9a8e
Mark A. Hershberger <mah@nichework.com>
2017-06-13 19:04:57 -0400

    Avoid a backtrace when running
    This populates the field with random data if there is none so that the primary key condition does not produce a backtrace.
    
    Without this change, the table contained 11 \0's in these columns—the result of forcing nulls to have a value (NOT NULL) in a binary field.
    
    Bug: T167843
    Change-Id: I7ce64bbe20f55282395c0dec287639727ec2b235

--- db_patches/patch-ref_id-phase2.sql
+++ db_patches/patch-ref_id-phase2.sql
@@ -1,3 +1,6 @@
+UPDATE /*_*/flow_wiki_ref SET ref_id=unhex(replace(uuid(), '-', '')) WHERE ref_id IS NULL OR ref_id=repeat(unhex(0), 11);
+UPDATE /*_*/flow_ext_ref SET ref_id=unhex(replace(uuid(), '-', '')) WHERE ref_id IS NULL OR ref_id=repeat(unhex(0), 11);
+
 ALTER TABLE /*_*/flow_wiki_ref CHANGE ref_id ref_id BINARY(11) NOT NULL;
 ALTER TABLE /*_*/flow_ext_ref CHANGE ref_id ref_id BINARY(11) NOT NULL;
 
