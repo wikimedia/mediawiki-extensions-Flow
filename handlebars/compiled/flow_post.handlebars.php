0bc0652f2c56776966712a475e9c6922b3577262
Ed Sanders <esanders@wikimedia.org>
2017-07-17 18:16:16 +0100

    Add mw-parser-output class to flow posts
    Bug: T164791
    Change-Id: I2df2c81627f4b56f758b4541887edf76ef190cf4

--- handlebars/compiled/flow_post.handlebars.php
+++ handlebars/compiled/flow_post.handlebars.php
@@ -137,7 +137,7 @@
 '.$sp.''.((LCRun3::ifvar($cx, ((isset($in['isModerated']) && is_array($in)) ? $in['isModerated'] : null))) ? '		<div class="flow-moderated-post-content">
 '.$sp.''.LCRun3::p($cx, 'flow_post_moderation_state', array(array($in),array()), '			').'		</div>
 '.$sp.'' : '').'
-'.$sp.'	<div class="flow-post-content">
+'.$sp.'	<div class="flow-post-content mw-parser-output">
 '.$sp.'		'.LCRun3::ch($cx, 'escapeContent', array(array(((isset($in['content']['format']) && is_array($in['content'])) ? $in['content']['format'] : null),((isset($in['content']['content']) && is_array($in['content'])) ? $in['content']['content'] : null)),array()), 'encq').'
 '.$sp.'	</div>
 '.$sp.'
