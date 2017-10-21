41d1d38e89aa5e67d6b63c7c4abf3855c9d33f2f
Roan Kattouw <roan.kattouw@gmail.com>
2017-10-20 22:28:21 -0700

    [WIP] Remove editor switching infrastructure in favor of VE's source mode
    This simplifies a lot of code. Essentially, EditorSwitcherWidget,
    VisualEditorWidget and bits of WikitextEditorWidget are all merged into
    EditorWidget.
    
    TODO: add a textarea fallback for when VE is not available/supported
    
    Bug: T155861
    Change-Id: Ie8014a64eae5f1b24495d6108e720464a8e75d9c

--- tests/browser/features/support/components/flow_editor.rb
+++ tests/browser/features/support/components/flow_editor.rb
@@ -2,10 +2,6 @@ module FlowEditor
   def visualeditor_or_textarea(form)
     parent = form.is_a?(String) ? form_element(css: form) : form
     parent.when_present
-    if parent.div_element(class: 'flow-ui-wikitextEditorWidget').exists?
-      parent.div_element(class: 'flow-ui-wikitextEditorWidget').text_area_element.when_enabled
-    else
-      parent.div_element(class: 've-ce-documentNode')
-    end
+    parent.div_element(class: 've-ce-documentNode')
   end
 end
