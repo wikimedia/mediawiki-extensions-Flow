a9035ca912979bfe2f3788a8656cbe9d73c81a0f
Reedy <reedy@wikimedia.org>
2017-12-04 03:26:43 +0000

    Update zordius/lightncandy v0.23 to v1.1.0
    Change-Id: I607aee7469c3d39da02d324083a6e8f8468c1c27
    Depends-On: Ida9e12e759cdc4508238c32153540000485a6561
    Depends-On: I734ee96b6b020f22a9743e1905b147a7d5e05123

--- handlebars/compiled/flow_block_header.handlebars.php
+++ handlebars/compiled/flow_block_header.handlebars.php
@@ -1,66 +1,76 @@
-<?php return function ($in, $debugopt = 1) {
-    $cx = array(
-        'flags' => array(
-            'jstrue' => false,
-            'jsobj' => false,
-            'spvar' => true,
-            'prop' => false,
-            'method' => false,
-            'mustlok' => false,
-            'echo' => false,
-            'debug' => $debugopt,
-        ),
-        'constants' => array(),
-        'helpers' => array(            'l10n' => 'Flow\TemplateHelper::l10n',
+use \LightnCandy\SafeString as SafeString;use \LightnCandy\Runtime as LR;return function ($in = null, $options = null) {
+    $helpers = array(            'l10n' => 'Flow\TemplateHelper::l10n',
             'html' => 'Flow\TemplateHelper::htmlHelper',
             'oouify' => 'Flow\TemplateHelper::oouify',
-),
-        'blockhelpers' => array(),
-        'hbhelpers' => array(),
-        'partials' => array('flow_header_title' => function ($cx, $in, $sp) {return ''.$sp.'<h2 class="flow-board-header-title">
+);
+    $partials = array('flow_header_title' => function ($cx, $in, $sp) {return ''.$sp.'<h2 class="flow-board-header-title">
 '.$sp.'	<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-speechBubbles"></span>
-'.$sp.'	'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq').'
+'.$sp.'	'.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq', $in)).'
 '.$sp.'</h2>
-';},'flow_header_edit_restrictions' => function ($cx, $in, $sp) {return ''.$sp.''.((!LCRun3::ifvar($cx, ((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions'])) ? $in['revision']['actions']['edit'] : null))) ? '	<p class="flow-board-header-restricted">
-'.$sp.'		'.LCRun3::ch($cx, 'oouify', array(array('lock'),array('type'=>'IconWidget','classes'=>'flow-board-header-restricted-icon')), 'raw').'
+';},
+'flow_header_edit_restrictions' => function ($cx, $in, $sp) {return ''.$sp.''.((!LR::ifvar($cx, ((isset($in['revision']['actions']) && is_array($in['revision']['actions']) && isset($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit'] : null), false)) ? '	<p class="flow-board-header-restricted">
+'.$sp.'		'.LR::raw($cx, LR::hbch($cx, 'oouify', array(array('lock'),array('type'=>'IconWidget','classes'=>'flow-board-header-restricted-icon')), 'raw', $in)).'
 '.$sp.'
-'.$sp.'		<span class="flow-board-header-restricted-label">'.LCRun3::ch($cx, 'l10n', array(array('flow-board-description-can-not-edit'),array()), 'encq').'</span>
+'.$sp.'		<span class="flow-board-header-restricted-label">'.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-board-description-can-not-edit'),array()), 'encq', $in)).'</span>
 '.$sp.'        </p>
-'.$sp.'' : '').'';},'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
+'.$sp.'' : '').'';},
+'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
+'.$sp.''.((LR::ifvar($cx, (isset($cx['sp_vars']['root']['errors']) ? $cx['sp_vars']['root']['errors'] : null), false)) ? '	<div class="flow-errors errorbox">
 '.$sp.'		<ul>
-'.$sp.''.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in)use($sp){return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
+'.$sp.''.LR::sec($cx, (isset($cx['sp_vars']['root']['errors']) ? $cx['sp_vars']['root']['errors'] : null), null, $in, true, function($cx, $in)use($sp){return '				<li>'.LR::encq($cx, LR::hbch($cx, 'html', array(array(((is_array($in) && isset($in['message'])) ? $in['message'] : null)),array()), 'encq', $in)).'</li>
 '.$sp.'';}).'		</ul>
 '.$sp.'	</div>
 '.$sp.'' : '').'</div>
-';},'flow_header_detail' => function ($cx, $in, $sp) {return ''.$sp.''.LCRun3::ch($cx, 'oouify', array(array(((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null),((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null),((isset($in['revision']['actions']['edit']['title']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['title'] : null)),array('type'=>'BoardDescriptionWidget','name'=>'flow-board-description')), 'raw').'
+';},
+'flow_header_detail' => function ($cx, $in, $sp) {return ''.$sp.''.LR::raw($cx, LR::hbch($cx, 'oouify', array(array(((isset($in['revision']['content']) && is_array($in['revision']['content']) && isset($in['revision']['content']['content'])) ? $in['revision']['content']['content'] : null),((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['url'])) ? $in['revision']['actions']['edit']['url'] : null),((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['title'])) ? $in['revision']['actions']['edit']['title'] : null)),array('type'=>'BoardDescriptionWidget','name'=>'flow-board-description')), 'raw', $in)).'
 '.$sp.'<a href="javascript:void(0);"
 '.$sp.'	class="mw-ui-button mw-ui-quiet side-rail-toggle-button"
 '.$sp.'	data-flow-interactive-handler="toggleSideRail">
 '.$sp.'	<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-close pull-right collapse-button"
-'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-collapse-description'),array()), 'encq').'"></span>
+'.$sp.'		  title="'.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-board-collapse-description'),array()), 'encq', $in)).'"></span>
 '.$sp.'	<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-speechBubbles pull-right expand-button"
-'.$sp.'		  title="'.LCRun3::ch($cx, 'l10n', array(array('flow-board-expand-description'),array()), 'encq').'"></span>
+'.$sp.'		  title="'.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-board-expand-description'),array()), 'encq', $in)).'"></span>
 '.$sp.'</a>
-';},'flow_header_categories' => function ($cx, $in, $sp) {return ''.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['categories']['items']) && is_array($cx['sp_vars']['root']['categories'])) ? $cx['sp_vars']['root']['categories']['items'] : null))) ? '<div id="catlinks" class="catlinks flow-board-header-category-view-nojs">
-'.$sp.'	<div id="mw-normal-catlinks" class="mw-normal-catlinks">'.LCRun3::ch($cx, 'html', array(array(((isset($cx['sp_vars']['root']['categories']['link']) && is_array($cx['sp_vars']['root']['categories'])) ? $cx['sp_vars']['root']['categories']['link'] : null)),array()), 'encq').'<ul class="flow-board-header-category-list">'.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['categories']['items']) && is_array($cx['sp_vars']['root']['categories'])) ? $cx['sp_vars']['root']['categories']['items'] : null), $in, true, function($cx, $in)use($sp){return '<li class="flow-board-header-category-item">'.LCRun3::ch($cx, 'html', array(array($in),array()), 'encq').'</li>';}).'</ul>
+';},
+'flow_header_categories' => function ($cx, $in, $sp) {return ''.$sp.''.((LR::ifvar($cx, (isset($cx['sp_vars']['root']['categories']['items']) ? $cx['sp_vars']['root']['categories']['items'] : null), false)) ? '<div id="catlinks" class="catlinks flow-board-header-category-view-nojs">
+'.$sp.'	<div id="mw-normal-catlinks" class="mw-normal-catlinks">'.LR::encq($cx, LR::hbch($cx, 'html', array(array((isset($cx['sp_vars']['root']['categories']['link']) ? $cx['sp_vars']['root']['categories']['link'] : null)),array()), 'encq', $in)).'<ul class="flow-board-header-category-list">'.LR::sec($cx, (isset($cx['sp_vars']['root']['categories']['items']) ? $cx['sp_vars']['root']['categories']['items'] : null), null, $in, true, function($cx, $in)use($sp){return '<li class="flow-board-header-category-item">'.LR::encq($cx, LR::hbch($cx, 'html', array(array($in),array()), 'encq', $in)).'</li>';}).'</ul>
 '.$sp.'	</div>
 '.$sp.'</div>
-'.$sp.'' : '').'';},'flow_header_footer' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board-header-footer">
+'.$sp.'' : '').'';},
+'flow_header_footer' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-board-header-footer">
 '.$sp.'  <hr />
 '.$sp.'  <p>
-'.$sp.'    '.LCRun3::ch($cx, 'html', array(array(((isset($in['copyrightMessage']) && is_array($in)) ? $in['copyrightMessage'] : null)),array()), 'encq').'
+'.$sp.'    '.LR::encq($cx, LR::hbch($cx, 'html', array(array(((is_array($in) && isset($in['copyrightMessage'])) ? $in['copyrightMessage'] : null)),array()), 'encq', $in)).'
 '.$sp.'  </p>
 '.$sp.'</div>
-';},),
+';});
+    $cx = array(
+        'flags' => array(
+            'jstrue' => false,
+            'jsobj' => false,
+            'jslen' => false,
+            'spvar' => true,
+            'prop' => false,
+            'method' => false,
+            'lambda' => false,
+            'mustlok' => false,
+            'mustlam' => false,
+            'echo' => false,
+            'partnc' => false,
+            'knohlp' => false,
+            'debug' => isset($options['debug']) ? $options['debug'] : 1,
+        ),
+        'constants' => array(),
+        'helpers' => isset($options['helpers']) ? array_merge($helpers, $options['helpers']) : $helpers,
+        'partials' => isset($options['partials']) ? array_merge($partials, $options['partials']) : $partials,
         'scopes' => array(),
-        'sp_vars' => array('root' => $in),
-        'lcrun' => 'LCRun3',
-
+        'sp_vars' => isset($options['data']) ? array_merge(array('root' => $in), $options['data']) : array('root' => $in),
+        'blparam' => array(),
+        'partialid' => 0,
+        'runtime' => '\LightnCandy\Runtime',
     );
     
     return '<div class="flow-board-header flow-load-interactive" data-flow-load-handler="loadSideRail">
-'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_edit_restrictions', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_detail', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_categories', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_header_footer', array(array($in),array()), '	').'</div>
+'.LR::p($cx, 'flow_header_title', array(array($in),array()),0, '	').''.LR::p($cx, 'flow_header_edit_restrictions', array(array($in),array()),0, '	').''.LR::p($cx, 'flow_errors', array(array($in),array()),0, '	').''.LR::p($cx, 'flow_header_detail', array(array($in),array()),0, '	').''.LR::p($cx, 'flow_header_categories', array(array($in),array()),0, '	').''.LR::p($cx, 'flow_header_footer', array(array($in),array()),0, '	').'</div>
 ';
-}
-?>
+};
--- handlebars/compiled/flow_block_header_edit.handlebars.php
+++ handlebars/compiled/flow_block_header_edit.handlebars.php
@@ -1,69 +1,76 @@
-<?php return function ($in, $debugopt = 1) {
-    $cx = array(
-        'flags' => array(
-            'jstrue' => false,
-            'jsobj' => false,
-            'spvar' => true,
-            'prop' => false,
-            'method' => false,
-            'mustlok' => false,
-            'echo' => false,
-            'debug' => $debugopt,
-        ),
-        'constants' => array(),
-        'helpers' => array(            'l10n' => 'Flow\TemplateHelper::l10n',
+use \LightnCandy\SafeString as SafeString;use \LightnCandy\Runtime as LR;return function ($in = null, $options = null) {
+    $helpers = array(            'l10n' => 'Flow\TemplateHelper::l10n',
             'html' => 'Flow\TemplateHelper::htmlHelper',
             'l10nParse' => 'Flow\TemplateHelper::l10nParse',
-),
-        'blockhelpers' => array(),
-        'hbhelpers' => array(),
-        'partials' => array('flow_header_title' => function ($cx, $in, $sp) {return ''.$sp.'<h2 class="flow-board-header-title">
+);
+    $partials = array('flow_header_title' => function ($cx, $in, $sp) {return ''.$sp.'<h2 class="flow-board-header-title">
 '.$sp.'	<span class="mw-ui-icon mw-ui-icon-before mw-ui-icon-speechBubbles"></span>
-'.$sp.'	'.LCRun3::ch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq').'
+'.$sp.'	'.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-board-header'),array()), 'encq', $in)).'
 '.$sp.'</h2>
-';},'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null))) ? '	<div class="flow-errors errorbox">
+';},
+'flow_errors' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-error-container">
+'.$sp.''.((LR::ifvar($cx, (isset($cx['sp_vars']['root']['errors']) ? $cx['sp_vars']['root']['errors'] : null), false)) ? '	<div class="flow-errors errorbox">
 '.$sp.'		<ul>
-'.$sp.''.LCRun3::sec($cx, ((isset($cx['sp_vars']['root']['errors']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['errors'] : null), $in, true, function($cx, $in)use($sp){return '				<li>'.LCRun3::ch($cx, 'html', array(array(((isset($in['message']) && is_array($in)) ? $in['message'] : null)),array()), 'encq').'</li>
+'.$sp.''.LR::sec($cx, (isset($cx['sp_vars']['root']['errors']) ? $cx['sp_vars']['root']['errors'] : null), null, $in, true, function($cx, $in)use($sp){return '				<li>'.LR::encq($cx, LR::hbch($cx, 'html', array(array(((is_array($in) && isset($in['message'])) ? $in['message'] : null)),array()), 'encq', $in)).'</li>
 '.$sp.'';}).'		</ul>
 '.$sp.'	</div>
 '.$sp.'' : '').'</div>
-';},'flow_form_cancel_button' => function ($cx, $in, $sp) {return ''.$sp.'<button data-flow-interactive-handler="cancelForm"
+';},
+'flow_form_cancel_button' => function ($cx, $in, $sp) {return ''.$sp.'<button data-flow-interactive-handler="cancelForm"
 '.$sp.'        data-role="cancel"
 '.$sp.'        type="reset"
 '.$sp.'        class="mw-ui-button mw-ui-destructive mw-ui-quiet mw-ui-flush-right flow-js"
 '.$sp.'
 '.$sp.'>
-'.$sp.''.((LCRun3::ifvar($cx, ((isset($in['msg']) && is_array($in)) ? $in['msg'] : null))) ? ''.LCRun3::ch($cx, 'l10n', array(array(((isset($in['msg']) && is_array($in)) ? $in['msg'] : null)),array()), 'encq').'' : ''.LCRun3::ch($cx, 'l10n', array(array('flow-cancel'),array()), 'encq').'').'</button>
-';},),
+'.$sp.''.((LR::ifvar($cx, ((is_array($in) && isset($in['msg'])) ? $in['msg'] : null), false)) ? ''.LR::encq($cx, LR::hbch($cx, 'l10n', array(array(((is_array($in) && isset($in['msg'])) ? $in['msg'] : null)),array()), 'encq', $in)).'' : ''.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-cancel'),array()), 'encq', $in)).'').'</button>
+';});
+    $cx = array(
+        'flags' => array(
+            'jstrue' => false,
+            'jsobj' => false,
+            'jslen' => false,
+            'spvar' => true,
+            'prop' => false,
+            'method' => false,
+            'lambda' => false,
+            'mustlok' => false,
+            'mustlam' => false,
+            'echo' => false,
+            'partnc' => false,
+            'knohlp' => false,
+            'debug' => isset($options['debug']) ? $options['debug'] : 1,
+        ),
+        'constants' => array(),
+        'helpers' => isset($options['helpers']) ? array_merge($helpers, $options['helpers']) : $helpers,
+        'partials' => isset($options['partials']) ? array_merge($partials, $options['partials']) : $partials,
         'scopes' => array(),
-        'sp_vars' => array('root' => $in),
-        'lcrun' => 'LCRun3',
-
+        'sp_vars' => isset($options['data']) ? array_merge(array('root' => $in), $options['data']) : array('root' => $in),
+        'blparam' => array(),
+        'partialid' => 0,
+        'runtime' => '\LightnCandy\Runtime',
     );
     
     return '<div class="flow-board-header">
-'.LCRun3::p($cx, 'flow_header_title', array(array($in),array()), '	').'	<div class="flow-board-header-edit-view">
-		<form method="POST" action="'.htmlentities((string)((isset($in['revision']['actions']['edit']['url']) && is_array($in['revision']['actions']['edit'])) ? $in['revision']['actions']['edit']['url'] : null), ENT_QUOTES, 'UTF-8').'" flow-api-action="edit-header" class="edit-header-form">
-'.LCRun3::p($cx, 'flow_errors', array(array($in),array()), '			').'			<input type="hidden" name="wpEditToken" value="'.htmlentities((string)((isset($cx['sp_vars']['root']['editToken']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['editToken'] : null), ENT_QUOTES, 'UTF-8').'" />
-'.((LCRun3::ifvar($cx, ((isset($in['revision']['revisionId']) && is_array($in['revision'])) ? $in['revision']['revisionId'] : null))) ? '				<input type="hidden" name="header_prev_revision" value="'.htmlentities((string)((isset($in['revision']['revisionId']) && is_array($in['revision'])) ? $in['revision']['revisionId'] : null), ENT_QUOTES, 'UTF-8').'" />
+'.LR::p($cx, 'flow_header_title', array(array($in),array()),0, '	').'	<div class="flow-board-header-edit-view">
+		<form method="POST" action="'.LR::encq($cx, ((isset($in['revision']['actions']['edit']) && is_array($in['revision']['actions']['edit']) && isset($in['revision']['actions']['edit']['url'])) ? $in['revision']['actions']['edit']['url'] : null)).'" flow-api-action="edit-header" class="edit-header-form">
+'.LR::p($cx, 'flow_errors', array(array($in),array()),0, '			').'			<input type="hidden" name="wpEditToken" value="'.LR::encq($cx, (isset($cx['sp_vars']['root']['editToken']) ? $cx['sp_vars']['root']['editToken'] : null)).'" />
+'.((LR::ifvar($cx, ((isset($in['revision']) && is_array($in['revision']) && isset($in['revision']['revisionId'])) ? $in['revision']['revisionId'] : null), false)) ? '				<input type="hidden" name="header_prev_revision" value="'.LR::encq($cx, ((isset($in['revision']) && is_array($in['revision']) && isset($in['revision']['revisionId'])) ? $in['revision']['revisionId'] : null)).'" />
 ' : '').'
 			<div class="flow-editor">
 				<textarea name="header_content"
-				          class="mw-ui-input mw-editfont-'.htmlentities((string)((isset($cx['sp_vars']['root']['editFont']) && is_array($cx['sp_vars']['root'])) ? $cx['sp_vars']['root']['editFont'] : null), ENT_QUOTES, 'UTF-8').'"
-				          placeholder="'.LCRun3::ch($cx, 'l10n', array(array('flow-edit-header-placeholder'),array()), 'encq').'"
+				          class="mw-ui-input mw-editfont-'.LR::encq($cx, (isset($cx['sp_vars']['root']['editFont']) ? $cx['sp_vars']['root']['editFont'] : null)).'"
+				          placeholder="'.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-edit-header-placeholder'),array()), 'encq', $in)).'"
 				          data-role="content"
-				>'.((LCRun3::ifvar($cx, ((isset($in['submitted']['content']) && is_array($in['submitted'])) ? $in['submitted']['content'] : null))) ? ''.htmlentities((string)((isset($in['submitted']['content']) && is_array($in['submitted'])) ? $in['submitted']['content'] : null), ENT_QUOTES, 'UTF-8').'' : ''.htmlentities((string)((isset($in['revision']['content']['content']) && is_array($in['revision']['content'])) ? $in['revision']['content']['content'] : null), ENT_QUOTES, 'UTF-8').'').'</textarea>
+				>'.((LR::ifvar($cx, ((isset($in['submitted']) && is_array($in['submitted']) && isset($in['submitted']['content'])) ? $in['submitted']['content'] : null), false)) ? ''.LR::encq($cx, ((isset($in['submitted']) && is_array($in['submitted']) && isset($in['submitted']['content'])) ? $in['submitted']['content'] : null)).'' : ''.LR::encq($cx, ((isset($in['revision']['content']) && is_array($in['revision']['content']) && isset($in['revision']['content']['content'])) ? $in['revision']['content']['content'] : null)).'').'</textarea>
 			</div>
 
 			<div class="flow-form-actions flow-form-collapsible">
 				<button data-role="submit"
-					class="mw-ui-button mw-ui-progressive">'.LCRun3::ch($cx, 'l10n', array(array('flow-edit-header-submit'),array()), 'encq').'</button>
-'.LCRun3::p($cx, 'flow_form_cancel_button', array(array($in),array()), '				').'				<small class="flow-terms-of-use plainlinks">'.LCRun3::ch($cx, 'l10nParse', array(array('flow-terms-of-use-edit'),array()), 'encq').'</small>
+					class="mw-ui-button mw-ui-progressive">'.LR::encq($cx, LR::hbch($cx, 'l10n', array(array('flow-edit-header-submit'),array()), 'encq', $in)).'</button>
+'.LR::p($cx, 'flow_form_cancel_button', array(array($in),array()),0, '				').'				<small class="flow-terms-of-use plainlinks">'.LR::encq($cx, LR::hbch($cx, 'l10nParse', array(array('flow-terms-of-use-edit'),array()), 'encq', $in)).'</small>
 			</div>
 		</form>
 	</div>
 </div>
 ';
-}
-?>
+};
