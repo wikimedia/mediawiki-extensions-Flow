aa952efd7ae76c44ed8c494c604635eb6334d695
Roan Kattouw <roan.kattouw@gmail.com>
2017-12-15 21:46:41 -0800

    Remove the old editor system
    It was no longer used anywhere. This is a relatively minimal removal:
    there are more pieces of the old UI code that are now unused, but I
    haven't removed them yet. It's not trivial to figure out which parts of
    the form handling/enhancement code are now unused, and which are still
    used by the moderation dialog, so I'll deal with that in a separate
    commit to avoid muddying the waters here.
    
    Also remove an old message related to the old switching system (which
    was already unused) and stop loading the textures module separately (we
    no longer use the pending texture directly from HTML).
    
    Change-Id: I95eee414de14c4f48c7773d7dc36698e3a939350

--- modules/flow/ui/tools/mw.flow.ui.MWEditModeTool.js
+++ modules/flow/ui/tools/mw.flow.ui.MWEditModeTool.js
@@ -48,16 +48,7 @@ OO.inheritClass( mw.flow.ui.MWEditModeSourceTool, mw.libs.ve.MWEditModeSourceToo
 OO.mixinClass( mw.flow.ui.MWEditModeSourceTool, mw.flow.ui.MWEditModeTool );
 
 mw.flow.ui.MWEditModeSourceTool.prototype.switch = function () {
-	var $editor = this.toolbar.getSurface().$element.closest( '.flow-editor' );
-	if ( this.getMode() === 'visual' ) {
-		if ( $editor.length ) {
-			// Old editor (FIXME kill the old editor and remove this)
-			mw.flow.editor.switchEditor( $editor.find( 'textarea' ), 'none' );
-		} else {
-			// New editor
-			this.toolbar.getTarget().switchMode();
-		}
-	}
+	this.toolbar.getTarget().switchMode();
 };
 
 ve.ui.toolFactory.register( mw.flow.ui.MWEditModeSourceTool );
