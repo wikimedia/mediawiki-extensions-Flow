4b13519e4f5f71c481adc13efc9c2f02e7be915a
Ed Sanders <esanders@wikimedia.org>
2017-12-20 13:16:33 +0000

    Fix editor switcher inheritance
    Inherit from the non-ve.ui version of the tool as that
    one only comes from the ArticleTarget module.
    
    Bug: T183360
    Change-Id: I550ebb717de417f99776b578656c1d9f05d59f1f

--- modules/flow/ui/tools/mw.flow.ui.MWEditModeTool.js
+++ modules/flow/ui/tools/mw.flow.ui.MWEditModeTool.js
@@ -6,20 +6,52 @@
  */
 
 /**
+ * MediaWiki UserInterface edit mode tool.
+ *
+ * @class
+ * @abstract
+ */
+mw.flow.ui.MWEditModeTool = function MwFlowUiMWEditModeTool() {
+};
+
+/* Inheritance */
+
+OO.initClass( mw.flow.ui.MWEditModeTool );
+
+/* Methods */
+
+/**
+ * @inheritdoc mw.libs.ve.MWEditModeTool
+ */
+mw.flow.ui.MWEditModeTool.prototype.getMode = function () {
+	return this.toolbar.getSurface().getMode();
+};
+
+/**
+ * @inheritdoc mw.libs.ve.MWEditModeTool
+ */
+mw.flow.ui.MWEditModeTool.prototype.isModeAvailable = function () {
+	// If we're showing the switcher, then both modes are available
+	return true;
+};
+
+/**
  * MediaWiki UserInterface edit mode source tool.
  *
  * @class
  * @extends mw.libs.ve.MWEditModeSourceTool
+ * @mixins mw.flow.ui.MWEditModeTool
  * @constructor
  * @param {OO.ui.ToolGroup} toolGroup
  * @param {Object} [config] Config options
  */
-mw.flow.ui.MWEditModeSourceTool = function VeUiMWEditModeSourceTool() {
+mw.flow.ui.MWEditModeSourceTool = function MwFlowUiMWEditModeSourceTool() {
 	// Parent constructor
 	mw.flow.ui.MWEditModeSourceTool.parent.apply( this, arguments );
 };
 
-OO.inheritClass( mw.flow.ui.MWEditModeSourceTool, ve.ui.MWEditModeSourceTool );
+OO.inheritClass( mw.flow.ui.MWEditModeSourceTool, mw.libs.ve.MWEditModeSourceTool );
+OO.mixinClass( mw.flow.ui.MWEditModeSourceTool, mw.flow.ui.MWEditModeTool );
 
 mw.flow.ui.MWEditModeSourceTool.prototype.switch = function () {
 	var $editor = this.toolbar.getSurface().$element.closest( '.flow-editor' );
@@ -41,16 +73,18 @@ ve.ui.toolFactory.register( mw.flow.ui.MWEditModeSourceTool );
  *
  * @class
  * @extends mw.libs.ve.MWEditModeVisualTool
+ * @mixins mw.flow.ui.MWEditModeTool
  * @constructor
  * @param {OO.ui.ToolGroup} toolGroup
  * @param {Object} [config] Config options
  */
-mw.flow.ui.MWEditModeVisualTool = function VeUiMWEditModeVisualTool() {
+mw.flow.ui.MWEditModeVisualTool = function MwFlowUiMWEditModeVisualTool() {
 	// Parent constructor
 	mw.flow.ui.MWEditModeVisualTool.parent.apply( this, arguments );
 };
 
-OO.inheritClass( mw.flow.ui.MWEditModeVisualTool, ve.ui.MWEditModeVisualTool );
+OO.inheritClass( mw.flow.ui.MWEditModeVisualTool, mw.libs.ve.MWEditModeVisualTool );
+OO.mixinClass( mw.flow.ui.MWEditModeVisualTool, mw.flow.ui.MWEditModeTool );
 
 mw.flow.ui.MWEditModeVisualTool.prototype.switch = function () {
 	this.toolbar.getTarget().switchMode();
