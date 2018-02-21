a703662e1c62c251ad1e83bf0b84715dfb5ff437
Jaziel Pauda <jazielpauda@outlook.com>
2018-02-20 21:33:02 -0700

    Fix "div" tags to "article" tags
    According to W3 and MDN standards,
    the "div" tags with classes
    "flow-post-content" and "mw-parser-output"
    should be "article" tags instead.
    
    Bug: T175937
    Change-Id: I1f0211d414b248b57f9b1bda2fba6bfcb980ef9d

--- handlebars/compiled/flow_post.handlebars.php
+++ handlebars/compiled/flow_post.handlebars.php
@@ -137,9 +137,9 @@
 '.$sp.''.((LCRun3::ifvar($cx, ((isset($in['isModerated']) && is_array($in)) ? $in['isModerated'] : null))) ? '		<div class="flow-moderated-post-content">
 '.$sp.''.LCRun3::p($cx, 'flow_post_moderation_state', array(array($in),array()), '			').'		</div>
 '.$sp.'' : '').'
-'.$sp.'	<div class="flow-post-content mw-parser-output">
+'.$sp.'	<article class="flow-post-content mw-parser-output">
 '.$sp.'		'.LCRun3::ch($cx, 'escapeContent', array(array(((isset($in['content']['format']) && is_array($in['content'])) ? $in['content']['format'] : null),((isset($in['content']['content']) && is_array($in['content'])) ? $in['content']['content'] : null)),array()), 'encq').'
-'.$sp.'	</div>
+'.$sp.'	</article>
 '.$sp.'
 '.$sp.''.LCRun3::p($cx, 'flow_post_meta_actions', array(array($in),array()), '	').''.LCRun3::p($cx, 'flow_post_actions', array(array($in),array()), '	').'</div>
 ';},'flow_anon_warning' => function ($cx, $in, $sp) {return ''.$sp.'<div class="flow-anon-warning">
