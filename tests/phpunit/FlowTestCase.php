e1e83925cec5c54d9429c994228ec1fda714259f
Aryeh Gregor <ayg@aryeh.name>
2018-10-07 15:41:47 +0300

    Use setMwGlobals instead of stashMwGlobals
    It's shorter, and allows us to take advantage of any future improvements
    like automatically resetting affected services.
    
    Change-Id: I1b220996acf2f66cf7b0f092b341584663df32f9

--- tests/phpunit/FlowTestCase.php
+++ tests/phpunit/FlowTestCase.php
@@ -46,8 +46,7 @@ class FlowTestCase extends \MediaWikiTestCase {
 		$data = $registry->readFromQueue( [ __DIR__ . '/../../extension.json' => 1 ] );
 		$perms = $data['globals']['wgGroupPermissions'];
 		unset( $perms[$registry::MERGE_STRATEGY] );
-		$this->stashMwGlobals( [ 'wgGroupPermissions' ] );
 		global $wgGroupPermissions;
-		$wgGroupPermissions = wfArrayPlus2d( $perms, $wgGroupPermissions );
+		$this->setMwGlobals( 'wgGroupPermissions', wfArrayPlus2d( $perms, $wgGroupPermissions ) );
 	}
 }
