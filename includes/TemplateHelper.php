1bf8292194d8f5d98197e976560d8d7c4ac2256c
Alangi Derick <alangiderick@gmail.com>
2018-10-20 22:17:19 +0100

    Fix countable PHP 7.2 error in Flow
    Use an equivalent condition in the case of count() as
    count( $postIds ) === 0 is equivalent to $postIds === [].
    Since if the count on $postIds returns zero, it means the
    array should be empty.
    
    Bug: T207563
    Change-Id: I70765e6e4f4b31cb776da258d060f3ca2ef4432c
    (cherry picked from commit 2595fbbca83d1e0d797df377ee327dab18bfb815)

--- includes/TemplateHelper.php
+++ includes/TemplateHelper.php
@@ -320,7 +320,7 @@ class TemplateHelper {
 
 		if ( $postIds && !is_array( $postIds ) ) {
 			$postIds = [ $postIds ];
-		} elseif ( count( $postIds ) === 0 ) {
+		} elseif ( $postIds === [] ) {
 			// Failure callback, if any
 			if ( !$inverse ) {
 				return null;
