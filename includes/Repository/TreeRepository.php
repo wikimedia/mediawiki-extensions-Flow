a444e5ff7fc8638428f3479f07cc99bdfa58824a
Umherirrender <umherirrender_de.wp@web.de>
2019-01-03 16:20:39 +0100

    Fix variable name when throw in TreeRepository::fetchSubtreeIdentityMap
    Change-Id: I669cb2d430eaa5b67a358a1a1af2e4b96193562f

--- includes/Repository/TreeRepository.php
+++ includes/Repository/TreeRepository.php
@@ -354,7 +354,7 @@ class TreeRepository {
 		}
 		$nodes = $this->fetchSubtreeNodeList( $roots );
 		if ( !$nodes ) {
-			throw new DataModelException( 'subtree node list should have at least returned root: ' . $root, 'process-data' );
+			throw new DataModelException( 'subtree node list should have at least returned root: ' . $roots, 'process-data' );
 		} elseif ( count( $nodes ) === 1 ) {
 			$parentMap = $this->fetchParentMap( reset( $nodes ) );
 		} else {
