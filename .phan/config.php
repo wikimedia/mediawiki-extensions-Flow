29953aa601cabfd28d04183bc436b4d863c77317
Daimona Eaytoy <daimona.wiki@gmail.com>
2019-03-12 19:53:10 +0100

    [WIP] Add phan
    Bug: T224759
    Depends-On: I292ad7bb815c1ad1a3e2fe709650794c8667ed69
    Change-Id: Ifd2b639c807224ae09c0c99995b251f2943995cf

---
+++ .phan/config.php
@@ -0,0 +1,45 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['file_list'] = array_merge(
+	$cfg['file_list'],
+	[
+		'container.php',
+		'defines.php',
+		'FlowActions.php',
+		'Hooks.php'
+	]
+);
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'],
+	[
+		'../../extensions/Echo',
+		'../../extensions/BetaFeatures',
+		'../../extensions/CentralAuth',
+		'../../extensions/LiquidThreads',
+		'../../extensions/Elastica',
+		'../../extensions/CirrusSearch',
+		'../../extensions/AbuseFilter',
+		'../../extensions/ConfirmEdit',
+		'../../extensions/SpamBlacklist',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'],
+	[
+		'../../extensions/Echo',
+		'../../extensions/BetaFeatures',
+		'../../extensions/CentralAuth',
+		'../../extensions/LiquidThreads',
+		'../../extensions/Elastica',
+		'../../extensions/CirrusSearch',
+		'../../extensions/AbuseFilter',
+		'../../extensions/ConfirmEdit',
+		'../../extensions/SpamBlacklist',
+	]
+);
+
+return $cfg;
