f707a784400c50ee083f11d08a48d08dea18f38b
Alexander Vorwerk <zabe@avorwerk.net>
2023-01-03 23:34:45 +0100

    Drop ContributionsRow::$revactor_timestamp
    The 'revision_actor_temp' table no longer exists.
    
    Change-Id: Iceeaf07ea7b72c2539c6febc1166db606ad7155a

--- includes/Formatter/ContributionsRow.php
+++ includes/Formatter/ContributionsRow.php
@@ -7,11 +7,4 @@ class ContributionsRow extends FormatterRow {
 	 * @var string
 	 */
 	public $rev_timestamp;
-
-	/**
-	 * Used when the query uses the 'revision_actor_temp' table
-	 *
-	 * @var string
-	 */
-	public $revactor_timestamp;
 }
